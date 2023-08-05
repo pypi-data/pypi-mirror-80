50aaa06b3e8563bfb23a13e00649d11c4708c99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:31 +0000

    Add package chemlib/2.0/chemlib-2.0.tar.gz

---
+++ packages/chemlib/chemlib-2.0.tar.gz/chemlib-2.0/chemlib/__init__.py
@@ -0,0 +1,9 @@
+from chemlib.chemistry import PeriodicTable
+
+pte = PeriodicTable()
+SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+AVOGADROS_NUMBER = 6.02e+23
+
+from chemlib.chemistry import Element, Compound, Reaction
+from chemlib.chemistry import Combustion
+
