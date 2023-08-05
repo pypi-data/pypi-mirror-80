eab479f1d159fd4b8f102b56e482450495b3b3da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:10 +0000

    Add package shopyo/1.1.2/shopyo-1.1.2.tar.gz

---
+++ packages/shopyo/shopyo-1.1.2.tar.gz/shopyo-1.1.2/shopyo/config.py
@@ -0,0 +1,34 @@
+class Config:
+    """Parent configuration class."""
+
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = "sqlite:///shopyo.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "qow32ijjdkc756osk5dmck"  # Need a generator
+    HOMEPAGE_URL = "/control_panel"
+
+
+class DevelopmentConfig(Config):
+    """Configurations for development"""
+
+    ENV = "development"
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Configurations for testsing"""
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///testing.db"
+    DEBUG = True
+    LIVESERVER_PORT = 8943
+    LIVESERVER_TIMEOUT = 10
+    
+    BCRYPT_LOG_ROUNDS = 4
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+
+app_config = {
+    "development": DevelopmentConfig,
+    "production": Config,
+    "testing": TestingConfig,
+}
