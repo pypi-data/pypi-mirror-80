4c9c446da34e7343b94a09633911d24f7a13523c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:50 +0000

    Add package bashrc/0.4.2/bashrc-0.4.2-py3-none-any.whl

---
+++ packages/bashrc/bashrc-0.4.2-py3-none-any.whl/bashrc/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""Bashrc Package."""
+import os
+import pathlib
+
+package = pathlib.Path(__file__).parent.resolve()
+project = package.parent
+
+scripts = package / 'scripts'
+scripts_relative = [str(item.relative_to(project)) for item in scripts.iterdir()]
+
+readme = project / 'README.md'
+description = package.name
+if readme.is_file():
+    try:
+        description = str(readme).splitlines()[0].split('#')[1]
+    except IndexError:
+        pass
+
+requirements = project / 'requirements.txt'
+if requirements.is_file():
+    install_requires = requirements.read_text().splitlines()
+else:
+    install_requires = list()
+
+atlas_url = f'mongodb+srv://{os.environ["USER"]}:' \
+      f'{os.environ["NFERX_ATLAS_PASSWORD"]}@pen.ydo6l.mongodb.net/pen?retryWrites=true&w=majority'
+
+__all__ = ['package', 'project', 'scripts', 'scripts_relative', 'readme', 'description']
+
+for global_var, global_value in os.environ.items():
+    # noinspection PyStatementEffect
+    globals()[global_var] = global_value
+    __all__.append(global_var)
