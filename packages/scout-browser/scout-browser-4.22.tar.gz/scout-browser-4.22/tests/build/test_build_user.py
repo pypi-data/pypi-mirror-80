4ac47200a7e82725124e5e1cb3f6a529032fbc84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:20 +0000

    Add package scout-browser/4.22/scout-browser-4.22.tar.gz

---
+++ packages/scout-browser/scout-browser-4.22.tar.gz/scout-browser-4.22/tests/build/test_build_user.py
@@ -0,0 +1,28 @@
+import datetime
+import pytest
+from scout.build.user import build_user
+
+
+def test_build_user(parsed_user):
+    ## GIVEN some parsed user information
+
+    ## WHEN building a user object
+    user_obj = build_user(parsed_user)
+
+    ## THEN assert that the build was correct
+    assert user_obj["_id"] == user_obj["email"] == parsed_user["email"]
+
+    assert user_obj["name"] == user_obj["name"]
+
+    assert isinstance(user_obj["created_at"], datetime.datetime)
+
+
+@pytest.mark.parametrize("key", ['email', 'name'])    
+def test_build_user_missing_key(parsed_user, key):
+    ## GIVEN a parsed_user (dict) containing user information
+
+    ## WHEN deleting key
+    parsed_user.pop(key)
+    ## THEN calling build_user() will raise KeyError
+    with pytest.raises(KeyError):
+        build_user(parsed_user)
