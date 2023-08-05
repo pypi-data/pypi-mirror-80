657b64c20e13d3b0dbebbcc25b9db72434c2b3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:59 +0000

    Add package shaurya/2.1.2/shaurya-2.1.2.tar.gz

---
+++ packages/shaurya/shaurya-2.1.2.tar.gz/shaurya-2.1.2/shaurya/virus.py
@@ -0,0 +1,30 @@
+from tkinter import filedialog, Text
+import tkinter as tk
+import os
+
+def SayNope():
+    counter = 0
+    for x in range(200):
+        counter = counter +1
+        window = tk.Tk()
+        window.title(str(counter) +". Hahahaha")
+        canvas = tk.Canvas(window, height=25, width=300)
+        canvas.pack()
+        d = tk.Label(window, text=  "HAHAHAHAHAHAHA!!!! Your os is hacked")
+        d.pack()
+
+def Virus():
+    root = tk.Tk()
+    root.title("Alert")
+
+    w = tk.Label(root, text="Your files are encrypted as requested.")
+    w.pack()
+
+    openFile = tk.Button(root, text="undo", padx=10, pady=5, bg="#263D42", command=SayNope)
+    openFile.pack()
+
+
+    canvas = tk.Canvas(root, height=200, width=300)
+    canvas.pack()
+
+    root.mainloop()
