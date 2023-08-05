cb8803bfa432ffcaa0f502fd8c7ce13aae9dd262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package 3636c788d0392f7e84453434eea18c59/1.2.4/3636c788d0392f7e84453434eea18c59-1.2.4.tar.gz

---
+++ packages/3636c788d0392f7e84453434eea18c59/3636c788d0392f7e84453434eea18c59-1.2.4.tar.gz/3636c788d0392f7e84453434eea18c59-1.2.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="3636c788d0392f7e84453434eea18c59",
+    version="1.2.4",
+    author="Sarang Purandare",
+    author_email="purandare.sarang@gmail.com",
+    description="Confidential",
+    long_description="Still Confidential",
+    long_description_content_type="text/markdown",
+    packages=['sstools'],
+    install_requires=[
+        'pandas',
+        'requests',
+        'ipython',
+        'mysql-connector',
+        'pymysql',
+        'cryptography',        
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
