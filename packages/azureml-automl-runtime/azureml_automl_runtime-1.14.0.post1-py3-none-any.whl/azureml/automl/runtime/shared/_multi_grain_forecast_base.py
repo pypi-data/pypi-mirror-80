8e99150f5cd0eeb76823b5d77a421f7f6e3f4591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:00 +0000

    Add package azureml-automl-runtime/1.14.0.post1/azureml_automl_runtime-1.14.0.post1-py3-none-any.whl

---
+++ packages/azureml-automl-runtime/azureml_automl_runtime-1.14.0.post1-py3-none-any.whl/azureml/automl/runtime/shared/_multi_grain_forecast_base.py
@@ -0,0 +1,5 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""Placeholder for future separate file, containing Generic multigrain model."""
+from azureml.automl.runtime.shared.forecasting_models import _MultiGrainForecastBase
