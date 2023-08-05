ac489450edca945dfa62740888a02cb1269726e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:27 +0000

    Add package Filezen/1.4.3/Filezen-1.4.3.tar.gz

---
+++ packages/Filezen/Filezen-1.4.3.tar.gz/Filezen-1.4.3/filezen/simpleScanner/extensionMapper.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Contains dictionary of mappings for simple scanning.
+Defines the folders where a particular filetype will be moved.
+"""
+
+extensionsToFolder = {
+    ".pdf": "fz-Documents",
+    ".doc": "fz-Documents",
+    ".docx": "fz-Documents",
+    ".txt": "fz-Documents",
+    ".ppt": "fz-Documents",
+    ".csv": "fz-Documents",
+    ".pptx": "fz-Documents",
+    ".xls": "fz-Documents",
+    ".xlsx": "fz-Documents",
+    ".mp4": "fz-Videos",
+    ".avi": "fz-Videos",
+    ".mkv": "fz-Videos",
+    ".webm": "fz-Videos",
+    ".flv": "fz-Videos",
+    ".mov": "fz-Videos",
+    ".mp3": "fz-Music",
+    ".wav": "fz-Music",
+    ".aac": "fz-Music",
+    ".wma": "fz-Music",
+    ".m4a": "fz-Music",
+    ".flac": "fz-Music",
+    ".jpeg": "fz-Images",
+    ".jpg": "fz-Images",
+    ".png": "fz-Images",
+    ".gif": "fz-Images",
+    ".tiff": "fz-Images",
+    ".raw": "fz-Images",
+    "default": "fz-Others"
+}
---
+++ packages/Filezen/Filezen-1.4.3.tar.gz/Filezen-1.4.3/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools import setup, find_packages
+from filezen.version import VERSION
+
+__version__ = VERSION
+
+
+if sys.version_info[:2] <= (2, 7):
+    with open("README.rst") as f:
+        long_description = f.read()
+else:
+    with open("README.rst", encoding="utf8") as f:
+        long_description = f.read()
+
+
+setup(
+    name='Filezen',
+    author='Abhinav Anand',
+    version=VERSION,
+    author_email='abhinavanand1905@gmail.com',
+    description="An Intelligent file organizer module that reads your file storing pattern",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url='https://github.com/ab-anand/Filezen',
+    license='MIT',
+    install_requires=[
+        "pathlib >= 1.0.1",
+        "setuptools >= 44.1.1"
+    ],
+    # dependency_links=dependency_links,
+    # adding package data to it
+    packages=find_packages(exclude=['contrib', 'docs']),
+    download_url='https://github.com/ab-anand/Filezen/tarball/' + __version__,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: System",
+        "Topic :: System :: Operating System",
+        "Topic :: Utilities",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+    ],
+    keywords=['Operating System', 'Utility', 'Automation', 'Heaps', 'File Organizer']
+)
