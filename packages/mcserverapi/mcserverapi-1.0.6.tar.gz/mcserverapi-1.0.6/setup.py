80eecbe37d81425f947037384d7f220e75fac036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:12 +0000

    Add package mcserverapi/1.0.6/mcserverapi-1.0.6.tar.gz

---
+++ packages/mcserverapi/mcserverapi-1.0.6.tar.gz/mcserverapi-1.0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mcserverapi',
+    version='1.0.6',
+    packages=['mcserverapi'],
+    url='https://github.com/TeamNightSky/MCServerWrapper',
+    license='GNU License',
+    author='Pixymon',
+    author_email='nlarsen23.student@gmail.com',
+    description='Integratable API for Local Minecraft Servers',
+    install_requires=['requests', 'psutil']
+)
