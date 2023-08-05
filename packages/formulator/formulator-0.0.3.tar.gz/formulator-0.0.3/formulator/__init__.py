bad8970aa39ef73b2c5a25e497c47767bdab3869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:32 +0000

    Add package formulator/0.0.3/formulator-0.0.3.tar.gz

---
+++ packages/formulator/formulator-0.0.3.tar.gz/formulator-0.0.3/formulator/__init__.py
@@ -0,0 +1,15 @@
+
+__version__ = '0.0.3'
+
+from formulator.ferry import *
+from .meta import *
+from .nodes import *
+from .terrors import *
+from .validation import *
+
+__all__ = [*ferry.__all__,
+           *meta.__all__,
+           *nodes.__all__,
+           *terrors.__all__,
+           *validation.__all__
+           ]           
