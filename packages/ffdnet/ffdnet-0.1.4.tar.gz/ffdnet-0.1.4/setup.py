a8bbd76b1303dcded3807da19247e3291259a1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:37 +0000

    Add package ffdnet/0.1.4/ffdnet-0.1.4.tar.gz

---
+++ packages/ffdnet/ffdnet-0.1.4.tar.gz/ffdnet-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name = 'ffdnet',
+    packages = ['ffdnet'],
+    version = '0.1.4',
+    license='GPLv3',
+    description = 'FFDNet CNN denoiser',
+    url = 'https://github.com/kidanger/ffdnet',
+    author = 'Jérémy Anger',
+    author_email = 'angerj.dev@gmail.com',
+    install_requires = [
+        'numpy',
+        'torch',
+        'fire',
+        'imageio',
+    ],
+    package_data={
+        "": ['*.pth'],
+    },
+    include_package_data=True,
+    python_requires=">=3",
+    entry_points="""
+    [console_scripts]
+    ffdnet=ffdnet:cli_main
+    """,
+)
