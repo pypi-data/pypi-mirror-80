b44cce0cf3760acd4b384b7d00ea85abae916288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:02 +0000

    Add package wapt-tools/1.9.2/wapt_tools-1.9.2-py2-none-any.whl

---
+++ packages/wapt-tools/wapt_tools-1.9.2-py2-none-any.whl/wapttools/download.py
@@ -0,0 +1,56 @@
+import glob
+import logging
+import os
+import requests
+import sys
+from .hash import sha256sum
+
+log = logging.getLogger()
+
+
+def download(url, pathname, clean=True, checksum=None):
+    """ Download the file at a given url and save it to pathname.
+
+    The files existing in the same folder and with the same extension are removed if clean=True.
+
+    Parameters
+    ----------
+    url: string
+        url of the file to be dowloaded
+    pathname: string
+        pathname of the file to be saved
+    clean: boolean
+        cleanup needed
+    """
+    extension = os.path.splitext(pathname)[1]
+
+    dirname = os.path.dirname(pathname)
+
+    if clean:
+        pattern = os.path.join(dirname, '*{}'.format(extension))
+        log.debug('cleaning files {} except {}'.format(pattern, pathname))
+        for file in glob.glob(pattern):
+            if file != pathname:
+                log.debug('removing {}'.format(file))
+                os.remove(file)
+
+    log.info('downloading {} from {}'.format(os.path.basename(pathname), url))
+    if not os.path.exists(pathname):
+        try:
+            r = requests.get(url, stream=True)
+            if not r.ok:
+                log.critical('failed to download {} from {}, 404 not found'.format(os.path.basename(pathname), url))
+                sys.exit(1)
+
+            with open(pathname, 'wb') as f:
+                for chunk in r.iter_content(1024):
+                    f.write(chunk)
+            del r
+        except Exception as e:
+            log.critical('failed to download {} from {}, {}'.format(os.path.basename(pathname), url, e))
+            sys.exit(1)
+    else:
+        log.debug('{} already exists'.format(pathname))
+
+    if checksum:
+        sha256sum(pathname, checksum)
---
+++ packages/wapt-tools/wapt_tools-1.9.2-py2-none-any.whl/wapttools/hash.py
@@ -0,0 +1,26 @@
+import hashlib
+import logging
+import os
+import sys
+
+log = logging.getLogger()
+
+
+def sha256sum(filename, checksum=None):
+    if not os.path.isfile(filename):
+        log.critical('file "{file}" does not exists'.format(file=filename))
+        sys.exit(1)
+
+    h = hashlib.sha256()
+    b = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        for n in iter(lambda: f.readinto(mv), 0):
+            h.update(mv[:n])
+    computed = h.hexdigest()
+
+    if checksum and computed != checksum:
+        log.critical('{file} checksum mismatch'.format(file=filename))
+        sys.exit(1)
+
+    return computed
