3e22c9260a8db2b6bc4ec48af43f156a05d55168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:54 +0000

    Add package gdalos/0.45.2/gdalos-0.45.2.tar.gz

---
+++ packages/gdalos/gdalos-0.45.2.tar.gz/gdalos-0.45.2/src/gdalos/viewshed/viewshed_params.py
@@ -0,0 +1,144 @@
+import copy
+from typing import Sequence
+import gdal
+
+st_seen = 5
+st_seenbut = 4
+st_hidbut = 3
+st_hidden = 2
+st_nodtm = 1
+st_nodata = 0  # out of range value
+
+viewshed_visible = st_seen
+viewshed_thresh = st_hidbut
+viewshed_invisible = st_hidden
+viewshed_out_of_range = st_nodata
+viewshed_ndv = st_nodata
+viewshed_comb_ndv = 255
+viewshed_comb_multi_val = 254
+
+atmospheric_refraction_coeff = 1/7
+
+
+class ViewshedParams(object):
+    __slots__ = ['max_r', 'min_r', 'min_r_shave', 'max_r_slant',
+                 'ox', 'oy', 'oz', 'tz', 'omsl', 'tmsl',
+                 'azimuth', 'h_aperture', 'elevation', 'v_aperture',
+                 'vv', 'iv', 'ov', 'ndv',
+                 'refraction_coeff', 'mode']
+
+    def __init__(self):
+        self.min_r = 0
+        self.max_r = None
+
+        self.min_r_shave = False
+        self.max_r_slant = True
+
+        self.ox = None
+        self.oy = None
+        self.oz = None
+        self.tz = None
+        self.omsl = False  # observer MSL
+        self.tmsl = False  # target MSL
+
+        self.azimuth = 0
+        self.h_aperture = 360
+        self.elevation = 0
+        self.v_aperture = 180
+
+        self.vv = viewshed_visible
+        self.iv = viewshed_invisible
+        self.ov = viewshed_out_of_range
+        self.ndv = viewshed_ndv
+
+        self.refraction_coeff = atmospheric_refraction_coeff
+        self.mode = 2
+
+    def is_omni_h(self):
+        return not self.h_aperture or abs(self.h_aperture - 360) < 0.0001
+
+    @property
+    def oxy(self):
+        return self.ox, self.oy
+
+    @oxy.setter
+    def oxy(self, oxy):
+        self.ox, self.oy = oxy
+
+    def get_as_gdal_params(self):
+        vp_params = \
+            'max_r', 'ox', 'oy', 'oz', 'tz', \
+            'vv', 'iv', 'ov', 'ndv', 'mode'
+
+        gdal_params = \
+            'maxDistance', 'observerX', 'observerY', 'observerHeight', 'targetHeight', \
+            'visibleVal', 'invisibleVal', 'outOfRangeVal', 'noDataVal', 'mode'
+        d = {k1: getattr(self, k0) for k0, k1 in
+             zip(vp_params, gdal_params)}
+        d['dfCurvCoeff'] = 1 - self.refraction_coeff
+        return d
+
+    def get_as_talos_params(self):
+        vp_params = \
+            'ox', 'oy', 'oz', 'max_r', 'min_r', 'min_r_shave', 'max_r_slant', 'tz', \
+            'omsl', 'tmsl', 'azimuth', 'h_aperture', 'elevation', 'v_aperture'
+
+        talos_params = \
+            'ox', 'oy', 'oz', 'MaxRange', 'MinRange', 'MinRangeShave', 'SlantRange', 'tz', \
+            'ObsMSL', 'TarMSL', 'Direction', 'Aperture', 'Elevation', 'ElevationAperture'
+        d = {k1: getattr(self, k0) for k0, k1 in
+             zip(vp_params, talos_params)}
+
+        SlackDummyHeight = -1000
+        result_dt = gdal.GDT_Byte
+        if d['oz'] is None or d['tz'] is None:
+            result_dt = gdal.GDT_Int16
+            if d['oz'] is None:
+                d['oz'] = SlackDummyHeight
+                if d['tz'] is None:
+                    raise Exception ('You have to specify at least one of oz or tz')
+            else:
+                d['tz'] = SlackDummyHeight
+        d['result_dt'] = result_dt
+        return d
+
+    def update(self, d: dict):
+        for k, v in d.items():
+            setattr(self, k, v)
+
+    @staticmethod
+    def get_list_from_lists_dict(d: dict, key_map=None) -> Sequence['ViewshedParams']:
+        max_len = max(len(v) if v else 0 for v in d.values())
+        result = []
+        vp = ViewshedParams()
+        for i in range(max_len):
+            vp = copy.deepcopy(vp)
+            for k, v in d.items():
+                if not v:
+                    continue
+                if key_map:
+                    k = key_map[k]
+                len_v = len(v)
+                if i < len_v:
+                    setattr(vp, k, v[i])
+                    # vp1.k = v[i]
+            result.append(vp)
+        return result
+
+
+# gdal_viewshed_params_short = \
+#     'max_r', 'ox', 'oy', 'oz', 'tz', \
+#     'vv', 'iv', 'ov', 'ndv', 'mode',
+#
+# gdal_viewshed_params_full = \
+#     'maxDistance', 'observerX', 'observerY', 'observerHeight', 'targetHeight', \
+#     'visibleVal', 'invisibleVal', 'outOfRangeVal', 'noDataVal', 'mode'
+# gdal_viewshed_keymap = dict(zip(gdal_viewshed_params_short, gdal_viewshed_params_full))
+
+viewshed_defaults = dict(vv=viewshed_visible,
+                         iv=viewshed_invisible,
+                         ov=viewshed_out_of_range,
+                         ndv=viewshed_ndv,
+                         )
+
+# gdal_viewshed_defaults = dict_util.replace_keys(viewshed_defaults,  gdal_viewshed_keymap)
