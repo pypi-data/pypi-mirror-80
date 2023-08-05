097d73b47a922160692df9c834edaa8c93fc00cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:39 +0000

    Add package yywtestpackage/0.1.3/yywtestpackage-0.1.3.tar.gz

---
+++ packages/yywtestpackage/yywtestpackage-0.1.3.tar.gz/yywtestpackage-0.1.3/yywtestpackage/__init__ .py
@@ -0,0 +1,10 @@
+from .core import *
+from . import myfunction
+
+# __all__ = ['myfunction']
+#
+# __all__.extend(myfunction.__all__)
+#
+# submodules = (myfunction)
+
+__version__ = '0.1.3'
---
+++ packages/yywtestpackage/yywtestpackage-0.1.3.tar.gz/yywtestpackage-0.1.3/yywtestpackage/core.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+# tsetpackage.py
+def print_test():
+    print('这是一个测试！')
+
