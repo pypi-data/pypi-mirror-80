048ff90a1860877a715fbaece2954f83b0a229df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:56 +0000

    Add package rss-reader/3.2/rss_reader-3.2-py3-none-any.whl

---
+++ packages/rss-reader/rss_reader-3.2-py3-none-any.whl/rss_reader/__main__.py
@@ -0,0 +1,19 @@
+from .reader import Reader
+import logging
+
+# TODO: Add pretty html/pdf
+logger = logging.getLogger("rss-reader")
+
+
+def main():
+    reader = Reader()
+    try:
+        reader.start()
+    except Exception as e:
+        logger.exception(e)
+        print(f"Rss reader crashed from {type(e).__name__}. "
+              "Consider using --verbose for more info")
+
+
+if __name__ == '__main__':
+    main()
