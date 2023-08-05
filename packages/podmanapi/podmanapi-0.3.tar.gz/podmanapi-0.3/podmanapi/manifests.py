fe56dea9db551c8879abf44676fc6293d7aa50b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:52 +0000

    Add package podmanapi/0.3/podmanapi-0.3.tar.gz

---
+++ packages/podmanapi/podmanapi-0.3.tar.gz/podmanapi-0.3/podmanapi/manifests.py
@@ -0,0 +1,27 @@
+"""API functions related to manifests."""
+
+import requests_unixsocket
+
+from podmanapi import api_endpoint
+
+session = requests_unixsocket.Session()
+
+
+def remove():
+    pass
+
+
+def add_manifest():
+    pass
+
+
+def inspect():
+    pass
+
+
+def push():
+    pass
+
+
+def create():
+    pass
---
+++ packages/podmanapi/podmanapi-0.3.tar.gz/podmanapi-0.3/podmanapi/networks.py
@@ -0,0 +1,23 @@
+"""API functions related to networks."""
+
+import requests_unixsocket
+
+from podmanapi import api_endpoint
+
+session = requests_unixsocket.Session()
+
+
+def remove():
+    pass
+
+
+def inspect():
+    pass
+
+
+def create():
+    pass
+
+
+def list_networks():
+    pass
