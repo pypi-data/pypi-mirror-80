b22fe8a01a9654d12e2342a1d47509762143362c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:34 +0000

    Add package argparseware/0.9.4/argparseware-0.9.4.tar.gz

---
+++ packages/argparseware/argparseware-0.9.4.tar.gz/argparseware-0.9.4/argparseware/__init__.py
@@ -0,0 +1,24 @@
+"""
+argparse middleware library.
+
+argparseware is designed as a turnkey library for developers who like the
+standard library, most specifically argparse. It extends the built-in argparse
+library by simply allowing one to define middleware components in a very
+simple syntax, improving reusability, while not compromising or reinventing
+the programming interfaces they already know.
+"""
+
+from .core import ArgumentParser
+
+
+# Package version
+__version__ = '0.9.4'
+__versiont__ = (0, 9, 4)
+
+# Author information
+__author__ = 'Francis Lacroix'
+__author_email__ = 'f@relevance.io'
+
+# License information
+__license__ = 'TBD'
+__copyright__ = 'Relevance.io 2019-'
