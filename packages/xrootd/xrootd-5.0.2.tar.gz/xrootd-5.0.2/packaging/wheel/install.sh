3279749b50dd6c6bdace939608a8d347a73a8c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:40 +0000

    Add package xrootd/5.0.2/xrootd-5.0.2.tar.gz

---
+++ packages/xrootd/xrootd-5.0.2.tar.gz/xrootd-5.0.2/packaging/wheel/install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+startdir="$(pwd)"
+mkdir xrootdbuild
+cd xrootdbuild
+
+# build only client
+# build python bindings
+# set install prefix
+# set the respective version of python
+# replace the default BINDIR with a custom one that can be easily removed afterwards
+#    (for the python bindings we don't want to install the binaries)
+CMAKE_ARGS="-DPYPI_BUILD=TRUE -DXRDCL_LIB_ONLY=TRUE -DENABLE_PYTHON=TRUE -DCMAKE_INSTALL_PREFIX=$1 -DXRD_PYTHON_REQ_VERSION=$2 -DCMAKE_INSTALL_BINDIR=$startdir/xrootdbuild/bin"
+
+cmake_path=$4
+$cmake_path .. $CMAKE_ARGS
+
+res=$?
+if [ "$res" -ne "0" ]; then
+    exit 1
+fi
+
+make -j8
+res=$?
+if [ "$res" -ne "0" ]; then
+    exit 1
+fi
+
+cd src
+make install
+res=$?
+if [ "$res" -ne "0" ]; then
+    exit 1
+fi
+
+cd ../bindings/python
+python$2 setup.py install $3
+res=$?
+if [ "$res" -ne "0" ]; then
+    exit 1
+fi
+
+cd $startdir
+rm -r xrootdbuild
