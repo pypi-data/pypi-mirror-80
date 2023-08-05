486d55a8e51c0957ff1199eff22629ea421b1bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:38 +0000

    Add package duckietown-challenges-daffy/6.0.14/duckietown-challenges-daffy-6.0.14.tar.gz

---
+++ packages/duckietown-challenges-daffy/duckietown-challenges-daffy-6.0.14.tar.gz/duckietown-challenges-daffy-6.0.14/src/duckietown_challenges/__init__.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+__version__ = "6.0.14"
+
+from zuper_commons.logs import ZLogger
+
+dclogger = logger = ZLogger(__name__)
+logger.info(f"version: {__version__}")
+
+from .challenges_constants import ChallengesConstants
+from .solution_interface import *
+from .constants import *
+from .exceptions import *
+from .challenge import *
+from .challenge_evaluator import *
+from .challenge_solution import *
+from .challenge_results import *
+from .cie_concrete import *
+from .follow import *
+
+from .make_readmes import make_readmes_main as make_readmes_main
+from .make_readme_templates import make_readmes_templates_main
