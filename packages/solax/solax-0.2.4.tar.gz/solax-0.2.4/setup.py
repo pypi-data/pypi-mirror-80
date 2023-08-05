0f63338015c2c8c1d03ffb4cd2de443a80f7dc25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:40 +0000

    Add package solax/0.2.4/solax-0.2.4.tar.gz

---
+++ packages/solax/solax-0.2.4.tar.gz/solax-0.2.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="solax",
+    version="0.2.4",
+    author="Robin Wohlers-Reichel",
+    author_email="me@robinwr.com",
+    description="Solax inverter API client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/squishykid/solax",
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    install_requires=[
+        'aiohttp>=3.5.4',
+        'voluptuous>=0.11.5'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
