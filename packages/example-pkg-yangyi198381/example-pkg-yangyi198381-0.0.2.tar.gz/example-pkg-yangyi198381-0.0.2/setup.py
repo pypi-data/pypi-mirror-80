9ddbce45b04671ddfb71859a54f8df319b853e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:37 +0000

    Add package example-pkg-yangyi198381/0.0.2/example-pkg-yangyi198381-0.0.2.tar.gz

---
+++ packages/example-pkg-yangyi198381/example-pkg-yangyi198381-0.0.2.tar.gz/example-pkg-yangyi198381-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-yangyi198381",
+    version="0.0.2",
+    author="yangyi198381",
+    author_email="54279855@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
