a24b3d2cbba217e2c37287c06af7413fad76e044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:34 +0000

    Add package cvnn/0.2.92/cvnn-0.2.92.tar.gz

---
+++ packages/cvnn/cvnn-0.2.92.tar.gz/cvnn-0.2.92/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import versioneer
+
+requirements = [
+    'tensorflow>=2.0',
+    'numpy',
+    'pandas', 'scipy', 'prettytable',                   # Data
+    'colorlog', 'openpyxl',                             # Logging
+    'matplotlib', 'seaborn', 'plotly', 'tikzplotlib',   # Plotting
+    'tqdm'
+]
+
+setup(
+    name='cvnn',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Library to help implement a complex-valued neural network (cvnn) using tensorflow as back-end",
+    license="MIT",
+    author="J Agustin BARRACHINA",
+    author_email='joseagustin.barra@gmail.com',
+    url='https://github.com/NEGU93/cvnn',
+    packages=['cvnn'],
+    entry_points={
+        'console_scripts': [
+            'cvnn=cvnn.cli:cli'
+        ]
+    },
+    install_requires=requirements,
+    keywords='cvnn',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
