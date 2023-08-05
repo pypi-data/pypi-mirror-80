fdc3885dc93514171090f2aa729b52224b0b2976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:35 +0000

    Add package jimraynorsmodule/0.0.2/jimraynorsmodule-0.0.2.tar.gz

---
+++ packages/jimraynorsmodule/jimraynorsmodule-0.0.2.tar.gz/jimraynorsmodule-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jimraynorsmodule',
+    version='0.0.2',
+    description='Marine utilities to free galaxy from Zerg infestation',
+    author='Jim Raynor',
+    author_email='jimraynor@spacemarines.com',
+    url='https://www.jimraynorspage.com',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    )
