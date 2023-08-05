9650c5e6ea68017753046206a037f85f5a3bd27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package pyvalid/0.9.3/pyvalid-0.9.3.tar.gz

---
+++ packages/pyvalid/pyvalid-0.9.3.tar.gz/pyvalid-0.9.3/setup.py
@@ -0,0 +1,81 @@
+from pyvalid import version
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def main():
+    # Describe installer
+    with open('README.rst') as fd:
+        long_description = fd.read()
+    with open('requirements.txt') as fd:
+        requirements = fd.read().splitlines()
+    setup(
+        name='pyvalid',
+        version=version,
+        author='Maxim Grischuk',
+        author_email='uzumaxy@gmail.com',
+        maintainer='Maxim Grischuk',
+        maintainer_email='uzumaxy@gmail.com',
+        packages=['pyvalid'],
+        url='http://uzumaxy.github.io/pyvalid/',
+        download_url='https://github.com/uzumaxy/pyvalid/releases',
+        license='BSD',
+        description='pyvalid is a Python validation tool for checking of '
+                    'input function parameters and return values.',
+        long_description=long_description,
+        install_requires=requirements,
+        keywords=[
+            'pyvalid', 'valid',
+            'validation', 'type',
+            'checking', 'check',
+            'decorator'
+        ],
+        platforms='Platform Independent',
+        package_data={
+            'pyvalid': ['LICENSE', 'README.rst']
+        },
+        test_suite="tests",
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Information Technology',
+            'License :: OSI Approved :: BSD License',
+            'Natural Language :: English',
+            'Operating System :: MacOS',
+            'Operating System :: Microsoft :: Windows :: Windows 7',
+            'Operating System :: Microsoft :: Windows :: Windows NT/2000',
+            'Operating System :: Microsoft :: Windows :: Windows Server 2003',
+            'Operating System :: Microsoft :: Windows :: Windows Server 2008',
+            'Operating System :: Microsoft :: Windows :: Windows Vista',
+            'Operating System :: Microsoft :: Windows :: Windows XP',
+            'Operating System :: Microsoft',
+            'Operating System :: OS Independent',
+            'Operating System :: POSIX :: BSD :: FreeBSD',
+            'Operating System :: POSIX :: Linux',
+            'Operating System :: POSIX :: SunOS/Solaris',
+            'Operating System :: POSIX',
+            'Operating System :: Unix',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.0',
+            'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: Implementation',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Debuggers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Testing'
+        ],
+    )
+
+
+if __name__ == '__main__':
+    main()
