12d662a2a852fd2cb54f61c83043476e6c36057e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:30 +0000

    Add package bashrc/0.1.141/bashrc-0.1.141-py3-none-any.whl

---
+++ packages/bashrc/bashrc-0.1.141-py3-none-any.whl/bashrc-0.1.141.data/scripts/project-upgrade.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# ${1} - name
+# shellcheck disable=SC2034
+export starting="${BASH_SOURCE[0]}"; debug.sh starting
+
+unset VIRTUAL_ENV PYTHONHOME
+deactivate > /dev/null 2>&1
+
+test -n "${DARWIN}" || sudo=sudo
+echo name
+if error="$( ${sudo} /usr/local/bin/pip3.8 install -vvvv --upgrade "${1}" 2>&1 )"; then
+  info.sh upgrade "${1}"
+else
+  error.sh upgrade "${1}" "${error}"; exit 1
+fi
+
+## I do not know why requires twice to get it into the path,
+${sudo} /usr/local/bin/pip3.8 install --upgrade "${1}" > /dev/null 2>&1
+
+unset starting error path name
