bd2388bd25fb7328d017ddfd5dcc79fc03f3458d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package learning-hypermodern/0.1.3/learning-hypermodern-0.1.3.tar.gz

---
+++ packages/learning-hypermodern/learning-hypermodern-0.1.3.tar.gz/learning-hypermodern-0.1.3/src/learning_hypermodern/wikipedia.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+import click
+import desert
+import marshmallow
+import requests
+
+API_URL: str = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
+
+
+@dataclass
+class Page:
+    title: str
+    extract: str
+
+
+schema = desert.schema(Page, meta={"unknown": marshmallow.EXCLUDE})
+
+
+def random_page(language: str = "en") -> Page:
+    url = API_URL.format(language=language)
+
+    try:
+        with requests.get(url) as response:
+            response.raise_for_status()
+            data = response.json()
+            return schema.load(data)
+    except (requests.RequestException, marshmallow.ValidationError) as error:
+        message = str(error)
+        raise click.ClickException(message)
