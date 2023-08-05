e508596948ec74ccfc8d079e53ee9a8024155c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:12 +0000

    Add package typeschemalib/0.2/typeschemalib-0.2.tar.gz

---
+++ packages/typeschemalib/typeschemalib-0.2.tar.gz/typeschemalib-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='typeschemalib',
+      version='0.2',
+      description='A yaml like schema that can be used to check dictionaries for correct schema',
+      url='https://github.com/JakeRoggenbuck/typeschemalib',
+      author='Jake Roggenbuck',
+      author_email='jake@jr0.org',
+      license='MIT',
+      packages=['typeschemalib'],
+      zip_safe=False)
