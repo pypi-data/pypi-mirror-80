b9cb159e40780243174243ed6744440860029dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:59 +0000

    Add package itomate/0.3.9/itomate-0.3.9.tar.gz

---
+++ packages/itomate/itomate-0.3.9.tar.gz/itomate-0.3.9/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+from itomate.itomate import version
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="itomate",
+    version=version,
+    author="Kamran Ahmed",
+    author_email="kamranahmed.se@gmail.com",
+    description="Automate your iTerm layouts and workflows",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kamranahmedse/itomate",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "iterm2>=1.1",
+        "PyYAML>=5.3.1",
+
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ],
+    python_requires='>=3.7.0',
+    license="MIT",
+    entry_points="""
+        [console_scripts]
+        itomate=itomate.itomate:main
+    """
+)
