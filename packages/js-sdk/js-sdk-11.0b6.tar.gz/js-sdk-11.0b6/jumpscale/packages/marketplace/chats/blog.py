703162e7d5713ecec348e021a2d24a6cd79bc995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:18 +0000

    Add package js-sdk/11.0b6/js-sdk-11.0b6.tar.gz

---
+++ packages/js-sdk/js-sdk-11.0b6.tar.gz/js-sdk-11.0b6/jumpscale/packages/marketplace/chats/blog.py
@@ -0,0 +1,28 @@
+from jumpscale.packages.marketplace.chats.publisher import Publisher
+from jumpscale.sals.chatflows.chatflows import chatflow_step
+
+
+class BlogDeploy(Publisher):
+
+    title = "Deploy a Blog"
+    SOLUTION_TYPE = "blog"  # chatflow used to deploy the solution
+
+    @chatflow_step(title="blog Setup")
+    def configuration(self):
+        self.user_email = self.user_info()["email"]
+        form = self.new_form()
+        title = form.string_ask("Title", required=True)
+        url = form.string_ask("Repository URL", required=True, is_git_url=True)
+        branch = form.string_ask("Branch", required=True)
+        form.ask(Publisher.MD_CONFIG_MSG, md=True)
+        self.envars = {
+            "TYPE": "blog",
+            "NAME": "entrypoint",
+            "TITLE": title.value,
+            "URL": url.value,
+            "BRANCH": branch.value,
+            "EMAIL": self.user_email,
+        }
+
+
+chat = BlogDeploy
