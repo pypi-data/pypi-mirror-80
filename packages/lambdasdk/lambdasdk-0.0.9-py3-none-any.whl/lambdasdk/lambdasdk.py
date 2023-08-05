4b3aba29a9c10309bfbef36ddb6496395e60a803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:02 +0000

    Add package lambdasdk/0.0.9/lambdasdk-0.0.9-py3-none-any.whl

---
+++ packages/lambdasdk/lambdasdk-0.0.9-py3-none-any.whl/lambdasdk/lambdasdk.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+import boto3, base64, json
+
+class Lambda:
+    '''
+      for invoking lambda functions
+    '''
+    def __init__(self, user=None, pw=None, region = 'ap-southeast-1'):
+      self.lambdaClient = boto3.client(
+          'lambda',
+          aws_access_key_id=user,
+          aws_secret_access_key=pw,
+          region_name = region
+        )
+    def invoke(self, functionName, input):
+      return self.lambdaClient.invoke(
+        FunctionName = functionName,
+        InvocationType= 'RequestResponse',
+        LogType='Tail',
+        ClientContext= base64.b64encode(json.dumps({'caller': 'sdk'}).encode()).decode(),
+        Payload= json.dumps(input)
+      )
