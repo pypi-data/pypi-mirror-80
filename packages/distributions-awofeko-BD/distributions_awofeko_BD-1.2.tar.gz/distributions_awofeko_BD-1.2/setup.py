26407a4cb14ab5e691a1f2b5277745e3d0caa9c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:03 +0000

    Add package distributions-awofeko-BD/1.2/distributions_awofeko_BD-1.2.tar.gz

---
+++ packages/distributions-awofeko-BD/distributions_awofeko_BD-1.2.tar.gz/distributions_awofeko_BD-1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='distributions_awofeko_BD',
+      version='1.2',
+      description='Gaussian and Binomial Distributions and probability',
+      packages=['distributions_awofeko_BD'],
+      author='MICHAEL AWOEFKO',
+      author_email='michaelawofeko@gmail.com',
+      zip_safe=False
+      )
