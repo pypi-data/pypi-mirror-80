5c74bf42b6eb1605dbb288154d9564f8affb9668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:56 +0000

    Add package rrparser/2.0.5/rrparser-2.0.5.tar.gz

---
+++ packages/rrparser/rrparser-2.0.5.tar.gz/rrparser-2.0.5/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+
+
+_readme = 'README.md'
+
+_extras_path = 'extras'
+
+with open(_extras_path+'/.env', 'r') as f:
+    line = f.readline()
+    if line.startswith('PACKAGE='):
+        _package = line.splitlines()[0].split('=')[1].lower()
+
+with open(_readme, 'r') as f:
+    long_description = f.read()
+
+required = []
+with open(_extras_path+'/requirements.txt', 'r') as f:
+    required = [line[:-1] for line in f]
+
+_release = 'RELEASE'
+# extra_files={
+#     'release': (_package, [_package+'/doc/'+_release])
+# }
+
+# with open(extra_files['release'][1][0], 'r') as f:
+with open(_release, 'r') as f:
+    _version = f.readline().split()[0]
+
+setup(
+    name=_package,
+    version=_version,
+    author='Thomas Duigou, Melchior du Lac, Joan Hérisson',
+    author_email='joan.herisson@univ-evry.fr',
+    description='Reaction Rules Parser',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/brsynth/RRulesParser',
+    packages=[_package],
+    package_dir={_package: 'src'},
+    include_package_data=True,
+    install_requires=required,
+    test_suite='discover_tests',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5',
+)
---
+++ packages/rrparser/rrparser-2.0.5.tar.gz/rrparser-2.0.5/src/__main__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from logging  import error as logging_error
+from rrparser import Parser, build_args_parser
+
+
+def _cli():
+    parser = build_args_parser()
+    args  = parser.parse_args()
+
+    try:
+        return Parser().parse_rules(rules_file=args.rules_file,
+                                    input_format=args.input_format,
+                                    rule_type=args.rule_type,
+                                    diameters=args.diameters,
+                                    outdir=args.outdir,
+                                    outfile=args.outfile,
+                                    output_format=args.output_format)
+    except ValueError as e:
+        logging_error(str(e))
+
+
+if __name__ == '__main__':
+    _cli()
