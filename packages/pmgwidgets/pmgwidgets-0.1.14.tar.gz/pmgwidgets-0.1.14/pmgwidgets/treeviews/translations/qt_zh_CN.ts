1db3e062090b24a3e0ac5240df56c564698de61a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:00 +0000

    Add package pmgwidgets/0.1.14/pmgwidgets-0.1.14.tar.gz

---
+++ packages/pmgwidgets/pmgwidgets-0.1.14.tar.gz/pmgwidgets-0.1.14/pmgwidgets/treeviews/translations/qt_zh_CN.ts
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>PMGFilesTreeview</name>
+    <message>
+        <location filename="../filetreeview.py" line="95"/>
+        <source>Open</source>
+        <translation type="unfinished">打开</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="97"/>
+        <source>Import</source>
+        <translation type="unfinished">导入</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="100"/>
+        <source>New..</source>
+        <translation type="unfinished">新建</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="102"/>
+        <source>File</source>
+        <translation type="unfinished">文件</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="103"/>
+        <source>Folder</source>
+        <translation type="unfinished">文件夹</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="106"/>
+        <source>Rename</source>
+        <translation type="unfinished">重命名</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="107"/>
+        <source>Delete</source>
+        <translation type="unfinished">删除</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="195"/>
+        <source>Please Input file name</source>
+        <translation type="unfinished">请输入文件名</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="215"/>
+        <source>Error</source>
+        <translation type="unfinished">错误</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="134"/>
+        <source>Folder name %s is illeagal!</source>
+        <translation type="unfinished">文件夹名称%s不合规范!</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="144"/>
+        <source>Folder %s already exists!</source>
+        <translation type="unfinished">文件夹%s 已经存在!</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="165"/>
+        <source>File %s already exists!</source>
+        <translation type="unfinished">文件%s已经存在!</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="200"/>
+        <source>Unable to Rename this file.</source>
+        <translation type="unfinished">无法重命名这个文件。</translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="215"/>
+        <source>Unable to Move this file to recycle bin.</source>
+        <translation type="unfinished">无法将这个文件移动到回收站。</translation>
+    </message>
+</context>
+<context>
+    <name>RewriteQFileSystemModel</name>
+    <message>
+        <location filename="../filetreeview.py" line="14"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="16"/>
+        <source>Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="18"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../filetreeview.py" line="20"/>
+        <source>Last Modified</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
