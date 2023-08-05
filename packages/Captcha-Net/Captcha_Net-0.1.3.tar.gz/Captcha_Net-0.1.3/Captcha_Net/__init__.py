f17a7c10fa0b1da6bab30b33a7e130c81c2f9cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:15 +0000

    Add package Captcha-Net/0.1.3/Captcha_Net-0.1.3.tar.gz

---
+++ packages/Captcha-Net/Captcha_Net-0.1.3.tar.gz/Captcha_Net-0.1.3/Captcha_Net/__init__.py
@@ -0,0 +1,8 @@
+from Captcha_Net.CenterNet import CenterNetModule
+from Captcha_Net.CNN import CnnNetModule
+
+# __all__ = ["utils"]
+#
+# if utils.__TORCH_AVAILABLE__:
+#     from . import dataset, backbone, module, networks
+#     __all__ += ["backbone", "dataset", "module", "networks"]
