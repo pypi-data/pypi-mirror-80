c8a26b9a460af28733f1511db31c48a9b06da46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:51 +0000

    Add package yourscript-test/0.1.4/yourscript_test-0.1.4-py3-none-any.whl

---
+++ packages/yourscript-test/yourscript_test-0.1.4-py3-none-any.whl/yourscirpt-test/__init__.py
@@ -0,0 +1,13 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+------------------------------------
+@Project : click_cli
+@Time    : 2020/9/21 15:16
+@Auth    : luozhongwen
+@Email   : luozw@inhand.com.cn
+@File    : __init__.py
+@IDE     : PyCharm
+------------------------------------
+"""
