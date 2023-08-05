4c6047594b6dc5f0316fa6fc4c3314f598de4388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:43 +0000

    Add package ucloud-sdk-python2/0.9.2/ucloud-sdk-python2-0.9.2.tar.gz

---
+++ packages/ucloud-sdk-python2/ucloud-sdk-python2-0.9.2.tar.gz/ucloud-sdk-python2-0.9.2/ucloud/core/typesystem/schema.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from ucloud.core.typesystem import abstract
+from ucloud.core.exc import ValidationException
+from ucloud.core.utils import compat
+
+logger = logging.getLogger(__name__)
+
+
+class Schema(abstract.Schema):
+    fields = {}
+
+    def dumps(self, d, name=None, **kwargs):
+        result = {}
+        errors = []
+        for k, field in self.fields.items():
+            v = d.get(k)
+            if v is None:
+                if field.required:
+                    errors.append(
+                        ValidationException(
+                            "the field {k} is required".format(k=k)
+                        )
+                    )
+                    continue
+                if field.default is None:
+                    continue
+                if isinstance(field.default, compat.Callable):
+                    v = field.default()
+                else:
+                    v = field.default
+            try:
+                serialized = field.dumps(v, name=k)
+            except ValidationException as e:
+                errors.extend(e.errors)
+                continue
+            result[field.dump_to or k] = serialized
+        if len(errors) > 0:
+            raise ValidationException(errors)
+        return result
+
+    def loads(self, d, name=None, **kwargs):
+        result = {}
+        errors = []
+        if not self.case_sensitive:
+            d = {k.lower(): v for k, v in d.items()}
+        for k, field in self.fields.items():
+            load_key = field.load_from or k
+            v = d.get(load_key if self.case_sensitive else load_key.lower())
+            if v is None:
+                if field.default is None:
+                    continue
+                if isinstance(field.default, compat.Callable):
+                    v = field.default()
+                else:
+                    v = field.default
+            try:
+                serialized = field.loads(v, name=k)
+            except ValidationException as e:
+                errors.extend(e.errors)
+                continue
+            result[k] = serialized
+        if len(errors) > 0:
+            raise ValidationException(errors)
+        return result
+
+
+class RequestSchema(Schema):
+    fields = {}
+
+    def dumps(self, d, name=None, **kwargs):
+        if not isinstance(d, dict):
+            raise ValidationException(
+                "invalid field {}, expect dict, got {}".format(name, type(d))
+            )
+        result = super(RequestSchema, self).dumps(d, name=name, **kwargs)
+        return result
+
+
+class ResponseSchema(Schema):
+    pass
