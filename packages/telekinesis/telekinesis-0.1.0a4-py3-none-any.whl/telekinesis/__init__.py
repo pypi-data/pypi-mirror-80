9384cc1c0d2ad17bc9c06b5c35ab92997fb6af50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:02 +0000

    Add package telekinesis/0.1.0a4/telekinesis-0.1.0a4-py3-none-any.whl

---
+++ packages/telekinesis/telekinesis-0.1.0a4-py3-none-any.whl/telekinesis/__init__.py
@@ -0,0 +1,5 @@
+from .client import Session, Connection, Channel
+from .broker import Broker
+from .remote_objects import spawn, RemoteController
+
+# warnings.warn('EXPERIMENTAL package. API may change. Use only in non-critical, sand-boxed applications. Submit issues at https://github.com/eneuman/camarere')
