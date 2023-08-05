007e9915f87525a61e2c1b838a073903e3fd17bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:08 +0000

    Add package pytorch-ignite/0.4.2/pytorch_ignite-0.4.2-py3.7.egg

---
+++ packages/pytorch-ignite/pytorch_ignite-0.4.2-py3.7.egg/ignite/metrics/loss.py
@@ -0,0 +1,71 @@
+from typing import Callable, Optional, Sequence, Union
+
+import torch
+
+from ignite.exceptions import NotComputableError
+from ignite.metrics.metric import Metric, reinit__is_reduced, sync_all_reduce
+
+__all__ = ["Loss"]
+
+
+class Loss(Metric):
+    """
+    Calculates the average loss according to the passed loss_fn.
+
+    Args:
+        loss_fn (callable): a callable taking a prediction tensor, a target
+            tensor, optionally other arguments, and returns the average loss
+            over all observations in the batch.
+        output_transform (callable): a callable that is used to transform the
+            :class:`~ignite.engine.engine.Engine`'s ``process_function``'s output into the
+            form expected by the metric.
+            This can be useful if, for example, you have a multi-output model and
+            you want to compute the metric with respect to one of the outputs.
+            The output is expected to be a tuple `(prediction, target)` or
+            (prediction, target, kwargs) where kwargs is a dictionary of extra
+            keywords arguments. If extra keywords arguments are provided they are passed to `loss_fn`.
+        batch_size (callable): a callable taking a target tensor that returns the
+            first dimension size (usually the batch size).
+        device (str of torch.device, optional): unused argument.
+
+    """
+
+    required_output_keys = None
+
+    def __init__(
+        self,
+        loss_fn: Callable,
+        output_transform: Callable = lambda x: x,
+        batch_size: Callable = lambda x: len(x),
+        device: Optional[Union[str, torch.device]] = None,
+    ):
+        super(Loss, self).__init__(output_transform, device=device)
+        self._loss_fn = loss_fn
+        self._batch_size = batch_size
+
+    @reinit__is_reduced
+    def reset(self) -> None:
+        self._sum = 0
+        self._num_examples = 0
+
+    @reinit__is_reduced
+    def update(self, output: Sequence[Union[torch.Tensor, dict]]) -> None:
+        if len(output) == 2:
+            y_pred, y = output
+            kwargs = {}
+        else:
+            y_pred, y, kwargs = output
+        average_loss = self._loss_fn(y_pred, y, **kwargs)
+
+        if len(average_loss.shape) != 0:
+            raise ValueError("loss_fn did not return the average loss.")
+
+        n = self._batch_size(y)
+        self._sum += average_loss.item() * n
+        self._num_examples += n
+
+    @sync_all_reduce("_sum", "_num_examples")
+    def compute(self) -> None:
+        if self._num_examples == 0:
+            raise NotComputableError("Loss must have at least one example before it can be computed.")
+        return self._sum / self._num_examples
