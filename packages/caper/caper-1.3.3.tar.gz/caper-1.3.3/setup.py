0117fdebe8636fa04fe945f3fe2a18867598b054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:45 +0000

    Add package caper/1.3.3/caper-1.3.3.tar.gz

---
+++ packages/caper/caper-1.3.3.tar.gz/caper-1.3.3/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='caper',
+    version='1.3.3',
+    python_requires='>=3.6',
+    scripts=[
+        'bin/caper',
+        'bin/run_mysql_server_docker.sh',
+        'bin/run_mysql_server_singularity.sh',
+        'scripts/gcp_caper_server/create_instance.sh',
+    ],
+    author='Jin Lee',
+    author_email='leepc12@gmail.com',
+    description='Cromwell Assisted Pipeline ExecutoR',
+    long_description='https://github.com/ENCODE-DCC/caper',
+    long_description_content_type='text/markdown',
+    url='https://github.com/ENCODE-DCC/caper',
+    packages=setuptools.find_packages(exclude=['mysql*', 'docs']),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+    ],
+    install_requires=[
+        'pyhocon>=0.3.53',
+        'requests>=2.20',
+        'pyopenssl',
+        'autouri>=0.2.2',
+        'miniwdl',
+        'humanfriendly',
+        'numpy>=1.8.2',
+        'pandas>=1.0',
+        'scikit-learn>=0.19.2',
+        'matplotlib>=1.5',
+    ],
+)
