79faa6f6c8eff69b3eb1658b3d828f5eeffa8d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package brcode/1.2/brcode-1.2.tar.gz

---
+++ packages/brcode/brcode-1.2.tar.gz/brcode-1.2/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(name='brcode',
+      version='1.2',
+      description='Extração de dados BR CODE',
+      url='https://github.com/alexandremulina/brqr',
+      author='Hudson Brendon',
+      author_email='alexandremulina@hotmail.com',
+      license='MIT',
+      packages=['brcode'],
+      zip_safe=False)
