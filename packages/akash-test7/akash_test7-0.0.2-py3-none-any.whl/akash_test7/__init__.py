505343b21817449c57ec22de16ec86bf1bfc783d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:19 +0000

    Add package akash-test7/0.0.2/akash_test7-0.0.2-py3-none-any.whl

---
+++ packages/akash-test7/akash_test7-0.0.2-py3-none-any.whl/akash_test7/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 11 18:44:35 2020
+
+@author: akash
+"""
+
+def sum(a,b):
+    c = a + b
+    print ("python")
+    return c
+
+def mult(a,b):
+    print (a*b)
+
+def div(a,b):
+    print(a/b)
+
+def substract(a,b):
+    print(a-b)
