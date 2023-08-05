29af986829cf120d7cdce174c545f36985381a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:31 +0000

    Add package testpypijuhee/0.3/testpypijuhee-0.3.tar.gz

---
+++ packages/testpypijuhee/testpypijuhee-0.3.tar.gz/testpypijuhee-0.3/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+  name = 'testpypijuhee',
+  packages = ['testpypijuhee'],
+  version = '0.3',
+  license='MIT',
+  description = 'test how to upload package on PyPi',
+  author = 'Kang Juhee',
+  author_email = 'treeinblu@gmail.com',
+  url = 'https://github.com/JuheeKang04/testpypijuhee',
+  download_url = 'https://github.com/JuheeKang04/testpypijuhee/archive/v_03.tar.gz',
+  keywords = ['TEST'],
+  install_requires=[
+          'kss',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
