904bc503f317e6c89261d294eff4ff9fcaf06a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:57 +0000

    Add package pycopy-venv/0.2/pycopy-venv-0.2.tar.gz

---
+++ packages/pycopy-venv/pycopy-venv-0.2.tar.gz/pycopy-venv-0.2/venv.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+
+if len(sys.argv) != 2:
+    print("usage: venv ENV_DIR")
+    sys.exit(1)
+
+env = sys.argv[1]
+os.makedirs(env + "/bin")
+with open(env + "/bin/activate", "w") as f:
+    f.write("""\
+export PYCOPYPATH=%s
+""" % os.path.abspath(env + "/lib"))
