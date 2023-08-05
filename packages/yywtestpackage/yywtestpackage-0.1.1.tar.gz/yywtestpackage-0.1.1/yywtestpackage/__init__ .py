490d0c9fa080405a0f67e06759aad8f533448284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:25 +0000

    Add package yywtestpackage/0.1.1/yywtestpackage-0.1.1.tar.gz

---
+++ packages/yywtestpackage/yywtestpackage-0.1.1.tar.gz/yywtestpackage-0.1.1/yywtestpackage/__init__ .py
@@ -0,0 +1,11 @@
+from . import myfunction
+
+from .myfunction import *
+
+__all__ = ['myfunction']
+
+__all__.extend(myfunction.__all__)
+
+submodules = (myfunction)
+
+__version__ = '0.1.1'
---
+++ packages/yywtestpackage/yywtestpackage-0.1.1.tar.gz/yywtestpackage-0.1.1/yywtestpackage/myfunction.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+# tsetpackage.py
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
