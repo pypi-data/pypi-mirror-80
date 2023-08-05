52051abf495a8d26ac2f784b087038f3aec06e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:15 +0000

    Add package PaperScissorsRock-Lib/0.1.0/PaperScissorsRock-Lib-0.1.0.tar.gz

---
+++ packages/PaperScissorsRock-Lib/PaperScissorsRock-Lib-0.1.0.tar.gz/PaperScissorsRock-Lib-0.1.0/PaperScissorRock/Game.py
@@ -0,0 +1,21 @@
+import random
+
+def play():
+	while True:
+		print("Paper, Scissors or Rock? (P/S/R)")
+		option = input(">>>")
+		option = option.lower()
+		options = ["p", "s", "r"]
+		if not option in options: 
+			print("Option Does Not Exist")
+		else:
+			logic = {"p": "r", "r": "s", "s": "p"}
+			bot_option = random.choice(options)
+			if logic[option] == bot_option:
+				print("U Win HAHA")
+			elif logic[bot_option] == option:
+				print("U Loose Haha")
+			else:
+				print("Its a draw")
+
+			print(f"Bot chose {bot_option}")
---
+++ packages/PaperScissorsRock-Lib/PaperScissorsRock-Lib-0.1.0.tar.gz/PaperScissorsRock-Lib-0.1.0/README.md
@@ -0,0 +1,13 @@
+# PAPER SCISSORS ROCK
+
+This is a game of paper scissors rock packed into a lib
+
+Get a game of paper scissors rock in 2 lines!
+
+usage (Python):
+
+```Python
+from PaperScissorRock import Game
+
+Game.play()
+```
