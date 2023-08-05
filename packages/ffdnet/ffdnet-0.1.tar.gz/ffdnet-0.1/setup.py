ab5ba531ab39727b6de05eebbd69771b86fb9c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:53 +0000

    Add package ffdnet/0.1/ffdnet-0.1.tar.gz

---
+++ packages/ffdnet/ffdnet-0.1.tar.gz/ffdnet-0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name = 'ffdnet',
+  packages = ['ffdnet'],
+  version = '0.1',
+  license='MIT',
+  description = 'FFDNet CNN denoiser',
+  author = 'Jérémy Anger',
+  author_email = 'angerj.dev@gmail.com',
+  install_requires=[
+  ],
+  classifiers=[
+    'Programming Language :: Python :: 3',
+  ],
+)
