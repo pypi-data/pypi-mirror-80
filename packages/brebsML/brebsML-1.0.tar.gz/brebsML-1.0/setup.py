49bb628e21709bd47c0dba29a2f5c3f5580ac4c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:19 +0000

    Add package brebsML/1.0/brebsML-1.0.tar.gz

---
+++ packages/brebsML/brebsML-1.0.tar.gz/brebsML-1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(name = 'brebsML',
+      version = 1.0,
+      description = 'Toutes les librairies que nous utiliseront pour ce comité',
+      install_requires=[
+          "numpy",
+          "tensorflow",
+          "keras",
+          "lightgbm",
+          "matplotlib",
+          "pandas",
+          "scikit-learn",
+          "scipy",
+          "seaborn",
+          "xgboost",
+          "scipy",
+          "sklearn-pandas"
+      ],
+
+      setup_requires=["wheel"] 
+
+
+
+
+)
+
+
+
