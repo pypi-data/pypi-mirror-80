aa880364d9309ac79bbb17edcef9ee613629ce89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:23 +0000

    Add package telekinesis/0.1.0a3/telekinesis-0.1.0a3.tar.gz

---
+++ packages/telekinesis/telekinesis-0.1.0a3.tar.gz/telekinesis-0.1.0a3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import warnings
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="telekinesis", 
+    version="0.1.0-alpha.3",
+    author="Elias Neuman",
+    author_email="elias.j.neuman@gmail.com.com",
+    description="A websockets based RPC to easily develop, deploy and call services online.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/e-neuman/telekinesis",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'websockets',
+        'cryptography',
+        'makefun',
+        'bson',
+        'ujson'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
