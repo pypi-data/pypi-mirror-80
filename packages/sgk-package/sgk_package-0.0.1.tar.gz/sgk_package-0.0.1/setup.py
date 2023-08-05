46ebcdcaabbb4ec8a3459b9c8087d8b6cc845517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:42 +0000

    Add package sgk-package/0.0.1/sgk_package-0.0.1.tar.gz

---
+++ packages/sgk-package/sgk_package-0.0.1.tar.gz/sgk_package-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="sgk_package", # Replace with your own username
+    version="0.0.1",
+    author="Swapnali Kapare",
+    author_email="swapnalikapare02@gmail.com",
+    description="This is sgk_packages",
+    long_description= "This is very long Description...",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=['sgk_package'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
