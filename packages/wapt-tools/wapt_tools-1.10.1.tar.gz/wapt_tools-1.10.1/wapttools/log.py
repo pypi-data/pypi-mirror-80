2d226033e0f0d661dba75ed8055409e868434ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:03 +0000

    Add package wapt-tools/1.10.1/wapt_tools-1.10.1.tar.gz

---
+++ packages/wapt-tools/wapt_tools-1.10.1.tar.gz/wapt_tools-1.10.1/wapttools/log.py
@@ -0,0 +1,15 @@
+import logging
+import os
+
+
+def logger(package):
+    logger = logging.getLogger(package)
+    logger.setLevel(logging.DEBUG)
+    logfile = os.path.join(os.environ['SYSTEMROOT'], 'WAPT', 'logs', package + '.log')
+    os.makedirs(os.path.dirname(logfile))
+    handler = logging.FileHandler(logfile)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s: %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
