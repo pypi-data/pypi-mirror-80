b8d1b9d88f5a14ec1d186b5ce1c3578edd0430aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:51 +0000

    Add package bashrc/0.1.39/bashrc-0.1.39-py3-none-any.whl

---
+++ packages/bashrc/bashrc-0.1.39-py3-none-any.whl/bashrc-0.1.39.data/scripts/home.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -x
+user="${1:-${USER}}"
+if [[ "${user}" == "root" ]]; then
+  home="/var/${user}"
+  test -n "${DARWIN}" || home="/${user}"
+else
+  home="/Users/${user}"
+  test -n "${DARWIN}" || home="/home/${user}"
+fi
+if ! test -d "${home}"; then
+  exit 1
+fi
+echo "${home}"
+set +x
