eab47d36e82b9b3434cdf94397380bf592b44746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:41 +0000

    Add package micropython-rosserial/0.2.6/micropython-rosserial-0.2.6.tar.gz

---
+++ packages/micropython-rosserial/micropython-rosserial-0.2.6.tar.gz/micropython-rosserial-0.2.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="micropython-rosserial", # Replace with your own username
+    version="0.2.6",
+    author="Steven Silva Mendoza",
+    author_email="sasilva1998@gmail.com",
+    description="A package done in order to use rosserial with micropython.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FunPythonEC/uPy-rosserial",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
