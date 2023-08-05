b43c4a2f21a00e05a42f46529e6afa5fa38adbc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:40 +0000

    Add package lambdasdk/0.0.10/lambdasdk-0.0.10.tar.gz

---
+++ packages/lambdasdk/lambdasdk-0.0.10.tar.gz/lambdasdk-0.0.10/lambdasdk/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = 'lambdasdk'
+__description__ = 'for interacting with aws lambda'
+__version__ = '0.0.10'
+__author__ = 'Nic Wanavit'
+__author_email__ = 'nwanavit@hatari.cc'
+__license__ = 'Apache 2.0'
+__url__ = 'https://github.com/thanakijwanavit/lambda-pip'
---
+++ packages/lambdasdk/lambdasdk-0.0.10.tar.gz/lambdasdk-0.0.10/lambdasdk/lambdasdk.py
@@ -0,0 +1,26 @@
+from pprint import pprint
+import boto3, base64, json
+
+class Lambda:
+    '''
+      for invoking lambda functions
+    '''
+    def __init__(self, user=None, pw=None, region = 'ap-southeast-1'):
+      self.lambdaClient = boto3.client(
+          'lambda',
+          aws_access_key_id=user,
+          aws_secret_access_key=pw,
+          region_name = region
+        )
+    def invoke(self, functionName, input):
+      result = self.lambdaClient.invoke(
+        FunctionName = functionName,
+        InvocationType= 'RequestResponse',
+        LogType='Tail',
+        ClientContext= base64.b64encode(json.dumps({'caller': 'sdk'}).encode()).decode(),
+        Payload= json.dumps(input)
+      )
+      if 'Payload' in result.keys():
+          return json.loads(result['Payload']).read())
+      else:
+          return result
