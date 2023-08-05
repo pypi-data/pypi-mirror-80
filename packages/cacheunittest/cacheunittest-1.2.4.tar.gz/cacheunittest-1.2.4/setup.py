e7c47f13b188de2dc10d20da0953864649fb8099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:51 +0000

    Add package cacheunittest/1.2.4/cacheunittest-1.2.4.tar.gz

---
+++ packages/cacheunittest/cacheunittest-1.2.4.tar.gz/cacheunittest-1.2.4/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import setuptools
+
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+	
+setup(
+	name='cacheunittest',
+	version='1.2.4',
+	author='Alapaje',
+	author_email='paje@github.com',
+	license='LICENSE.txt',
+	description='Varnish unit test',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/ArteGEIE/cache-unit-test",
+    packages=setuptools.find_packages(),
+	py_modules=['cacheunittest'],
+	install_requires=[
+		"requests"],
+	python_requires='>=3.7',
+)
