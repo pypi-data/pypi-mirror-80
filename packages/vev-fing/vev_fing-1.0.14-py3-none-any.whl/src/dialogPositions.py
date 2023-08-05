fea215d4b92897cc9dc6b2c0f3ea1b866d0ed9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:23 +0000

    Add package vev-fing/1.0.14/vev_fing-1.0.14-py3-none-any.whl

---
+++ packages/vev-fing/vev_fing-1.0.14-py3-none-any.whl/src/dialogPositions.py
@@ -0,0 +1,32 @@
+from PyQt5 import QtWidgets,uic
+from src.dialogError import dError
+import os
+pathPip=str(os.path.dirname(uic.__file__)).split('PyQt5')[0]
+path_buttons=pathPip+'src/buttons_and_background/'
+class valuesPositions(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(valuesPositions, self).__init__()
+        uic.loadUi(pathPip+'src/ui_files/dialogPositions.ui', self)
+        self.pushButton.clicked.connect(self.setParameters)
+        self.fvpValue=774
+        self.lvpValue=7332
+    def setParameters(self):
+
+        fvp = self.FVP.toPlainText()
+        lvp = self.LVP.toPlainText()
+        try:
+            if fvp == '':
+                fvp = 0
+            if lvp == '':
+                lvp = 0
+            self.fvpValue = int(fvp)
+            self.lvpValue = int(lvp)
+            self.close()
+        except:
+            de = dError("Error: Enter only integers")
+            de.exec()
+
+    def getFVP(self):
+        return self.fvpValue
+    def getLVP(self):
+        return self.lvpValue
