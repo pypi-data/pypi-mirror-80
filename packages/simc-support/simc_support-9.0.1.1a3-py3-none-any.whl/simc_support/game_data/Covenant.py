546234363608603ad0f18b913f348168a7f9d904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:55 +0000

    Add package simc-support/9.0.1.1a3/simc_support-9.0.1.1a3-py3-none-any.whl

---
+++ packages/simc-support/simc_support-9.0.1.1a3-py3-none-any.whl/simc_support/game_data/Covenant.py
@@ -0,0 +1,10 @@
+from simc_support.game_data.SimcObject import SimcObject
+
+
+class Covenant(SimcObject):
+    """Shadowlands specific faction. Each character joins one."""
+
+    pass
+
+
+COVENANTS = ()
