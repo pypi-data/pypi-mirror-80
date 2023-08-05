33cd929e8fbf11470f15c60bae15db45c3483019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:43 +0000

    Add package kcli/99.0.202009211255/kcli-99.0.202009211255-py3-none-any.whl

---
+++ packages/kcli/kcli-99.0.202009211255-py3-none-any.whl/kvirt/openshift/apps/argocd/install.sh
@@ -0,0 +1,20 @@
+export ARGOCD_VERSION={{ 'argoproj/argo-cd' | githubversion(argocd_version| default('latest')) }}
+oc create namespace argocd
+oc adm policy add-scc-to-user anyuid system:serviceaccount:argocd:default
+oc -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/$ARGOCD_VERSION/manifests/install.yaml
+ARGOCD_PASSWORD=$(oc -n argocd get pod -l "app.kubernetes.io/name=argocd-server" -o jsonpath='{.items[*].metadata.name}')
+PATCH='{"spec":{"template":{"spec":{"$setElementOrder/containers":[{"name":"argocd-server"}],"containers":[{"command":["argocd-server","--insecure","--staticassets","/shared/app"],"name":"argocd-server"}]}}}}'
+oc -n argocd patch deployment argocd-server -p $PATCH
+oc -n argocd create route edge argocd-server --service=argocd-server --port=http --insecure-policy=Redirect
+ARGOCD_HOST=$(oc get route -n argocd argocd-server -o=jsonpath='{ .spec.host }')
+oc patch serviceaccount -n argocd argocd-dex-server --type='json' -p="[{\"op\": \"add\", \"path\": \"/metadata/annotations/serviceaccounts.openshift.io~1oauth-redirecturi.argocd\", \"value\":\"https://$ARGOCD_HOST/api/dex/callback\"}]"
+ARGOCD_SECRET=$(oc serviceaccounts get-token argocd-dex-server -n argocd)
+sed "s/SECRET/$ARGOCD_SECRET/" configmap.yml | oc replace -f - -n argocd
+echo argo ui available at https://$ARGOCD_HOST
+echo Use Openshift Credentials or Initial Password $ARGOCD_PASSWORD
+{% if argocd_download_cli %}
+OS="linux"
+[ -d /Users ] && OS="darwin"
+curl https://github.com/argoproj/argo-cd/releases/download/$ARGOCD_VERSION/argocd-$OS-amd64 > {{ cwd }}/argcod
+chmod u+x {{ cwd }}/argcod
+{% endif %}
