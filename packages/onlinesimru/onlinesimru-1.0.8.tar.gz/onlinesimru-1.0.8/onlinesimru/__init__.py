61468369ce79aec1749397f5e4524adf287a9b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:10 +0000

    Add package onlinesimru/1.0.8/onlinesimru-1.0.8.tar.gz

---
+++ packages/onlinesimru/onlinesimru-1.0.8.tar.gz/onlinesimru-1.0.8/onlinesimru/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from .GetForward import GetForward
+from .GetFree import GetFree
+from .GetNumbers import GetNumbers
+from .GetProxy import GetProxy
+from .GetRent import GetRent
+from .GetUser import GetUser
+from .Driver import Driver
+
+__author__ = 's00d'
+__version__ = '1.0.8'
+__contact__ = 'https://github.com/s00d'
