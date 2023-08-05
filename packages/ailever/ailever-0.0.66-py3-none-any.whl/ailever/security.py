b0e18632665427da1c56202ea589143bb546dba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:53 +0000

    Add package ailever/0.0.66/ailever-0.0.66-py3-none-any.whl

---
+++ packages/ailever/ailever-0.0.66-py3-none-any.whl/ailever/security.py
@@ -0,0 +1,40 @@
+class SecurityError(Exception):
+    pass
+
+class Supervisor:
+    def __init__(self):
+        self.__token = None
+        self.__ailever_verification_code = 'ailever'
+        verification = input("[AILEVER] enter ailever verification code : ")
+        if self.__ailever_verification_code == verification : pass
+        else : raise SecurityError('[AILEVER] permission denied.')
+        from collections import OrderedDict
+        self.__users = OrderedDict()
+        self.__users['sudo'] = 'ailever'
+    
+    @property
+    def tokenextract(self):           
+        return self.__token
+ 
+    @tokeninsert.setter
+    def tokeninsert(self, value):    
+        self.__token = value
+
+    def enroll(self, account, passwd):
+        self.__users[account] = passwd
+
+    def members(self):
+        return self.__users.keys()
+    
+    def identify(self, account, passwd):
+        if self.__users[account] == passwd:
+            return True
+        else:
+            return False
+
+sudo = Supervisor()
+sudo.enroll('dongmyeong', 'dongmyeong')
+sudo.enroll('eunseo', 'eunseo')
+sudo.enroll('kyuhun', 'kyuhun')
+sudo.enroll('hyehyun', 'hyehyun')
+
