08b6d3d874f1b85d5b304ca4bc9923ccdd4042d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:05 +0000

    Add package sweatygoals/0.11/sweatygoals-0.11.tar.gz

---
+++ packages/sweatygoals/sweatygoals-0.11.tar.gz/sweatygoals-0.11/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sweatygoals",
+    version='0.11',
+    author="Andres Alcocer",
+    author_email="andresalcocer7@yahoo.com",
+    description="A CLI tool that retrieves latest football matches",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AndresXI/sweaty-goals",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'Click',
+        'Requests',
+        'Python-dateutil',
+    ],
+    entry_points='''
+     [console_scripts]
+     sweatygoals=sweatygoals.scripts.sweatygoals:sweatygoals
+ ''',
+)
