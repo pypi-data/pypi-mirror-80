f7f78160c982c95837fab5aa276f49eeac755c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:12 +0000

    Add package qtimgren/0.3.2/qtimgren-0.3.2.tar.gz

---
+++ packages/qtimgren/qtimgren-0.3.2.tar.gz/qtimgren-0.3.2/qtimgren/app.py
@@ -0,0 +1,37 @@
+#  Copyright (c) 2020  SBA - MIT License
+
+from PySide2.QtWidgets import QApplication
+from PySide2.QtCore import QLocale, QTranslator, QLibraryInfo
+from .main_window import MainWindow
+import argparse
+try:
+    from . import resource
+except ImportError:
+    pass
+import sys
+
+
+def parse(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--lang', '-l',
+                        help='force a specific language (or native)')
+    return parser.parse_known_args(argv)
+
+
+def run():
+    params = parse(sys.argv[1:])[0]
+    app = QApplication(sys.argv)
+    if params.lang or (params.lang != 'native'):
+        loc = QLocale(params.lang)
+        qt_trans = QTranslator()
+        qt_trans.load(loc, 'qt', '_',
+                      QLibraryInfo.location(QLibraryInfo.TranslationsPath))
+        app.installTranslator(qt_trans)
+        translator = QTranslator()
+        translator.load(loc, 'qtimgren', '_', ':')
+        app.installTranslator(translator)
+    app.setOrganizationName('SBA')
+    app.setApplicationName('QtImgren')
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
