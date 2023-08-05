a6f10db20fc0e421ddbed10a196c65a2dca31b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:21 +0000

    Add package widget-periodictable/2.1.3/widget_periodictable-2.1.3.tar.gz

---
+++ packages/widget-periodictable/widget_periodictable-2.1.3.tar.gz/widget_periodictable-2.1.3/widget_periodictable/_version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Giovanni Pizzi and Dou Du.
+# Distributed under the terms of the Modified BSD License.
+
+version_info = (2, 1, 3)
+__version__ = ".".join(map(str, version_info))
