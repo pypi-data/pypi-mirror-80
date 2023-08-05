ac9d143686263385b9bb697d2a4ddd40023d64f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:53 +0000

    Add package pjy/0.11.0/pjy-0.11.0.tar.gz

---
+++ packages/pjy/pjy-0.11.0.tar.gz/pjy-0.11.0/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# this project is licensed under the WTFPLv2, see COPYING.txt for details
+
+from setuptools import setup
+
+
+setup(
+    version='0.11.0',  # $version
+    scripts=['pjy'],
+)
