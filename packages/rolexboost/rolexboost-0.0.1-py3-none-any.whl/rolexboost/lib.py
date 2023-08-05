d3db457e7cf0f83d6e10d260d9b5ef51dda1ab28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:42 +0000

    Add package rolexboost/0.0.1/rolexboost-0.0.1-py3-none-any.whl

---
+++ packages/rolexboost/rolexboost-0.0.1-py3-none-any.whl/rolexboost/lib.py
@@ -0,0 +1,9 @@
+from sklearn.decomposition import PCA as SklearnPCA
+
+
+class PCA(SklearnPCA):
+    """PCA is not the central part of the algorithms, so we just use PCA provided by scikit-learn for now.
+    If there is enough time for the project, we would try to implement PCA by ourselves.
+    """
+
+    pass
