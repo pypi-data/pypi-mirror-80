399185d6620434b5f4867a01058bd1a1729c1cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:17 +0000

    Add package jupyterblack/0.2.4/jupyterblack-0.2.4.tar.gz

---
+++ packages/jupyterblack/jupyterblack-0.2.4.tar.gz/jupyterblack-0.2.4/jupyterblack/cout.py
@@ -0,0 +1,47 @@
+"""Print error message to console."""
+
+
+def no_args():
+    """Error message for no args"""
+    print(
+        """jupyterblack takes at least one argument with .ipynb extension.\n
+Try 'jblack [-h, --help]' for help."""
+    )
+
+
+def invalid_options():
+    """Error message for invalid options"""
+    print(
+        """Invalid option(s):
+Use only [-h, --help] or [-l, --line_count].\n
+Try 'jblack [-h, --help]' for help."""
+    )
+
+
+def invalid_linecount():
+    """Error message for invalid line count (not of type int)"""
+    print(
+        """Invalid line count value:
+[-l, --line_count] must be the first argument followed by an int.\n
+Try 'jblack [-h, --help]' for help."""
+    )
+
+
+def invalid_filename(filename):
+    """Error message for file not found"""
+    print(
+        """Error: Path {} does not exist.\n
+Try 'jblack [-h, --help]' for help.""".format(
+            filename
+        )
+    )
+
+
+def invalid_extension(filename):
+    """Error message for file without .ipynb extension"""
+    print(
+        """Error: File {} does not have extension .ipynb.\n
+Try 'jblack [-h, --help]' for help.""".format(
+            filename
+        )
+    )
