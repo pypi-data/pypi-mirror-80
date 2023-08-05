07447e908e7b3ed9f9df5d7ed52048e1e8c03a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:20 +0000

    Add package ezdxf/0.15a0/ezdxf-0.15a0.zip

---
+++ packages/ezdxf/ezdxf-0.15a0.zip/ezdxf-0.15a0/integration_tests/test_drawing_matplotlib_backend.py
@@ -0,0 +1,33 @@
+# Created: 06.2020
+# Copyright (c) 2020, Matthew Broadway
+# License: MIT License
+import pytest
+
+plt = pytest.importorskip('matplotlib.pyplot')
+
+from ezdxf.addons.drawing.matplotlib import MatplotlibBackend, _get_line_style_pattern
+from ezdxf.addons.drawing.properties import Properties
+
+
+@pytest.fixture()
+def backend():
+    fig, ax = plt.subplots()
+    return MatplotlibBackend(ax)
+
+
+def test_get_text_width(backend):
+    assert backend.get_text_line_width('   abc',
+                                       100) > backend.get_text_line_width('abc',
+                                                                          100)
+    assert backend.get_text_line_width('  abc ',
+                                       100) == backend.get_text_line_width(
+        '  abc', 100)
+    assert backend.get_text_line_width('   ', 100) == 0
+    assert backend.get_text_line_width('  ', 100) == 0
+
+
+def test_get_line_style(backend):
+    p = Properties()
+    p.linetype_pattern = (1, 1)
+    assert _get_line_style_pattern(p, 2) == (0, [6, 6])  # in points!
+    assert _get_line_style_pattern(Properties(), 2) == 'solid'
