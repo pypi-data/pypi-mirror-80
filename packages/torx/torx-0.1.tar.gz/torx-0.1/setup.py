3ff969acd93136647bc06d81b8d3ae7611db682c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:46 +0000

    Add package torx/0.1/torx-0.1.tar.gz

---
+++ packages/torx/torx-0.1.tar.gz/torx-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name = "torx", 
+    version = "0.1",
+    description = "Torrent library / client",
+    # long_description=open("README.md", "r").read(),
+    # long_description_content_type="text/markdown",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.8',
+)
