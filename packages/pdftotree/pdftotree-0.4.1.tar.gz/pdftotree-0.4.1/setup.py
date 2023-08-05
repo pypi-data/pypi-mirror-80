ee86eea707bec21fa8b0e3738dacbb75d53a3033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:21 +0000

    Add package pdftotree/0.4.1/pdftotree-0.4.1.tar.gz

---
+++ packages/pdftotree/pdftotree-0.4.1.tar.gz/pdftotree-0.4.1/setup.py
@@ -0,0 +1,52 @@
+"""For pip."""
+from setuptools import find_packages, setup
+
+exec(open("pdftotree/_version.py").read())
+setup(
+    name="pdftotree",
+    version=__version__,
+    description="Parse PDFs into HTML-like trees.",
+    long_description=open("README.rst").read(),
+    packages=find_packages(),
+    install_requires=[
+        "IPython",
+        "PyPDF2",
+        "beautifulsoup4",
+        "chardet",
+        "future",
+        "h5py",
+        "keras==2.0.8",
+        "numpy",
+        "pandas",
+        "pdfminer.six",
+        "pillow",
+        "scipy",
+        "selectivesearch",
+        "six",
+        "sklearn",
+        "tabula-py",
+        "tensorflow",
+        "wand",
+    ],
+    keywords=["pdf", "parsing", "html"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    url="https://github.com/HazyResearch/pdftotree",
+    scripts=["bin/pdftotree", "bin/extract_tables"],
+    classifiers=[  # https://pypi.python.org/pypi?:action=list_classifiers
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    project_urls={
+        "Tracker": "https://github.com/HazyResearch/pdftotree/issues",
+        "Source": "https://github.com/HazyResearch/pdftotree",
+    },
+    python_requires=">3",
+    author="Hazy Research",
+    author_email="senwu@cs.stanford.edu",
+    license="MIT",
+)
---
+++ packages/pdftotree/pdftotree-0.4.1.tar.gz/pdftotree-0.4.1/tests/test_basic.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import pdftotree
+
+
+def test_heuristic_completion():
+    """Simply test that parse runs to completion without errors."""
+    output = pdftotree.parse("tests/input/paleo.pdf")
+    assert output is not None
+
+
+def test_ml_completion():
+    """Simply test that ML-based parse runs without errors."""
+    output = pdftotree.parse(
+        "tests/input/paleo.pdf",
+        model_type="ml",
+        model_path="tests/input/paleo_model.pkl",
+    )
+    assert output is not None
+
+
+def test_visual_completion():
+    """Simply test that ML-based parse runs without errors."""
+    output = pdftotree.parse(
+        "tests/input/paleo.pdf",
+        model_type="visual",
+        model_path="tests/input/paleo_visual_model.h5",
+    )
+    assert output is not None
