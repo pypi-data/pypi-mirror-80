891ae52b6eb4efa555962e608699f9db687d9d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:12 +0000

    Add package domination/1.1/domination-1.1.tar.gz

---
+++ packages/domination/domination-1.1.tar.gz/domination-1.1/setup.py
@@ -0,0 +1,22 @@
+from pkg_resources import parse_requirements
+from setuptools import setup
+
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+setup(
+    name='domination',
+    author='Baptiste Azéma',
+    author_email='baptiste@azema.tech',
+    version='1.1',
+    packages=['domination'],
+    include_package_data=True,
+    python_requires='~=3.6',
+    install_requires=REQUIREMENTS,
+    description='Real-time application in order to dominate Humans.',
+    license='LICENSE',
+    entry_points={
+        'console_scripts': ['domination=domination.__main__:main']
+    },
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
