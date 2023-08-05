2a7e33ce23f905b626f3ffcb5518377c924f3be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:11 +0000

    Add package djangolovessandstorm/0.0.4/djangolovessandstorm-0.0.4-py3-none-any.whl

---
+++ packages/djangolovessandstorm/djangolovessandstorm-0.0.4-py3-none-any.whl/djangolovessandstorm/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import SandstormUser
+
+
+admin.site.register(SandstormUser)
---
+++ packages/djangolovessandstorm/djangolovessandstorm-0.0.4-py3-none-any.whl/djangolovessandstorm/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DLSConfig(AppConfig):
+    name = "djangolovessandstorm"
+    verbose_name = "Django Loves Sandstorm"
