732560f0978031db1a23adba64f2c4ea24c015ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:46 +0000

    Add package pyspedas/1.0.11/pyspedas-1.0.11-py2.py3-none-any.whl

---
+++ packages/pyspedas/pyspedas-1.0.11-py2.py3-none-any.whl/pyspedas/analysis/tests/tests.py
@@ -0,0 +1,55 @@
+
+"""
+File:
+    test.py
+
+Description:
+    Automated tests for the analysis functions.
+
+"""
+
+import unittest
+from pyspedas.analysis.tsmooth import smooth
+from pyspedas import subtract_average, subtract_median, tsmooth
+from pytplot import get_data, store_data
+
+import numpy as np
+
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        store_data('test', data={'x': [1., 2., 3., 4., 5., 6.],
+                                 'y': [3., 5., 8., 15., 20., 1.]})
+
+
+class AnalysisTestCases(BaseTestCase):
+
+    def test_subtract_median(self):
+        subtract_median('test')
+        d = get_data('test-m')
+        self.assertTrue(d[1].tolist() == [-3.5, -1.5,  1.5,  8.5, 13.5, -5.5])
+
+    def test_subtract_average(self):
+        subtract_average('test')
+        d = get_data('test-d')
+        self.assertTrue((np.round(d[1].tolist()) == [-6., -4., -1.,
+                         6., 11., -8.]).all())
+
+    def test_tsmooth(self):
+        a = [1.0, 1.0, 2.0, 3.0, 4.0, 1.0, 4.0, 3.0, 2.0, 1.0, 1.0]
+        x = smooth(a, 3)
+        r = [1.0, 1.3333333333333333, 2.0, 3.0, 2.6666666666666665,
+             3.0, 2.6666666666666665, 3.0, 2.0, 1.3333333333333333, 1.0]
+        self.assertTrue(x == r)
+        b = [1.0, 1.0, 2.0, 3.0, np.NaN, np.NaN, np.NaN, np.NaN, 2.0, 1.0, 1.0]
+        y = smooth(b, width=3)
+        ry = [1.0, 1.3333333333333333, 2.0, 1.6666666666666665, 1.0,
+              np.nan, np.nan, 0.6666666666666666, 1.0, 1.3333333333333333, 1.0]
+        self.assertTrue(y == ry)
+        tsmooth('test')
+        d = get_data('test-s')
+        self.assertTrue(d[1].tolist() == [3.,  5.,  8., 15., 20.,  1.])
+
+
+if __name__ == '__main__':
+    unittest.main()
