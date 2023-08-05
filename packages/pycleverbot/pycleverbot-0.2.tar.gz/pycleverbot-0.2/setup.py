d1253f68641314756041d789d87df0827737f2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:45 +0000

    Add package pycleverbot/0.2/pycleverbot-0.2.tar.gz

---
+++ packages/pycleverbot/pycleverbot-0.2.tar.gz/pycleverbot-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pycleverbot',
+    version='0.2',
+    packages=['pycleverbot'],
+    url='',
+    license='Apache',
+    author='jarbasAI',
+    install_requires=["selenium"],
+    author_email='jarbasai@mailfence.com',
+    description='cleverbot api'
+)
