1c7ef21222bcf6a74a63b3982eab5bf8158cc3a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:36 +0000

    Add package slacktivate/0.2.3/slacktivate-0.2.3-py3-none-any.whl

---
+++ packages/slacktivate/slacktivate-0.2.3-py3-none-any.whl/slacktivate/__init__.py
@@ -0,0 +1,7 @@
+"""
+Slacktivate is a Python library and Command-Line Interface
+to assist in the provisioning of a Slack workspace, using
+both the Slack API and the Slack SCIM API.
+"""
+
+__version__ = "0.2.3"
