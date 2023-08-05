e6b6dfc2887260a96aa61f47784e3d6293ec53c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:07 +0000

    Add package shopyo/1.1.1/shopyo-1.1.1.tar.gz

---
+++ packages/shopyo/shopyo-1.1.1.tar.gz/shopyo-1.1.1/shopyo/__main__.py
@@ -0,0 +1,11 @@
+import sys
+
+from .shopyoapi.cmd import new_project
+
+def main(args):
+    if args[1] == "new" and len(args) == 4:
+        new_project(args[2], args[3])
+
+
+if __name__ == "__main__":
+    main(sys.argv)
