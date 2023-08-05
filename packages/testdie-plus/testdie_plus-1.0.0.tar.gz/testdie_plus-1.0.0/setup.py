71f4358aa57c90d86eff4a3917d535bbaf61dbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:51 +0000

    Add package testdie-plus/1.0.0/testdie_plus-1.0.0.tar.gz

---
+++ packages/testdie-plus/testdie_plus-1.0.0.tar.gz/testdie_plus-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+ name='testdie_plus',  #这个是最终打包的文件名
+ author='yangyi198381',
+ author_email='54279855@qq.com', 
+ version='1.0.0',
+ py_modules=['test20200918', 'die'], #要打包哪些，.py文件，
+ )
