250b7f697dc58db8521e7520633e25d5e49a80ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:25 +0000

    Add package Filezen/1.4.2/Filezen-1.4.2-py3-none-any.whl

---
+++ packages/Filezen/Filezen-1.4.2-py3-none-any.whl/tests/test_simpleScanner.py
@@ -0,0 +1,33 @@
+import unittest
+import sys
+
+import os
+import shutil
+from filezen.simpleScanner import simplescanner
+
+sys.path.insert(0, '..')
+
+
+class TestSimpleScanner(unittest.TestCase):
+
+    def setUp(self):
+        self.simpleScanner = simplescanner.SimpleScanner()
+        cwd = os.getcwd()
+        test_directory = "scantest"
+        path = os.path.join(cwd, test_directory)
+        if not os.path.exists(path):
+            os.mkdir(path)
+        self.test_dir_path = path
+
+    def test_setOutputPath(self):
+        self.simpleScanner.setOutputPath(self.test_dir_path)
+        result = self.simpleScanner.outputPath
+
+        self.assertEqual(self.test_dir_path, result)
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir_path, ignore_errors=True)
+
+
+if __name__ == '__main__':
+    unittest.main()
