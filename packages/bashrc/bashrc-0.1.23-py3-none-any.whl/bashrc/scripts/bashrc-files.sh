e6c98db024510e11819b893f99573c0ab1827f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:34 +0000

    Add package bashrc/0.1.23/bashrc-0.1.23-py3-none-any.whl

---
+++ packages/bashrc/bashrc-0.1.23-py3-none-any.whl/bashrc/scripts/bashrc-files.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+export source="${BASH_SOURCE[0]}"; debug.sh source
+
+test -n "${BASHRC_FILE}" || { error.bash BASHRC_FILE 'not defined'; return 1; }
+
+if ! grep source ~/.bashrc | grep "${BASHRC_FILE}" > /dev/null 2>&1; then
+  if bashrc_path="$( command -v "${BASHRC_FILE}" 2>&1 )"; then
+    echo "source \"${bashrc_path}\"" >> ~/.bashrc
+    info.sh files .bashrc "${bashrc_path}"
+  else
+    error.sh files .bashrc "${BASHRC_FILE} - command not found"
+  fi
+fi
+
+unset source bashrc_path
