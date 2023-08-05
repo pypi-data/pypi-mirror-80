93046bde11b3f917a10ae459e8ce12faee055566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package skipulagsstofnun/0.1.4/skipulagsstofnun-0.1.4.tar.gz

---
+++ packages/skipulagsstofnun/skipulagsstofnun-0.1.4.tar.gz/skipulagsstofnun-0.1.4/README.md
@@ -0,0 +1,31 @@
+# skipulagsstofnun
+
+Library to lookup polygons, coordinates and metadata of approved local site plans in Iceland.
+
+Used by [Planitor](https://www.planitor.io).
+
+## Install
+
+```bash
+poetry add skipulagsstofnun
+```
+
+or
+
+```bash
+pip install skipulagsstofnun
+```
+
+## Use
+
+```python
+>>> from skipulagsstofnun import plans
+>>> shape, plan = plans.get_plan(64.1525571, -21.9508792)
+>>> plan
+{'id': 'skipulag_deiliskipulag.198969', 'type': 'Feature', 'skipnr': '8136', 'nrsveitarf': '0', 'sveitarfelag': 'Reykjavíkurborg', 'heiti': 'Deiliskipulag stgr. 1.116 og 1.115.3, Slippa- og Ellingsensreitur', 'skipstig': 'deiliskipulag', 'malsmed': 'nytt', 'dagsinnsett': None, 'dagsleidrett': datetime.date(2016, 4, 14), 'gagnaeigandi': 'Skipulagsstofnun', 'dagsheimild': None, 'heimild': None, 'nakvaemnix': '0', 'vinnslufer': None}
+```
+
+You can construct a link to a page hosted by Skipulagsstofnun with the PDF
+scans for this local site plan.
+
+`http://skipulagsaaetlanir.skipulagsstofnun.is/skipulagvefur/display.aspx?numer={skipnr}`
---
+++ packages/skipulagsstofnun/skipulagsstofnun-0.1.4.tar.gz/skipulagsstofnun-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['skipulagsstofnun']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fiona>=1.8.17,<2.0.0', 'shapely>=1.7.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'skipulagsstofnun',
+    'version': '0.1.4',
+    'description': 'Library to lookup polygons, coordinates and metadata of approved local site plans in Iceland',
+    'long_description': "# skipulagsstofnun\n\nLibrary to lookup polygons, coordinates and metadata of approved local site plans in Iceland.\n\nUsed by [Planitor](https://www.planitor.io).\n\n## Install\n\n```bash\npoetry add skipulagsstofnun\n```\n\nor\n\n```bash\npip install skipulagsstofnun\n```\n\n## Use\n\n```python\n>>> from skipulagsstofnun import plans\n>>> shape, plan = plans.get_plan(64.1525571, -21.9508792)\n>>> plan\n{'id': 'skipulag_deiliskipulag.198969', 'type': 'Feature', 'skipnr': '8136', 'nrsveitarf': '0', 'sveitarfelag': 'Reykjavíkurborg', 'heiti': 'Deiliskipulag stgr. 1.116 og 1.115.3, Slippa- og Ellingsensreitur', 'skipstig': 'deiliskipulag', 'malsmed': 'nytt', 'dagsinnsett': None, 'dagsleidrett': datetime.date(2016, 4, 14), 'gagnaeigandi': 'Skipulagsstofnun', 'dagsheimild': None, 'heimild': None, 'nakvaemnix': '0', 'vinnslufer': None}\n```\n\nYou can construct a link to a page hosted by Skipulagsstofnun with the PDF\nscans for this local site plan.\n\n`http://skipulagsaaetlanir.skipulagsstofnun.is/skipulagvefur/display.aspx?numer={skipnr}`\n",
+    'author': 'Jökull Sólberg',
+    'author_email': 'jokull@solberg.is',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/jokull/skipulagsstofnun',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
