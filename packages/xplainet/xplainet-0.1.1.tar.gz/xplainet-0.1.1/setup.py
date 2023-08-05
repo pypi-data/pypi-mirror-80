0486c2251d51eae386390eb2b35c46bbb9d064d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:07 +0000

    Add package xplainet/0.1.1/xplainet-0.1.1.tar.gz

---
+++ packages/xplainet/xplainet-0.1.1.tar.gz/xplainet-0.1.1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['xplainet']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['scikit-learn>=0.21,<1.0.0',
+ 'tensorflow-addons==0.11.2',
+ 'tensorflow>=2.2.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'xplainet',
+    'version': '0.1.1',
+    'description': 'Explainable Neural network in Keras',
+    'long_description': '# XplaiNet\n',
+    'author': 'Hartorn',
+    'author_email': 'hartorn.github@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Hartorn/XplaiNet',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
