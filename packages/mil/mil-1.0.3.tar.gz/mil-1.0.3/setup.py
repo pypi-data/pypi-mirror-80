aa757cb624e36bc4366145bbec5bbd0d8ffe29dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:50 +0000

    Add package mil/1.0.3/mil-1.0.3.tar.gz

---
+++ packages/mil/mil-1.0.3.tar.gz/mil-1.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mil", 
+    version="1.0.3",
+    author="Rosas, Alberto",
+    author_email="alberto.rosas@upc.edu",
+    description="Multiple instance learning package for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rosasalberto/mil",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)
