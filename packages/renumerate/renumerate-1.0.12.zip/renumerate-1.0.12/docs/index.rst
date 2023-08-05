c68f4df252869f26a01f9e165206e9f4725f843e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:53 +0000

    Add package renumerate/1.0.12/renumerate-1.0.12.zip

---
+++ packages/renumerate/renumerate-1.0.12.zip/renumerate-1.0.12/docs/index.rst
@@ -0,0 +1,5 @@
+renumerate documentation
+========================
+
+.. include:: ../README.rst
+.. include:: ../CHANGES.rst
---
+++ packages/renumerate/renumerate-1.0.12.zip/renumerate-1.0.12/src/renumerate/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2016-2020 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/licenses/Zlib
+
+from .__about__ import * ; del __about__
+from ._renumerate import *
