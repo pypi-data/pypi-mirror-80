d6f4ba2b884999680717fa9dcc93b32f85cd4970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:04 +0000

    Add package fksplines/0.6.1/fksplines-0.6.1.tar.gz

---
+++ packages/fksplines/fksplines-0.6.1.tar.gz/fksplines-0.6.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import setuptools
+setup(
+    name='fksplines',
+    version='0.6.1',
+    packages=setuptools.find_packages(),
+    url='https://github.com/ged1182/splines',
+    license='MIT License',
+    author='George Dekermenjian',
+    author_email='ged1182@gmail.com',
+    description='A package to do uni-variate and multi-variate free-knot spline regression.',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
+    python_requires='>=3.7',
+    long_description='A package to do uni-variate and multi-variate free-knot spline regression.'
+)
