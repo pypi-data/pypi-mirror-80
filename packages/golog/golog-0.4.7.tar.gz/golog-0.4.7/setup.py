7aa98d0f2aa24c7bdf1a14f66237101b5e3498b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:34 +0000

    Add package golog/0.4.7/golog-0.4.7.tar.gz

---
+++ packages/golog/golog-0.4.7.tar.gz/golog-0.4.7/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="golog",
+    version="0.4.7",
+    author="Lorenzo Coacci",
+    author_email="lorenzo@coacci.it",
+    description="The package contains some useful general functions for logging, and more.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lollococce/golog",
+    packages=setuptools.find_packages(),
+    keywords='',
+    license='MIT',
+    include_package_data=True,
+    install_requires=[
+       'termcolor',
+       'alive_progress',
+       'twilio',
+       'slackclient',
+       'validators'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+    ]
+)
