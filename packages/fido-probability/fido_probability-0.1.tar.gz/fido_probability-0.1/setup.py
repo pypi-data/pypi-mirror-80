8974dcbd80c60844280547cf45f2adf46f9e4e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:09 +0000

    Add package fido-probability/0.1/fido_probability-0.1.tar.gz

---
+++ packages/fido-probability/fido_probability-0.1.tar.gz/fido_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fido_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['fido_probability'],
+      author = 'Confidence Vandu',
+      author_email = 'confidencevandu@gmail.com',
+      zip_safe=False)
