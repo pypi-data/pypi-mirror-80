d0a71fe008b2a270f65e428e6b22737ea91b7a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:51 +0000

    Add package smart-meter-texas/0.4.3/smart_meter_texas-0.4.3.tar.gz

---
+++ packages/smart-meter-texas/smart_meter_texas-0.4.3.tar.gz/smart_meter_texas-0.4.3/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["aiohttp>=3.6.0", "python-dateutil>=2.8.1", "tenacity>=6.2.0"]
+
+setup_requirements = ["pytest-runner"]
+
+test_requirements = ["pytest>=3"]
+
+setup(
+    author="Graham Wetzler",
+    author_email="graham@wetzler.dev",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Package to connect to and retrieve data from the unofficial \
+Smart Meter Texas API",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    include_package_data=True,
+    keywords="smart_meter_texas",
+    name="smart_meter_texas",
+    packages=find_packages(include=["smart_meter_texas", "smart_meter_texas.*"]),
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/grahamwetzler/smart-meter-texas",
+    version="0.4.3",
+    zip_safe=False,
+)
---
+++ packages/smart-meter-texas/smart_meter_texas-0.4.3.tar.gz/smart_meter_texas-0.4.3/smart_meter_texas/__init__.py
@@ -0,0 +1,211 @@
+from __future__ import annotations
+
+import asyncio
+import datetime
+import logging
+from random import randrange
+
+import dateutil
+from aiohttp import ClientSession
+from tenacity import retry, retry_if_exception_type
+
+from .const import (
+    AUTH_ENDPOINT,
+    BASE_ENDPOINT,
+    BASE_URL,
+    CLIENT_HEADERS,
+    LATEST_OD_READ_ENDPOINT,
+    METER_ENDPOINT,
+    OD_READ_ENDPOINT,
+    OD_READ_RETRY_TIME,
+    TOKEN_EXPRIATION,
+    USER_AGENT_TEMPLATE,
+)
+from .exceptions import (
+    SmartMeterTexasAPIError,
+    SmartMeterTexasAuthError,
+    SmartMeterTexasRateLimitError,
+    SmartMeterTexasAuthExpired,
+)
+
+__author__ = "Graham Wetzler"
+__email__ = "graham@wetzler.dev"
+__version__ = "0.4.3"
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Meter:
+    def __init__(self, meter: str, esiid: str, address: str):
+        self.meter = meter
+        self.esiid = esiid
+        self.address = address
+        self.reading_data = None
+
+    async def read_meter(self, client: Client):
+        """Triggers an on-demand meter read and returns it when complete."""
+        _LOGGER.debug("Requesting meter reading")
+
+        # Trigger an on-demand meter read.
+        await client.request(
+            OD_READ_ENDPOINT, json={"ESIID": self.esiid, "MeterNumber": self.meter},
+        )
+
+        # Occasionally check to see if on-demand meter reading is complete.
+        while True:
+            json_response = await client.request(
+                LATEST_OD_READ_ENDPOINT, json={"ESIID": self.esiid},
+            )
+            try:
+                data = json_response["data"]
+                status = data["odrstatus"]
+            except KeyError:
+                _LOGGER.error("Error reading meter: ", json_response)
+                raise SmartMeterTexasAPIError(
+                    f"Error parsing response: {json_response}"
+                )
+            else:
+                if status == "COMPLETED":
+                    _LOGGER.debug("Reading completed: %s", self.reading_data)
+                    self.reading_data = data
+                    return self.reading_data
+                elif status == "PENDING":
+                    _LOGGER.debug("Meter reading %s", status)
+                    _LOGGER.debug("Sleeping for %s seconds", OD_READ_RETRY_TIME)
+                    await asyncio.sleep(OD_READ_RETRY_TIME)
+                else:
+                    _LOGGER.error("Unknown meter reading status: %s", status)
+                    raise SmartMeterTexasAPIError(f"Unknown meter status: {status}")
+
+    @property
+    def reading(self):
+        """Returns the latest meter reading in kWh."""
+        return float(self.reading_data["odrread"])
+
+    @property
+    def reading_datetime(self):
+        """Returns the UTC datetime of the latest reading."""
+        date = dateutil.parser.parse(self.reading_data["odrdate"])
+        date_as_utc = date.astimezone(datetime.timezone.utc)
+        return date_as_utc
+
+
+class Account:
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
+    async def fetch_meters(self, client: "Client"):
+        """Returns a list of the meters associated with the account"""
+        json_response = await client.request(METER_ENDPOINT, json={"esiid": "*"})
+
+        meters = []
+        for meter_data in json_response["data"]:
+            address = meter_data["address"]
+            meter = meter_data["meterNumber"]
+            esiid = meter_data["esiid"]
+            meter = Meter(meter, esiid, address)
+            meters.append(meter)
+
+        return meters
+
+
+class Client:
+    def __init__(self, websession: ClientSession, account: "Account"):
+        self.websession = websession
+        self.account = account
+        self.token = None
+        self.authenticated = False
+        self.token_expiration = datetime.datetime.now()
+        self.user_agent = None
+
+    async def _init_websession(self):
+        """Make an initial GET request to initialize the session otherwise
+        future POST requests will timeout."""
+        await self.websession.get(BASE_URL, headers=self._agent_headers())
+
+    def _agent_headers(self):
+        """Build the user agent header."""
+        if not self.user_agent:
+            self.user_agent = USER_AGENT_TEMPLATE.format(
+                BUILD=randrange(1001, 9999), REV=randrange(12, 999)
+            )
+
+        return {"User-Agent": self.user_agent}
+
+    def _update_token_expiration(self):
+        self.token_expiration = datetime.datetime.now() + TOKEN_EXPRIATION
+
+    @retry(retry=retry_if_exception_type(SmartMeterTexasAuthExpired))
+    async def request(
+        self, path: str, method: str = "post", **kwargs,
+    ):
+        """Helper method to make API calls against the SMT API."""
+        await self.authenticate()
+        resp = await self.websession.request(
+            method, f"{BASE_ENDPOINT}{path}", headers=self.headers, **kwargs
+        )
+        if resp.status == 401:
+            _LOGGER.debug("Authentication token expired; requesting new token")
+            self.authenticated = False
+            await self.authenticate()
+            raise SmartMeterTexasAuthExpired
+
+        # Since API call did not return a 400 code, update the token_expiration.
+        self._update_token_expiration()
+
+        json_response = await resp.json()
+        return json_response
+
+    async def authenticate(self):
+        if not self.token_valid:
+            _LOGGER.debug("Requesting login token")
+
+            # Make an initial GET request otherwise subsequent calls will timeout.
+            await self._init_websession()
+
+            resp = await self.websession.request(
+                "POST",
+                f"{BASE_ENDPOINT}{AUTH_ENDPOINT}",
+                json={
+                    "username": self.account.username,
+                    "password": self.account.password,
+                    "rememberMe": "true",
+                },
+                headers=self.headers,
+            )
+
+            if resp.status == 400:
+                raise SmartMeterTexasAuthError("Username or password was not accepted")
+
+            if resp.status == 403:
+                raise SmartMeterTexasRateLimitError(
+                    "Reached ratelimit or brute force protection"
+                )
+
+            json_response = await resp.json()
+
+            try:
+                self.token = json_response["token"]
+            except KeyError:
+                raise SmartMeterTexasAPIError(
+                    "API returned unknown login json: %s", json_response
+                )
+            self._update_token_expiration()
+            self.authenticated = True
+            _LOGGER.debug("Successfully retrieved login token")
+
+    @property
+    def headers(self):
+        headers = {**self._agent_headers(), **CLIENT_HEADERS}
+        if self.token:
+            headers["Authorization"] = f"Bearer {self.token}"
+        return headers
+
+    @property
+    def token_valid(self):
+        if self.authenticated or (datetime.datetime.now() < self.token_expiration):
+            return True
+
+        return False
