1d43a0b4959741a16d8476f298f0bbfed9400d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:10 +0000

    Add package ft4222/1.2.1/ft4222-1.2.1-cp38-cp38-win_amd64.whl

---
+++ packages/ft4222/ft4222-1.2.1-cp38-cp38-win_amd64.whl/ft4222/GPIO/__init__.py
@@ -0,0 +1,61 @@
+#  _____ _____ _____
+# |_    |   __| __  |
+# |_| | |__   |    -|
+# |_|_|_|_____|__|__|
+# MSR Electronics GmbH
+# SPDX-License-Identifier: MIT
+#
+
+from enum import IntEnum
+
+class Trigger(IntEnum):
+    """GPIO Trigger in 'interrupt' mode
+
+    Attributes:
+        RISING: Rising edge
+        FALLING: Falling edge
+        LEVEL_HIGH: High level
+        LEVEL_LOW: Low level
+
+    """
+    RISING         = 0x01
+    FALLING        = 0x02
+    LEVEL_HIGH     = 0x04
+    LEVEL_LOW      = 0X08
+
+class Output(IntEnum):
+    """GPIO Output
+
+    Attributes:
+        LOW: Logic low, 0
+        HIGH: Logic high, 1
+
+    """
+    LOW  = 0
+    HIGH = 1
+
+class Port(IntEnum):
+    """GPIO Port
+
+    Attributes:
+        P0: Port 0
+        P1: Port 1
+        P2: Port 2
+        P3: Port 3
+
+    """
+    P0  = 0
+    P1  = 1
+    P2  = 2
+    P3  = 3
+
+class Dir(IntEnum):
+    """GPIO Output direction
+
+    Attributes:
+        OUTPUT: Use as output
+        INPUT: Use as input
+
+    """
+    OUTPUT = 0
+    INPUT  = 1
---
+++ packages/ft4222/ft4222-1.2.1-cp38-cp38-win_amd64.whl/ft4222/__init__.py
@@ -0,0 +1,28 @@
+#  _____ _____ _____
+# |_    |   __| __  |
+# |_| | |__   |    -|
+# |_|_|_|_____|__|__|
+# MSR Electronics GmbH
+# SPDX-License-Identifier: MIT
+#
+
+"""Control FTDI USB chips for I2C/SPI/GPIO.
+
+Open a handle using one of the ft4222.openBy... functions and use the methods
+on the object thus returned.
+"""
+
+from __future__ import absolute_import
+from .ft4222 import *
+
+__all__ = [
+    'FT2XXDeviceError',
+    'FT4222DeviceError',
+    'SysClock',
+    'createDeviceInfoList',
+    'getDeviceInfoDetail',
+    'openBySerial',
+    'openByDescription',
+    'openByLocation',
+    'FT4222',
+]
