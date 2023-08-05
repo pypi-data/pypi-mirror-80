dabd40da08e3941ed7ba40e1450c83afcd133da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:57 +0000

    Add package model_template/0.0.12/model_template-0.0.12-py2.py3-none-any.whl

---
+++ packages/model_template/model_template-0.0.12-py2.py3-none-any.whl/model_template/__init__.py
@@ -0,0 +1,11 @@
+"""
+A package to run any machine learning model in production
+"""
+
+# flake8: noqa
+
+from .model_template import ModelTemplate
+from .model_template import ModelPieces
+from .model_template import ModelThemes
+
+__version__ = "0.0.12"
