54a2a65f624191effcfc2265ac9d5b165ff6ab8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:35 +0000

    Add package pytal/0.0.8/pytal-0.0.8.zip

---
+++ packages/pytal/pytal-0.0.8.zip/pytal-0.0.8/src/pytal/monitor/build/precache-manifest.05035cd5116bf1c5ff82cf45d7ac378b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc7ff2cd2be34fe64539bb835da8f19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "888ca14c33b51c234641",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc111d21a82b984da390",
+    "url": "/static/js/2.34625c45.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/static/js/2.34625c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "888ca14c33b51c234641",
+    "url": "/static/js/main.ecba0060.chunk.js"
+  },
+  {
+    "revision": "afe65997df98ff032b37",
+    "url": "/static/js/runtime-main.b772fc2b.js"
+  }
+]);
