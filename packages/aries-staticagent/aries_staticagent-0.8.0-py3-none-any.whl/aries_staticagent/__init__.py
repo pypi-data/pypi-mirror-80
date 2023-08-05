7e22b0e4b5bcdbbd0ceb3ad0d2db90a755321d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:17 +0000

    Add package aries-staticagent/0.8.0/aries_staticagent-0.8.0-py3-none-any.whl

---
+++ packages/aries-staticagent/aries_staticagent-0.8.0-py3-none-any.whl/aries_staticagent/__init__.py
@@ -0,0 +1,30 @@
+""" Aries Static Agent Library.
+"""
+
+from .static_connection import (
+    StaticConnection, Keys, Target, MessageDeliveryError
+)
+from .module import Module, route
+from .message import Message
+
+from . import crypto
+
+
+def keygen():
+    """ Convenience method for generating and printing a keypair and DID.
+        DID is generated from the first 16 bytes of the VerKey.
+    """
+    vk_bytes, sk_bytes = crypto.create_keypair()
+
+    # TODO implement DID creation as described in the Peer DID Spec
+    # Link: https://openssi.github.io/peer-did-method-spec/index.html
+    did_bytes = vk_bytes[0:16]
+
+    my_vk = crypto.bytes_to_b58(vk_bytes)
+    my_sk = crypto.bytes_to_b58(sk_bytes)
+    did = crypto.bytes_to_b58(did_bytes)
+
+    print('Static Agent Connection info for full agent:\n\tDID: {}\n\tVK: {}\n'
+          .format(did, my_vk))
+    print('Static Agent Connection info for static agent:\n\tVK: {}\n\tSK: {}'
+          .format(my_vk, my_sk))
