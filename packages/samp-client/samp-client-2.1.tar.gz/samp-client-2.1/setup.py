90a74ccb269bfa8449bdc3ae523e2d3c9d1c1f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:55 +0000

    Add package samp-client/2.1/samp-client-2.1.tar.gz

---
+++ packages/samp-client/samp-client-2.1.tar.gz/samp-client-2.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+try:
+    long_description = open('README.md', 'r').read()
+except:
+    long_description = ''
+
+setup(
+    name='samp-client',
+    version='2.1',
+    packages=['samp_client'],
+    url='https://github.com/mick88/samp-client',
+    license='MIT',
+    author='Michal Dabski',
+    author_email='contact@michaldabski.com',
+    install_requires=['future'],
+    description='SA-MP API client for python supporting both query and RCON APIs',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+)
