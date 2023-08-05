d61f110ae435961a3fd90eff121418ef8d38f9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:46 +0000

    Add package jox/0.1/jox-0.1.tar.gz

---
+++ packages/jox/jox-0.1.tar.gz/jox-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name = "jox", 
+    version = "0.1",
+    description = "Making file management great again",
+    # long_description=open("README.md", "r").read(),
+    # long_description_content_type="text/markdown",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.8',
+)
