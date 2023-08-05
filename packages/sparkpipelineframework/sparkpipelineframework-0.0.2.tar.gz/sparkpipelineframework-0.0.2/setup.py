016aabfb515a3838183f64380d0cc26c1c20a36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:32 +0000

    Add package sparkpipelineframework/0.0.2/sparkpipelineframework-0.0.2.tar.gz

---
+++ packages/sparkpipelineframework/sparkpipelineframework-0.0.2.tar.gz/sparkpipelineframework-0.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from os import path, getcwd
+
+# from https://packaging.python.org/tutorials/packaging-projects/
+
+package_name = 'sparkpipelineframework'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+try:
+    with open(path.join(getcwd(), 'VERSION')) as version_file:
+        version = version_file.read().strip()
+except IOError:
+    raise
+
+setup(
+    name=package_name,
+    version=version,
+    author="Imran Qureshi",
+    author_email="imranq2@hotmail.com",
+    description="Framework for simpler Spark Pipelines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/imranq2/SparkPipelineFramework",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
