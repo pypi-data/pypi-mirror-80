82bd44eb0005fc002bd71230a8800d5e805b6753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:30 +0000

    Add package adversarial-robustness-toolbox/1.4.0/adversarial-robustness-toolbox-1.4.0.tar.gz

---
+++ packages/adversarial-robustness-toolbox/adversarial-robustness-toolbox-1.4.0.tar.gz/adversarial-robustness-toolbox-1.4.0/art/attacks/__init__.py
@@ -0,0 +1,5 @@
+"""
+Module providing adversarial attacks under a common interface.
+"""
+from art.attacks.attack import Attack, EvasionAttack, PoisoningAttack, PoisoningAttackBlackBox, PoisoningAttackWhiteBox
+from art.attacks.attack import PoisoningAttackTransformer, ExtractionAttack, InferenceAttack, AttributeInferenceAttack
