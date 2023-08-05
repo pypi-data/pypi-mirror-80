e40c05808b777c8bce963ed7df238a4c66fb6dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:09 +0000

    Add package py-iss-telemetry/1.1/py-iss-telemetry-1.1.tar.gz

---
+++ packages/py-iss-telemetry/py-iss-telemetry-1.1.tar.gz/py-iss-telemetry-1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-iss-telemetry", # Replace with your own username
+    version="1.1",
+    author="Ben Appleby",
+    author_email="ben.appleby@sky.com",
+    description="Stream live International Space Station Telemetry.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BApplB/py-iss-telemetry",
+    packages=setuptools.find_packages(),
+	include_package_data=True,
+	package_data={"": ["*.list"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
