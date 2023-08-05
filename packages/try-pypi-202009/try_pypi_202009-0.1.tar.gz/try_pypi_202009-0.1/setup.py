cf7251bc335864592a452a9a65a81dd414eb480c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package try-pypi-202009/0.1/try_pypi_202009-0.1.tar.gz

---
+++ packages/try-pypi-202009/try_pypi_202009-0.1.tar.gz/try_pypi_202009-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='try_pypi_202009',
+      version='0.1',
+      description='Try upload a package to pypi',
+      packages=['try_pypi_202009'],
+      author = 'udacity',
+      zip_safe=False)
