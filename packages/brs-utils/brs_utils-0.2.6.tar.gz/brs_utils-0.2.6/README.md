aa49d10374443e2d9c9b8ce5a407e803e8f07365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:04 +0000

    Add package brs-utils/0.2.6/brs_utils-0.2.6.tar.gz

---
+++ packages/brs-utils/brs_utils-0.2.6.tar.gz/brs_utils-0.2.6/README.md
@@ -0,0 +1,25 @@
+# brs-utils
+
+[![Anaconda-Server Badge](https://anaconda.org/brsynth/brs_utils/badges/latest_release_date.svg)](https://anaconda.org/brsynth/brs_utils) [![Anaconda-Server Badge](https://anaconda.org/brsynth/brs_utils/badges/version.svg)](https://anaconda.org/brsynth/brs_utils)
+
+## total_size
+Computes the deep size of an object.
+
+## print functions
+Defines some fancy print functions
+
+## download, extract_gz, download_and_extract_gz
+Functions to handle download and extract files
+
+## file_length
+Returns the number of lines of the given file
+
+
+## Authors
+
+* **Melchior du Lac**
+* **Joan Hérisson**
+
+
+## Licence
+brs-utils is released under the MIT licence. See the LICENCE file for details.
---
+++ packages/brs-utils/brs_utils-0.2.6.tar.gz/brs_utils-0.2.6/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+from pathlib import Path
+
+_readme = 'README.md'
+
+_extras_path = 'extras'
+
+with open(_extras_path+'/.env', 'r') as f:
+    line = f.readline()
+    if line.startswith('PACKAGE='):
+        _package = line.splitlines()[0].split('=')[1].lower()
+
+with open(_readme, 'r') as f:
+    long_description = f.read()
+
+required = []
+with open(_extras_path+'/requirements.txt', 'r') as f:
+    required = [line[:-1] for line in f]
+
+_release = 'RELEASE'
+
+with open(_release, 'r') as f:
+    _version = f.readline().split()[0]
+
+setup(
+    name=_package,
+    version=_version,
+    author='Melchior du Lac, Joan Hérisson',
+    author_email='joan.herisson@univ-evry.fr',
+    description='BRSynth Utilities',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/brsynth/brs-utils',
+    packages=[_package],
+    package_dir={_package: 'src'},
+    include_package_data=True,
+    install_requires=required,
+    test_suite='discover_tests',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5',
+)
