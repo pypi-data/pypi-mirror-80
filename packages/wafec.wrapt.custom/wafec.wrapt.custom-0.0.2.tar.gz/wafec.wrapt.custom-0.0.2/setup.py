c18532ab2089907c9b4ffb311d63c547c3348c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:34 +0000

    Add package wafec.wrapt.custom/0.0.2/wafec.wrapt.custom-0.0.2.tar.gz

---
+++ packages/wafec.wrapt.custom/wafec.wrapt.custom-0.0.2.tar.gz/wafec.wrapt.custom-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="wafec.wrapt.custom",
+    version="0.0.2",
+    author="Wallace",
+    author_email="wallacefcardoso@gmail.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
