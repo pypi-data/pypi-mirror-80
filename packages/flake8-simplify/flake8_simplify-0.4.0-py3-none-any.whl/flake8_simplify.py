7c5021960c53973b35003d2ee9d0484d0def684f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:31 +0000

    Add package flake8-simplify/0.4.0/flake8_simplify-0.4.0-py3-none-any.whl

---
+++ packages/flake8-simplify/flake8_simplify-0.4.0-py3-none-any.whl/flake8_simplify.py
@@ -0,0 +1,489 @@
+# Core Library
+import ast
+import sys
+from collections import defaultdict
+from typing import Any, DefaultDict, Generator, List, Tuple, Type
+
+# Third party
+import astor
+
+if sys.version_info < (3, 8):  # pragma: no cover (<PY38)
+    # Third party
+    import importlib_metadata
+else:  # pragma: no cover (PY38+)
+    # Core Library
+    import importlib.metadata as importlib_metadata
+
+SIM101 = (
+    "SIM101 Multiple isinstance-calls which can be merged into a single "
+    "call for variable '{var}'"
+)
+SIM102 = "SIM102 Use a single if-statement instead of nested if-statements"
+SIM103 = "SIM103 Return the condition {cond} directly"
+SIM104 = "SIM104 Use 'yield from {iterable}'"
+SIM105 = "SIM105 Use 'contextlib.suppress({exception})'"
+SIM201 = "SIM201 Use '{left} != {right}' instead of 'not {left} == {right}'"
+SIM202 = "SIM202 Use '{left} == {right}' instead of 'not {left} != {right}'"
+SIM203 = "SIM203 Use '{a} not in {b}' instead of 'not {a} in {b}'"
+SIM204 = "SIM204 Use '{a} >= {b}' instead of 'not ({a} < {b})'"
+SIM205 = "SIM205 Use '{a} > {b}' instead of 'not ({a} <= {b})'"
+SIM206 = "SIM206 Use '{a} <= {b}' instead of 'not ({a} > {b})'"
+SIM207 = "SIM207 Use '{a} < {b}' instead of 'not ({a} >= {b})'"
+SIM208 = "SIM208 Use '{a}' instead of 'not (not {a})'"
+SIM210 = "SIM210 Use 'bool({cond})' instead of 'True if {cond} else False'"
+SIM211 = "SIM211 Use 'not {cond}' instead of 'False if {cond} else True'"
+
+
+def strip_parenthesis(string: str) -> str:
+    if string[0] == "(" and string[-1] == ")":
+        return string[1:-1]
+    return string
+
+
+def _get_duplicated_isinstance_call_by_node(node: ast.BoolOp) -> List[str]:
+    """
+    Get a list of isinstance arguments which could be shortened.
+
+    This checks SIM101.
+
+    Examples
+    --------
+    >> g = _get_duplicated_isinstance_call_by_node
+    >> g("isinstance(a, int) or isinstance(a, float) or isinstance(b, int)
+    ['a']
+    >> g("isinstance(a, int) or isinstance(b, float) or isinstance(b, int)
+    ['b']
+    """
+    counter: DefaultDict[str, int] = defaultdict(int)
+
+    for call in node.values:
+        # Make sure that this function call is actually a call of the built-in
+        # "isinstance"
+        if not isinstance(call, ast.Call) or len(call.args) != 2:
+            continue
+        function_name = astor.to_source(call.func).strip()
+        if function_name != "isinstance":
+            continue
+
+        # Collect the name of the argument
+        isinstance_arg0_name = astor.to_source(call.args[0]).strip()
+        counter[isinstance_arg0_name] += 1
+    return [arg0_name for arg0_name, count in counter.items() if count > 1]
+
+
+def _get_duplicated_isinstance_calls(
+    node: ast.BoolOp,
+) -> List[Tuple[int, int, str]]:
+    """Get a positions where the duplicate isinstance problem appears."""
+    errors: List[Tuple[int, int, str]] = []
+    if not isinstance(node.op, ast.Or):
+        return errors
+
+    for var in _get_duplicated_isinstance_call_by_node(node):
+        errors.append((node.lineno, node.col_offset, SIM101.format(var=var)))
+    return errors
+
+
+def _get_sim102(node: ast.If) -> List[Tuple[int, int, str]]:
+    """Get a list of all nested if-statements without else-blocks."""
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        node.orelse != []
+        or len(node.body) != 1
+        or not isinstance(node.body[0], ast.If)
+        or node.body[0].orelse != []
+    ):
+        return errors
+    errors.append((node.lineno, node.col_offset, SIM102))
+    return errors
+
+
+def _get_not_equal_calls(node: ast.UnaryOp) -> List[Tuple[int, int, str]]:
+    """
+    Get a list of all calls where an unary 'not' is used for an equality.
+
+    This checks SIM201.
+    """
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        not isinstance(node.op, ast.Not)
+        or not isinstance(node.operand, ast.Compare)
+        or len(node.operand.ops) != 1
+        or not isinstance(node.operand.ops[0], ast.Eq)
+    ):
+        return errors
+    comparison = node.operand
+    left = astor.to_source(comparison.left).strip()
+    right = astor.to_source(comparison.comparators[0]).strip()
+    errors.append(
+        (node.lineno, node.col_offset, SIM201.format(left=left, right=right))
+    )
+
+    return errors
+
+
+def _get_not_non_equal_calls(node: ast.UnaryOp) -> List[Tuple[int, int, str]]:
+    """
+    Get a list of all calls where an unary 'not' is used for an quality.
+
+    This checks SIM202.
+    """
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        not isinstance(node.op, ast.Not)
+        or not isinstance(node.operand, ast.Compare)
+        or len(node.operand.ops) != 1
+        or not isinstance(node.operand.ops[0], ast.NotEq)
+    ):
+        return errors
+    comparison = node.operand
+    left = astor.to_source(comparison.left).strip()
+    right = astor.to_source(comparison.comparators[0]).strip()
+    errors.append(
+        (node.lineno, node.col_offset, SIM202.format(left=left, right=right))
+    )
+
+    return errors
+
+
+def _get_not_in_calls(node: ast.UnaryOp) -> List[Tuple[int, int, str]]:
+    """
+    Get a list of all calls where an unary 'not' is used for an in-check.
+
+    This checks SIM203.
+    """
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        not isinstance(node.op, ast.Not)
+        or not isinstance(node.operand, ast.Compare)
+        or len(node.operand.ops) != 1
+        or not isinstance(node.operand.ops[0], ast.In)
+    ):
+        return errors
+    comparison = node.operand
+    left = astor.to_source(comparison.left).strip()
+    right = astor.to_source(comparison.comparators[0]).strip()
+    errors.append(
+        (node.lineno, node.col_offset, SIM203.format(a=left, b=right))
+    )
+
+    return errors
+
+
+def _get_sim204(node: ast.UnaryOp) -> List[Tuple[int, int, str]]:
+    """Get a list of all calls of the type "not (a < b)"."""
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        not isinstance(node.op, ast.Not)
+        or not isinstance(node.operand, ast.Compare)
+        or len(node.operand.ops) != 1
+        or not isinstance(node.operand.ops[0], ast.Lt)
+    ):
+        return errors
+    comparison = node.operand
+    left = astor.to_source(comparison.left).strip()
+    right = astor.to_source(comparison.comparators[0]).strip()
+    errors.append(
+        (node.lineno, node.col_offset, SIM204.format(a=left, b=right))
+    )
+    return errors
+
+
+def _get_sim205(node: ast.UnaryOp) -> List[Tuple[int, int, str]]:
+    """Get a list of all calls of the type "not (a <= b)"."""
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        not isinstance(node.op, ast.Not)
+        or not isinstance(node.operand, ast.Compare)
+        or len(node.operand.ops) != 1
+        or not isinstance(node.operand.ops[0], ast.LtE)
+    ):
+        return errors
+    comparison = node.operand
+    left = astor.to_source(comparison.left).strip()
+    right = astor.to_source(comparison.comparators[0]).strip()
+    errors.append(
+        (node.lineno, node.col_offset, SIM205.format(a=left, b=right))
+    )
+    return errors
+
+
+def _get_sim206(node: ast.UnaryOp) -> List[Tuple[int, int, str]]:
+    """Get a list of all calls of the type "not (a > b)"."""
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        not isinstance(node.op, ast.Not)
+        or not isinstance(node.operand, ast.Compare)
+        or len(node.operand.ops) != 1
+        or not isinstance(node.operand.ops[0], ast.Gt)
+    ):
+        return errors
+    comparison = node.operand
+    left = strip_parenthesis(astor.to_source(comparison.left).strip())
+    right = strip_parenthesis(
+        astor.to_source(comparison.comparators[0]).strip()
+    )
+    errors.append(
+        (node.lineno, node.col_offset, SIM206.format(a=left, b=right))
+    )
+    return errors
+
+
+def _get_sim207(node: ast.UnaryOp) -> List[Tuple[int, int, str]]:
+    """Get a list of all calls of the type "not (a >= b)"."""
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        not isinstance(node.op, ast.Not)
+        or not isinstance(node.operand, ast.Compare)
+        or len(node.operand.ops) != 1
+        or not isinstance(node.operand.ops[0], ast.GtE)
+    ):
+        return errors
+    comparison = node.operand
+    left = astor.to_source(comparison.left).strip()
+    right = astor.to_source(comparison.comparators[0]).strip()
+    errors.append(
+        (node.lineno, node.col_offset, SIM207.format(a=left, b=right))
+    )
+    return errors
+
+
+def _get_sim208(node: ast.UnaryOp) -> List[Tuple[int, int, str]]:
+    """Get a list of all calls of the type "not (not a)"."""
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        not isinstance(node.op, ast.Not)
+        or not isinstance(node.operand, ast.UnaryOp)
+        or not isinstance(node.operand.op, ast.Not)
+    ):
+        return errors
+    a = astor.to_source(node.operand.operand).strip()
+    errors.append((node.lineno, node.col_offset, SIM208.format(a=a)))
+    return errors
+
+
+def _get_sim210(node: ast.IfExp) -> List[Tuple[int, int, str]]:
+    """Get a list of all calls of the type "True if a else False"."""
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        not isinstance(node.body, (ast.Constant, ast.NameConstant))
+        or node.body.value is not True
+        or not isinstance(node.orelse, (ast.Constant, ast.NameConstant))
+        or node.orelse.value is not False
+    ):
+        return errors
+    cond = strip_parenthesis(astor.to_source(node.test).strip())
+    errors.append((node.lineno, node.col_offset, SIM210.format(cond=cond)))
+    return errors
+
+
+def _get_sim211(node: ast.IfExp) -> List[Tuple[int, int, str]]:
+    """Get a list of all calls of the type "False if a else True"."""
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        not isinstance(node.body, (ast.Constant, ast.NameConstant))
+        or node.body.value is not False
+        or not isinstance(node.orelse, (ast.Constant, ast.NameConstant))
+        or node.orelse.value is not True
+    ):
+        return errors
+    cond = strip_parenthesis(astor.to_source(node.test).strip())
+    errors.append((node.lineno, node.col_offset, SIM211.format(cond=cond)))
+    return errors
+
+
+def _get_sim103(node: ast.If) -> List[Tuple[int, int, str]]:
+    """
+    Get a list of all calls that wrap a condition to return a bool.
+
+    if cond:
+        return True
+    else:
+        return False
+
+    which is
+
+        If(
+            test=Name(id='cond', ctx=Load()),
+            body=[
+                Return(
+                    value=Constant(value=True, kind=None),
+                ),
+            ],
+            orelse=[
+                Return(
+                    value=Constant(value=False, kind=None),
+                ),
+            ],
+        ),
+
+    """
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        len(node.body) != 1
+        or not isinstance(node.body[0], ast.Return)
+        or not isinstance(node.body[0].value, (ast.Constant, ast.NameConstant))
+        or not (
+            node.body[0].value.value is True
+            or node.body[0].value.value is False
+        )
+        or len(node.orelse) != 1
+        or not isinstance(node.orelse[0], ast.Return)
+        or not isinstance(
+            node.orelse[0].value, (ast.Constant, ast.NameConstant)
+        )
+        or not (
+            node.orelse[0].value.value is True
+            or node.orelse[0].value.value is False
+        )
+    ):
+        return errors
+    cond = astor.to_source(node.test).strip()
+    errors.append((node.lineno, node.col_offset, SIM103.format(cond=cond)))
+    return errors
+
+
+def _get_sim104(node: ast.For) -> List[Tuple[int, int, str]]:
+    """
+    Get a list of all "iterate and yield" patterns.
+
+    for item in iterable:
+        yield item
+
+    which is
+
+        For(
+            target=Name(id='item', ctx=Store()),
+            iter=Name(id='iterable', ctx=Load()),
+            body=[
+                Expr(
+                    value=Yield(
+                        value=Name(id='item', ctx=Load()),
+                    ),
+                ),
+            ],
+            orelse=[],
+            type_comment=None,
+        ),
+
+    """
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        len(node.body) != 1
+        or not isinstance(node.body[0], ast.Expr)
+        or not isinstance(node.body[0].value, ast.Yield)
+        or not isinstance(node.target, ast.Name)
+        or not isinstance(node.body[0].value.value, ast.Name)
+        or node.target.id != node.body[0].value.value.id
+        or node.orelse != []
+    ):
+        return errors
+    iterable = astor.to_source(node.iter).strip()
+    errors.append(
+        (node.lineno, node.col_offset, SIM104.format(iterable=iterable))
+    )
+    return errors
+
+
+def _get_sim105(node: ast.Try) -> List[Tuple[int, int, str]]:
+    """
+    Get a list of all "try-except-pass" patterns.
+
+    try:
+        foo()
+    except ValueError:
+        pass
+
+    which is
+
+        Try(
+            body=[
+                Expr(
+                    value=Call(
+                        func=Name(id='foo', ctx=Load()),
+                        args=[],
+                        keywords=[],
+                    ),
+                ),
+            ],
+            handlers=[
+                ExceptHandler(
+                    type=Name(id='ValueError', ctx=Load()),
+                    name=None,
+                    body=[Pass()],
+                ),
+            ],
+            orelse=[],
+            finalbody=[],
+        ),
+
+
+    """
+    errors: List[Tuple[int, int, str]] = []
+    if (
+        len(node.body) != 1
+        or len(node.handlers) != 1
+        or not isinstance(node.handlers[0], ast.ExceptHandler)
+        or len(node.handlers[0].body) != 1
+        or not isinstance(node.handlers[0].body[0], ast.Pass)
+        or node.orelse != []
+    ):
+        return errors
+    exception = astor.to_source(node.handlers[0].type).strip()
+    errors.append(
+        (node.lineno, node.col_offset, SIM105.format(exception=exception))
+    )
+    return errors
+
+
+class Visitor(ast.NodeVisitor):
+    def __init__(self) -> None:
+        self.errors: List[Tuple[int, int, str]] = []
+
+    def visit_BoolOp(self, node: ast.BoolOp) -> None:
+        self.errors += _get_duplicated_isinstance_calls(node)
+        self.generic_visit(node)
+
+    def visit_If(self, node: ast.If) -> None:
+        self.errors += _get_sim102(node)
+        self.errors += _get_sim103(node)
+        self.generic_visit(node)
+
+    def visit_For(self, node: ast.For) -> None:
+        self.errors += _get_sim104(node)
+        self.generic_visit(node)
+
+    def visit_Try(self, node: ast.Try) -> None:
+        self.errors += _get_sim105(node)
+        self.generic_visit(node)
+
+    def visit_UnaryOp(self, node: ast.UnaryOp) -> None:
+        self.errors += _get_not_equal_calls(node)
+        self.errors += _get_not_non_equal_calls(node)
+        self.errors += _get_not_in_calls(node)
+        self.errors += _get_sim204(node)
+        self.errors += _get_sim205(node)
+        self.errors += _get_sim206(node)
+        self.errors += _get_sim207(node)
+        self.errors += _get_sim208(node)
+        self.generic_visit(node)
+
+    def visit_IfExp(self, node: ast.IfExp) -> None:
+        self.errors += _get_sim210(node)
+        self.errors += _get_sim211(node)
+        self.generic_visit(node)
+
+
+class Plugin:
+    name = __name__
+    version = importlib_metadata.version(__name__)
+
+    def __init__(self, tree: ast.AST):
+        self._tree = tree
+
+    def run(self) -> Generator[Tuple[int, int, str, Type[Any]], None, None]:
+        visitor = Visitor()
+        visitor.visit(self._tree)
+
+        for line, col, msg in visitor.errors:
+            yield line, col, msg, type(self)
