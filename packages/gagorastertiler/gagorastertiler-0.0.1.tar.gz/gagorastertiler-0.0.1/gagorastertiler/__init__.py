849a7c8bab4a256382f3ea14b7ae181cfb969e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:06 +0000

    Add package gagorastertiler/0.0.1/gagorastertiler-0.0.1.tar.gz

---
+++ packages/gagorastertiler/gagorastertiler-0.0.1.tar.gz/gagorastertiler-0.0.1/gagorastertiler/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time   : 2020/9/22 10:45 AM
+# @Author : Dutt
+# @Email  : dutengteng1@163.com
+# @File   : __init__.py.py
---
+++ packages/gagorastertiler/gagorastertiler-0.0.1.tar.gz/gagorastertiler-0.0.1/gagorastertiler/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# IDE: PyCharm 2019.1.2
+# Author: Dajiang Ren<rendajiang@gagogroup.com>
+# Created on 2019-06-01
+
+
+import os
+
+if __name__ == '__main__':
+
+    cmd = "python raster2tiles.py -p tianditu --zoom=0-13 -f Lerc /Users/gago/temp/test/lerc_test/Result_v8.tif /Users/gago/temp/test/lerc_test/temp"
+    os.system(cmd)
+
+
+
+
+
+
+
+
