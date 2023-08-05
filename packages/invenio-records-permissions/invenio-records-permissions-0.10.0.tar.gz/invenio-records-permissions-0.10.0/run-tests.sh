dd3795bbe522614beed4ce1e20ec371ad9aba714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:16 +0000

    Add package invenio-records-permissions/0.10.0/invenio-records-permissions-0.10.0.tar.gz

---
+++ packages/invenio-records-permissions/invenio-records-permissions-0.10.0.tar.gz/invenio-records-permissions-0.10.0/run-tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019-2020 CERN.
+# Copyright (C) 2019-2020 Northwestern University.
+#
+# Invenio-Records-Permissions is free software; you can redistribute it
+# and/or modify it under the terms of the MIT License; see LICENSE file for
+# more details.
+
+python -m pydocstyle invenio_records_permissions tests docs && \
+python -m isort invenio_records_permissions tests --check-only --diff && \
+python -m check_manifest --ignore ".travis-*" && \
+python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
+docker-services-cli up es postgresql
+python -m pytest
+tests_exit_code=$?
+docker-services-cli down
+exit "$tests_exit_code"
---
+++ packages/invenio-records-permissions/invenio-records-permissions-0.10.0.tar.gz/invenio-records-permissions-0.10.0/setup.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+# Copyright (C) 2019 Northwestern University.
+#
+# Invenio-Records-Permissions is free software; you can redistribute it
+# and/or modify it under the terms of the MIT License; see LICENSE file for
+# more details.
+
+"""Permission policies for Invenio records."""
+
+import os
+
+from setuptools import find_packages, setup
+
+readme = open('README.rst').read()
+history = open('CHANGES.rst').read()
+
+sphinx_require = 'Sphinx>=1.5.1'
+
+tests_require = [
+    'pytest-mock>=1.6.0',
+    'pytest-invenio>=1.3.3,<2.0.0',
+    'invenio-accounts>=1.3.0,<2.0.0',
+    'invenio-app>=1.3.0,<2.0.0',
+    sphinx_require,
+]
+
+# Should follow inveniosoftware/invenio versions
+invenio_db_version = '>=1.0.5,<2.0.0'
+invenio_search_version = '>=1.4.0,<2.0.0'
+
+extras_require = {
+    'elasticsearch6': [
+        'invenio-search[elasticsearch6]>={}'.format(invenio_search_version),
+    ],
+    'elasticsearch7': [
+        'invenio-search[elasticsearch7]>={}'.format(invenio_search_version),
+    ],
+    'mysql': [
+        'invenio-db[mysql,versioning]{}'.format(invenio_db_version),
+    ],
+    'postgresql': [
+        'invenio-db[postgresql,versioning]{}'.format(invenio_db_version),
+    ],
+    'sqlite': [
+        'invenio-db[versioning]{}'.format(invenio_db_version),
+    ],
+    'docs': [
+        sphinx_require,
+    ],
+    'tests': tests_require,
+}
+
+extras_require['all'] = []
+for name, reqs in extras_require.items():
+    if name[0] == ':' or name in ('elasticsearch6', 'elasticsearch7',
+                                  'mysql', 'postgresql', 'sqlite'):
+        continue
+    extras_require['all'].extend(reqs)
+
+setup_requires = [
+    'Babel>=1.3',
+    'pytest-runner>=3.0.0,<5',
+]
+
+install_requires = [
+    'invenio-access>=1.4.1,<2.0.0',
+    'invenio-i18n>=1.2.0',
+    'invenio-records>=1.3.2'
+]
+
+packages = find_packages()
+
+
+# Get the version string. Cannot be done with import!
+g = {}
+with open(os.path.join('invenio_records_permissions', 'version.py'), 'rt') as fp:
+    exec(fp.read(), g)
+    version = g['__version__']
+
+setup(
+    name='invenio-records-permissions',
+    version=version,
+    description=__doc__,
+    long_description=readme + '\n\n' + history,
+    keywords='invenio permissions',
+    license='MIT',
+    author='CERN',
+    author_email='info@inveniosoftware.org',
+    url='https://github.com/inveniosoftware/invenio-records-permissions',
+    packages=packages,
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    entry_points={
+        'invenio_base.apps': [
+            'invenio_records_permissions = invenio_records_permissions:InvenioRecordsPermissions',
+        ],
+        'invenio_i18n.translations': [
+            'messages = invenio_records_permissions',
+        ],
+    },
+    extras_require=extras_require,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Development Status :: 3 - Alpha',
+    ],
+)
