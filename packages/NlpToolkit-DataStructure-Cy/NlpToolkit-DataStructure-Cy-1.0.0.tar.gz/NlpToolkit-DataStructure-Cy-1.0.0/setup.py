00327bbb87367c4be9855f404d17a549adf3c9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:52 +0000

    Add package NlpToolkit-DataStructure-Cy/1.0.0/NlpToolkit-DataStructure-Cy-1.0.0.tar.gz

---
+++ packages/NlpToolkit-DataStructure-Cy/NlpToolkit-DataStructure-Cy-1.0.0.tar.gz/NlpToolkit-DataStructure-Cy-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize(["DataStructure/*.pyx", "DataStructure/Cache/*.pyx", "DataStructure/Cache/*.pxd"], compiler_directives={'language_level' : "3"}),
+    name='NlpToolkit-DataStructure-Cy',
+    version='1.0.0',
+    packages=['DataStructure', 'DataStructure.Cache'],
+    url='https://github.com/olcaytaner/DataStructure-Cy',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='Simple Data Structures Library'
+)
