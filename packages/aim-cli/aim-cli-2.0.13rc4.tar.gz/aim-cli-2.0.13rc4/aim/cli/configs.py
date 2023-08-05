303230b5e1886256019ef3eb96cc8d72b37a8b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:03 +0000

    Add package aim-cli/2.0.13rc4/aim-cli-2.0.13rc4.tar.gz

---
+++ packages/aim-cli/aim-cli-2.0.13rc4.tar.gz/aim-cli-2.0.13rc4/aim/cli/configs.py
@@ -0,0 +1,11 @@
+INIT_NAME = 'init'
+EXPERIMENT_NAME = 'experiment'
+COMMIT_NAME = 'commit'
+RESET_NAME = 'reset'
+VERSION_NAME = 'version'
+DE_NAME = 'de'
+STATUS_NAME = 'status'
+CONFIG_NAME = 'config'
+
+# Shortcuts
+DE_UP_SHORTCUT = 'up'
