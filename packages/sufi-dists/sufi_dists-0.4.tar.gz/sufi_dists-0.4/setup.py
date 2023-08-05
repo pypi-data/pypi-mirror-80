9c67f4ad270df1b0f2102a99502b445e529d3324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:17 +0000

    Add package sufi-dists/0.4/sufi_dists-0.4.tar.gz

---
+++ packages/sufi-dists/sufi_dists-0.4.tar.gz/sufi_dists-0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sufi_dists',
+      version='0.4',
+      description='Testing usage of packaging by Sufi',
+      packages=['sufi_dists'],
+      author='Sufian Kaki Aslam',
+      author_email='sufiankaki@gmail.com',
+      zip_safe=False)
---
+++ packages/sufi-dists/sufi_dists-0.4.tar.gz/sufi_dists-0.4/sufi_dists/about.py
@@ -0,0 +1,7 @@
+class about():
+    dob='13-06-1992'
+    def print_dob(self):
+        print(self.dob)
+
+def hey():
+    print('hello')
