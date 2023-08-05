16a528364ddf8e4314f65bb17d9eb146b6fff1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:58 +0000

    Add package lumpyrem/0.0.7/lumpyrem-0.0.7.tar.gz

---
+++ packages/lumpyrem/lumpyrem-0.0.7.tar.gz/lumpyrem-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lumpyrem", 
+    version="0.0.7",
+    author="Rui Hugman",
+    author_email="rthugman@gmail.com",
+    description="This package reads, writes and runs LUMPREM2 and associated utility programs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rhugman/lumpyrem",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
