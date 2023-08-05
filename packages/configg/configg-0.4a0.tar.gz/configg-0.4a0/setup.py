fe2d24df1a9cae2b0538b3bab997876582caf577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:45 +0000

    Add package configg/0.4a0/configg-0.4a0.tar.gz

---
+++ packages/configg/configg-0.4a0.tar.gz/configg-0.4a0/setup.py
@@ -0,0 +1,11 @@
+
+import setuptools
+
+setuptools.setup(name='configg',
+      version='v0.4-alpha',
+      description='Config data manager',
+      author='Oliver',
+      author_email='mail.ok65@googlemail.com',
+      url='https://github.com/ok65/Configg',
+      packages=['configg'],
+     )
