ab8890d19c9d73d0413ede87d968d675f3506245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:25 +0000

    Add package django-wellknown-security/0.15/django-wellknown-security-0.15.tar.gz

---
+++ packages/django-wellknown-security/django-wellknown-security-0.15.tar.gz/django-wellknown-security-0.15/wellknown_security/apps.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+__author__ = "Peter Gastinger"
+__copyright__ = "Copyright 2020"
+__credits__ = ["Peter Gastinger"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Peter Gastinger"
+__email__ = "peter@secitec.net"
+__status__ = "Production"
+
+from django.apps import AppConfig
+
+
+class WellknownSecurityConfig(AppConfig):
+    name = "wellknown_security"
