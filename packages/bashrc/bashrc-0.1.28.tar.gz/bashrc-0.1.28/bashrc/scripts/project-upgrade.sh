5e3d721319dcf5e487c555d9b43a690e2123e845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:36 +0000

    Add package bashrc/0.1.28/bashrc-0.1.28.tar.gz

---
+++ packages/bashrc/bashrc-0.1.28.tar.gz/bashrc-0.1.28/bashrc/scripts/project-upgrade.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# ${1} - name
+# shellcheck disable=SC2034
+export source="${BASH_SOURCE[0]}"; debug.sh source
+
+path="${1:-.}"
+
+cd "${path}" > /dev/null 2>&1 || { error.sh "${path}" "invalid"; exit 1; }
+
+path="$( pwd )"
+name="$( basename "${path}" )"
+export path name; debug.sh path name
+
+unset VIRTUAL_ENV PYTHONHOME
+deactivate > /dev/null 2>&1
+
+if error="$( /usr/local/bin/pip3.8 install -vvvv --upgrade "${name}" 2>&1 )"; then
+  info.sh upgrade "${name}"
+else
+  error.sh upgrade "${name}" "${error}"; exit 1
+fi
+
+## I do not know why requires twice to get it into the path,
+/usr/local/bin/pip3.8 install --upgrade "${name}" > /dev/null 2>&1
+
+unset source error path name
