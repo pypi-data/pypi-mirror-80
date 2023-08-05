913274f98afeb65815131422c775b8d0a321cd50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:12 +0000

    Add package tql/2020.9.21.10.39.23/tql-2020.9.21.10.39.23-py3-none-any.whl

---
+++ packages/tql/tql-2020.9.21.10.39.23-py3-none-any.whl/tql/ml/optimizer/tuner/cab_tuner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : tql-Python.
+# @File         : cab_tuner
+# @Time         : 2020/9/21 12:39 上午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : https://github.com/optuna/optuna/blob/master/examples/catboost_simple.py
+
+
---
+++ packages/tql/tql-2020.9.21.10.39.23-py3-none-any.whl/tql/ml/optimizer/tuner/xgb_tuner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : tql-Python.
+# @File         : xgb_tuner
+# @Time         : 2020/9/21 12:37 上午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : https://github.com/optuna/optuna/blob/master/examples/xgboost_cv.py
+# https://github.com/optuna/optuna/blob/master/examples/pruning/xgboost_cv_integration.py
+
+
+# TODO: tensorboard_callback
