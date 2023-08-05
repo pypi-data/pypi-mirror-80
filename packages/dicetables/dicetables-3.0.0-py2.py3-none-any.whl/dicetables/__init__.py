a5101346fcf2ac116a3aaa19916f126c7e34bbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package dicetables/3.0.0/dicetables-3.0.0-py2.py3-none-any.whl

---
+++ packages/dicetables/dicetables-3.0.0-py2.py3-none-any.whl/dicetables/__init__.py
@@ -0,0 +1,11 @@
+from dicetables.additiveevents import AdditiveEvents
+from dicetables.bestworstmid import BestOfDicePool, WorstOfDicePool, UpperMidOfDicePool, LowerMidOfDicePool
+from dicetables.dicerecord import DiceRecord
+from dicetables.dicetable import DiceTable, DetailedDiceTable
+from dicetables.dieevents import Die, ModDie, WeightedDie, ModWeightedDie, StrongDie, Modifier, ExplodingOn, Exploding
+from dicetables.eventsbases.eventerrors import DiceRecordError, InvalidEventsError
+from dicetables.eventsinfo import (EventsCalculations, EventsInformation,
+                                   events_range, mean, stddev, percentage_points, percentage_axes, stats,
+                                   full_table_string)
+from dicetables.parser import Parser, ParseError, LimitsError
+from dicetables.roller import Roller
