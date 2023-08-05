81453d6f74da90a7f6dcfc5cf937caee44d1e18f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:14 +0000

    Add package subito/0.1.2/subito-0.1.2.tar.gz

---
+++ packages/subito/subito-0.1.2.tar.gz/subito-0.1.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['subito', 'subito.db', 'subito.operations']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['gql>=3.0.0-alpha.1,<4.0.0',
+ 'python-box>=5.1.1,<6.0.0',
+ 'tinydb>=4.1.1,<5.0.0']
+
+setup_kwargs = {
+    'name': 'subito',
+    'version': '0.1.2',
+    'description': 'Quickly create stateless microservices based on GraphQL subscription messages.',
+    'long_description': None,
+    'author': 'Milos Grujic',
+    'author_email': 'srbiotik@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/srbiotik/subito',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
