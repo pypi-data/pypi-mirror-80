c294dd898513d3f9a11618974423aacc99cce30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:42 +0000

    Add package docs-generator/0.1.4/docs_generator-0.1.4.tar.gz

---
+++ packages/docs-generator/docs_generator-0.1.4.tar.gz/docs_generator-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='docs_generator',
+    version_format='{tag}.dev{commitcount}+{gitsha}',
+    install_requires=[
+        'jupyter',
+        'mkdocs',
+        'pyyaml',
+        'mkdocs-material',
+        'pytest'
+    ],
+    setup_requires=['setuptools-git-version'],
+    packages=['docs_generator'],
+    url='https://github.com/sirily11/algorithms',
+    license='',
+    author='sirily11',
+    author_email='sirily1997@gmail.com',
+    description='A program which convert jupyter notebook to mkdocs'
+)
