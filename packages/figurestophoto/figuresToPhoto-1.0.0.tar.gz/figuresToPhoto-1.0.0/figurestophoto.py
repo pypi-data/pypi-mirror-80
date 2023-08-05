0c1acf309f1484089646ece0733a7073b2a9b819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:49 +0000

    Add package figurestophoto/1.0.0/figuresToPhoto-1.0.0.tar.gz

---
+++ packages/figurestophoto/figuresToPhoto-1.0.0.tar.gz/figuresToPhoto-1.0.0/figurestophoto.py
@@ -0,0 +1,36 @@
+from PIL import Image, ImageDraw
+
+class figures():
+    def __init__(self,name):
+        self.img = Image.open(name)
+        self.idraw = ImageDraw.Draw(self.img)
+    
+    def squareToJpg(self,a:int,b:int,c:int,d:int,color:str,filename):
+        self.idraw.rectangle((a,b,c,d), fill= color)
+        img = self.img.convert("RGB")
+        img.save(f"{filename}.jpg","jpeg")
+
+    def squareToPng(self,a:int,b:int,c:int,d:int,color:str,filename):
+        self.idraw.rectangle((a,b,c,d), fill= color)
+        img = self.img.convert("RGBA")
+        img.save(f"{filename}.png","png")
+
+    def circleToPng(self,a:int,b:int,c:int,d:int,color:str,filename):
+        self.idraw.ellipse((a,b,c,d), fill= color)
+        img = self.img.convert("RGBA")
+        img.save(f"{filename}.png","png")
+
+    def circleToJpg(self,a:int,b:int,c:int,d:int,color:str,filename):
+        self.idraw.ellipse((a,b,c,d), fill= color)
+        img = self.img.convert("RGB")
+        img.save(f"{filename}.jpg","jpeg")
+    
+    def polygonToJpg(self,a:int,b:int,c:int,d:int,e: int, f: int, color:str,filename):
+        self.idraw.polygon([(a,b),(c,d),(e,f)], fill= color)
+        img = self.img.convert("RGB")
+        img.save(f"{filename}.jpg","jpeg")
+
+    def polygonToPng(self,a:int,b:int,c:int,d:int,e: int, f: int, color:str,filename):
+        self.idraw.polygon([(a,b),(c,d),(e,f)], fill= color)
+        img = self.img.convert("RGBA")
+        img.save(f"{filename}.png","png")
