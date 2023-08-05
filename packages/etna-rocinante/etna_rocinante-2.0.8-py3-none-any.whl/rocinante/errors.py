c6f8459b4bf8da9ded072d5440f701e0c5411393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:06 +0000

    Add package etna-rocinante/2.0.8/etna_rocinante-2.0.8-py3-none-any.whl

---
+++ packages/etna-rocinante/etna_rocinante-2.0.8-py3-none-any.whl/rocinante/errors.py
@@ -0,0 +1,11 @@
+class RetryableError(BaseException):
+    """
+    Exception wrapper class to mark an error as retryable
+    """
+
+    def __init__(self, cause):
+        super().__init__()
+        self.__cause__ = cause
+
+    def __str__(self):
+        return str(self.__cause__)
