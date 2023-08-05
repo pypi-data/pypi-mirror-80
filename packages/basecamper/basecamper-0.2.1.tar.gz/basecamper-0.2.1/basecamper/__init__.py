5ef2a1145fbdb992208533f986b9bcb2114a2efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:53 +0000

    Add package basecamper/0.2.1/basecamper-0.2.1.tar.gz

---
+++ packages/basecamper/basecamper-0.2.1.tar.gz/basecamper-0.2.1/basecamper/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for Granular basecamper."""
+
+__author__ = """Sid Gupta, Sagar Verma"""
+__email__ = 'sid@granular.ai, sagar@granular.ai'
+__version__ = 'v0.2.0'
+
