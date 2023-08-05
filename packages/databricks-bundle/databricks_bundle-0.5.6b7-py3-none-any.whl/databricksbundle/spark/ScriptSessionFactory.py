4d5a1859fb2147e7f3627f0b1e6515cdb4d092fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:38 +0000

    Add package databricks-bundle/0.5.6b7/databricks_bundle-0.5.6b7-py3-none-any.whl

---
+++ packages/databricks-bundle/databricks_bundle-0.5.6b7-py3-none-any.whl/databricksbundle/spark/ScriptSessionFactory.py
@@ -0,0 +1,27 @@
+from typing import List
+from pyspark.sql import SparkSession
+from databricksbundle.spark.SparkSessionLazy import SparkSessionLazy
+from databricksbundle.spark.config.ConfiguratorInterface import ConfiguratorInterface
+
+class ScriptSessionFactory:
+
+    def __init__(
+        self,
+        configurators: List[ConfiguratorInterface] = None,
+    ):
+        self.__configurators = configurators or []
+
+    def create(self) -> SparkSessionLazy:
+        def createLazy():
+            spark = SparkSession \
+                .builder \
+                .getOrCreate()
+
+            # .config('spark.master', 'local') \
+
+            #for configurator in self.__configurators:
+            #    configurator.configure(spark)
+
+            return spark
+
+        return SparkSessionLazy(createLazy)
