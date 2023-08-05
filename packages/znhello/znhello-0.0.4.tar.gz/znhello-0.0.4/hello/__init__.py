aabd3421b00150c3cd79dd4a22ed8cd6b199dd65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:23 +0000

    Add package znhello/0.0.4/znhello-0.0.4.tar.gz

---
+++ packages/znhello/znhello-0.0.4.tar.gz/znhello-0.0.4/hello/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/22 下午3:24
+# @Author  : jinzening
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ packages/znhello/znhello-0.0.4.tar.gz/znhello-0.0.4/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/22 下午3:29
+# @Author  : jinzening
+# @File    : setup.py
+# @Software: PyCharm
+
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='znhello',  # 包的名字
+    version='0.0.4',  # 版本号
+    description='project describe',  # 描述
+    author='jinzening',  # 作者
+    author_email="zening918@163.com",
+
+    packages=setuptools.find_packages(),  # 包内需要引用的文件夹
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # BSD认证
+        'Programming Language :: Python',  # 支持的语言
+        'Programming Language :: Python :: 3',  # python版本 。。。
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+    zip_safe=True,
+)
