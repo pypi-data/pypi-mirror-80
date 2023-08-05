f0ad070f04751c029b1b539ac7724d0fe5578d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:23 +0000

    Add package pdb-profiling/0.1.4/pdb_profiling-0.1.4-py3-none-any.whl

---
+++ packages/pdb-profiling/pdb_profiling-0.1.4-py3-none-any.whl/build/lib/pdb_profiling/__init__.py
@@ -0,0 +1,7 @@
+# @Created Date: 2020-05-13 08:54:03 pm
+# @Filename: __init__.py
+# @Email:  1730416009@stu.suda.edu.cn
+# @Author: ZeFeng Zhu
+# @Last Modified: 2020-05-13 08:54:09 pm
+# @Copyright (c) 2020 MinghuiGroup, Soochow University
+__version__ = '0.1.4'
