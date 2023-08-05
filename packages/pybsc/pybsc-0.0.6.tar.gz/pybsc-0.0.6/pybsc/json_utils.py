b97a0bfc9895b0fe48681a0e56f4a8e29ad73f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:46 +0000

    Add package pybsc/0.0.6/pybsc-0.0.6.tar.gz

---
+++ packages/pybsc/pybsc-0.0.6.tar.gz/pybsc-0.0.6/pybsc/json_utils.py
@@ -0,0 +1,43 @@
+import json
+
+
+def load_json(file_path):
+    """Load json function.
+
+    Parameters
+    ----------
+    file_path : str or pathlib.PosixPath
+        json file path
+
+    Returns
+    -------
+    data : dict
+        loaded json data
+    """
+    with open(str(file_path), "r") as f:
+        return json.load(f)
+
+
+def save_json(data, filename,
+              save_pretty=True,
+              sort_keys=True,
+              ensure_ascii=True):
+    """Save json function.
+
+    Parameters
+    ----------
+    data : dict
+        save data
+    filename : str
+        save path
+    """
+    filename = str(filename)
+    with open(filename, "w") as f:
+        if save_pretty:
+            f.write(json.dumps(data, indent=4,
+                               ensure_ascii=ensure_ascii,
+                               sort_keys=sort_keys,
+                               separators=(',', ': ')))
+        else:
+            json.dump(data, f)
+        f.write('\n')
