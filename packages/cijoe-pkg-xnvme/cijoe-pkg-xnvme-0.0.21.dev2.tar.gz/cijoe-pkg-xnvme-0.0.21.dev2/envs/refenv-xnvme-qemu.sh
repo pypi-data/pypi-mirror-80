4eb4efa1890921f69bb8489fe3de35dcb67010d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:46 +0000

    Add package cijoe-pkg-xnvme/0.0.21.dev2/cijoe-pkg-xnvme-0.0.21.dev2.tar.gz

---
+++ packages/cijoe-pkg-xnvme/cijoe-pkg-xnvme-0.0.21.dev2.tar.gz/cijoe-pkg-xnvme-0.0.21.dev2/envs/refenv-xnvme-qemu.sh
@@ -0,0 +1,139 @@
+#!/usr/bin/env bash
+#
+# QEMU target environment skeleton
+#
+# The SSH configuration is overwritten such that everything using the "ssh::"
+# module will be targeted towards the QEMU guest.
+#
+# To access the QEMU host an explicit "QEMU_HOST" and utility functions are
+# provided by the "qemu::" module
+#
+
+# CIJOE: QEMU_* environment variables
+: "${QEMU_HOST:=localhost}"; export QEMU_HOST
+: "${QEMU_HOST_USER:=$USER}"; export QEMU_HOST_USER
+: "${QEMU_HOST_PORT:=22}"; export QEMU_HOST_PORT
+: "${QEMU_HOST_SYSTEM_BIN:=/opt/qemu/x86_64-softmmu/qemu-system-x86_64}"; export QEMU_HOST_SYSTEM_BIN
+: "${QEMU_HOST_IMG_BIN:=qemu-img}"; export QEMU_HOST_IMG_BIN
+
+: "${QEMU_GUESTS:=/opt/guests}"; export QEMU_GUESTS
+: "${QEMU_GUEST_NAME:=emujoe}"; export QEMU_GUEST_NAME
+: "${QEMU_GUEST_SSH_FWD_PORT:=2222}"; export QEMU_GUEST_SSH_FWD_PORT
+: "${QEMU_GUEST_CONSOLE:=file}"; export QEMU_GUEST_CONSOLE
+: "${QEMU_GUEST_MEM:=6G}"; export QEMU_GUEST_MEM
+: "${QEMU_GUEST_SMP:=4}"; export QEMU_GUEST_SMP
+#: "${QEMU_GUEST_KERNEL:=1}"; export QEMU_GUEST_KERNEL
+#: "${QEMU_GUEST_APPEND:=net.ifnames=0 biosdevname=0}"; export QEMU_GUEST_APPEND
+
+# CIJOE: SSH_* environment variables
+: "${SSH_HOST:=localhost}"; export SSH_HOST
+: "${SSH_PORT:=$QEMU_GUEST_SSH_FWD_PORT}"; export SSH_PORT
+: "${SSH_USER:=root}"; export SSH_USER
+: "${SSH_NO_CHECKS:=1}"; export SSH_NO_CHECKS
+
+#
+# xNVMe: where are libraries and share stored on the target system? This is
+# needed to find the xNVMe fio io-engine, fio scripts etc.
+#
+: "${XNVME_LIB_ROOT:=/usr/lib}"; export XNVME_LIB_ROOT
+: "${XNVME_SHARE_ROOT:=/usr/share/xnvme}"; export XNVME_SHARE_ROOT
+
+#
+# xNVMe: which device is used for testing? set PCI_DEV_NAME, NVME_CNTID, and
+# NVME_NSID based on NVME_NSTYPE
+#
+if [[ -v NVME_NSTYPE ]]; then
+  case $NVME_NSTYPE in
+  lblk)
+    : "${PCI_DEV_NAME=0000:03:00.0}"
+    : "${NVME_CNTID=0}"
+    : "${NVME_NSID=1}"
+    : "${NVME_DEV_NAME:=nvme${NVME_CNTID}n${NVME_NSID}}"
+    : "${NVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
+    ;;
+  zoned)
+    : "${PCI_DEV_NAME=0000:03:00.0}"
+    : "${NVME_CNTID=0}"
+    : "${NVME_NSID=2}"
+    : "${NVME_DEV_NAME:=nvme${NVME_CNTID}n${NVME_NSID}}"
+    : "${NVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
+    ;;
+  *)
+    echo "# ERROR: invalid NVME_NSTYPE(${NVME_NSTYPE})"
+    exit 1
+    ;;
+  esac
+
+  export PCI_DEV_NAME
+  export NVME_NSID
+  export NVME_CNTID
+  export NVME_DEV_NAME
+  export NVME_DEV_PATH
+fi
+
+#
+# xNVMe: set XNVME_BE if not assigned
+# NOTE: This will be deprecated as the backends are now: LINUX, FBSD, and SPDK
+#
+#: "${XNVME_BE:=SPDK}"
+#: "${XNVME_BE:=FIOC}"
+#: "${XNVME_BE:=LIOC}"
+#: "${XNVME_BE:=LAIO}"
+#: "${XNVME_BE:=LIOU}"
+
+#
+# xNVMe: set XNVME_URI and possibly HUGEMEM
+#
+if [[ -v XNVME_BE && -v NVME_NSTYPE ]]; then
+
+  case $XNVME_BE in
+  LINUX|LIOU|LIOC)
+    : "${XNVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
+    : "${XNVME_URI=${XNVME_DEV_PATH}?async=iou}"
+    ;;
+  LAIO)
+    : "${XNVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
+    : "${XNVME_URI=${XNVME_DEV_PATH}?async=aio}"
+    ;;
+  NWRP)
+    : "${XNVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
+    : "${XNVME_URI=${XNVME_DEV_PATH}?async=nil}"
+    ;;
+  FBSD|FREEBSD|FIOC)
+    : "${XNVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
+    : "${XNVME_URI=${XNVME_DEV_PATH}}"
+    ;;
+  SPDK)
+    : "${XNVME_DEV_PATH:=/dev/nvme${NVME_CNTID}n${NVME_NSID}}"
+    : "${XNVME_URI=pci:${PCI_DEV_NAME}?nsid=${NVME_NSID}}"
+
+    # Set a default HUGEMEM that can be overwritten by testplan
+    : "${HUGEMEM:=4096}"
+    export HUGEMEM
+    ;;
+  *)
+    echo "# ERROR: invalid XNVME_BE(${XNVME_BE})"
+    exit 1
+  esac
+
+  # Operating system device path
+  export XNVME_DEV_PATH
+  # xNVMe URI
+  export XNVME_URI
+fi
+
+# These are for running fio
+if [[ -v NVME_NSTYPE ]]; then
+  : "${SPDK_FIOE_ROOT:=/opt/aux}"; export SPDK_FIOE_ROOT
+fi
+
+# The external fio io-engines usually depend on the fio-version on which they
+# built against. So, we set the FIO_BIN to point it to the version that comes
+# with the xNVMe dockerize reference environment
+: "${FIO_BIN:=/opt/aux/fio}"; export FIO_BIN
+
+#
+# These are for the nullblock hook, specifically when loading it
+#
+: "${NULLBLK_QUEUE_MODE=2}"; export NULLBLK_QUEUE_MODE
+: "${NULLBLK_IRQMODE=0}"; export NULLBLK_IRQMODE
