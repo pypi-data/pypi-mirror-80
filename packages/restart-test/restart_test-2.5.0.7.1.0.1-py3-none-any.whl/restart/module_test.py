8193a7f84b41fa6ffbe650968cec30bc5932fdfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:49 +0000

    Add package restart-test/2.5.0.7.1.0.1/restart_test-2.5.0.7.1.0.1-py3-none-any.whl

---
+++ packages/restart-test/restart_test-2.5.0.7.1.0.1-py3-none-any.whl/restart/module_test.py
@@ -0,0 +1,9 @@
+"""Tester for main model module."""
+
+from restart.restart import RestartModel
+
+if __name__ == "__main__":
+    oes_test = RestartModel(
+        population="oes", state="California"
+    )
+    dict_test = RestartModel(population="dict")
