e7153b72b391fd1058357ff54269aacefde8305c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:24 +0000

    Add package flask-jobs/0.0.19/flask_jobs-0.0.19-py3-none-any.whl

---
+++ packages/flask-jobs/flask_jobs-0.0.19-py3-none-any.whl/flask_jobs/worker.py
@@ -0,0 +1,55 @@
+import datetime
+import threading
+from .jobs import Job
+
+
+class Worker:
+    def __init__(self, db=None):
+        self._running = True
+        self._db = db
+        self._timer = None
+        self._lock = threading.Lock()
+        self.Refresh()
+
+    def Refresh(self, newTimeout=0.1):
+        # print('Refresh(', newTimeout)
+        self.StopTimer()
+        self._timer = threading.Timer(newTimeout, self.DoJobs)
+        self._timer.start()
+
+    def StopTimer(self):
+        if self._timer:
+            if self._timer.is_alive():
+                self._timer.cancel()
+
+    def DoJobs(self):
+        # print('self.DoJobs(', self)
+        if self._running:
+            with self._lock:
+                self.StopTimer()
+                nowDT = datetime.datetime.utcnow()
+
+                # Do all jobs that are past their self['dt']
+                jobs = list(self._db.FindAll(Job, status='pending', _orderBy='dt'))
+                # print('nowDT=', nowDT)
+                # print('32 jobs=', jobs)
+                for job in jobs:
+                    if job['dt'] < nowDT:
+                        job.DoJob(self)
+                        del job  # forces the job to be committed to db
+
+                # Find the next 'schedule' or 'repeat' Job
+                nextJobList = list(self._db.FindAll(Job, status='pending', _orderBy='dt', _limit=1))
+                # print('nextJobList=', nextJobList)
+                if nextJobList:
+                    nextJob = nextJobList[0]
+                    delta = (nextJob['dt'] - nowDT).total_seconds()
+                    # print('delta=', delta)
+                    self.Refresh(delta)
+
+    def __del__(self):
+        self.Kill()
+
+    def Kill(self):
+        self._running = False
+        self.StopTimer()
