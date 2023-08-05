bdb7da1b90977ce3dc4fd8baefef0b610a7ade69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:31 +0000

    Add package cookiepy/0.8.1/cookiepy-0.8.1-py2.py3-none-any.whl

---
+++ packages/cookiepy/cookiepy-0.8.1-py2.py3-none-any.whl/cookiepy/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from .files import *
+from .xml_to_csv import *
+from .recipe import *
+from .generate_tfrecords import csv_to_tfrecord
+from .tf_models import clone_models
+"""Top-level package for cookiepy."""
+
+__author__ = """Nikos Koumbakis"""
+__email__ = 'n.koumbakis@gmail.com'
+__version__ = '0.8.1'
