3b3d1a4b5072cdf671d0bc90641f37794bf7a94d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:18 +0000

    Add package django-unicorn/0.6.5/django-unicorn-0.6.5.tar.gz

---
+++ packages/django-unicorn/django-unicorn-0.6.5.tar.gz/django-unicorn-0.6.5/django_unicorn/templatetags/unicorn.py
@@ -0,0 +1,54 @@
+from django import template
+from django.conf import settings
+
+from ..components import UnicornView
+
+
+register = template.Library()
+
+
+@register.inclusion_tag("unicorn/scripts.html")
+def unicorn_scripts():
+    return {"debug": settings.DEBUG}
+
+
+@register.inclusion_tag("unicorn/styles.html")
+def unicorn_styles():
+    return {}
+
+
+@register.inclusion_tag("unicorn/errors.html", takes_context=True)
+def unicorn_errors(context):
+    return {"unicorn": {"errors": context.get("unicorn", {}).get("errors", {})}}
+
+
+def unicorn(parser, token):
+    try:
+        (tag_name, component_name) = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            "%r tag requires a single argument" % token.contents.split()[0]
+        )
+
+    if not (
+        component_name[0] == component_name[-1] and component_name[0] in ('"', "'")
+    ):
+        raise template.TemplateSyntaxError(
+            "%r tag's argument should be in quotes" % tag_name
+        )
+
+    return UnicornNode(component_name[1:-1])
+
+
+class UnicornNode(template.Node):
+    def __init__(self, component_name):
+        self.component_name = component_name
+
+    def render(self, context):
+        view = UnicornView.create(component_name=self.component_name)
+        rendered_component = view.render(init_js=True)
+
+        return rendered_component
+
+
+register.tag("unicorn", unicorn)
