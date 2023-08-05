34eea9144c3fce871ab3541d88aedd861a31d082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:51 +0000

    Add package dnb-arelle/0.1.0/dnb_arelle-0.1.0-py3-none-any.whl

---
+++ packages/dnb-arelle/dnb_arelle-0.1.0-py3-none-any.whl/arelle/Version.py
@@ -0,0 +1,10 @@
+'''
+This module represents the time stamp when Arelle was last built
+
+@author: Mark V Systems Limited
+(c) Copyright 2020 Mark V Systems Limited, All rights reserved.
+
+'''
+__version__ = '1.2020.05.10'  # number version of code base and date compiled
+version = '2020-05-10 16:47 UTC'  # string version of date compiled
+copyrightLatestYear = '2020'  # string version of year compiled
