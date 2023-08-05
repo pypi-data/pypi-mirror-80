3ae9e714b6d6f17c06418f77af93e992ca3efe68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:22 +0000

    Add package neuro-extras/20.9.21a1/neuro_extras-20.9.21a1-py3-none-any.whl

---
+++ packages/neuro-extras/neuro_extras-20.9.21a1-py3-none-any.whl/neuro_extras/__init__.py
@@ -0,0 +1,10 @@
+from neuromation.api import PluginManager
+
+from .main import main  # noqa
+
+
+__version__ = "20.9.21a1"
+
+
+def setup_plugin(manager: PluginManager) -> None:
+    manager.config.define_str("extra", "remote-project-dir")
