0f4940cd61edb9a213b49d386a823553fc5ca6f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:15 +0000

    Add package valera/0.0.1/valera-0.0.1.tar.gz

---
+++ packages/valera/valera-0.0.1.tar.gz/valera-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="valera",
+    version="0.0.1",
+    description="",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nikita Tsvetkov",
+    author_email="nikitanovosibirsk@yandex.com",
+    url="https://github.com/nikitanovosibirsk/valera",
+    license="Apache-2.0",
+    install_requires=[],
+    tests_require=[],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
