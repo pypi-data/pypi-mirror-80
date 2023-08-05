c457e2ed6a40fdc87e1d9a0e7476abf0089f8cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:10 +0000

    Add package pprint36/3.9.0.1/pprint36-3.9.0.1.tar.gz

---
+++ packages/pprint36/pprint36-3.9.0.1.tar.gz/pprint36-3.9.0.1/__pkginfo__.py
@@ -0,0 +1,62 @@
+#  This file is managed by 'repo_helper'. Don't edit it directly.
+#  Copyright (C) 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  This file is distributed under the same license terms as the program it came with.
+#  There will probably be a file called LICEN[S/C]E in the same directory as this file.
+#
+#  In any case, this program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# This script based on https://github.com/rocky/python-uncompyle6/blob/master/__pkginfo__.py
+#
+
+# stdlib
+import pathlib
+
+__all__ = [
+		"__copyright__",
+		"__version__",
+		"modname",
+		"pypi_name",
+		"__license__",
+		"__author__",
+		"short_desc",
+		"author",
+		"author_email",
+		"github_username",
+		"web",
+		"github_url",
+		"repo_root",
+		"install_requires",
+		"extras_require",
+		"project_urls",
+
+		"import_name",
+		]
+
+__copyright__ = """
+2020 Python Software Foundation <dominic@davis-foster.co.uk>
+"""
+
+__version__ = "3.9.0.1"
+modname = "pprint36"
+pypi_name = "pprint36"
+import_name = "pprint36"
+__license__ = "PSFLicense"
+short_desc = 'Backport of pprint from Python 3.9 to Python 3.6-3.8'
+__author__ = author = 'Python Software Foundation'
+author_email = 'dominic@davis-foster.co.uk'
+github_username = "domdfcoding"
+web = github_url = "https://github.com/domdfcoding/pprint36"
+repo_root = pathlib.Path(__file__).parent
+install_requires = (repo_root / "requirements.txt").read_text(encoding="utf-8").split('\n')
+extras_require = {'all': []}
+
+
+
+project_urls = {
+		"Documentation": "https://pprint36.readthedocs.io",
+		"Issue Tracker": f"{github_url}/issues",
+		"Source Code": github_url,
+		}
