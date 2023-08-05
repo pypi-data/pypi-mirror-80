2a75066aa4e71ef13512a233a3d09dcbd6a74694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:20 +0000

    Add package js-sdk/11.0b6/js_sdk-11.0b6-py3-none-any.whl

---
+++ packages/js-sdk/js_sdk-11.0b6-py3-none-any.whl/jumpscale/packages/marketplace/frontend/data.js
@@ -0,0 +1,56 @@
+const APPS = {
+  wiki: {
+    name: "Wiki",
+    type: "wiki",
+    image: "./assets/doc-flat.svg",
+    description: "Wiki is a versatile online encyclopedia builder, accessible via a public web address."
+  },
+  website: {
+    name: "Website",
+    type: "website",
+    image: "./assets/web.png",
+    description: "Website is a P2P alternative to centralized cloud-hosted websites. Host your own website with access via a public web address."
+  },
+  blog: {
+    name: "Blog",
+    type: "blog",
+    image: "./assets/blog.png",
+    description: "Blog is a P2P alternative to centralized blogging platforms like Tumblr or Blogspot."
+  },
+  mattermost: {
+    name: "Mattermost",
+    type: "mattermost",
+    image: "./assets/mattermost.png",
+    description: "Mattermost is a flexible, open source messaging platform that enables secure team collaboration."
+  },
+  peertube: {
+    name: "Peertube",
+    type: "peertube",
+    image: "./assets/peertube.png",
+    description: "Peertube is an open-source video platform that uses peer-to-peer technologies to reduce load on individual servers when viewing videos."
+  },
+  gitea: {
+    name: "Gitea",
+    type: "gitea",
+    image: "./assets/gitea.png",
+    description: "Gitea is a painless self-hosted Git service. It is similar to GitHub, Bitbucket, and GitLab."
+  },
+  discourse: {
+    name: "Discourse",
+    type: "discourse",
+    image: "./assets/discourse.png",
+    description: "Discourse is an open source Internet forum and mailing list management software application built to educate members about civil community engagement."
+  },
+  cryptpad: {
+    name: "Cryptpad",
+    type: "cryptpad",
+    image: "./assets/cryptpad.png",
+    description: "Cryptpad is a fully-secured, encrypted alternative to popular office tools and cloud services."
+  },
+  taiga: {
+    name: "Taiga",
+    type: "taiga",
+    image: "./assets/taiga.png",
+    description: "Taiga is a P2P alternative to centralized project management tool for multi-functional agile teams."
+  },
+}
