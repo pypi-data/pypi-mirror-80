dc8304346fb7dbeff65f673d443ed71736f56eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:49 +0000

    Add package pystarworlds/0.0.3/pystarworlds-0.0.3-py3-none-any.whl

---
+++ packages/pystarworlds/pystarworlds-0.0.3-py3-none-any.whl/pystarworlds/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 16:14:41 2019
+
+@author: ben
+"""
+
+from . import Agent, Environment, Event, Identifiable
+
+__all__ = ('Agent', 'Environment', 'Event', 'Identifiable')
