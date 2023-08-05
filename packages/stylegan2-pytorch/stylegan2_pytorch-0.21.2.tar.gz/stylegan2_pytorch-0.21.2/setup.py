7cab0b6658243f6fe00ffc6ac0beabf4c3524e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:51 +0000

    Add package stylegan2-pytorch/0.21.2/stylegan2_pytorch-0.21.2.tar.gz

---
+++ packages/stylegan2-pytorch/stylegan2_pytorch-0.21.2.tar.gz/stylegan2_pytorch-0.21.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'stylegan2_pytorch',
+  packages = find_packages(),
+  entry_points={
+      'console_scripts': [
+          'stylegan2_pytorch = stylegan2_pytorch.cli:main',
+      ],
+  },
+  version = '0.21.2',
+  license='GPLv3+',
+  description = 'StyleGan2 in Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/stylegan2-pytorch',
+  download_url = 'https://github.com/lucidrains/stylegan2-pytorch/archive/v_036.tar.gz',
+  keywords = ['generative adversarial networks', 'artificial intelligence'],
+  install_requires=[
+      'fire',
+      'numpy',
+      'retry',
+      'tqdm',
+      'torch',
+      'torchvision',
+      'pillow',
+      'adamp',
+      'contrastive_learner>=0.1.0',
+      'linear_attention_transformer',
+      'vector-quantize-pytorch'
+  ],
+  classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.6',
+  ],
+)
