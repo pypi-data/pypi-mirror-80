a3f2b0a337c7987618fe761a8d1abd65ab36783b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package yourscript-test/0.1.2/yourscript_test-0.1.2.tar.gz

---
+++ packages/yourscript-test/yourscript_test-0.1.2.tar.gz/yourscript_test-0.1.2/click_demo/myClickDemo.py
@@ -0,0 +1,29 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+------------------------------------
+@Project : click_cli
+@Time    : 2020/9/21 15:16
+@Auth    : luozhongwen
+@Email   : luozw@inhand.com.cn
+@File    : myClickDemo.py
+@IDE     : PyCharm
+------------------------------------
+"""
+
+
+def say_something():
+    print("this is some test function")
+
+
+def say_something2():
+    print("this is some test function")
+
+
+def say_something3():
+    print("this is some test function")
+
+
+def say_something4():
+    print("this is some test function")
---
+++ packages/yourscript-test/yourscript_test-0.1.2.tar.gz/yourscript_test-0.1.2/setup.py
@@ -0,0 +1,37 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+------------------------------------
+@Project : click_cli
+@Time    : 2020/9/21 11:27
+@Auth    : luozhongwen
+@Email   : luozw@inhand.com.cn
+@File    : setup.py
+@IDE     : PyCharm
+------------------------------------
+"""
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='yourscript_test',
+    version='0.1.2',
+    author='chineseluo',
+    author_email='848257135@qq.com',
+    url='https://github.com/chineseluo/UI_auto_project',
+    description='this is a upload pkg test',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    py_modules=['yourscript', 'myClickDemo'],
+    install_requires=[
+        'click',
+    ],
+    packages=find_packages(),
+    entry_points="""
+    [console_scripts]
+    yourscript=yourscript:cli
+    """
+)
