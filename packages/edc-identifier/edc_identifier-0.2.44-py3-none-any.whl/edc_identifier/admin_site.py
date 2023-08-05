11f6d5abd81ef4e21b158ad44967f5142bb64dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:49 +0000

    Add package edc-identifier/0.2.44/edc_identifier-0.2.44-py3-none-any.whl

---
+++ packages/edc-identifier/edc_identifier-0.2.44-py3-none-any.whl/edc_identifier/admin_site.py
@@ -0,0 +1,12 @@
+from django.contrib.admin.sites import AdminSite
+
+
+class EdcIdentifierAdminSite(AdminSite):
+    site_header = "Edc Identifier"
+    site_title = "Edc Identifier"
+    index_title = "Edc Identifier"
+    site_url = "/administration/"
+
+
+edc_identifier_admin = EdcIdentifierAdminSite(name="edc_identifier_admin")
+edc_identifier_admin.disable_action("delete_selected")
