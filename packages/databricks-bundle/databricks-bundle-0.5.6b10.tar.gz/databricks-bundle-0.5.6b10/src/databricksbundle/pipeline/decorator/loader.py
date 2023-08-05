ec74b4f39c5132f223871ebcfa34361ff8d3dd07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:16 +0000

    Add package databricks-bundle/0.5.6b10/databricks-bundle-0.5.6b10.tar.gz

---
+++ packages/databricks-bundle/databricks-bundle-0.5.6b10.tar.gz/databricks-bundle-0.5.6b10/src/databricksbundle/pipeline/decorator/loader.py
@@ -0,0 +1,7 @@
+from databricksbundle.detector import isDatabricks
+from databricksbundle.notebook.helpers import isNotebookEnvironment
+
+if isDatabricks() and isNotebookEnvironment():
+    from databricksbundle.pipeline.decorator.environment.databricksNotebook import pipelineFunction, dataFrameLoader, transformation, dataFrameSaver
+else:
+    from databricksbundle.pipeline.decorator.environment.pyscript import pipelineFunction, dataFrameLoader, transformation, dataFrameSaver
