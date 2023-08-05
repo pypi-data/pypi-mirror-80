232ec96a70a30ab9756f95bbc537cb6ff293793d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:15 +0000

    Add package sideex-webservice-client/0.9.1/sideex-webservice-client-0.9.1.tar.gz

---
+++ packages/sideex-webservice-client/sideex-webservice-client-0.9.1.tar.gz/sideex-webservice-client-0.9.1/sideex-webservice-client/SideeXWebServiceClientAPI.py
@@ -0,0 +1,62 @@
+import json
+import asyncio
+import aiohttp
+import os
+import ssl
+from enum import Enum
+
+class ProtocalType(Enum):
+    HTTP = 0
+    HTTPS_DISABLE = 1
+    HTTPS_ENABLE = 2
+
+class SideeXWebServiceClientAPI():
+    def __init__(self, baseURL, protocalType=ProtocalType.HTTP, caFilePath=None):
+        self.baseURL = baseURL
+        self.protocalType = protocalType
+        self.caFilePath = caFilePath
+        self.sslContext = None
+
+        if self.baseURL[-1] != '/':
+            self.baseURL = self.baseURL + "/"
+
+        if self.protocalType == ProtocalType.HTTPS_DISABLE:
+            self.sslContext = False
+        elif self.protocalType == ProtocalType.HTTPS_ENABLE:
+            self.sslContext = ssl.create_default_context(cafile=self.caFilePath)
+
+    async def runTestSuite(self, file):
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=self.sslContext)) as session:
+            data = aiohttp.FormData()
+            data.add_field('file', file, filename=os.path.basename(file.name), content_type='application/x-www-form-urlencoded')
+            async with session.post(self.baseURL+"sideex-webservice/runTestSuites", data = data) as resp:
+                return await resp.text()
+    
+    async def getState(self, token):
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=self.sslContext)) as session:
+            data = aiohttp.FormData()
+            data.add_field('token', token, content_type='application/x-www-form-urlencoded')
+
+            async with session.get(self.baseURL+"sideex-webservice/getState", data = data) as resp:
+                return await resp.text()
+
+    async def download(self, formData, filePath, option):
+        tempURL = self.baseURL
+        if option == 0:
+            tempURL = tempURL + "sideex-webservice/downloadReports"
+        else:
+            tempURL = tempURL + "sideex-webservice/downloadLogs"
+
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=self.sslContext)) as session:
+            async with session.get(tempURL, data = formData) as resp:
+                test = await resp.read()
+                with open(filePath, "wb") as f:
+                    f.write(test)
+    
+    async def deleteJob(self, token):
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=self.sslContext)) as session:
+            data = aiohttp.FormData()
+            data.add_field('token', token, content_type='application/x-www-form-urlencoded')
+
+            async with session.post(self.baseURL+"sideex-webservice/deleteJob", data = data) as resp:
+                return await resp.text()
