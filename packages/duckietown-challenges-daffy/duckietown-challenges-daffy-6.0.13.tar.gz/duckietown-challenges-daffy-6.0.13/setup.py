29a3a2adedefa0917ec7ef6c7d743efdd62060c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:30 +0000

    Add package duckietown-challenges-daffy/6.0.13/duckietown-challenges-daffy-6.0.13.tar.gz

---
+++ packages/duckietown-challenges-daffy/duckietown-challenges-daffy-6.0.13.tar.gz/duckietown-challenges-daffy-6.0.13/setup.py
@@ -0,0 +1,60 @@
+import sys
+
+from setuptools import find_packages, setup
+
+
+def get_version(filename):
+    import ast
+
+    version = None
+    with open(filename) as f:
+        for line in f:
+            if line.startswith("__version__"):
+                version = ast.parse(line).body[0].value.s
+                break
+        else:
+            raise ValueError("No version found in %r." % filename)
+    if version is None:
+        raise ValueError(filename)
+    return version
+
+
+install_requires = [
+    "termcolor",
+    "decorator",
+    "PyYAML",
+    "python-dateutil",
+    "oyaml",
+    "numpy",
+    "six",
+    "future",
+    "zuper-ipce-z6>=6",
+    "networkx>=2.2",
+]
+
+system_version = tuple(sys.version_info)[:3]
+if system_version < (3, 7):
+    install_requires.append("dataclasses")
+
+version = get_version(filename="src/duckietown_challenges/__init__.py")
+line = "daffy"
+setup(
+    name=f"duckietown-challenges-{line}",
+    version=version,
+    download_url="http://github.com/duckietown/duckietown-challenges/tarball/%s" % version,
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=install_requires,
+    tests_require=[],
+    # This avoids creating the egg file, which is a zip file, which makes our data
+    # inaccessible by dir_from_package_name()
+    zip_safe=False,
+    # without this, the stuff is included but not installed
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "dt-challenges-make-readme-definitions  = duckietown_challenges:make_readmes_main",
+            "dt-challenges-make-readme-templates  = duckietown_challenges:make_readmes_templates_main",
+        ]
+    },
+)
