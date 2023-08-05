a4e8e85c70d30593d44cddca3af23ce99a20981e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:05 +0000

    Add package nbtex/1.0.7/nbtex-1.0.7-py3-none-any.whl

---
+++ packages/nbtex/nbtex-1.0.7-py3-none-any.whl/nbtex/__init__.py
@@ -0,0 +1,6 @@
+from nbtex.core.Ast import Var, makeVar
+from nbtex.Matrix import Matrix, MatrixBuilder, MatrixWithDots
+from nbtex.Vector import Vector, makeVector
+from nbtex.Dots import Dots
+from nbtex.platforms.jupyter import latex, platex
+from nbtex.Symbols import Greek, Set, Operator, Arrow
