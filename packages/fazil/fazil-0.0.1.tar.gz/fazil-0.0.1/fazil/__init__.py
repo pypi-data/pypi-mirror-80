a044c48679dea458c6012063118de782485431ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:34 +0000

    Add package fazil/0.0.1/fazil-0.0.1.tar.gz

---
+++ packages/fazil/fazil-0.0.1.tar.gz/fazil-0.0.1/fazil/__init__.py
@@ -0,0 +1,11 @@
+def displayMe(name):
+    value="I am "+str(name)
+    return value
+
+def displayGreet(name):
+    value="Hello, "+str(name)
+    return value
+
+def displaySad(name):
+    value="Sorry, "+str(name)
+    return value
