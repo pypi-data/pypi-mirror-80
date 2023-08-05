2167cbd77f9b2a331d5fa13709efa9529dbcaa27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:51 +0000

    Add package moodlepy/0.11.0/moodlepy-0.11.0-py3-none-any.whl

---
+++ packages/moodlepy/moodlepy-0.11.0-py3-none-any.whl/moodle/core/__init__.py
@@ -0,0 +1,30 @@
+from .badges import BaseBadges
+from .block import BaseBlock
+from .blog import BaseBlog
+from .calendar import BaseCalendar
+from .cohort import BaseCohort
+from .comment import BaseComment
+from .competency import BaseCompetency
+from .course import BaseCourse
+from .message import BaseMessage
+from .notes import BaseNotes
+from .user import BaseUser
+from .webservice import BaseWebservice
+
+from .core import Core
+
+__all__ = [
+    'BaseBadges',
+    'BaseBlock',
+    'BaseBlog',
+    'BaseCalendar',
+    'BaseCohort',
+    'BaseComment',
+    'BaseCompetency',
+    'BaseCourse',
+    'BaseMessage',
+    'BaseNotes',
+    'BaseUser',
+    'BaseWebservice',
+    'Core',
+]
---
+++ packages/moodlepy/moodlepy-0.11.0-py3-none-any.whl/moodle/mod/__init__.py
@@ -0,0 +1,7 @@
+from .assign import BaseAssign
+from .forum import BaseForum
+from .lesson import BaseLesson
+
+from .mod import Mod
+
+__all__ = ['BaseAssign', 'BaseForum', 'BaseLesson', 'Mod']
