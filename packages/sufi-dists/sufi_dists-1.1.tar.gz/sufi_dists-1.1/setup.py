414805dcb61e518ba20a802cb62a044f1afd01ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:40 +0000

    Add package sufi-dists/1.1/sufi_dists-1.1.tar.gz

---
+++ packages/sufi-dists/sufi_dists-1.1.tar.gz/sufi_dists-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sufi_dists',
+      version='1.1',
+      description='Testing usage of packaging',
+      packages=['sufi_dists'],
+      author='Sufian Kaki Aslam',
+      author_email='sufiankaki@gmail.com',
+      zip_safe=False)
