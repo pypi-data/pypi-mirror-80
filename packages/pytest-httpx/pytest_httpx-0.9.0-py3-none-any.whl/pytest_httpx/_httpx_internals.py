8778a48a458e710aeb5780170d90f7008a81c71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:03 +0000

    Add package pytest-httpx/0.9.0/pytest_httpx-0.9.0-py3-none-any.whl

---
+++ packages/pytest-httpx/pytest_httpx-0.9.0-py3-none-any.whl/pytest_httpx/_httpx_internals.py
@@ -0,0 +1,33 @@
+from typing import Union, Any
+from json import dumps
+
+import httpcore
+
+
+class IteratorStream(httpcore.AsyncIteratorByteStream, httpcore.IteratorByteStream):
+    def __init__(self, iterator):
+        httpcore.AsyncIteratorByteStream.__init__(self, aiterator=iterator)
+        httpcore.IteratorByteStream.__init__(self, iterator=iterator)
+
+
+def stream(
+    data, files, json: Any, boundary: bytes
+) -> Union[httpcore.AsyncByteStream, httpcore.SyncByteStream]:
+    if files:
+        # TODO Get rid of this internal import
+        # import is performed at runtime when needed to reduce impact of internal changes in httpx
+        from httpx._multipart import MultipartStream
+
+        return MultipartStream(data=data or {}, files=files, boundary=boundary)
+
+    if json is not None:
+        data = dumps(json).encode("utf-8")
+    elif isinstance(data, str):
+        data = data.encode("utf-8")
+    elif data is None:
+        data = b""
+
+    if isinstance(data, bytes):
+        return httpcore.PlainByteStream(data)
+
+    return IteratorStream(data)
