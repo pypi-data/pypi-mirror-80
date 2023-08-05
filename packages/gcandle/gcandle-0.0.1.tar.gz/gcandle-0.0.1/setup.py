9f09668ef176bb99daf8e4d24ad55608e33974a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:44 +0000

    Add package gcandle/0.0.1/gcandle-0.0.1.tar.gz

---
+++ packages/gcandle/gcandle-0.0.1.tar.gz/gcandle-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gcandle", # Replace with your own username
+    version="0.0.1",
+    author="ych",
+    author_email="yangchanghua@gmail.com",
+    description="A library for quant trade",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yangchanghua/gcandle",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
