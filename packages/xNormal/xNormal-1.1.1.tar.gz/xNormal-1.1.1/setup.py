2ba9feed93700478b6caef203eab984727dc5c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:27 +0000

    Add package xNormal/1.1.1/xNormal-1.1.1.tar.gz

---
+++ packages/xNormal/xNormal-1.1.1.tar.gz/xNormal-1.1.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xNormal",
+    version="1.1.1",
+    author="Daniel Holden, Paul Greveson",
+    author_email="contact@theorangeduck.com",
+    description="Wrapper for xNormal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/orangeduck/Python-xNormal",
+    py_modules=['xNormal'],
+)
