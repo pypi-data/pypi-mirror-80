ae1e755fbf3273417453cb4e0fc3deb8fd215b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:35 +0000

    Add package pytubekit/0.0.2/pytubekit-0.0.2.tar.gz

---
+++ packages/pytubekit/pytubekit-0.0.2.tar.gz/pytubekit-0.0.2/pytubekit/endpoints/group_default.py
@@ -0,0 +1,49 @@
+"""
+The default group of operations that pytubekit has
+"""
+from pytconf import register_endpoint, register_function_group
+
+import pytubekit
+import pytubekit.version
+
+GROUP_NAME_DEFAULT = "default"
+GROUP_DESCRIPTION_DEFAULT = "all pytubekit commands"
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = [
+]
+APP_NAME = "pytubekit"
+
+
+def register_group_default():
+    """
+    register the name and description of this group
+    """
+    register_function_group(
+        function_group_name=GROUP_NAME_DEFAULT,
+        function_group_description=GROUP_DESCRIPTION_DEFAULT,
+    )
+
+
+@register_endpoint(group=GROUP_NAME_DEFAULT, )
+def version() -> None:
+    """
+    Print version
+    """
+    print(pytubekit.version.VERSION_STR)
+
+
+@register_endpoint(group=GROUP_NAME_DEFAULT, )
+def playlists() -> None:
+    """
+    Show all playlists in your youtube account
+    """
+    print("TBD")
+
+
+@register_endpoint(group=GROUP_NAME_DEFAULT, )
+def cleanup() -> None:
+    """
+    Cleanup a specific playlist from deleted or privatized entries
+    """
+    print("TBD")
