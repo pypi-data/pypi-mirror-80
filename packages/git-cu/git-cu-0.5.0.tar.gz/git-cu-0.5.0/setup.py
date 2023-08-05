c1694cb76f286ed83ee60f0282cd1404c6b156d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:27 +0000

    Add package git-cu/0.5.0/git-cu-0.5.0.tar.gz

---
+++ packages/git-cu/git-cu-0.5.0.tar.gz/git-cu-0.5.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['git_cu']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['git-cu = git_cu.main:main']}
+
+setup_kwargs = {
+    'name': 'git-cu',
+    'version': '0.5.0',
+    'description': 'git-cu helps keep your local git repositories organized by cloning them into a directory structure based on their URL.',
+    'long_description': None,
+    'author': 'Vasili Revelas',
+    'author_email': 'vasili.revelas@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
