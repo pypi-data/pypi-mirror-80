8aa2a4930b7216ccead163aaf71f58058e6d039e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:36 +0000

    Add package cScell/0.0.18/cScell-0.0.18.tar.gz

---
+++ packages/cScell/cScell-0.0.18.tar.gz/cScell-0.0.18/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+setup(
+  name = 'cScell',
+  packages = ['cScell'],
+  version = '0.0.18',
+  license='MIT',
+  description = 'A framework made with python for C cli projects',
+  author = 'Eugenio Vinicio Berretta',
+  author_email = 'euberdeveloper@gmail.com',
+  url = 'https://github.com/euberdeveloper/cScell',
+  download_url = 'https://github.com/euberdeveloper/cScell/archive/v0.0.18.tar.gz',
+  package_data={'cScell': ['templates/*.txt']},
+  include_package_data=True,
+  entry_points = {
+    'console_scripts': ['cScell=cScell.command_line:main'],
+  },
+  keywords = ['cScell', 'shell', 'c', 'framework'],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+)
