0fe8c30ab6cd887ca095a00d2a709a88f403a84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:05 +0000

    Add package rancher-yarl/0.3/rancher_yarl-0.3.tar.gz

---
+++ packages/rancher-yarl/rancher_yarl-0.3.tar.gz/rancher_yarl-0.3/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+
+
+
+setup(
+  name = 'rancher_yarl',
+  packages = ['rancher_yarl'],
+  version = '0.3',
+  description = 'Standalone rancher library, with API keys control',
+  long_description="TEST",
+author = 'gusto',                  
+  author_email = 'shivering.fortune@gmail.com',      
+  url = 'https://github.com/memdealer/YARL-YetAnotherRancherLibrary',  
+  download_url = 'https://github.com/memdealer/YARL-YetAnotherRancherLibrary/archive/0.1alpha.tar.gz',   
+  keywords = ['rancher', 'rancher api', 'rancher token'],   
+  install_requires=[        
+      'certifi',
+      'chardet',
+      'idna',
+      'requests',
+      'six',
+      'urllib3',
+      'websocket-client-py3'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
