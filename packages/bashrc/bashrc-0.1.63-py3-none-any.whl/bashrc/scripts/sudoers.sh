e22304441c316f5b79c919e4e1c1ff30500f82dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:28 +0000

    Add package bashrc/0.1.63/bashrc-0.1.63-py3-none-any.whl

---
+++ packages/bashrc/bashrc-0.1.63-py3-none-any.whl/bashrc/scripts/sudoers.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+export starting="${BASH_SOURCE[0]}"; debug.sh starting
+test -n "${PASSWORD}" || { error.sh PASSWORD 'not defined'; exit 1; }
+
+function darwin() {
+  # "${1}" - force
+  local group
+  for group in staff admin wheel; do
+    if ! test -f "/etc/sudoers.d/${group}" || test "${1}" = force; then
+      echo "${1}"
+#      echo "${PASSWORD}" | sudo -S true
+#      echo "%${group} ALL=(ALL) NOPASSWD:ALL" | sudo tee "/etc/sudoers.d/${group}" && info sudoers "${group}"
+#      echo 'Defaults !env_reset' | sudo tee -a "/etc/sudoers.d/${group}" >/dev/null 2>&1
+#      echo 'Defaults env_delete = "HOME"' | sudo tee -a "/etc/sudoers.d/${group}" >/dev/null 2>&1
+#      echo 'Defaults env_delete = "PS1"' | sudo tee -a "/etc/sudoers.d/${group}" >/dev/null 2>&1
+#      echo "Defaults: %${group} !logfile, !syslog" | sudo tee -a "/etc/sudoers.d/${group}" >/dev/null 2>&1
+    fi
+  done
+}
+
+function kali() {
+  true
+}
+
+! test -n "${DARWIN}" || darwin "$@" || exit 1
+! test -n "${KALI}" || kali "$@" || exit 1
+
+unset starting
