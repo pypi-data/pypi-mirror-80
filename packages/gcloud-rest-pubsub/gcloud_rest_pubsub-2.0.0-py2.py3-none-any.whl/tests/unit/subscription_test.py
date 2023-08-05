a620edefbce59390606faffbb546236da3b3ee65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:08 +0000

    Add package gcloud-rest-pubsub/2.0.0/gcloud_rest_pubsub-2.0.0-py2.py3-none-any.whl

---
+++ packages/gcloud-rest-pubsub/gcloud_rest_pubsub-2.0.0-py2.py3-none-any.whl/tests/unit/subscription_test.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    from future import standard_library
+standard_library.install_aliases()
+from gcloud.rest.auth import BUILD_GCLOUD_REST  # pylint: disable=no-name-in-module
+
+
+def test_importable():
+    assert True
+
+
+if BUILD_GCLOUD_REST:
+    pass
+
+
+else:
+    from gcloud.rest.pubsub.subscriber_client import FlowControl
+    from gcloud.rest.pubsub.subscriber_client import SubscriberClient
+    from gcloud.rest.pubsub.subscriber_message import SubscriberMessage  # pylint: disable=unused-import
+
+
+    def test_construct_subscriber_client():
+        SubscriberClient()
+
+    def test_construct_flow_control():
+        FlowControl()
