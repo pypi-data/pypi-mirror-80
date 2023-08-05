327527d50108e47abdaf2301fb6f0bc12b3b7ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:04 +0000

    Add package anchors/0.3.0/anchors-0.3.0-py2.py3-none-any.whl

---
+++ packages/anchors/anchors-0.3.0-py2.py3-none-any.whl/anchors/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for anchors."""
+from .score import get_guide_residuals, get_gene_residuals
+
+__author__ = """Peter Carl DeWeirdt"""
+__email__ = 'petedeweirdt@gmail.com'
+__version__ = '0.3.0'
