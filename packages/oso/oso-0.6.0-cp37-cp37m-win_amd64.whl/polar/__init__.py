b1c9325ca22cb525bf2cb2c80e721265789d2abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:42 +0000

    Add package oso/0.6.0/oso-0.6.0-cp37-cp37m-win_amd64.whl

---
+++ packages/oso/oso-0.6.0-cp37-cp37m-win_amd64.whl/polar/__init__.py
@@ -0,0 +1,7 @@
+import json
+from pathlib import Path
+
+from .polar import Polar, polar_class
+from .query import Query, QueryResult
+from .variable import Variable
+from .predicate import Predicate
