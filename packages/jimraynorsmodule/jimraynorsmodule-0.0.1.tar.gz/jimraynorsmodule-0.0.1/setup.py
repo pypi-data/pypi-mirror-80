48d5623019748b21a880c52e2d7d8161408b50a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:35 +0000

    Add package jimraynorsmodule/0.0.1/jimraynorsmodule-0.0.1.tar.gz

---
+++ packages/jimraynorsmodule/jimraynorsmodule-0.0.1.tar.gz/jimraynorsmodule-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jimraynorsmodule',
+    version='0.0.1',
+    description='Board Game Funds utilities',
+    author='Enrique Basañez Mercader',
+    author_email='enrique.basanez@gmail.com',
+    url='https://www.boardgamefunds.com',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    )
