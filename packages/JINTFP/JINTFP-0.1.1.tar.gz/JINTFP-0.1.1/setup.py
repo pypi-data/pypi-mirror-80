a2d79a33a94585eeb39bed87bcfefa458a2edce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:52 +0000

    Add package JINTFP/0.1.1/JINTFP-0.1.1.tar.gz

---
+++ packages/JINTFP/JINTFP-0.1.1.tar.gz/JINTFP-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JINTFP",
+    version="0.1.1",
+    author="grasshopperTrainer",
+    author_email="grasshoppertrainer@gmail.com",
+    description="Python package for creating node-based function pipeline.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/grasshopperTrainer/JINTFP",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    keywords='function pipeline, functional pipeline, pipeline',
+    python_requires='>=3'
+)
