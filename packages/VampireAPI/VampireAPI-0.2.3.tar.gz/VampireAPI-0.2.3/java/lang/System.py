239695574fed6e835503fd0682de7335367cfdb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:39 +0000

    Add package VampireAPI/0.2.3/VampireAPI-0.2.3.tar.gz

---
+++ packages/VampireAPI/VampireAPI-0.2.3.tar.gz/VampireAPI-0.2.3/java/lang/System.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2020 - Sebastian Ritter <bastie@users.noreply.github.com>
+# SPDX-License-Identifier: Apache-2.0
+
+'''
+Created on 01.09.2020
+
+@author: Sͬeͥbͭaͭsͤtͬian
+'''
+from os.path import sys
+from java.lang.Object import Object
+
+
+class System(Object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+
+    @classmethod
+    def exit(self, status: int):
+        sys.exit(status)
