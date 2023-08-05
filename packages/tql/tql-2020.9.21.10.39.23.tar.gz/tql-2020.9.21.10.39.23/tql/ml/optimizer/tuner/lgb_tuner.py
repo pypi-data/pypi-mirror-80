d245c39f41cc241da97c99465fb4baa6cfbe04e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:13 +0000

    Add package tql/2020.9.21.10.39.23/tql-2020.9.21.10.39.23.tar.gz

---
+++ packages/tql/tql-2020.9.21.10.39.23.tar.gz/tql-2020.9.21.10.39.23/tql/ml/optimizer/tuner/lgb_tuner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : tql-Python.
+# @File         : lgb_tuner
+# @Time         : 2020/9/21 12:39 上午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : https://github.com/optuna/optuna/blob/master/examples/lightgbm_simple.py
+
+
