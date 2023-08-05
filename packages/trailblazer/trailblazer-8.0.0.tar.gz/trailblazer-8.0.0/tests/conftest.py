4cb697335cb44ed9ba20fc4e334f4c92545c71c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:35 +0000

    Add package trailblazer/8.0.0/trailblazer-8.0.0.tar.gz

---
+++ packages/trailblazer/trailblazer-8.0.0.tar.gz/trailblazer-8.0.0/tests/conftest.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from functools import partial
+
+from click.testing import CliRunner
+import ruamel.yaml
+
+from trailblazer.cli import base
+from trailblazer.mip import sacct, files as files_api
+from trailblazer.log import LogAnalysis
+from trailblazer.store import Store
+
+
+@pytest.fixture
+def cli_runner():
+    runner = CliRunner()
+    return runner
+
+
+@pytest.fixture
+def invoke_cli(cli_runner):
+    return partial(cli_runner.invoke, base)
+
+
+@pytest.fixture(scope="session")
+def files():
+    return {
+        "config": "tests/fixtures/case/case_config.yaml",
+        "sampleinfo": "tests/fixtures/case/case_qc_sample_info.yaml",
+        "qcmetrics": "tests/fixtures/case/case_qc_metrics.yaml",
+        "sacct": "tests/fixtures/case/mip_2019-07-04T10:47:15.log.status",
+    }
+
+
+@pytest.fixture(scope="session")
+def files_raw(files):
+    return {
+        "config": ruamel.yaml.safe_load(open(files["config"])),
+        "sampleinfo": ruamel.yaml.safe_load(open(files["sampleinfo"])),
+        "qcmetrics": ruamel.yaml.safe_load(open(files["qcmetrics"])),
+        "sacct": list(open(files["sacct"])),
+    }
+
+
+@pytest.fixture(scope="session")
+def files_data(files_raw):
+    return {
+        "config": files_api.parse_config(files_raw["config"]),
+        "sampleinfo": files_api.parse_sampleinfo(files_raw["sampleinfo"]),
+        "qcmetrics": files_api.parse_qcmetrics(files_raw["qcmetrics"]),
+        "sacct": sacct.parse_sacct(files_raw["sacct"]),
+    }
+
+
+@pytest.fixture(scope="session")
+def allfailed_sacct_jobs():
+    with open("tests/fixtures/sacct/allfailed.log.status") as stream:
+        sacct_jobs = sacct.parse_sacct(stream)
+    return sacct_jobs
+
+
+@pytest.yield_fixture(scope="function")
+def store(tmpdir):
+    _store = Store(uri="sqlite://", families_dir=tmpdir)
+    _store.setup()
+    yield _store
+    _store.drop_all()
+
+
+@pytest.yield_fixture(scope="function")
+def sample_store(store):
+    sample_data = ruamel.yaml.safe_load(open("tests/fixtures/sample-data.yaml"))
+    for user_data in sample_data["users"]:
+        store.add_user(user_data["name"], user_data["email"])
+    for analysis_data in sample_data["analyses"]:
+        analysis_data["user"] = store.user(analysis_data["user"])
+        failed_jobs = analysis_data.get("failed_jobs", [])
+        analysis_data["failed_jobs"] = [
+            store.Job(**job_data) for job_data in failed_jobs
+        ]
+        store.add(store.Analysis(**analysis_data))
+    store.commit()
+    store.add_pending(family="gentlebird", email="tom.cruise@magnolia.com")
+    yield store
+
+
+@pytest.fixture
+def log_analysis(store):
+    _log_analysis = LogAnalysis(store)
+    return _log_analysis
---
+++ packages/trailblazer/trailblazer-8.0.0.tar.gz/trailblazer-8.0.0/trailblazer/store/api.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+from typing import List
+import datetime as dt
+
+import alchy
+import sqlalchemy as sqa
+
+from trailblazer.constants import COMPLETED_STATUS, FAILED_STATUS, ONGOING_STATUSES
+from . import models
+
+
+class BaseHandler:
+    User = models.User
+    Analysis = models.Analysis
+    Job = models.Job
+    Info = models.Info
+
+    def setup(self):
+        self.create_all()
+        # add inital metadata record (for web interface)
+        new_info = self.Info()
+        self.add_commit(new_info)
+
+    def find_analysis(self, family, started_at, status):
+        """Find a single analysis."""
+        query = self.Analysis.query.filter_by(
+            family=family, started_at=started_at, status=status
+        )
+        return query.first()
+
+    def find_analyses_with_comment(self, comment):
+        """Find a analyses containing comment."""
+        analysis_query = self.Analysis.query
+
+        analysis_query = analysis_query.filter(
+            self.Analysis.comment.like(f"%{comment}%")
+        )
+        return analysis_query
+
+    def analyses(
+        self,
+        *,
+        family: str = None,
+        query: str = None,
+        status: str = None,
+        deleted: bool = None,
+        temp: bool = False,
+        before: dt.datetime = None,
+        is_visible: bool = None,
+    ):
+        """Fetch analyses form the database."""
+        analysis_query = self.Analysis.query
+        if family:
+            analysis_query = analysis_query.filter_by(family=family)
+        elif query:
+            analysis_query = analysis_query.filter(
+                sqa.or_(
+                    self.Analysis.family.like(f"%{query}%"),
+                    self.Analysis.status.like(f"%{query}%"),
+                )
+            )
+        if status:
+            analysis_query = analysis_query.filter_by(status=status)
+        if isinstance(deleted, bool):
+            analysis_query = analysis_query.filter_by(is_deleted=deleted)
+        if temp:
+            analysis_query = analysis_query.filter(
+                self.Analysis.status.in_(ONGOING_STATUSES)
+            )
+        if before:
+            analysis_query = analysis_query.filter(self.Analysis.started_at < before)
+        if is_visible is not None:
+            analysis_query = analysis_query.filter_by(is_visible=is_visible)
+        return analysis_query.order_by(self.Analysis.started_at.desc())
+
+    def analysis(self, analysis_id: int) -> models.Analysis:
+        """Get a single analysis."""
+        return self.Analysis.query.get(analysis_id)
+
+    def track_update(self):
+        """Update the lastest updated date in the database."""
+        metadata = self.info()
+        metadata.updated_at = dt.datetime.now()
+        self.commit()
+
+    def is_latest_analysis_ongoing(self, case_id: str) -> bool:
+        """Check if the latest analysis is ongoing for a case_id"""
+        latest_analysis = self.analyses(family=case_id).first()
+        if latest_analysis and latest_analysis.status in ONGOING_STATUSES:
+            return True
+        return False
+
+    def is_latest_analysis_failed(self, case_id: str) -> bool:
+        """Check if the latest analysis is failed for a case_id"""
+        latest_analysis = self.analyses(family=case_id).first()
+        if latest_analysis and latest_analysis.status == FAILED_STATUS:
+            return True
+        return False
+
+    def get_latest_analysis_status(self, case_id: str) -> str:
+        """Get latest analysis status for a case_id"""
+        latest_analysis = self.analyses(family=case_id).first()
+        return latest_analysis.status
+
+    def is_latest_analysis_completed(self, case_id: str) -> bool:
+        """Check if the latest analysis is completed for a case_id"""
+        latest_analysis = self.analyses(family=case_id).first()
+        if latest_analysis and latest_analysis.status == COMPLETED_STATUS:
+            return True
+        return False
+
+    def info(self) -> models.Info:
+        """Return metadata entry."""
+        return self.Info.query.first()
+
+    def add_pending(self, family: str, email: str = None) -> models.Analysis:
+        """Add pending entry for an analysis."""
+        started_at = dt.datetime.now()
+        new_log = self.Analysis(family=family, status="pending", started_at=started_at)
+        new_log.user = self.user(email) if email else None
+        self.add_commit(new_log)
+        return new_log
+
+    def add_user(self, name: str, email: str) -> models.User:
+        """Add a new user to the database."""
+        new_user = self.User(name=name, email=email)
+        self.add_commit(new_user)
+        return new_user
+
+    def user(self, email: str) -> models.User:
+        """Fetch a user from the database."""
+        return self.User.query.filter_by(email=email).first()
+
+    def aggregate_failed(self, since_when: dt.date = None) -> List:
+        """Count the number of failed jobs per category (name)."""
+
+        categories = self.session.query(
+            self.Job.name.label("name"), sqa.func.count(self.Job.id).label("count")
+        ).filter(self.Job.status != "cancelled")
+
+        if since_when:
+            categories = categories.filter(self.Job.started_at > since_when)
+
+        categories = categories.group_by(self.Job.name).all()
+
+        data = [
+            {"name": category.name, "count": category.count} for category in categories
+        ]
+        return data
+
+    def jobs(self):
+        """Return all jobs in the database."""
+        return self.Job.query
+
+
+class Store(alchy.Manager, BaseHandler):
+    def __init__(self, uri: str, families_dir: str):
+        super(Store, self).__init__(
+            config=dict(SQLALCHEMY_DATABASE_URI=uri), Model=models.Model
+        )
+        self.families_dir = families_dir
