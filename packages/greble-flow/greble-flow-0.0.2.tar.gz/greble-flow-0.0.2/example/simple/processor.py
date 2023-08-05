26c79e945b4232fd9a99069909d22c8222c8283f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:07 +0000

    Add package greble-flow/0.0.2/greble-flow-0.0.2.tar.gz

---
+++ packages/greble-flow/greble-flow-0.0.2.tar.gz/greble-flow-0.0.2/example/simple/processor.py
@@ -0,0 +1,8 @@
+from greble_flow.processors.base import BaseFlowProcessor
+
+
+class SimpleProcessor(BaseFlowProcessor):
+    name = "simple-flow-processor"
+
+    def execute(self, *args, **kwargs):
+        print("I'am executed")
