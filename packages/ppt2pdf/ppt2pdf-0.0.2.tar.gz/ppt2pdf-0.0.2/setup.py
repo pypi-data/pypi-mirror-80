75c41fde841c661ee51f3c649374a6a3215536cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:46 +0000

    Add package ppt2pdf/0.0.2/ppt2pdf-0.0.2.tar.gz

---
+++ packages/ppt2pdf/ppt2pdf-0.0.2.tar.gz/ppt2pdf-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ppt2pdf", # Replace with your own username
+    version="0.0.2",
+    author="Swarag Narayanasetty",
+    description="Converts PPT to PDF",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Swarag-N/powerpoint-to-pdf",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['ppt2pdf'],
+    install_requires=[
+        'click',
+        'comtypes'
+    ],
+    entry_points={
+        'console_scripts':[
+            'ppt2pdf=ppt2pdf.scripts.cli:cli'
+        ]
+    }
+)
