8681e528d0017fb5fc5349258e1c7cd47732675a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:02 +0000

    Add package bashrc/0.1.15/bashrc-0.1.15-py3-none-any.whl

---
+++ packages/bashrc/bashrc-0.1.15-py3-none-any.whl/bashrc/scripts/bashrc-files.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+export source="${BASH_SOURCE[0]}"; debug.sh source
+
+test -n "${PASSWD_PATH}" || { error.bash PASSWD_PATH 'not defined'; return 1; }
+
+
+if ! grep "${BASHRC_FILE}" ~/.bashrc; then
+  echo "source \"$( command -v "${}" )\"" >> ~/.bashrc
+fi
+
+unset source
