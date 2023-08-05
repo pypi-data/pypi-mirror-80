b475409d769af8df65246d986d065e66da6695c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package vishad-distributions/0.1/vishad_distributions-0.1.tar.gz

---
+++ packages/vishad-distributions/vishad_distributions-0.1.tar.gz/vishad_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='vishad_distributions',
+      version='0.1',
+      description='Test your Gaussian distributions',
+      packages=['vishad_distributions'],
+      zip_safe=False)
