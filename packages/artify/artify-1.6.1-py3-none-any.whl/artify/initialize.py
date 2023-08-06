0574b6ab8f11d3f344c0ab886c06bcb4485db287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:30 +0000

    Add package artify/1.6.1/artify-1.6.1-py3-none-any.whl

---
+++ packages/artify/artify-1.6.1-py3-none-any.whl/artify/initialize.py
@@ -0,0 +1,6 @@
+# To-do Generate project key for sonarQube using API
+#  curl -u admin:admin -X POST 
+# 'http://localhost:9000/sonar/api/projects/
+# create?key=myKey&name=test-project-latest&project=test-project-latest'
+
+# To-do Generate UID for key in parameter above
