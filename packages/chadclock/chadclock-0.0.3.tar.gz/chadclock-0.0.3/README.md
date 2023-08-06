ca2bf4cf230df50bf8a9b5fbeb23beb604b68991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:55:26 +0000

    Add package chadclock/0.0.3/chadclock-0.0.3.tar.gz

---
+++ packages/chadclock/chadclock-0.0.3.tar.gz/chadclock-0.0.3/README.md
@@ -0,0 +1,1 @@
+<h1>Only Chads can use this</h1>
---
+++ packages/chadclock/chadclock-0.0.3.tar.gz/chadclock-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='chadclock',
+    version='0.0.3',
+    description='chad clock',
+    long_description=open('README.md').read() + '\n\n' +
+    open('CHANGELOG.txt').read(),
+    url='',
+    author='Andrew',
+    author_email='achen08@email.wm.edu',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='chad',
+    packages=find_packages(),
+    install_requires=["matplotlib>=2.2.0"]
+)
