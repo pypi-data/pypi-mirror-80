6dff267f9afb7bd6530312b74e1377fac56c8351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:02 +0000

    Add package MicroscPSF-Py/0.2/MicroscPSF-Py-0.2.tar.gz

---
+++ packages/MicroscPSF-Py/MicroscPSF-Py-0.2.tar.gz/MicroscPSF-Py-0.2/README.md
@@ -0,0 +1,33 @@
+## MicroscPSF-Py ##
+
+This is a Python implementation of the fast microscope PSF generation tool (using the Gibson-Lanni model).
+Technical details can be found [here](http://www.ee.cuhk.edu.hk/~jzli/MicroscPSF/).
+
+[![Linux Build Status](https://travis-ci.org/MicroscPSF/MicroscPSF-Py.svg?branch=master)](https://travis-ci.org/MicroscPSF/MicroscPSF-Py)
+[![PyPI version](https://badge.fury.io/py/MicroscPSF-Py.svg)](https://badge.fury.io/py/MicroscPSF-Py)
+
+### Install ###
+
+#### PyPI ####
+
+```
+$ python -m pip install MicroscPSF-Py
+````
+
+#### Source ####
+
+```
+$ git clone https://github.com/MicroscPSF/MicroscPSF-Py.git
+$ cd MicroscPSF-Py
+$ python setup.py install
+```
+
+### Usage ###
+
+Please see the [examples.ipynb](https://github.com/MicroscPSF/MicroscPSF-Py/blob/master/examples.ipynb) Jupyter notebook.
+
+### Acknowledgements ###
+
+- Original algorithm Li, J., Xue, F., & Blu, T. (2017). Fast and accurate three-dimensional point spread function computation for fluorescence microscopy. JOSA A, 34(6), 1029-1034. [link](https://doi.org/10.1364/JOSAA.34.001029).
+
+- Python implementation by Kyle Douglass and Hazen Babcock.
---
+++ packages/MicroscPSF-Py/MicroscPSF-Py-0.2.tar.gz/MicroscPSF-Py-0.2/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+from setuptools import setup, find_packages
+
+# Get the long description from the relevant file
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='MicroscPSF-Py',
+    version=0.2,
+    description='Gibson-Lanni PSF calculation code.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Kyle Douglass, Hazen Babcock',
+    author_email='hbabcock@mac.com',
+    url='https://github.com/MicroscPSF/MicroscPSF-Py',
+
+    zip_safe=False,
+    packages=find_packages(),
+
+    package_data={},
+    exclude_package_data={},
+    include_package_data=True,
+
+    requires=[],
+
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+
+    license='MIT',
+    keywords='PSF,microscopy',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: C",
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',        
+    ],
+)
