e6dc982fa17feebb6250fa5d8dba447d4de9b447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:58 +0000

    Add package pytest-warnings/0.3.1/pytest-warnings-0.3.1.tar.gz

---
+++ packages/pytest-warnings/pytest-warnings-0.3.1.tar.gz/pytest-warnings-0.3.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name="pytest-warnings",
+    description='pytest plugin to list Python warnings in pytest report',
+    long_description=open("README.rst").read(),
+    license="MIT license",
+    version='0.3.1',
+    author='Florian Schulze',
+    author_email='florian.schulze@gmx.net',
+    url='https://github.com/fschulze/pytest-warnings',
+    packages=['pytest_warnings'],
+    entry_points={'pytest11': ['pytest_warnings = pytest_warnings']},
+    install_requires=['pytest'],
+    classifiers=[
+        "Framework :: Pytest",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Development Status :: 7 - Inactive",
+        ],
+)
