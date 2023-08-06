524a49e3dc0e687f381b374a94b1101fdf12997c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:55:27 +0000

    Add package conjugar/0.68/conjugar-0.68.tar.gz

---
+++ packages/conjugar/conjugar-0.68.tar.gz/conjugar-0.68/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+setup(
+      name="conjugar",
+      version = "0.68",
+      description="handle,.in progressing..,APIs",
+      author="dapeli",
+      url="https://github.com/ihgazni2/conjugar",
+      author_email='terryinzaghi@163.com', 
+      license="MIT",
+      long_description = "refer to .md files in https://github.com/ihgazni2/conjugar",
+      entry_points = {
+         'console_scripts': [
+             'essila=conjugar.bin.es_sila:main',
+             'esconju=conjugar.bin.es_word_conjugar:main',
+             'esverb=conjugar.bin.es_verb_list:main'
+          ]
+      },
+      package_data = {
+          'resources':["conjugar/data/*"]  #necessary for resources   ,must be used with MANIFEST.in
+      },
+      include_package_data=True, #necessary for resources   ,must be used with MANIFEST.in
+      classifiers=[
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Programming Language :: Python',
+          ],
+      packages= find_packages(),
+      py_modules=['conjugar'], 
+      )
+
+
+# python3 setup.py bdist --formats=tar
+# python3 setup.py sdist
+
