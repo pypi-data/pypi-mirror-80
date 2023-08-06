f2513d60efc0483779b495dde96ec705b4802c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:03 +0000

    Add package apkutils/0.8.1/apkutils-0.8.1.tar.gz

---
+++ packages/apkutils/apkutils-0.8.1.tar.gz/apkutils-0.8.1/setup.py
@@ -0,0 +1,44 @@
+import os.path
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="apkutils",
+
+    version='0.8.1',
+
+    description=("Utils for parsing apk."),
+    long_description=read('README.rst'),
+
+    url="https://github.com/mikusjelly/apkutils",
+
+    author="mikusjelly",
+    author_email="mikusjelly@gmail.com",
+
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+
+    keywords="apk dex axml",
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=[
+        "anytree",
+        "cigam",
+        "pyelftools",
+        "pyopenssl",
+        "TextWizard",
+        "xmltodict",
+        "beautifulsoup4",
+    ],
+)
