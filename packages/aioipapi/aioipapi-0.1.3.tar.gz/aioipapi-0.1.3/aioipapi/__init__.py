b2b8e0178c13d21205c9e37ff2c0dadd7ec8a0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:50 +0000

    Add package aioipapi/0.1.3/aioipapi-0.1.3.tar.gz

---
+++ packages/aioipapi/aioipapi-0.1.3.tar.gz/aioipapi-0.1.3/aioipapi/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from importlib_metadata import version, PackageNotFoundError
+
+from aioipapi import _logging  # noqa
+from aioipapi._config import Config, config
+from aioipapi._constants import FIELDS, LANGS
+from aioipapi._client import IpApiClient, location, location_stream
+from aioipapi._exceptions import IpApiError, ClientError, HttpError, TooManyRequests, TooLargeBatchSize, AuthError
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = '0.0.0.dev'
+
+__all__ = [
+    '__version__',
+    'Config',
+    'config',
+    'FIELDS',
+    'LANGS',
+    'IpApiClient',
+    'IpApiError',
+    'ClientError',
+    'HttpError',
+    'TooManyRequests',
+    'TooLargeBatchSize',
+    'AuthError',
+    'location',
+    'location_stream',
+]
