d75a9b4e11989ae1b6381c45332d6fac7b0dc541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:41 +0000

    Add package arweave-python-client/1.0.14/arweave-python-client-1.0.14.tar.gz

---
+++ packages/arweave-python-client/arweave-python-client-1.0.14.tar.gz/arweave-python-client-1.0.14/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+  name="arweave-python-client",
+  packages = ['arweave'], # this must be the same as the name above
+  version="1.0.14",
+  description="Client interface for sending transactions on the Arweave permaweb",
+  author="Mike Hibbert",
+  author_email="mike@hibbertitsolutions.co.uk",
+  url="https://github.com/MikeHibbert/arweave-python-client",
+  download_url="https://github.com/MikeHibbert/arweave-python-client",
+  keywords=['arweave', 'crypto'],
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  install_requires=[
+    'arrow',
+    'python-jose',
+    'pynacl',
+    'pycryptodome',
+    'cryptography',
+    'requests',
+    'psutil'
+  ],
+)
