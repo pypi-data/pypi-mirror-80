66716f6425b99e7c278c929eb165cee62c2c8a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:12 +0000

    Add package pynonymizer/1.11.2/pynonymizer-1.11.2.tar.gz

---
+++ packages/pynonymizer/pynonymizer-1.11.2.tar.gz/pynonymizer-1.11.2/pynonymizer/database/basic/output.py
@@ -0,0 +1,37 @@
+import gzip
+import os
+"""
+Streaming output files for streamable providers
+"""
+
+
+class UnknownOutputTypeError(Exception):
+    def __init__(self, filename):
+        super().__init__("Unable to detect output type for file: {}".format(filename))
+
+
+class GzipOutput:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def open(self):
+        return gzip.open(self.filename, "wb")
+
+
+class RawOutput:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def open(self):
+        return open(self.filename, "wb")
+
+
+def resolve_output(filename):
+    name, ext = os.path.splitext(filename)
+
+    if ext == ".sql":
+        return RawOutput(filename)
+    elif ext == ".gz":
+        return GzipOutput(filename)
+    else:
+        raise UnknownOutputTypeError(filename)
