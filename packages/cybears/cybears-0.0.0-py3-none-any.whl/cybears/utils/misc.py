5e6dd501ce67b34f64ac6775091dbf0cf49ddd98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:40 +0000

    Add package cybears/0.0.0/cybears-0.0.0-py3-none-any.whl

---
+++ packages/cybears/cybears-0.0.0-py3-none-any.whl/cybears/utils/misc.py
@@ -0,0 +1,46 @@
+"""
+Misc utilities like downloading, uploading, whatever.
+"""
+import logging
+import os
+import requests
+
+LOGGER = logging.getLogger(__name__)
+
+
+def download_file(url, overwrite=False, new_name="", dir_path=""):
+    """
+    Download a file
+
+    :param str url: the file url to download
+    :param bool overwrite: overwrite an existing file
+    :param str dir_path: path where to save the file
+    :return: downloaded filename
+    """
+    if new_name:
+        file_name = new_name
+    else:
+        file_name = url.split("/")[-1]
+
+    if dir_path:
+        try:
+            os.makedirs(dir_path)
+        except FileExistsError:
+            pass
+
+    file_path = os.path.join(dir_path, file_name)
+
+    # Do not overwrite if told
+    if os.path.isfile(file_path) and not overwrite:
+        LOGGER.info('File %s already exists. Skipping.', file_path)
+        return file_path
+
+    with requests.get(url, stream=True) as request:
+        LOGGER.info('Downloading: %s...', url)
+        request.raise_for_status()
+        with open(file_path, "wb") as file:
+            for chunk in request.iter_content(chunk_size=8192):
+                file.write(chunk)
+        LOGGER.info('File downloaded to %s', file_path)
+
+    return file_path
