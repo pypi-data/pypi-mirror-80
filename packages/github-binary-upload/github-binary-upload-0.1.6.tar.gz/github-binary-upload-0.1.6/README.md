f36e11ad3f3e308349883aecd409205a0755ac6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:22 +0000

    Add package github-binary-upload/0.1.6/github-binary-upload-0.1.6.tar.gz

---
+++ packages/github-binary-upload/github-binary-upload-0.1.6.tar.gz/github-binary-upload-0.1.6/README.md
@@ -0,0 +1,79 @@
+# GitHub Binary Upload
+
+## Introduction
+
+`github-binary-upload` is a script for creating GitHub releases from Git tags with attached assets.
+
+## Installation
+
+- From PyPI:
+  ```bash
+  python3 -m pip install github-binary-upload
+  ```
+- Self-contained executables for Linux x86_64 with glibc >= 2.17 (any recent Linux distribution) and macOS Catalina and
+  newer are available on the [releases page](https://github.com/sciapp/github-binary-upload/releases/latest).
+- AUR package for Arch Linux users:
+  [python-github-binary-upload](https://aur.archlinux.org/packages/python-github-binary-upload/)
+
+## Usage
+
+After installation you can run `github-binary-upload`:
+
+```
+usage: github-binary-upload [-h] [-g GITHUB_SERVER] [-c CREDENTIALS_FILE] [-l]
+                            [-n] [-u USERNAME] [-V]
+                            [project] [tag] [assets [assets ...]]
+
+github-binary-upload is a utility for publishing releases from tags with attached files on GitHub.
+
+positional arguments:
+  project               GitHub project in the format "<user>/<project name>"
+  tag                   tag that will be published as a release, ignored if '
+                        --latest' is given
+  assets                files that will be attached to the release
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -g GITHUB_SERVER, --github-server GITHUB_SERVER
+                        GitHub server hostname (default: github.com)
+  -c CREDENTIALS_FILE, --credentials-file CREDENTIALS_FILE
+                        path to a file containing username and password/access
+                        token (on two separate lines, default: ~/.github-
+                        binary-uploadrc)
+  -l, --latest          get the latest tag from the GitHub API
+  -n, --dry-run         only print which releases would be published
+  -u USERNAME, --user USERNAME
+                        user account for querying the GitHub API; the password
+                        is read from stdin
+  -V, --version         print the version number and exit
+```
+
+### Example
+
+Run
+
+```bash
+github-binary-upload -u ExampleUser -l ExampleUser/ExampleProject MyFirstAsset.zip MySecondAsset.whl
+```
+
+to create a release from the latest tag in the GitHub project `ExampleUser/ExampleProject`. The files `MyFirstAsset.zip`
+and `MySecondAsset.whl` will be attached as downloadable files.
+
+`github-binary-upload` can be called multiple times on the same tag. The release will be recreated each time. This is
+especially useful to CI pipelines which can run more than once.
+
+### Python API
+
+`github-binary-upload` defines a function `publish_release_from_tag` which can be called from Python code:
+
+```python
+from github_binary_upload import publish_release_from_tag
+
+
+publish_release_from_tag(
+    project, tag, assets, github_server, username, password, dry_run
+)
+```
+
+If `tag` is `None`, the latest tag will be converted to a GitHub release. `dry_run` is an optional parameter which
+defaults to `False`.
---
+++ packages/github-binary-upload/github-binary-upload-0.1.6.tar.gz/github-binary-upload-0.1.6/setup.py
@@ -0,0 +1,98 @@
+import os
+import runpy
+import subprocess
+from setuptools import setup
+from distutils.cmd import Command
+from tempfile import TemporaryDirectory
+
+
+class PyinstallerCommand(Command):
+    description = "create a self-contained executable with PyInstaller"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        with TemporaryDirectory() as temp_dir:
+            subprocess.check_call(["python3", "-m", "venv", os.path.join(temp_dir, "env")])
+            subprocess.check_call([os.path.join(temp_dir, "env/bin/pip"), "install", "."])
+            subprocess.check_call([os.path.join(temp_dir, "env/bin/pip"), "install", "pyinstaller"])
+            with open(os.path.join(temp_dir, "entrypoint.py"), "w") as f:
+                f.write(
+                    """
+#!/usr/bin/env python3
+
+from github_binary_upload import main
+
+
+if __name__ == "__main__":
+    main()
+                    """.strip()
+                )
+            subprocess.check_call(
+                [
+                    os.path.join(temp_dir, "env/bin/pyinstaller"),
+                    "--clean",
+                    "--name=github-binary-upload",
+                    "--onefile",
+                    "--strip",
+                    os.path.join(temp_dir, "entrypoint.py"),
+                ]
+            )
+
+
+def get_version_from_pyfile(version_file="github_binary_upload.py"):
+    file_globals = runpy.run_path(version_file)
+    return file_globals["__version__"]
+
+
+def get_long_description_from_readme(readme_filename="README.md"):
+    long_description = None
+    if os.path.isfile(readme_filename):
+        with open(readme_filename, "r", encoding="utf-8") as readme_file:
+            long_description = readme_file.read()
+    return long_description
+
+
+version = get_version_from_pyfile()
+long_description = get_long_description_from_readme()
+
+setup(
+    name="github-binary-upload",
+    version=version,
+    py_modules=["github_binary_upload"],
+    python_requires="~=3.3",
+    install_requires=["requests", "yacl"],
+    entry_points={"console_scripts": ["github-binary-upload = github_binary_upload:main"]},
+    cmdclass={"bdist_pyinstaller": PyinstallerCommand},
+    author="Ingo Meyer",
+    author_email="i.meyer@fz-juelich.de",
+    description="github-binary-upload is a utility for publishing releases from tags with attached files on GitHub.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/sciapp/github-binary-upload",
+    keywords=["git", "GitHub", "release", "assets"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Version Control :: Git",
+        "Topic :: Utilities",
+    ],
+)
