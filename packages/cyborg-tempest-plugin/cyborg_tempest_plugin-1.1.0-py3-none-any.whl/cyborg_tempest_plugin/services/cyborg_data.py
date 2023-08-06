1e14b5109bd6b2beba1b62c3fe72477a134548b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:21 +0000

    Add package cyborg-tempest-plugin/1.1.0/cyborg_tempest_plugin-1.1.0-py3-none-any.whl

---
+++ packages/cyborg-tempest-plugin/cyborg_tempest_plugin-1.1.0-py3-none-any.whl/cyborg_tempest_plugin/services/cyborg_data.py
@@ -0,0 +1,8 @@
+NORMAL_DEVICE_PROFILE_DATA1 = [{
+    "name": "fpga-num-1-dp1",
+    "groups": [
+        {
+            "resources:FPGA": "1",
+            "trait:CUSTOM_FAKE_DEVICE": "required"
+        }]
+    }]
