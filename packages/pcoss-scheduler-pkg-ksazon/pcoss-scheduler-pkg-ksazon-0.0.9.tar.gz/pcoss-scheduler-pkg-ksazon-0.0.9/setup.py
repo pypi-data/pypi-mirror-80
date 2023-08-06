9688d5c2d0819cc69e335f050949b7217a925def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:27 +0000

    Add package pcoss-scheduler-pkg-ksazon/0.0.9/pcoss-scheduler-pkg-ksazon-0.0.9.tar.gz

---
+++ packages/pcoss-scheduler-pkg-ksazon/pcoss-scheduler-pkg-ksazon-0.0.9.tar.gz/pcoss-scheduler-pkg-ksazon-0.0.9/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pcoss-scheduler-pkg-ksazon",
+    version="0.0.9",
+    author="Krzysztof Sazon",
+    author_email="krzysztof.sazon@gmail.com",
+    description="Tabular data PCOSS calculations module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ksazon/mgr/mgr_client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'networkx',
+        'pandas',
+        'numpy',
+        'matplotlib>=3',
+        'aiohttp>=3.6',
+        'requests',
+        'urllib3',
+        'toml',
+        'plotly'
+   ]
+)
