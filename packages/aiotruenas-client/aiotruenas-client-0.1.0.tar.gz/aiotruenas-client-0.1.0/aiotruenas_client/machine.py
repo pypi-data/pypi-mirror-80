040032938fc9b5473befa7e75cddc8c79c3c52ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:46 +0000

    Add package aiotruenas-client/0.1.0/aiotruenas-client-0.1.0.tar.gz

---
+++ packages/aiotruenas-client/aiotruenas-client-0.1.0.tar.gz/aiotruenas-client-0.1.0/aiotruenas_client/machine.py
@@ -0,0 +1,24 @@
+from abc import abstractmethod, ABC
+from typing import List
+from .disk import Disk
+from .pool import Pool
+from .virtualmachine import VirtualMachine
+
+
+class Machine(ABC):
+    @staticmethod
+    @abstractmethod
+    async def create():
+        """Create and validate authentication with the concrete implementation of this class."""
+
+    @abstractmethod
+    async def get_disks(self) -> List[Disk]:
+        """Get the disks on the remote machine."""
+
+    @abstractmethod
+    async def get_pools(self) -> List[Pool]:
+        """Get the pools on the remote machine."""
+
+    @abstractmethod
+    async def get_vms(self) -> List[VirtualMachine]:
+        """Get the disks on the remote machine."""
