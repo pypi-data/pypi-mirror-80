c88685db97cd78afb8915cbdf2d7158c0ad91476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:29 +0000

    Add package stats-dist-prob-gb/0.1/stats_dist_prob_gb-0.1.tar.gz

---
+++ packages/stats-dist-prob-gb/stats_dist_prob_gb-0.1.tar.gz/stats_dist_prob_gb-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stats_dist_prob_gb',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['stats_dist_prob_gb'],
+      author='Erin Gill',
+      author_email='erin.gill81@gmail.com',
+      zip_safe=False)
