dddfb4d1f393502fc327cebd5284a0304208bf77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:49 +0000

    Add package pyyeti/0.98.7/pyyeti-0.98.7.tar.gz

---
+++ packages/pyyeti/pyyeti-0.98.7.tar.gz/pyyeti-0.98.7/pyyeti/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+pyYeti
+======
+
+pyYeti has tools mostly related to structural dynamics:
+
+    * Solve matrix equations of motion in the time and
+      frequency domains
+    * Shock response spectrum (SRS)
+    * Fatigue damage equivalent power spectral densities (PSD)
+    * Force limiting analysis tools
+    * Hurty-Craig-Bampton model checks
+    * Coupled loads analysis tools
+    * Statistics tools for computing k-factors (for tolerance
+      bounds and intervals) and for order statistics
+    * Read/write Nastran output4 (.op4) files
+    * Limited capability to read Nastran output2 (.op2) files
+    * A data-cursor for interacting with 2D x-y plots
+    * Rainflow cycle counting
+    * Resample data with the Lanczos method
+    * A "vectorized" writing module
+    * Eigensolution with the subspace iteration method
+    * Tools for working with the "nas2cam" Nastran DMAP
+    * Other miscellaneous tools
+
+"""
+
+__version__ = "0.98.7"
