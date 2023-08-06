7880ccbfed20c998a14cadeb99d7875d03f8c83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:27 +0000

    Add package django-privateurl/1.4.0/django-privateurl-1.4.0.tar.gz

---
+++ packages/django-privateurl/django-privateurl-1.4.0.tar.gz/django-privateurl-1.4.0/CHANGELOG.md
@@ -0,0 +1,39 @@
+1.4.0 (2020-09-23)
+==================
+  * Added supporting Django v3.1
+
+
+1.3.0 (2020-02-15)
+==================
+  * Added supporting Django v3.0
+
+
+1.2.0 (2019-04-06)
+==================
+  * Added supporting Django v2.2
+
+
+1.1.1 (2018-08-07)
+==================
+  * Added supporting Django v2.1
+
+
+1.1.0 (2018-06-04)
+==================
+  * Add support Django 2.0
+
+
+1.0.1 (2017-08-23)
+==================
+  * Migrations fix
+
+
+1.0.0 (2017-05-02)
+==================
+  * Add support Django 1.11
+  * Add support Python 3.6
+
+
+0.0.1 (2016-10-23)
+==================
+  * First release
---
+++ packages/django-privateurl/django-privateurl-1.4.0.tar.gz/django-privateurl-1.4.0/setup.py
@@ -0,0 +1,43 @@
+import os
+
+from setuptools import setup, find_packages
+
+import privateurl
+
+setup(
+    name='django-privateurl',
+    version=privateurl.__version__,
+    description='Django Private URL',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    license='MIT',
+    author='Igor Melnyk @liminspace',
+    author_email='liminspace@gmail.com',
+    url='https://github.com/liminspace/django-privateurl',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    include_package_data=True,
+    zip_safe=False,  # because include static
+    install_requires=[
+        'django>=1.8,<3.2',
+    ],
+    keywords=[
+        'django', 'url', 'private', 'private url', 'django-privateurl',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
