5e786518692f9c9577682a08d0fbf8ca69c44990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:37 +0000

    Add package babblebox/0.0.9/babblebox-0.0.9.tar.gz

---
+++ packages/babblebox/babblebox-0.0.9.tar.gz/babblebox-0.0.9/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="babblebox",
+    version="0.0.9",
+    author="Mr.S Mechatronics",
+    author_email="mr.smechatronics@gmail.com",
+    description="An Ai Driven ChatBot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MrSMechatronics/ChatBox",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'nltk',
+        'numpy',
+        'tflearn',
+        'tensorflow'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='<3.8',
+)
