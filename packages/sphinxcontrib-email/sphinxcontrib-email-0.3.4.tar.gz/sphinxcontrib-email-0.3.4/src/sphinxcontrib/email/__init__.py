71210962f9b0dd79b0813206144016dcf1a25f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:46 +0000

    Add package sphinxcontrib-email/0.3.4/sphinxcontrib-email-0.3.4.tar.gz

---
+++ packages/sphinxcontrib-email/sphinxcontrib-email-0.3.4.tar.gz/sphinxcontrib-email-0.3.4/src/sphinxcontrib/email/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from typing import Any
+
+import sphinx.util
+from sphinx.application import Sphinx
+
+from .handlers import html_page_context_handler
+from .roles import EmailRole
+
+try:
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata
+
+try:
+    __version__ = ".".join(metadata.version("sphinxcontrib-email").split(".")[:3])
+except metadata.PackageNotFoundError:
+    pass
+
+logger = sphinx.util.logging.getLogger(__name__)
+
+
+def setup(app: Sphinx) -> dict[str, Any]:
+    app.add_config_value(name="email_automode", default=False, rebuild="env")
+    app.connect("html-page-context", html_page_context_handler)
+    app.add_role("email", EmailRole())
+    return {"version": __version__}
