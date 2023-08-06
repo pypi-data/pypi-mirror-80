b878846c43e3f3a57ef3605ac933084517406310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:04 +0000

    Add package jict/2.6/jict-2.6.tar.gz

---
+++ packages/jict/jict-2.6.tar.gz/jict-2.6/jict/helpers.py
@@ -0,0 +1,50 @@
+from time import time
+from threading import Thread
+import os
+
+def evaluate(foo, itter=1 , threaded = False ):
+
+    if threaded == True:
+        def threadedfoo():
+            Thread(target = lambda: evaluate(foo,itter) ).start()
+
+        threadedfoo()
+        return
+
+    t0 = time()
+    for _ in range(itter):
+        foo()
+
+    print( time() - t0, "seconds wall time" )
+
+def sqlconnect(str):
+    if 'mysql-connector' in nolibs:
+        raise Exception('strore sql rquieres \'mysql-connector\' module')
+
+    user,pawd,host,database = re.findall(
+        "(.*):(.*)@([0-9]{0,3}.[0-9]{0,3}.[0-9]{0,3}.[0-9]{0,3}):(.*)"
+    , str )[0]
+    cnt = mysql.connector.connect(
+        host = host,
+        database = database,
+        user = user,
+        password = pawd
+    )
+    return cnt
+
+
+def file( *args, **kwargs ):
+    dirname = os.path.dirname(filestr)
+
+    if os.path.isfile( filestr ):
+        return open( *args, **kwargs )
+
+    else:
+        if not os.path.exists( dirname ):
+            os.makedirs( dirname )
+
+        f = open( *args, **kwargs )
+        f.write("")
+        f.close()
+
+    return open( *args, **kwargs )
