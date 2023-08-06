07aed573abc179cd54e6f4c01299dcd6e9074f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:45 +0000

    Add package scvi-tools/0.7.0a6/scvi_tools-0.7.0a6-py3-none-any.whl

---
+++ packages/scvi-tools/scvi_tools-0.7.0a6-py3-none-any.whl/scvi/model/__init__.py
@@ -0,0 +1,8 @@
+from .scvi import SCVI
+from .totalvi import TOTALVI
+from .linear_scvi import LinearSCVI
+from .autozi import AUTOZI
+from .scanvi import SCANVI
+from .gimvi import GIMVI
+
+__all__ = ["SCVI", "TOTALVI", "LinearSCVI", "AUTOZI", "SCANVI", "GIMVI"]
