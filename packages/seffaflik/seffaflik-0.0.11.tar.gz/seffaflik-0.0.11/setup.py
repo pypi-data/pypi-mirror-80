973ed5038b11e258cc5c6d4dee24260abdadf220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:33 +0000

    Add package seffaflik/0.0.11/seffaflik-0.0.11.tar.gz

---
+++ packages/seffaflik/seffaflik-0.0.11.tar.gz/seffaflik-0.0.11/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import seffaflik
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="seffaflik",
+    version=seffaflik.__version__,
+    author='Dr. Nuri Şensoy',
+    author_email='nuri.sensoy@epias.com.tr',
+    description="EPİAŞ tarafından Şeffaflık Platformunda yayımlanmakta olan verileri çekmek için tasarlanmış Python "
+                "kütüphanesi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['seffaflik', 'transparency', 'Turkish Electricity Market'],
+    url='https://github.com/nurisensoy/seffaflik',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+        'python-dateutil',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
