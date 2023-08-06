db31793e217149cdf5f8652f245a1b09b0d66ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:06 +0000

    Add package pndex/0.1.2/pndex-0.1.2.tar.gz

---
+++ packages/pndex/pndex-0.1.2.tar.gz/pndex-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="pndex",
+    version="0.1.2",
+    description="A Python package to manage all python files in a folder at once in console",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/adityadand/pndex",
+    author="Aditya Dand",
+    author_email="dandaditya@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["p_ndex"],
+    include_package_data=True,
+    install_requires=['pyfiglet','importlib','pylint'],
+    entry_points={
+        "console_scripts": [
+            "pndex=p_nder.pndex:main",
+        ]
+    },
+)
