23e4ae0df7ecc33608391d295b85eccb32e66567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:20 +0000

    Add package pframe/20200929.1/pframe-20200929.1.tar.gz

---
+++ packages/pframe/pframe-20200929.1.tar.gz/pframe-20200929.1/pframe/version.py
@@ -0,0 +1,15 @@
+# !/usr/local/python/bin/python
+# -*- coding: utf-8 -*-
+# (C) Wu Dong, 2019
+# All rights reserved
+# @Author: 'Wu Dong <wudong@eastwu.cn>'
+# @Time: '2020-03-10 09:49'
+
+# ChangeLog:
+# V20200929.1
+# - Add date operate for near
+#
+# V20200310.1
+# - Init Version
+
+version = "20200929.1"
