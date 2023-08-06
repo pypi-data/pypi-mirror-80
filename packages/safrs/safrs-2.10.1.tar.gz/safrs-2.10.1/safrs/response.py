0cf4448fc93e6d21a95312c77dfdae211df5b522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:19 +0000

    Add package safrs/2.10.1/safrs-2.10.1.tar.gz

---
+++ packages/safrs/safrs-2.10.1.tar.gz/safrs-2.10.1/safrs/response.py
@@ -0,0 +1,15 @@
+# Response class
+from flask import Response
+
+
+class SAFRSResponse(Response):
+    """
+        Response class
+    """
+
+    safrs_headers = {"Content-Type": "application/vnd.api+json"}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.headers.update(self.safrs_headers)
