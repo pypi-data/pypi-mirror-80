1ecce162b760d971b9e23abfaf714b65d7fdc1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:39 +0000

    Add package swolfpy-processmodels/0.1.3/swolfpy_processmodels-0.1.3-py3-none-any.whl

---
+++ packages/swolfpy-processmodels/swolfpy_processmodels-0.1.3-py3-none-any.whl/swolfpy_processmodels/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Mojtaba Sardarmehni
+
+Life_cycle process models for the swolfpy
+"""
+
+__all__ = [
+    'ProcessModel',
+    'Distance',
+    'LF',
+    'WTE',
+    'Comp',
+    'AD',
+    'SS_MRF',
+    'Reproc',
+    'SF_Col'
+]
+
+__version__ = '0.1.3'
+
+
+from .ProcessModel import ProcessModel
+from .Distance import Distance
+from .LF import LF
+from .WTE import WTE
+from .Comp import Comp
+from .AD import AD
+from .SS_MRF import SS_MRF
+from .Reproc import Reproc
+from .SF_Col import SF_Col
