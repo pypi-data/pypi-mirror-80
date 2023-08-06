b54d19f44664e23f12da1962ff2eb4e59c74d0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:08 +0000

    Add package rastasteady/0.2.12/rastasteady-0.2.12.tar.gz

---
+++ packages/rastasteady/rastasteady-0.2.12.tar.gz/rastasteady-0.2.12/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rastasteady',
+    version='0.2.12',
+    description='',
+    long_description='',
+    install_requires=[
+        'click',
+        'flask'
+    ],
+    entry_points='''
+        [console_scripts]
+        rastasteady=rastasteady.cli:cli
+        rastasteady-cli=rastasteady.cli:cli
+        rastasteady-web=rastasteady.web.web:web
+    ''',
+    packages=find_packages(),
+    zip_safe=False,
+)
