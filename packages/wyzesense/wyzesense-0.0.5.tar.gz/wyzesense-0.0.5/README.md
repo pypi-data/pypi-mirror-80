98bdc6091573dc61cce41d4631c2b37d2119e59b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:12 +0000

    Add package wyzesense/0.0.5/wyzesense-0.0.5.tar.gz

---
+++ packages/wyzesense/wyzesense-0.0.5.tar.gz/wyzesense-0.0.5/README.md
@@ -0,0 +1,2 @@
+# WyzeSensePy
+WyzeSensePy is a python package trying to implement the communication protocol between WyzeCamera and WyzeSense USB dongle. 
---
+++ packages/wyzesense/wyzesense-0.0.5.tar.gz/wyzesense-0.0.5/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wyzesense",
+    version="0.0.5",
+    author="Xuan Xing",
+    author_email="hclxing@gmail.com",
+    description="WyzeSense gateway library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HclX/WyzeSensePy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
