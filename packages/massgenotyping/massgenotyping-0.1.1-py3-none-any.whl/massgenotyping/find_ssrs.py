8251348fec347276d71fde2049691dba60fb7217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:52:32 +0000

    Add package massgenotyping/0.1.1/massgenotyping-0.1.1-py3-none-any.whl

---
+++ packages/massgenotyping/massgenotyping-0.1.1-py3-none-any.whl/massgenotyping/find_ssrs.py
@@ -0,0 +1,211 @@
+import re
+from itertools import product
+from typing import Iterator, List, Optional, Tuple, Union
+
+from fuzzysearch.common import group_matches
+
+from .base import RepData, revc
+
+rep_seq_pat = re.compile("\\(([ACGT]+)\\)_{([0-9]+)}([ACTG]*)")
+
+
+def rotate(string: str) -> Iterator[str]:
+    """Rotate the characters in the input string one by one."""
+    for i in range(len(string)):
+        yield string[i:] + string[:i]
+
+
+def motif_set(motif_class: str) -> Iterator[str]:
+    """Return all sequences of the same motif class."""
+    for i in rotate(motif_class):
+        yield i
+    for i in rotate(revc(motif_class)):
+        yield i
+
+
+def gen_motif_classes(min_motif_len: int = 2, max_motif_len: int = 6) -> Iterator[str]:
+    """Generate repeat-motif classes for a given range of sequence lengths."""
+    motifs = []
+    for mlen in range(min_motif_len, max_motif_len + 1):
+        for mcls_ in product("ACGT", repeat=mlen):
+            mcls = "".join(mcls_)
+            if len(set(mcls)) == 1:
+                continue
+            if mcls not in motifs:
+                motifs.extend(list(motif_set(mcls)))
+                yield mcls
+
+
+def get_motif_class(motif: str) -> str:
+    """Return the class of the given motif."""
+    for mcls in gen_motif_classes(len(motif), len(motif) + 1):
+        for m in motif_set(mcls):
+            if m == motif:
+                return mcls
+    else:
+        raise ValueError(
+            "Unable to find the class of the given motif. "
+            "Maybe it contains a character other than ['A', 'C', 'G', 'T']?"
+        )
+
+
+def remove_gaps(seq: str) -> Tuple[str, List[int]]:
+    """Remove gaps in the given sequence."""
+    seqout, idx = "", []
+    for i, char in enumerate(seq):
+        if char != "-":
+            seqout += char
+            idx.append(i)
+    return seqout, idx
+
+
+def _find_ssrs(
+    seq: str, motif: str, min_repeats: int = 3, max_interrupt: int = 0
+) -> Iterator[Tuple[int, int, int, str]]:
+    seqng, idx = remove_gaps(seq.upper())
+    mlen = len(motif)
+
+    def find_in_index_range(
+        string: str, substring: str, n: int, start_index: int, end_index: int = None
+    ) -> Tuple[int, int, int]:
+        try:
+            start = string.find(substring * n, start_index, end_index)
+        except AttributeError:
+            raise ValueError("'seq' must be a str object")
+        match = start
+        while match > -1:
+            n += 1
+            match = string.find(substring * n, start_index, start + len(substring) * n)
+        n_reps = n - 1
+        return start, start + mlen * n_reps - 1, n_reps
+
+    start, end, n_reps = find_in_index_range(seqng, motif, min_repeats, 0)
+    while start > -1:
+        rep_seq = "({})_{{{}}}".format(motif, n_reps)
+
+        if max_interrupt:
+            # 3'-end side
+            start_new, end_new, n_reps_new = find_in_index_range(
+                seqng, motif, 1, end + 1, end + 1 + max_interrupt + mlen
+            )
+            while start_new > -1:
+                interrupt_seq = seqng[(end + 1) : start_new]
+                rep_seq += "{}({})_{{{}}}".format(interrupt_seq, motif, n_reps_new)
+                n_reps += n_reps_new + len(interrupt_seq) // mlen
+                end = end_new
+                start_new, end_new, n_reps_new = find_in_index_range(
+                    seqng, motif, 1, end + 1, end + 1 + max_interrupt + mlen
+                )
+
+            # 5'-end side
+            start_rev = len(seqng) - start
+            start_new, end_new, n_reps_new = find_in_index_range(
+                seqng[::-1],
+                motif[::-1],
+                1,
+                start_rev,
+                start_rev + max_interrupt + mlen,
+            )
+            while start_new > -1:
+                interrupt_seq = seqng[::-1][start_rev:start_new][::-1]
+                rep_seq_new = "({})_{{{}}}{}".format(motif, n_reps_new, interrupt_seq)
+                rep_seq = rep_seq_new + rep_seq
+                n_reps += n_reps_new + len(interrupt_seq) // mlen
+                start = len(seqng) - (end_new + 1)
+                start_rev = len(seqng) - start
+                start_new, end_new, n_reps_new = find_in_index_range(
+                    seqng[::-1],
+                    motif[::-1],
+                    1,
+                    start_rev,
+                    start_rev + max_interrupt + mlen,
+                )
+
+        yield idx[start], idx[end] + 1, n_reps, rep_seq
+        start, end, n_reps = find_in_index_range(seqng, motif, min_repeats, end + 1)
+
+
+def find_ssrs(
+    seq: str,
+    min_repeats: int = 3,
+    motif: Union[str, List[str]] = [],
+    motif_class: Union[str, List[str]] = [],
+    min_motif_len: int = 2,
+    max_motif_len: int = 6,
+    max_interrupt: int = 0,
+    start: int = 0,
+    end: Optional[int] = None,
+    **kwargs,
+) -> Optional[List[RepData]]:
+    """
+    Find short sequence repeats in the given sequence string.
+
+    Parameters
+    ----------
+    seq: str
+        input sequence string
+    min_repeats: int
+        minimum number of repeats to search (default: 3)
+    motif: str or list
+        repeat motif to search (default: None)
+    motif_class: str or list
+        class of repeat motif to search (default: None)
+    min_motif_len: int
+        minimum length of repeat motif (default: 2)
+    max_motif_len: int
+        maximum length of repeat motif (default: 6)
+    max_interrupt: int
+        maximum length of interruption to allow (default: None)
+    start: int
+        starting postion where ssr needs to be search
+    end: int
+        ending postion where ssr needs to be search
+
+    """
+    subseq = seq[start:end]
+    matches = []
+    if motif:
+        if isinstance(motif, list):
+            motifs = motif
+        elif isinstance(motif, str):
+            motifs = [motif]
+        else:
+            raise TypeError("motif must be a str or list")
+
+        motif_classes = [get_motif_class(m) for m in motifs]
+        for m, mcls in zip(motifs, motif_classes):
+            for s, e, n, rs in _find_ssrs(subseq, m, min_repeats, max_interrupt):
+                matches.append(RepData(s + start, e + start, n, rs, m, mcls))
+    else:
+        if not motif_class:
+            motif_classes = list(gen_motif_classes(min_motif_len, max_motif_len))
+        elif isinstance(motif_class, list):
+            motif_classes = motif_class
+        elif isinstance(motif_class, str):
+            motif_classes = [motif_class]
+        else:
+            raise TypeError("motif_class must be a str or list")
+
+        for mcls in motif_classes:
+            for m in motif_set(mcls):
+                for s, e, n, rs in _find_ssrs(subseq, m, min_repeats, max_interrupt):
+                    matches.append(RepData(s + start, e + start, n, rs, m, mcls))
+
+    match_groups = group_matches(matches)
+    best_matches = [get_longest_RepData(g) for g in match_groups if g]
+
+    return list(sorted(best_matches, key=lambda m: m.start))
+
+
+def get_longest_RepData(group: List[RepData]) -> RepData:
+    """Return the best match RepData in the group."""
+    return max(group, key=lambda x: (x.n_reps, -x.start))
+
+
+def unfold_rep_seq(rep_seq: str) -> str:
+    """Unfold the rep_seq string."""
+    unfolded = ""
+    matches = rep_seq_pat.findall(rep_seq)
+    for m in matches:
+        unfolded += m[0] * int(m[1]) + m[2]
+    return unfolded
---
+++ packages/massgenotyping/massgenotyping-0.1.1-py3-none-any.whl/massgenotyping/trim_sequence.py
@@ -0,0 +1,123 @@
+from typing import Iterator, List, Tuple, Union
+
+import numpy as np
+from Bio import SeqIO
+from fuzzysearch import find_near_matches
+from fuzzysearch.common import get_best_match_in_group
+
+from .base import revc
+
+
+def sliding_window(
+    x: List[int], window_size: int = 10, step_size: int = 1
+) -> Iterator[List[int]]:
+    """
+    Generate substrings by the sliding window algorithm.
+
+    Parameters
+    ----------
+    x : str
+        input string
+    window_size : int
+        window size (default: 10)
+    step_size : int
+        step size (default: 1)
+
+    """
+    n_chunks = ((len(x) - window_size) / step_size) + 1
+    for i in range(0, int(n_chunks * step_size), step_size):
+        yield x[i : i + window_size]
+
+
+def trim_low_qual(
+    seq_record: SeqIO.SeqRecord,
+    threshold: int = 20,
+    window_size: int = 10,
+    step_size: int = 1,
+    return_n_trim: bool = False,
+) -> Union[SeqIO.SeqRecord, Tuple[SeqIO.SeqRecord, int]]:
+    """
+    Trim low quality bases in the 3'- side of the sequence based on the phred quality
+    scores.
+
+    Parameters
+    ----------
+    seq_record : Bio.Seq.SeqRecord
+        input sequence record with quality scores
+    threshold : int
+        cut off threshold of the quality score (default: 20)
+    window_size : int
+        window size (default: 10)
+    step_size : int
+        step size (default: 1)
+    return_n_trim : bool
+        return number of bases trimmed (default: False)
+
+    """
+    qual = seq_record.letter_annotations["phred_quality"]
+    for i, q in enumerate(sliding_window(qual[::-1], window_size, step_size)):
+        if np.mean(q) > threshold:
+            break
+
+    n_trim = i * step_size
+    if n_trim:
+        tr = seq_record[:-n_trim]
+    else:
+        tr = seq_record[:]
+
+    if return_n_trim:
+        return tr, n_trim
+    else:
+        return tr
+
+
+def trim_primer(
+    seq_record: SeqIO.SeqRecord,
+    primer_seqs: List[str],
+    max_mismatch: Union[float, int] = 0.14,
+) -> SeqIO.SeqRecord:
+    """
+    Trim primer sequences.
+
+    Parameters
+    ----------
+    seq_record : Bio.Seq.SeqRecord
+        input sequence record
+    primer_seqs : list
+        list of the foward and reverse primer sequnces
+    max_mismatch : float
+        Maximum number (or proportion) of mismatches allowed for searching primer
+        sequeces (default: 0.14)
+
+    """
+    seq = seq_record.seq
+    fwd, rev = primer_seqs
+    rev_rc = revc(rev)
+    len_fwd, len_rev = len(fwd), len(rev)
+
+    if max_mismatch > 1:
+        max_l_dist1 = max_l_dist2 = max_mismatch
+    elif max_mismatch > 0:
+        max_l_dist1 = round(len_fwd * max_mismatch)
+        max_l_dist2 = round(len_rev * max_mismatch)
+    else:
+        raise ValueError("max_mismatch must be a positive value")
+
+    m0 = find_near_matches(fwd, str(seq), max_l_dist=max_l_dist1)
+    m1 = find_near_matches(rev_rc, str(seq), max_l_dist=max_l_dist2)
+
+    if len(m0) > 0:
+        match_fwd = get_best_match_in_group(m0)
+    if len(m1) > 0:
+        match_rev_rc = get_best_match_in_group(m1)
+
+    if len(m0) > 0 and len(m1) > 0:
+        tr = seq_record[match_fwd.end : match_rev_rc.start]
+    elif len(m0) > 0:
+        tr = seq_record[match_fwd.end :]
+    elif len(m1) > 0:
+        tr = seq_record[: match_rev_rc.start]
+    else:
+        tr = seq_record[:]
+
+    return tr
