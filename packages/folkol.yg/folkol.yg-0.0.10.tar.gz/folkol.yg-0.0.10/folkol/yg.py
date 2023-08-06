85854c415e8673cd64ff555a17c919b81885f89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:43 +0000

    Add package folkol.yg/0.0.10/folkol.yg-0.0.10.tar.gz

---
+++ packages/folkol.yg/folkol.yg-0.0.10.tar.gz/folkol.yg-0.0.10/folkol/yg.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python3
+import argparse
+import pathlib
+import re
+import sys
+
+import yaml
+
+from signal import signal, SIGPIPE, SIG_DFL
+
+signal(SIGPIPE, SIG_DFL)
+
+
+def grep(pattern, file):
+    with file.open() as f:
+        data = yaml.safe_load(f)
+    hits = {k: v for k, v in data.items() if re.search(pattern, k)}
+    if hits:
+        print('==>', file, '<==')
+        yaml.dump(hits, sys.stdout, default_flow_style=False)
+        print()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='searches and prints YAML (top-level) properties and their values')
+    parser.add_argument(
+        'pattern',
+        action='store',
+        help='regex to filter YAML properties on',
+    )
+    parser.add_argument(
+        'files',
+        nargs='*',
+        help='Files to search, defaults to .yaml under the current directory (recursively).',
+        default=[pathlib.Path('.')],
+        type=pathlib.Path,
+    )
+    args = parser.parse_args()
+
+    try:
+        pattern = re.compile(args.pattern)
+        for root in args.files or ['.']:
+            if root.is_dir():
+                for file in pathlib.Path(root).rglob('*.yaml'):
+                    grep(pattern, file)
+            elif root.is_file():
+                grep(pattern, root)
+            else:
+                print('Not a file:', root, file=sys.stderr)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/folkol.yg/folkol.yg-0.0.10.tar.gz/folkol.yg-0.0.10/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='folkol.yg',
+    version='0.0.10',
+    description='a unix filter for "greping" in YAML files',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/folkol/yg',
+    author='folkol',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Terminals',
+        'Topic :: Text Processing :: Filters',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='shell, unix filter',
+    packages=["folkol"],
+    install_requires=['pyyaml'],
+    python_requires='>=3.6, <4',
+    entry_points={
+        'console_scripts': [
+            'yg=folkol.yg:main',
+        ],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/folkol/yg/issues',
+        'Source': 'https://github.com/folkol/yg',
+    },
+)
