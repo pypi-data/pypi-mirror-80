a0b13d7769c8a4f146c8a6bb833bf5706faa73d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:00:47 +0000

    Add package jaustin/0.0.3/jaustin-0.0.3.tar.gz

---
+++ packages/jaustin/jaustin-0.0.3.tar.gz/jaustin-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="jaustin", 
+    version="0.0.3",
+    author="Jacob Austin",
+    url="https://github.com/jacobaustin123/jaustin",
+    author_email="jacob.austin@columbia.edu",
+    description="A set of core Python utilities for machine learning and general programming",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+
