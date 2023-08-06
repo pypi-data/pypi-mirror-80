df0be12157ce249cfd401037cda2fd88d631c3d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:51 +0000

    Add package grdata/0.0.2/grdata-0.0.2.tar.gz

---
+++ packages/grdata/grdata-0.0.2.tar.gz/grdata-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="grdata",
+    version="0.0.2",
+    author="Zac Thiel",
+    author_email="digitalservices@grcity.us",
+    description="Package to process data using the City of Grand Rapids systems",
+    long_description_content_type="text/markdown",
+    url="https://github.com/GRInnovation/grdata",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.8.5"
+)
