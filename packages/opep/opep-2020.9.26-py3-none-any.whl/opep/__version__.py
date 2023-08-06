f849e420c463be31ba633c6b76b8bace3fb144e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:35 +0000

    Add package opep/2020.9.26/opep-2020.9.26-py3-none-any.whl

---
+++ packages/opep/opep-2020.9.26-py3-none-any.whl/opep/__version__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__title__ = "opep"
+__description__ = "Open PEPs on your terminal."
+__url__ = "https://github.com/vinayak-mehta/opep"
+__version__ = "2020.09.26"
+__author__ = "Vinayak Mehta"
+__author_email__ = "vmehta94@gmail.com"
+__license__ = "Apache 2.0"
