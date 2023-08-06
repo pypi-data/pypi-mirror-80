786444234c3a69795bc87da8ad5c5d6621b17c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:34 +0000

    Add package PyFEMP/0.0.1/PyFEMP-0.0.1-py3-none-any.whl

---
+++ packages/PyFEMP/PyFEMP-0.0.1-py3-none-any.whl/PyFEMP/__init__.py
@@ -0,0 +1,8 @@
+# initialisation file for package
+
+from PyFEMP.FEM_SOLVER import *
+from PyFEMP.meshing.mesh_2d import *
+from PyFEMP.meshing.mesh_1d import *
+
+import os
+assets = os.path.join(os.path.dirname(__file__), "assets")
