77ec867d41576920e9141c3a508a57aeb909c24c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package robotframework-exceldatadriver/1.1.7/robotframework_exceldatadriver-1.1.7-py3-none-any.whl

---
+++ packages/robotframework-exceldatadriver/robotframework_exceldatadriver-1.1.7-py3-none-any.whl/ExcelDataDriver/ExcelParser/ParserContext.py
@@ -0,0 +1,41 @@
+from collections import OrderedDict
+
+
+class ParserContext:
+
+    def __init__(self, parser_strategy):
+        self.parser_strategy = parser_strategy
+
+    def parse(self, wb):
+        """
+        Parse excel test data row based on Excel Parser Strategy
+        :return: List of ExcelTestDataRow
+        """
+        validate_result = {'is_pass': True, 'error_message': ''}
+
+        # Validate column
+        print('Validate reference column')
+        for ws in self.parser_strategy.get_all_worksheet(wb):
+            validate_result = self.parser_strategy.is_ws_column_valid(ws, validate_result)
+        if validate_result['is_pass'] is not True:
+            raise ValueError(validate_result['error_message'])
+        print('Done validate reference column')
+
+        # Parsing test data
+        print('Parsing reference data')
+        ws_test_data_rows = OrderedDict()
+        for ws in self.parser_strategy.get_all_worksheet(wb):
+            ws_column_indexs = self.parser_strategy.parsing_column_indexs(ws)
+            test_data_rows = self.parser_strategy.parse_test_data_properties(ws, ws_column_indexs)
+            ws_test_data_rows[ws.title] = test_data_rows
+        print('Done validate reference data')
+        return ws_test_data_rows
+
+    def insert_extra_columns(self, wb, columns):
+        for ws in list(wb):
+            ws_column_indexs = self.parser_strategy.parsing_column_indexs(ws)
+            for column in reversed(columns):
+                if column in ws_column_indexs:
+                    continue
+                ws.insert_cols(1)
+                ws['A' + str(self.parser_strategy.start_row)] = column
