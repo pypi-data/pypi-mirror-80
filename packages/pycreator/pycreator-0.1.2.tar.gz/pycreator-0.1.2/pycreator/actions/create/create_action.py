6c99a76ce30d54233e10a7b93a263047e0fac480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:26 +0000

    Add package pycreator/0.1.2/pycreator-0.1.2.tar.gz

---
+++ packages/pycreator/pycreator-0.1.2.tar.gz/pycreator-0.1.2/pycreator/actions/create/create_action.py
@@ -0,0 +1,40 @@
+import os
+
+from pycreator.framework.messages import Messages
+
+from pycreator.actions.action import Action
+from pycreator.core import create_folders
+
+
+class CreateAction(Action):
+    ACTION = "create"
+    PARAM_NAME = "ACTION"
+
+    def fill_parser_arguments(self):
+        self.parser.add_argument('-n', '--name', type=str, help='Name for the new project', required=True)
+        self.parser.add_argument('-l', '--location', type=str, help='Custom location to create a new app', default=None)
+
+    def process_action(self, configuration):
+        target_location = configuration.location if configuration.location else os.getcwd()
+        if self.check_app_exists(target_location, configuration.name):
+            exit(1)
+        self.create_boilerplate(target_location, configuration.name)
+
+    @staticmethod
+    def create_boilerplate(tgt_location: str, app_name: str):
+        initial_steps = (
+            create_folders.create_application_dirs,
+            create_folders.create_inits,
+            create_folders.create_additional_files,
+            create_folders.create_files_from_templates
+        )
+        for step in initial_steps:
+            step(tgt_location, app_name)
+
+    @staticmethod
+    def check_app_exists(tgt_location: str, app_name: str):
+        potential_app_location = os.path.join(tgt_location, app_name)
+        if os.path.exists(potential_app_location) and os.path.isdir(potential_app_location):
+            Messages.error(f"Application {app_name} exist at {tgt_location}. Choose different name or location.")
+            return True
+        return False
