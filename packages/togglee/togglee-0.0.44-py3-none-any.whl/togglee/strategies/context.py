bdf1b61e6a0b4312b7b0c411acfe12ef82f73c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:26 +0000

    Add package togglee/0.0.44/togglee-0.0.44-py3-none-any.whl

---
+++ packages/togglee/togglee-0.0.44-py3-none-any.whl/togglee/strategies/context.py
@@ -0,0 +1,18 @@
+operations_maps = {
+    'eq': lambda first, second: first == second,
+    'ne': lambda first, second: first != second,
+    'gt': lambda first, second: first > second,
+    'ge': lambda first, second: first >= second,
+    'lt': lambda first, second: first < second,
+    'le': lambda first, second: first <= second,
+}
+
+
+def context_strategy(toggle, context):
+    for condition in toggle['conditions']:
+        value = context[condition['field']]
+        expected_value = condition['value']
+        operation = condition['operation']
+        if not operations_maps[operation](expected_value, value):
+            return False
+    return True
---
+++ packages/togglee/togglee-0.0.44-py3-none-any.whl/togglee/togglee.py
@@ -0,0 +1,40 @@
+import threading
+import time
+
+import requests
+
+from togglee.helpers.mappers import map_json_to_toggles
+from togglee.strategies.main import strategy_maps
+
+
+class Togglee:
+    def __init__(self, url: str, refresh_rate: int, defaults: list):
+        self._url = url
+        self._refresh_rate = refresh_rate
+        self._toggles = map_json_to_toggles(defaults) if isinstance(defaults, list) else None
+        self._thread = threading.Thread(target=self._scheduler_event)
+        self._thread.daemon = True
+        self._thread.start()
+
+    def is_enabled(self, prop: str, context=None):
+        return self._get_value(prop, context) if self._toggles is not None and prop in self._toggles else False
+
+    def _get_value(self, prop, context):
+        try:
+            toggle = self._toggles[prop]
+            return strategy_maps[toggle['type']](toggle, context)
+        except:
+            return False
+
+    def _scheduler_event(self):
+        self._refresh_toggles()
+        while True:
+            time.sleep(self._refresh_rate)
+            self._refresh_toggles()
+
+    def _refresh_toggles(self):
+        try:
+            response = requests.get(self._url)
+            self._toggles = map_json_to_toggles(response.json()['toggles'])
+        except:
+            pass
