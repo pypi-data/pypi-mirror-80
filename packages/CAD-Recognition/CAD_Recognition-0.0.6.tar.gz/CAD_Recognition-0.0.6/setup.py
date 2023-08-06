daa55d0140dffa09a2272088733c17976c4c012f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:20 +0000

    Add package CAD-Recognition/0.0.6/CAD_Recognition-0.0.6.tar.gz

---
+++ packages/CAD-Recognition/CAD_Recognition-0.0.6.tar.gz/CAD_Recognition-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CAD_Recognition",
+    version="0.0.6",
+    author="francis",
+    author_email="francirk@163.com",
+    description="CAD Recognition.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kenblikylee/imgkernel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['tensorflow','Keras','kmeans-smote','python-speech-features'],
+    python_requires='>=3.5',
+)
