69815ee3e68eb421ff799522d5d33628dbeca112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:32 +0000

    Add package solomon/0.0.3/solomon-0.0.3-py2-none-any.whl

---
+++ packages/solomon/solomon-0.0.3-py2-none-any.whl/solomon/base/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MVPViewController.swift
+//  SolomonExample
+//
+//  Created by ztimc on 2019/10/17.
+//  Copyright © 2019 sabinetek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class ViewController<P>: UIViewController {
+    
+    public var precenter: P!
+    
+}
