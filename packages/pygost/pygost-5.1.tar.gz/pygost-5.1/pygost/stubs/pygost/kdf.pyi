61153d5031397d6a957d8d10247475b276078bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:57 +0000

    Add package pygost/5.1/pygost-5.1.tar.gz

---
+++ packages/pygost/pygost-5.1.tar.gz/pygost-5.1/pygost/stubs/pygost/kdf.pyi
@@ -0,0 +1,21 @@
+from typing import Sequence
+
+from pygost.gost3410 import GOST3410Curve
+
+
+PublicKey = Tuple[int, int]
+
+
+def kdf_gostr3411_2012_256(key: bytes, label: bytes, seed: bytes) -> bytes: ...
+
+
+def kdf_tree_gostr3411_2012_256(
+        key: bytes,
+        label: bytes,
+        seed: bytes,
+        keys: int,
+        i_len=1,
+) -> Sequence[bytes]: ...
+
+
+def keg(curve: GOST3410Curve, prv: int, pub: PublicKey, h: bytes) -> bytes: ...
