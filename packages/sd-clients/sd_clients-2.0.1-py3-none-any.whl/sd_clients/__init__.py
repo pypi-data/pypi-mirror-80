a7a6a90f048ed882976ac1668ba7b874322d95bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package sd-clients/2.0.1/sd_clients-2.0.1-py3-none-any.whl

---
+++ packages/sd-clients/sd_clients-2.0.1-py3-none-any.whl/sd_clients/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "2.0.1"
+
+__all__ = ["ServiceDirectoryClient", "ApiGatewayProvider"]
+
+try:
+    # Attempts to import the client class
+    # Allowed to fail importing so the package metadata can be read for building
+    from .api_gateway_client import ApiGatewayProvider
+    from .service_directory_client import ServiceDirectoryClient
+except (ImportError, ModuleNotFoundError):  # pragma: no cover
+    pass  # pragma: no cover
