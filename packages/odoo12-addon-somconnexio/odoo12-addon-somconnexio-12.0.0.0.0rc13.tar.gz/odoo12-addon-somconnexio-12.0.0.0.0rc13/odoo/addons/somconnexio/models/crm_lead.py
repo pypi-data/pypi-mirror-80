eb958c28ff63a25543b4f83094fc3c7a9835c759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:18 +0000

    Add package odoo12-addon-somconnexio/12.0.0.0.0rc13/odoo12-addon-somconnexio-12.0.0.0.0rc13.tar.gz

---
+++ packages/odoo12-addon-somconnexio/odoo12-addon-somconnexio-12.0.0.0.0rc13.tar.gz/odoo12-addon-somconnexio-12.0.0.0.0rc13/odoo/addons/somconnexio/models/crm_lead.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+    subscription_request_id = fields.Many2one(
+        'subscription.request', 'Subscription Request'
+    )
