b382cc646e219972a0af5f45087ec008c31f4854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:59 +0000

    Add package instauto/0.0.14/instauto-0.0.14.tar.gz

---
+++ packages/instauto/instauto-0.0.14.tar.gz/instauto-0.0.14/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+  name='instauto',
+  packages=setuptools.find_packages(),
+  version='0.0.14',
+  license='GNU GPLv3',
+  description='Python wrapper for the private Instagram API',
+  author='Stan van Rooy',
+  author_email='stan@rooy.dev',
+  url='https://github.com/stanvanrooy/instauto',
+  download_url='https://github.com/stanvanrooy/instauto/archive/0.0.14.tar.gz',
+  keywords=['instagram api', 'private instagram api'],
+  install_requires=[
+          'requests',
+          'apscheduler',
+          'pycryptodomex',
+          'imagesize'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)  ',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
