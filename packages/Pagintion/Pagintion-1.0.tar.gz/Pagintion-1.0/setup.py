81331941ee8cdf85bf99be8bee4196fa85966aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:39 +0000

    Add package Pagintion/1.0/Pagintion-1.0.tar.gz

---
+++ packages/Pagintion/Pagintion-1.0.tar.gz/Pagintion-1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+setuptools.setup(
+    name="Pagintion",
+    version="v1.0",
+    author="ZhangJian",
+    author_email="15332962628@163.com",
+    description="分页组件",
+    long_description="",
+    packages=setuptools.find_packages(),
+
+    # classifiers=[
+    #
+    # ],
+    install_requires=[
+        'django',
+    ],
+    python_requires='>=3',
+
+
+)
