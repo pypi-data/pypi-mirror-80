bd5a12ec7d12adfc32a004b61465da308ef95669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:15 +0000

    Add package certbot-dns-corenetworks/0.1.4/certbot_dns_corenetworks-0.1.4-py2.py3-none-any.whl

---
+++ packages/certbot-dns-corenetworks/certbot_dns_corenetworks-0.1.4-py2.py3-none-any.whl/certbot_dns_corenetworks/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Default package."""
+
+__author__ = "Robert Kaussow"
+__project__ = "certbot_dns_corenetworks"
+__license__ = "MIT"
+__maintainer__ = "Robert Kaussow"
+__email__ = "mail@thegeeklab.de"
+__url__ = "https://github.com/thegeeklab/certbot-dns-corenetworks"
+__version__ = "0.1.4"
