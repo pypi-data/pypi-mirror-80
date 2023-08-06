a6ad369eedfed2979aa47e8d891d2f361689e115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:38 +0000

    Add package tap-pendo/0.0.1/tap-pendo-0.0.1.tar.gz

---
+++ packages/tap-pendo/tap-pendo-0.0.1.tar.gz/tap-pendo-0.0.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-pendo",
+    version="0.0.1",
+    description="Singer.io tap for extracting data",
+    author="wclark@agari.com",
+    url="https://github.com/jwalterclark/tap-pendo",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_pendo"],
+    install_requires=[
+        'singer-python==5.2.1',
+        "requests",
+        'pyhumps==1.3.1',
+        'backoff==1.3.2'
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-pendo=tap_pendo:main
+    """,
+    packages=["tap_pendo"],
+    package_data={
+        "schemas": ["tap_pendo/schemas/*.json"]
+    },
+    include_package_data=True,
+)
