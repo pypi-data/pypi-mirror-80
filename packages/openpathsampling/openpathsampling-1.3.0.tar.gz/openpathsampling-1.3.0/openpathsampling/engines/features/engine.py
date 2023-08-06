c7562b3595d7ea249845295c6ae1ccbaf4f42ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:52 +0000

    Add package openpathsampling/1.3.0/openpathsampling-1.3.0.tar.gz

---
+++ packages/openpathsampling/openpathsampling-1.3.0.tar.gz/openpathsampling-1.3.0/openpathsampling/engines/features/engine.py
@@ -0,0 +1,26 @@
+"""
+Attributes
+----------
+engine : :class:`openpathsampling.DynamicsEngine`
+    reference to the engine used to generate the snapshot
+"""
+
+variables = ['engine']
+
+
+def netcdfplus_init(store):
+    store.create_variable(
+        'engine',
+        'obj.engines',
+        description="reference to the engine for the current simulation box"
+    )
+
+
+@property
+def descriptor(snapshot):
+    return snapshot.engine.descriptor
+
+
+@property
+def topology(snapshot):
+    return snapshot.engine.topology
