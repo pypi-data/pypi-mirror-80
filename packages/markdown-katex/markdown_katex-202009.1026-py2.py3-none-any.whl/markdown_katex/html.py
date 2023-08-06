7d2648624181886dc6e12283d3a3a13ed46d0161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:35 +0000

    Add package markdown-katex/202009.1026/markdown_katex-202009.1026-py2.py3-none-any.whl

---
+++ packages/markdown-katex/markdown_katex-202009.1026-py2.py3-none-any.whl/markdown_katex/html.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# This file is part of the markdown-katex project
+# https://gitlab.com/mbarkhau/markdown-katex
+#
+# Copyright (c) 2020 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+_STYLESHEET_LINK = """
+<link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
+    integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
+    crossorigin="anonymous" />
+"""
+_KATEX_IMAGE_STYLES = """
+<style type="text/css">
+    .katex img {
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: inherit;
+    }
+</style>
+"""
+KATEX_STYLES = _STYLESHEET_LINK + _KATEX_IMAGE_STYLES
+HTML_TEMPLATE = """
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Test Katex</title>
+  {{stylesheet_link}}
+  <style type="text/css">
+    body{background: white; }
+  </style>
+</head>
+<body>
+Generated with markdown-katex
+<hr/>
+{{content}}
+</body>
+</html>
+"""
+HTML_TEMPLATE = HTML_TEMPLATE.replace('{{stylesheet_link}}', _STYLESHEET_LINK)
