6f7a8da9cb2aff928712b31f12401e4ce8fb011a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:00 +0000

    Add package httpython/0.2.7/httpython-0.2.7.tar.gz

---
+++ packages/httpython/httpython-0.2.7.tar.gz/httpython-0.2.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="httpython",
+    version="0.2.7",
+    author="Jão Bernardi",
+    author_email="joao_bernardi@outlook.com",
+    description="A simple-to-use framework for http and https servers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jaobernardi/Httpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
