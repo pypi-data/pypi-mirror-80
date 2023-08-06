46002d44466d9d1357c322c2b2dee6968a83a4fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:51 +0000

    Add package slicednormals/0.1.12a0/slicednormals-0.1.12a0.tar.gz

---
+++ packages/slicednormals/slicednormals-0.1.12a0.tar.gz/slicednormals-0.1.12a0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+  name = 'slicednormals',
+  packages = ['slicednormals'],
+  version = '0.1.12-alpha',
+  license='gpl-3.0',
+  description = 'Library for fitting basic and scaled sliced normal distributions',
+  author = 'Alexander Wimbush',
+  author_email = 'alexanderpwimbush@gmail.com', 
+  url = 'https://github.com/Institute-for-Risk-and-Uncertainty/Sliced-Normals-Python',
+  download_url = 'https://github.com/Institute-for-Risk-and-Uncertainty/Sliced-Normals-Python/archive/0.1.12-alpha.tar.gz', 
+  keywords = ['dependence', 'expansion', 'density'], 
+  install_requires=[
+          'numpy',
+		  'scipy',
+          'functools',
+          'itertools',
+          'operator'
+      ],
+)
