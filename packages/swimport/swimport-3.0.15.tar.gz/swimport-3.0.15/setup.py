f82a40c549d62a0547eb52b527581e7c941c9e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:52 +0000

    Add package swimport/3.0.15/swimport-3.0.15.tar.gz

---
+++ packages/swimport/swimport-3.0.15.tar.gz/swimport-3.0.15/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+from swimport_data.__data__ import (
+    __name__,
+    __author__,
+    __author_email__,
+    __license__,
+    __url__,
+    __version__,
+)
+
+setup(
+    name=__name__,
+    version=__version__,
+    author=__author__,
+    author_email=__author_email__,
+    license=__license__,
+    url=__url__,
+    packages=['swimport', 'swimport.model', 'swimport.pools', 'swimport_data', 'swimport.pools.derived_types'],
+    install_requires=['CppHeaderParser>=2', ],
+    python_requires='>=3.6.0',
+    include_package_data=True,
+    data_files=[
+        ('', ['README.md', 'CHANGELOG.md']),
+    ],
+    extras_require={
+        'memory testing': ['psutil']
+    },
+)
