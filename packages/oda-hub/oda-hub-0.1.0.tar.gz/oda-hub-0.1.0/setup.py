63ac5cb1962b3b72eaf21051da30f1ff0baedf5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:01 +0000

    Add package oda-hub/0.1.0/oda-hub-0.1.0.tar.gz

---
+++ packages/oda-hub/oda-hub-0.1.0.tar.gz/oda-hub-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(name='oda-hub',
+      version='0.1.0',
+      description='a queue manager (yet another)',
+      author='Volodymyr Savchenko',
+      author_email='vladimir.savchenko@gmail.com',
+      license='MIT',
+      packages=['dqueue'],
+      entry_points={
+          'console_scripts':  [
+              'dqueue=dqueue.cli:main',
+              'oda-hub=dqueue.cli:main',
+                ]
+          },
+      install_requires=[
+          'marshmallow',
+          'apispec',
+          'flasgger',
+          'bravado',
+          'termcolor',
+          'pymysql',
+          'peewee',
+          'retrying',
+          'oda-knowledge-base[cwl]>=0.6.18', # should be an option
+          'pyjwt',
+          ],
+      zip_safe=False,
+     )
