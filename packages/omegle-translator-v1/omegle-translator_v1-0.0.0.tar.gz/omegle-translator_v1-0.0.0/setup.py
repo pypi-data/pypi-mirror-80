9e63883ce7f5f41b6e7fb92cfaa138b567e60c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:33 +0000

    Add package omegle-translator-v1/0.0.0/omegle-translator_v1-0.0.0.tar.gz

---
+++ packages/omegle-translator-v1/omegle-translator_v1-0.0.0.tar.gz/omegle-translator_v1-0.0.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="omegle-translator_v1", # Replace with your own username
+    versioin='0.1.3',
+    author="Shivank Anchal",
+    author_email="shivajay295@gmail.com",
+    description="A small example package,to communicate on omegle in any language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://githuself.b.com/frozenmafia/omego_transo.git",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'googletrans',
+        'selenium',
+        'json',
+        'threading',
+        'io',
+        'WConio2'
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    download_url='https://github.com/frozenmafia/omegle_translator/archive/0.0.1.tar.gz'
+)
