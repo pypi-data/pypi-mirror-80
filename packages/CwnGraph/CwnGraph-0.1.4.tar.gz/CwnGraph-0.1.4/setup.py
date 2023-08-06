170aca16fc0c9e15965b16a564321b6e8d274eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:10 +0000

    Add package CwnGraph/0.1.4/CwnGraph-0.1.4.tar.gz

---
+++ packages/CwnGraph/CwnGraph-0.1.4.tar.gz/CwnGraph-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CwnGraph',
+    version='0.1.4',
+    packages=find_packages(),
+    license='GPL GNUv3',
+    author="NTUGIL LOPE Lab",   
+    setup_requires=["wheel"],
+    install_requires=["gdown"],
+    description="A CWN Python binding with graph structure",
+    long_description="A CWN Python binding with graph structure"
+)
