44dd42b425ab6d3e8d9b274ad55a73f9238d4a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:48 +0000

    Add package webviz-subsurface/0.1.3/webviz-subsurface-0.1.3.tar.gz

---
+++ packages/webviz-subsurface/webviz-subsurface-0.1.3.tar.gz/webviz-subsurface-0.1.3/CHANGELOG.md
@@ -0,0 +1,25 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased] - YYYY-MM-DD
+### Added
+- [#417](https://github.com/equinor/webviz-subsurface/pull/417) - Added an optional argument `--testdata-folder` to `pytest`, can be used when [test data](https://github.com/equinor/webviz-subsurface-testdata) is in non-default location.
+- [#422](https://github.com/equinor/webviz-subsurface/pull/422) - `HistoryMatch` plugin now
+quietly excludes all realizations lacking an `OK` file written by `ERT` on completion of realization workflow, similar to behavior of other plugins that read from individual realizations. Previously wrote warnings for missing data.
+- [#428](https://github.com/equinor/webviz-subsurface/pull/428) - Plugin controls, such as dropdown selections, set by the user is kept on page reload.
+- [#435](https://github.com/equinor/webviz-subsurface/pull/435) - Suppress a warning in SurfaceViewerFMU when calculating statistics from surfaces where one or more surface only has NaN values. [#399](https://github.com/equinor/webviz-subsurface/pull/399)
+- [#438](https://github.com/equinor/webviz-subsurface/pull/438) - Improved documentation of generation of data input for `RelativePermability` plugin.
+- [#434](https://github.com/equinor/webviz-subsurface/pull/434) - Improved hillshading and colors in plugins with map views.
+- [#439](https://github.com/equinor/webviz-subsurface/pull/439) - Pie chart and bar chart are now visualized together in `DiskUsage`. Free space is now visualized as well.
+
+### Fixed
+- [#432](https://github.com/equinor/webviz-subsurface/pull/432) - Bug fix in ReservoirSimulationTimeSeries. Vectors starting with A, V, G, I, N, T, V and L resulted in crash due to a bug introduced in [#373](https://github.com/equinor/webviz-subsurface/pull/373) (most notably group and aquifer vectors).
+- [#442](https://github.com/equinor/webviz-subsurface/pull/442) - Bug fix in ReservoirSimulationTimeSeries. Wrong realization number was shown if data set contained missing realizations. Now uses correct realization number from data.
+- [#447](https://github.com/equinor/webviz-subsurface/pull/447) - Changed two `webvizstore` decorated functions such that they do not take in `pandas` objects as arguments, which are known to not have `repr()` useful for hashing.
+
+## [0.1.2] - 2020-08-24
+### Changed
+- [#415](https://github.com/equinor/webviz-subsurface/pull/415) - Now using `xml` package from standard Python library (together with [`defusexml`](https://pypi.org/project/defusedxml/)) instead of [`bs4`](https://pypi.org/project/beautifulsoup4/).
---
+++ packages/webviz-subsurface/webviz-subsurface-0.1.3.tar.gz/webviz-subsurface-0.1.3/setup.py
@@ -0,0 +1,97 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+TESTS_REQUIRE = [
+    "selenium>=3.141",
+    "pylint",
+    "mock",
+    "black>=20.8b1",
+    "bandit",
+    "pytest-xdist",
+]
+
+setup(
+    name="webviz-subsurface",
+    description="Webviz config plugins for subsurface data",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/equinor/webviz-subsurface",
+    author="R&T Equinor",
+    packages=find_packages(exclude=["tests"]),
+    package_data={
+        "webviz_subsurface": [
+            "_abbreviations/abbreviation_data/*.json",
+            "_assets/css/*.css",
+        ]
+    },
+    entry_points={
+        "webviz_config_plugins": [
+            "ParameterDistribution = webviz_subsurface.plugins:ParameterDistribution",
+            "ParameterCorrelation = webviz_subsurface.plugins:ParameterCorrelation",
+            "ParameterResponseCorrelation = "
+            + "webviz_subsurface.plugins:ParameterResponseCorrelation",
+            "DiskUsage = webviz_subsurface.plugins:DiskUsage",
+            "SubsurfaceMap = webviz_subsurface.plugins:SubsurfaceMap",
+            "HistoryMatch = webviz_subsurface.plugins:HistoryMatch",
+            "MorrisPlot = webviz_subsurface.plugins:MorrisPlot",
+            "InplaceVolumes = webviz_subsurface.plugins:InplaceVolumes",
+            "InplaceVolumesOneByOne = webviz_subsurface.plugins:InplaceVolumesOneByOne",
+            "ReservoirSimulationTimeSeries = "
+            + "webviz_subsurface.plugins:ReservoirSimulationTimeSeries",
+            "ReservoirSimulationTimeSeriesOneByOne = "
+            + "webviz_subsurface.plugins:ReservoirSimulationTimeSeriesOneByOne",
+            "SurfaceViewerFMU = webviz_subsurface.plugins:SurfaceViewerFMU",
+            "SegyViewer = webviz_subsurface.plugins:SegyViewer",
+            "SurfaceWithGridCrossSection = "
+            + "webviz_subsurface.plugins:SurfaceWithGridCrossSection",
+            "SurfaceWithSeismicCrossSection = "
+            + "webviz_subsurface.plugins:SurfaceWithSeismicCrossSection",
+            "WellCrossSection = webviz_subsurface.plugins:WellCrossSection",
+            "WellCrossSectionFMU = webviz_subsurface.plugins:WellCrossSectionFMU",
+            "ParameterParallelCoordinates = "
+            + "webviz_subsurface.plugins:ParameterParallelCoordinates",
+            "RunningTimeAnalysisFMU = webviz_subsurface.plugins:RunningTimeAnalysisFMU",
+            "RelativePermeability = webviz_subsurface.plugins:RelativePermeability",
+            "ReservoirSimulationTimeSeriesRegional = "
+            + "webviz_subsurface.plugins:ReservoirSimulationTimeSeriesRegional",
+            "RftPlotter =  webviz_subsurface.plugins:RftPlotter",
+            "HorizonUncertaintyViewer = webviz_subsurface.plugins:HorizonUncertaintyViewer",
+            "PvtPlot = webviz_subsurface.plugins:PvtPlot",
+        ]
+    },
+    install_requires=[
+        "dash>=1.11",
+        "dash_bootstrap_components>=0.10.3",
+        "defusedxml>=0.6.0",
+        # "ecl2df>=0.6.1; sys_platform=='linux'",
+        "fmu-ensemble>=1.2.3",
+        # "opm>=2020.10.1; sys_platform=='linux'",
+        "pandas>=0.24",
+        "pillow>=6.1",
+        "pyscal>=0.4.1",
+        "scipy>=1.2",
+        "webviz-config>=0.0.55",
+        "webviz-subsurface-components>=0.2.0",
+        "xtgeo>=2.8",
+    ],
+    tests_require=TESTS_REQUIRE,
+    extras_require={"tests": TESTS_REQUIRE},
+    setup_requires=["setuptools_scm~=3.2"],
+    python_requires="~=3.6",
+    use_scm_version=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Environment :: Web Environment",
+        "Framework :: Dash",
+        "Framework :: Flask",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
