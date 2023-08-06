97568b2afca6b2115c3ce488d8a60aecca6e6ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:26 +0000

    Add package expsolver/0.3/expsolver-0.3.tar.gz

---
+++ packages/expsolver/expsolver-0.3.tar.gz/expsolver-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='expsolver',
+      version='0.3',
+      description='Expression solver',
+      packages=['expsolver'],
+      long_description = 'README.md',
+      zip_safe=False)
