01b4c2c158857287566e9adb312fc84007ec878c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:05 +0000

    Add package plone.app.portlets/4.4.6/plone.app.portlets-4.4.6.tar.gz

---
+++ packages/plone.app.portlets/plone.app.portlets-4.4.6.tar.gz/plone.app.portlets-4.4.6/plone/app/portlets/tests/base.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Base class for integration tests."""
+from plone.app.portlets.testing import PLONE_APP_PORTLETS_INTEGRATION_TESTING
+
+import unittest
+
+
+class PortletsTestCase(unittest.TestCase):
+    """Base class for integration tests for plone.app.portlets. This may
+    provide specific set-up and tear-down operations, or provide convenience
+    methods.
+    """
+
+    layer = PLONE_APP_PORTLETS_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.app = self.layer['app']
+        self.portal = self.layer['portal']
+        self.folder = self.portal['folder']
+        self.request = self.layer['request']
+
+        self.afterSetUp()
+
+    def afterSetUp(self):
+        pass
