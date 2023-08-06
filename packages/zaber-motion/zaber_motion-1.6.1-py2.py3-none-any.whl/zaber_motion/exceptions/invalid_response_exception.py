2327a0ed71a910357623b3fecdce13c714073b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:48 +0000

    Add package zaber-motion/1.6.1/zaber_motion-1.6.1-py2.py3-none-any.whl

---
+++ packages/zaber-motion/zaber_motion-1.6.1-py2.py3-none-any.whl/zaber_motion/exceptions/invalid_response_exception.py
@@ -0,0 +1,10 @@
+﻿# ===== THIS FILE IS GENERATED FROM A TEMPLATE ===== #
+# ============== DO NOT EDIT DIRECTLY ============== #
+
+from .motion_lib_exception import MotionLibException
+
+
+class InvalidResponseException(MotionLibException):
+    """
+    Thrown when a device sends a response with unexpected type or data.
+    """
