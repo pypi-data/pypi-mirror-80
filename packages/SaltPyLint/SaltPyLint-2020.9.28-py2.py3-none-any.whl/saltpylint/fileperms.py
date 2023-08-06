8cbaa1064f11acbf6a612b74013ca5cfb9062397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:10 +0000

    Add package SaltPyLint/2020.9.28/SaltPyLint-2020.9.28-py2.py3-none-any.whl

---
+++ packages/SaltPyLint/SaltPyLint-2020.9.28-py2.py3-none-any.whl/saltpylint/fileperms.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+'''
+    :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
+    :copyright: © 2015 by the SaltStack Team, see AUTHORS for more details.
+    :license: Apache 2.0, see LICENSE for more details.
+
+
+    ====================================
+    PyLint File Permissions Check Plugin
+    ====================================
+
+    PyLint plugin which checks for specific file permissions
+'''
+
+# Import Python libs
+from __future__ import absolute_import
+import os
+import sys
+import glob
+import stat
+
+# Import PyLint libs
+from pylint.interfaces import IRawChecker
+from saltpylint.checkers import BaseChecker
+
+
+class FilePermsChecker(BaseChecker):
+    '''
+    Check for files with undesirable permissions
+    '''
+
+    __implements__ = IRawChecker
+
+    name = 'fileperms'
+    msgs = {'E0599': ('Module file has the wrong file permissions(expected %s): %s',
+                      'file-perms',
+                      ('Wrong file permissions')),
+           }
+
+    priority = -1
+
+    options = (('fileperms-default',
+                {'default': '0644', 'type': 'string', 'metavar': 'ZERO_PADDED_PERM',
+                 'help': 'Desired file permissons. Default: 0644'}
+               ),
+               ('fileperms-ignore-paths',
+                {'default': (), 'type': 'csv', 'metavar': '<comma-separated-list>',
+                 'help': 'File paths to ignore file permission. Glob patterns allowed.'}
+               )
+              )
+
+    def process_module(self, node):
+        '''
+        process a module
+        '''
+
+        for listing in self.config.fileperms_ignore_paths:
+            if node.file.split('{0}/'.format(os.getcwd()))[-1] in glob.glob(listing):
+                # File is ignored, no checking should be done
+                return
+
+        desired_perm = self.config.fileperms_default
+        if '-' in desired_perm:
+            desired_perm = desired_perm.split('-')
+        else:
+            desired_perm = [desired_perm]
+
+        if len(desired_perm) > 2:
+            raise RuntimeError('Permission ranges should be like XXXX-YYYY')
+
+        for idx, perm in enumerate(desired_perm):
+            desired_perm[idx] = desired_perm[idx].strip('"').strip('\'').lstrip('0').zfill(4)
+            if desired_perm[idx][0] != '0':
+                # Always include a leading zero
+                desired_perm[idx] = '0{0}'.format(desired_perm[idx])
+            if sys.version_info > (3,):
+                # The octal representation in python 3 has changed to 0o644 instead of 0644
+                if desired_perm[idx][1] != 'o':
+                    desired_perm[idx] = '0o' + desired_perm[idx][1:]
+            if sys.platform.startswith('win'):
+                # Windows does not distinguish between user/group/other.
+                # They must all be the same. Also, Windows will automatically
+                # set the execution bit on files with a known extension
+                # (eg .exe, .bat, .com). So we cannot reliably test the
+                # execution bit on other files such as .py files.
+                user_perm_noexec = int(desired_perm[idx][-3])
+                if user_perm_noexec % 2 == 1:
+                    user_perm_noexec -= 1
+                desired_perm[idx] = desired_perm[idx][:-3] + (str(user_perm_noexec) * 3)
+
+        module_perms = oct(stat.S_IMODE(os.stat(node.file).st_mode))
+        if sys.version_info < (3,):
+            module_perms = str(module_perms)
+
+        if len(desired_perm) == 1:
+            if module_perms != desired_perm[0]:
+                if sys.platform.startswith('win'):
+                    # Check the variant with execution bit set due to the
+                    # unreliability of checking the execution bit on Windows.
+                    user_perm_noexec = int(desired_perm[0][-3])
+                    desired_perm_exec = desired_perm[0][:-3] + (str(user_perm_noexec + 1) * 3)
+                    if module_perms == desired_perm_exec:
+                        return
+                self.add_message('E0599', line=1, args=(desired_perm[0], module_perms))
+        else:
+            if module_perms < desired_perm[0] or module_perms > desired_perm[1]:
+                if sys.platform.startswith('win'):
+                    # Check the variant with execution bit set due to the
+                    # unreliability of checking the execution bit on Windows.
+                    user_perm_noexec0 = int(desired_perm[0][-3])
+                    desired_perm_exec0 = desired_perm[0][:-3] + (str(user_perm_noexec0 + 1) * 3)
+                    user_perm_noexec1 = int(desired_perm[1][-3])
+                    desired_perm_exec1 = desired_perm[1][:-3] + (str(user_perm_noexec1 + 1) * 3)
+                    if desired_perm_exec0 <= module_perms <= desired_perm_exec1:
+                        return
+                desired_perm = '>= {0} OR <= {1}'.format(*desired_perm)
+                self.add_message('E0599', line=1, args=(desired_perm, module_perms))
+
+
+def register(linter):
+    '''
+    required method to auto register this checker
+    '''
+    linter.register_checker(FilePermsChecker(linter))
---
+++ packages/SaltPyLint/SaltPyLint-2020.9.28-py2.py3-none-any.whl/saltpylint/thirdparty.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+'''
+    :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
+    :copyright: © 2017 by the SaltStack Team, see AUTHORS for more details.
+    :license: Apache 2.0, see LICENSE for more details.
+
+
+    saltpylint.thirdparty
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    Checks all imports against a list of known and allowed 3rd-party modules
+    and raises a lint error if an import not in that known 3rd-party modules list
+    is not gated.
+'''
+
+# Import python libs
+from __future__ import absolute_import
+import os
+
+# Import pylint libs
+import astroid
+import astroid.exceptions
+import pkgutil
+from astroid.modutils import is_relative, is_standard_module
+from pylint.interfaces import IAstroidChecker
+from saltpylint.checkers import BaseChecker, utils
+
+MSGS = {
+    'W8410': ('3rd-party module import is not gated in a try/except: %r',
+              '3rd-party-module-not-gated',
+              '3rd-party module imported without being gated in a try/except.'),
+    'C8410': ('3rd-party local module import is not gated in a try/except: %r. '
+              'Consider importing at the module global scope level and gate it '
+              'in a try/except.',
+              '3rd-party-local-module-not-gated',
+              '3rd-party module locally imported without being gated. Consider importing '
+              'at the module global scope level and gate it in a try/except'),
+    }
+
+
+def get_import_package(modname):
+    '''
+    Return the import package.
+
+    Given modname is 'salt.utils', returns 'salt'
+    '''
+    return modname.split('.')[0]
+
+
+class ThirdPartyImportsChecker(BaseChecker):
+
+    __implements__ = IAstroidChecker
+
+    name = '3rd-party-imports'
+    msgs = MSGS
+    priority = -2
+
+    options = (
+        ('allowed-3rd-party-modules', {
+            'default': (),
+            'type': 'csv',
+            'metavar': '<3rd-party-modules>',
+            'help': 'Known 3rd-party modules which don\' require being gated, separated by a comma'}),
+    )
+
+    known_py2_modules = ['__builtin__', 'exceptions']
+    known_py3_modules = ['builtins']
+
+    unix_modules = ('posix', 'pwd', 'spwd', 'grp', 'crypt', 'termios', 'tty', 'pty', 'fcntl', 'pipes', 'resource', 'nis', 'syslog', 'posixpath')
+    win_modules = ('msilib', 'msvcrt', 'winreg', 'winsound', 'ntpath')
+
+    all_modules = {m[1]: m[0].path for m in pkgutil.iter_modules()}
+    std_modules_path = all_modules["os"]
+    std_modules = []
+    for mod, path in all_modules.items():
+        if path == std_modules_path and mod not in unix_modules + win_modules:
+            std_modules.append(mod)
+
+    known_std_modules = known_py2_modules + known_py3_modules + std_modules
+
+    def __init__(self, linter=None):
+        BaseChecker.__init__(self, linter)
+        self._inside_try_except = False
+        self._inside_funcdef = False
+        self._inside_if = False
+        self.cwd = self.allowed_3rd_party_modules = None
+
+    def open(self):
+        super(ThirdPartyImportsChecker, self).open()
+        self.cwd = os.getcwd()
+        self.allowed_3rd_party_modules = set(self.config.allowed_3rd_party_modules)  # pylint: disable=no-member
+
+    # pylint: disable=unused-argument
+    @utils.check_messages('3rd-party-imports')
+    def visit_if(self, node):
+        self._inside_if = True
+
+    @utils.check_messages('3rd-party-imports')
+    def leave_if(self, node):
+        self._inside_if = True
+
+    @utils.check_messages('3rd-party-imports')
+    def visit_tryexcept(self, node):
+        self._inside_try_except = True
+
+    @utils.check_messages('3rd-party-imports')
+    def leave_tryexcept(self, node):
+        self._inside_try_except = False
+
+    @utils.check_messages('3rd-party-imports')
+    def visit_functiondef(self, node):
+        self._inside_funcdef = True
+
+    @utils.check_messages('3rd-party-imports')
+    def leave_functiondef(self, node):
+        self._inside_funcdef = False
+    # pylint: enable=unused-argument
+
+    @utils.check_messages('3rd-party-imports')
+    def visit_import(self, node):
+        names = [name for name, _ in node.names]
+        for name in names:
+            self._check_third_party_import(node, name)
+
+    @utils.check_messages('3rd-party-imports')
+    def visit_importfrom(self, node):
+        self._check_third_party_import(node, node.modname)
+
+    def _check_third_party_import(self, node, modname):
+        if modname in self.known_std_modules:
+            # Don't even care about these
+            return
+        module_file = node.root().file
+
+        if is_relative(modname, module_file):
+            # Is the import relative to the curent module being checked
+            return
+
+        base_modname = modname.split('.', 1)[0]
+        import_modname = modname
+        while True:
+            try:
+                imported_module = node.do_import_module(import_modname)
+                if not imported_module:
+                    break
+                if imported_module.file.startswith(self.cwd):
+                    # This is an import to package under the project being tested
+                    return
+                # If we reached this far, we were able to import the module but it's
+                # not considered a module from within the project being checked
+                break
+            except Exception:  # pylint: disable=broad-except
+                # This is, for example, from salt.ext.six.moves import Y
+                # Because `moves` is a dynamic/runtime module
+                import_modname = import_modname.rsplit('.', 1)[0]
+
+            if import_modname == base_modname or not import_modname:
+                break
+
+        try:
+            if not is_standard_module(modname):
+                if self._inside_try_except is False:
+                    if get_import_package(modname) in self.allowed_3rd_party_modules:
+                        return
+                    if self._inside_if or self._inside_funcdef:
+                        message_id = '3rd-party-local-module-not-gated'
+                    else:
+                        message_id = '3rd-party-module-not-gated'
+                    self.add_message(message_id, node=node, args=modname)
+        except astroid.exceptions.AstroidBuildingException:
+            # Failed to import
+            if self._inside_try_except is False:
+                if get_import_package(modname) in self.allowed_3rd_party_modules:
+                    return
+                if self._inside_if or self._inside_funcdef:
+                    message_id = '3rd-party-local-module-not-gated'
+                else:
+                    message_id = '3rd-party-module-not-gated'
+                self.add_message(message_id, node=node, args=modname)
+        except astroid.exceptions.InferenceError:
+            # Failed to import
+            if self._inside_try_except is False:
+                if get_import_package(modname) in self.allowed_3rd_party_modules:
+                    return
+                if self._inside_if or self._inside_funcdef:
+                    message_id = '3rd-party-local-module-not-gated'
+                else:
+                    message_id = '3rd-party-module-not-gated'
+                self.add_message(message_id, node=node, args=modname)
+        except ImportError:
+            # Definitly not a standard library import
+            if self._inside_try_except is False:
+                if get_import_package(modname) in self.allowed_3rd_party_modules:
+                    return
+                if self._inside_if or self._inside_funcdef:
+                    message_id = '3rd-party-local-module-not-gated'
+                else:
+                    message_id = '3rd-party-module-not-gated'
+                self.add_message(message_id, node=node, args=modname)
+
+
+def register(linter):
+    '''required method to auto register this checker '''
+    linter.register_checker(ThirdPartyImportsChecker(linter))
