601f44af86169fabb4916dcd7d2845a9ea0e0983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:27 +0000

    Add package cpcli/0.1/cpcli-0.1.tar.gz

---
+++ packages/cpcli/cpcli-0.1.tar.gz/cpcli-0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+install_requires = [
+    'lxml>=4.5.0',
+    'zope.interface>=4.1.3',
+]
+
+setup(
+    name='cpcli',
+    version='0.1',
+    description='Competitive Programming CLI',
+    author='Aditya Kumar',
+    author_email='k.aditya00@gmail.com',
+    long_description=open('README.rst', 'r', encoding='utf-8').read(),
+    package_dir={'': 'src'},
+    entry_points={'console_scripts': ['cpcli=cpcli.cmdline:execute']},
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    url='https://github.com/adityaa30/cpcli',
+    download_url='https://github.com/adityaa30/cpcli/releases/tag/0.1',
+    keywords=['CLI', 'Competitive Programming'],
+    zip_safe=False,
+    license='MIT License',
+    classifiers=[
+        'Topic :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
