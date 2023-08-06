20879ed8ce0d1ab6e5edf13ead1672fa775d30d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:09 +0000

    Add package async-translate/0.0.9/async_translate-0.0.9.tar.gz

---
+++ packages/async-translate/async_translate-0.0.9.tar.gz/async_translate-0.0.9/async_translate/errors.py
@@ -0,0 +1,15 @@
+# Errors and Exceptions
+
+class TranslatorException(Exception):
+    """Base exception to be able to catch all exceptions in this module"""
+    pass
+
+
+class ProviderAlreadyAdded(TranslatorException):
+    def __init__(self, name):
+        super().__init__(f"A provider with the name '{name}' has already been added.")
+
+
+class LanguageNotSupported(TranslatorException):
+    def __init__(self, language):
+        super().__init__(f"Language {language} is not supported with the current providers.")
