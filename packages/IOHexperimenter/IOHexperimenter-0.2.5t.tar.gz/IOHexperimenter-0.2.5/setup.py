f5fb27385923f991fda9aa8341efa116c03bbd0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:59 +0000

    Add package IOHexperimenter/0.2.5/IOHexperimenter-0.2.5t.tar.gz

---
+++ packages/IOHexperimenter/IOHexperimenter-0.2.5t.tar.gz/IOHexperimenter-0.2.5/setup.py
@@ -0,0 +1,50 @@
+import os
+import platform
+from setuptools import setup, Extension, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+if True:#platform.system() == "Darwin":
+    os.environ["CC"] = "clang"
+    os.environ["CXX"] = "clang"
+
+
+
+wrap_file = os.path.join("src", "IOHprofiler_wrap.cpp")
+if os.path.isfile(wrap_file):
+    os.remove(wrap_file)
+
+
+iohprofiler = Extension('_IOHprofiler', 
+    swig_opts=["-c++", "-outdir", "IOHexperimenter"], 
+    sources=[
+        os.path.join("src", x)
+        for x in os.listdir("src") if x.endswith(".cpp") or x.endswith(".i")
+    ],
+    extra_compile_args=["-std=c++14"]
+)
+
+
+setup(
+    name="IOHexperimenter",
+    version="0.2.5",
+    author="Furong Ye, Diederick Vermetten, and Hao Wang",
+    author_email="f.ye@liacs.leidenuniv.nl",
+    packages=find_packages(),
+    description="The experimenter for Iterative Optimization Heuristic",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IOHprofiler/IOHexperimenter",
+    ext_modules = [iohprofiler],
+    package_dir={'IOHexperimenter': 'IOHexperimenter'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+    python_requires='>=3.6',
+    install_requires=[ 
+        "numpy",        
+    ]
+)
