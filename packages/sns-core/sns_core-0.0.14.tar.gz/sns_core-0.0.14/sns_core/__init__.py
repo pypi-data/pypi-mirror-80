29d7c1c35103aa85e3aadb816bbb7167ee970884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:08 +0000

    Add package sns-core/0.0.14/sns_core-0.0.14.tar.gz

---
+++ packages/sns-core/sns_core-0.0.14.tar.gz/sns_core-0.0.14/sns_core/__init__.py
@@ -0,0 +1,15 @@
+"""
+pyexample.
+
+An example python library.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Wout Haakman'
+
+from . import exceptions
+from . import requests
+from . import parser
+from . import server
+from . import objects
+from . import records
