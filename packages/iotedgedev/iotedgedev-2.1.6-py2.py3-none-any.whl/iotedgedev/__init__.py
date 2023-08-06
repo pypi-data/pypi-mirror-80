71899be8386f54135d731002d85fcce9a4a48213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:35 +0000

    Add package iotedgedev/2.1.6/iotedgedev-2.1.6-py2.py3-none-any.whl

---
+++ packages/iotedgedev/iotedgedev-2.1.6-py2.py3-none-any.whl/iotedgedev/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Azure IoT Edge Dev Tool."""
+
+__author__ = 'Microsoft Corporation'
+__email__ = 'vsciet@microsoft.com'
+__version__ = '2.1.6'
+__AIkey__ = 'fed7fc65-5b4a-4e66-9d46-c5f016d4e2b4'
