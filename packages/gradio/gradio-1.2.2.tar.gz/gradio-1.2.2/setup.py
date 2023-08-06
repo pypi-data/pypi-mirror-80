f271c147db33ac515f6855b7ac8aad51d2060bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:16 +0000

    Add package gradio/1.2.2/gradio-1.2.2.tar.gz

---
+++ packages/gradio/gradio-1.2.2.tar.gz/gradio-1.2.2/setup.py
@@ -0,0 +1,27 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='gradio',
+    version='1.2.2',
+    include_package_data=True,
+    description='Python library for easily interacting with trained machine learning models',
+    author='Abubakar Abid',
+    author_email='a12d@stanford.edu',
+    url='https://github.com/gradio-app/gradio-UI',
+    packages=['gradio'],
+    keywords=['machine learning', 'visualization', 'reproducibility'],
+    install_requires=[
+        'numpy',
+        'requests',
+        'flask',
+        'paramiko',
+        'scipy',
+        'IPython',
+        'scikit-image',
+        'analytics-python',
+        'pandas'
+    ],
+)
