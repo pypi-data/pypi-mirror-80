8a0b94f912de097684fd759864722f84bbe2d515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:25 +0000

    Add package akbase2233/0.1.4/akbase2233-0.1.4.tar.gz

---
+++ packages/akbase2233/akbase2233-0.1.4.tar.gz/akbase2233-0.1.4/akbase2233/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+"""
+    @header __init__.py
+    @abstract   
+    
+    @MyBlog: http://www.kuture.com.cn
+    @author  Created by Kuture on 2020/9/22
+    @version 1.0.0 2020/9/22 Creation()
+    
+    @Copyright © 2020年 Mr.Li All rights reserved
+"""
