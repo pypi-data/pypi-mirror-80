b62ee83890e978733247aa15c326afadb1a57060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:00 +0000

    Add package xapiparser/0.0.6/xapiparser-0.0.6-py2.py3-none-any.whl

---
+++ packages/xapiparser/xapiparser-0.0.6-py2.py3-none-any.whl/xapiparser/cli.py
@@ -0,0 +1,20 @@
+import argparse
+
+import lxml.etree as etree
+
+from xapiparser import __version__
+from xapiparser import parse
+
+parser = argparse.ArgumentParser(description='xAPI ssh expression parser.')
+parser.add_argument('expression', metavar='expression', nargs='?',
+                    help="xAPI ssh expression")
+parser.add_argument('--version', action='version',
+                    version='%(prog)s v{version}'.format(version=__version__))
+
+
+def main(args=None):
+    args = parser.parse_args(args=args)
+    print(type(args.expression))
+    if not args.expression:
+        parser.error("Error.  No xAPI ssh expression supplied.")
+    print(etree.tostring(parse(args.expression), pretty_print=True, encoding='unicode'))
