149f793473e222af0217a08584280dc49b1539bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:21 +0000

    Add package ZnProject/0.0.3/ZnProject-0.0.3-py2-none-any.whl

---
+++ packages/ZnProject/ZnProject-0.0.3-py2-none-any.whl/ZnProject/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/27 下午4:44
+# @Author  : jinzening
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+
---
+++ packages/ZnProject/ZnProject-0.0.3-py2-none-any.whl/ZnProject/operation.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/27 下午4:45
+# @Author  : jinzening
+# @File    : add.py
+# @Software: PyCharm
+
+
+class Operation:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def zeadd(self):
+        return self.a + self.b
+
+    def znreduce(self):
+        return self.a + self.b
+
+    def znride(self):
+        return self.a * self.b
+
+
+
+
