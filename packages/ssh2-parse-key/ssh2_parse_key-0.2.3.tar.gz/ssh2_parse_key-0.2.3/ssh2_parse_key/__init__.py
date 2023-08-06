c65da68e80fc1beda115fab30c2ced205ab2f883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:51 +0000

    Add package ssh2-parse-key/0.2.3/ssh2_parse_key-0.2.3.tar.gz

---
+++ packages/ssh2-parse-key/ssh2_parse_key-0.2.3.tar.gz/ssh2_parse_key-0.2.3/ssh2_parse_key/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for SSH2 Key Parsing."""
+from .ssh2_parse_key import Ssh2Key as _Ssh2Key
+
+Ssh2Key = _Ssh2Key
+
+__author__ = "Nigel Metheringham"
+__email__ = "nigelm@cpan.org"
+__version__ = "0.2.3"
