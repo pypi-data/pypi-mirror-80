3bea6c627ea314e7742cbcc37aa8fa086f7ac024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:22 +0000

    Add package hexin/0.1/hexin-0.1-py3-none-any.whl

---
+++ packages/hexin/hexin-0.1-py3-none-any.whl/hexin/__init__.py
@@ -0,0 +1,16 @@
+info = '''Hello! I'm hexin
+让点赞关注赞赏来得更猛烈些吧！！！
+微信公众号：【AutoML机器学习】
+知乎主页：https://www.zhihu.com/people/hexin_hexin
+
+彩蛋：
+hexin.a
+hexin.b
+hexin.c
+'''
+print(info)
+__version__ = '0.1'
+__author__ = 'hexin'
+a = '你是最帅的'
+b = '你是最棒的'
+c = '但是都不如我，哈哈哈哈'
