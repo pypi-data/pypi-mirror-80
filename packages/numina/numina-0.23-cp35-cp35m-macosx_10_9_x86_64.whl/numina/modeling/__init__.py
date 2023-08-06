ea7f641aba67d481cffab6b60c04c19440b22717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:00:21 +0000

    Add package numina/0.23/numina-0.23-cp35-cp35m-macosx_10_9_x86_64.whl

---
+++ packages/numina/numina-0.23-cp35-cp35m-macosx_10_9_x86_64.whl/numina/modeling/__init__.py
@@ -0,0 +1,12 @@
+#
+# Copyright 2014 Universidad Complutense de Madrid
+#
+# This file is part of Numina
+#
+# SPDX-License-Identifier: GPL-3.0+
+# License-Filename: LICENSE.txt
+#
+
+from .enclosed import EnclosedGaussian
+from .gaussbox import GaussBox
+from .sumofgauss import sum_of_gaussian_factory
