2ef7c3d8f1b9f3cfa70360541ee6c66889cc37ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:05 +0000

    Add package spotify-api.py/0.0.3/spotify_api.py-0.0.3-py3-none-any.whl

---
+++ packages/spotify-api.py/spotify_api.py-0.0.3-py3-none-any.whl/spotifyapi/Playlist.py
@@ -0,0 +1,33 @@
+# Import Packages
+from .Util import encodeURIComponent
+from .Exception import *
+import requests
+
+# Playlist Class
+class Playlist():
+    def __init__(self, token: str):
+        self.token = token
+
+    def get(self, PlaylistID: str):
+        return requests.request(
+            'GET',
+            'https://api.spotify.com/v1/playlists/' + PlaylistID,
+            headers={'Authorization': 'Bearer ' + self.token},
+            params={
+                'market': 'US'
+            }
+        ).json()
+
+    def tracks(self, PlaylistID: str, limit: int = 1):
+        if not 0 < limit < 50:
+            raise LimitOutOfRangeError('limit must be under 50')
+        return requests.request(
+            'GET',
+            'https://api.spotify.com/v1/playlists/' + PlaylistID +
+            "/tracks,
+            headers={'Authorization': 'Bearer ' + self.token},
+            params={
+                'market': 'US',
+                'limit': limit
+            }
+        ).json()
