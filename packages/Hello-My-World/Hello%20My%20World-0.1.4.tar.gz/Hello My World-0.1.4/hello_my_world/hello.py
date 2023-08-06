3a1fe4428ed2dd6f9925ec4dc1c7c97c5b83e86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:54:21 +0000

    Add package Hello-My-World/0.1.4/Hello%20My%20World-0.1.4.tar.gz

---
+++ packages/Hello-My-World/Hello%20My%20World-0.1.4.tar.gz/Hello My World-0.1.4/hello_my_world/hello.py
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+"""This is my first professional package"""
+
+=======
+>>>>>>> 33579dfa140191a250a2d4af34aeebc12e8a6659
+def hello():
+    return 'Hello world'
---
+++ packages/Hello-My-World/Hello%20My%20World-0.1.4.tar.gz/Hello My World-0.1.4/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+project_urls = {
+    "Homepage": "https://github.com/eocode/Hello-My-World",
+    "Issue tracker": "https://github.com/eocode/Hello-My-World/issues",
+    "Code": "https://github.com/eocode/Hello-My-World",
+    "Documentation": "https://github.com/eocode/Hello-My-World/wiki",
+}
+
+setuptools.setup(
+    name='Hello My World',
+    version='0.1.4',
+    author='eocode',
+    author_email='hola@eliasojedamedina.com',
+    description='A simple package for python',
+    license = 'MIT License',
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read()
+    + "\n\n"
+    + open("CHANGELOG.md").read(),
+    packages=setuptools.find_packages(exclude=("tests",)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    project_urls=project_urls,
+    install_requires=[],
+    python_requires=">=3.6",
+)
