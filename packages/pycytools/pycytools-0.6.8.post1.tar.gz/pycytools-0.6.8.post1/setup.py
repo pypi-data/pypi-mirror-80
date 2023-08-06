82f0ee3c81e973c44da0c0dc4ace99bc705f8d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:35 +0000

    Add package pycytools/0.6.8.post1/pycytools-0.6.8.post1.tar.gz

---
+++ packages/pycytools/pycytools-0.6.8.post1.tar.gz/pycytools-0.6.8.post1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='pycytools',
+    version='0.6.8post1',
+    description='Helper functions to handle image cytometry data.',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='Vito Zanotelli, Matthias Leutenegger, Bodenmiller Lab UZH',
+    author_email='vito.zanotelli@gmail.com',
+    url='https://github.com/BodenmillerGroup/pycytools',
+    license='BSD-3 License',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires = ['numpy',
+                        'scipy',
+                        'pandas',
+                        'requests',
+                        'scikit-image',
+                        'tifffile'
+                        ],
+)
+
