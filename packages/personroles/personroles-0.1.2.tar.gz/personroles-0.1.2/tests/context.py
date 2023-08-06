33223d5a7f61c7d7119bc45054ea710a92127aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:34 +0000

    Add package personroles/0.1.2/personroles-0.1.2.tar.gz

---
+++ packages/personroles/personroles-0.1.2.tar.gz/personroles-0.1.2/tests/context.py
@@ -0,0 +1,24 @@
+# context.py
+import os
+import sys
+
+PACKAGE_PARENT = ".."
+SCRIPT_DIR = os.path.dirname(
+    os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__)))
+)  # isort:skip # noqa # pylint: disable=wrong-import-position
+sys.path.append(
+    os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT))
+)  # isort: skip # noqa # pylint: disable=wrong-import-position
+
+from personroles import (  # type: ignore # isort:skip # noqa # pylint: disable=unused-import, wrong-import-position
+    person,
+)
+
+from personroles.resources import (  # type: ignore # isort:skip # noqa # pylint: disable=unused-import, wrong-import-position
+    constants,
+    helpers,
+)
+from personroles import (  # type: ignore # isort:skip # noqa # pylint: disable=unused-import, wrong-import-position
+    mop_role,
+    politician_role,
+)
