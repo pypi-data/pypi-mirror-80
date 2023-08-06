ad154020a0f32b867d39e942b3756952640fd78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:04 +0000

    Add package geofileops/0.0.9/geofileops-0.0.9.tar.gz

---
+++ packages/geofileops/geofileops-0.0.9.tar.gz/geofileops-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="geofileops", 
+    version="0.0.9",
+    author="Pieter Roggemans",
+    author_email="pieter.roggemans@gmail.com",
+    description="Package to do spatial operations on geo files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/theroggy/geofileops",
+    packages=setuptools.find_packages(),
+    install_requires=["geopandas>=0.8", "pygeos", "psutil"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
