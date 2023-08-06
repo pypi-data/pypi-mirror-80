ae7f0c7e8de6b07f27adf3a8f15c840e5417b878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:23 +0000

    Add package GnuplotPy3/0.0.2/GnuplotPy3-0.0.2-py3-none-any.whl

---
+++ packages/GnuplotPy3/GnuplotPy3-0.0.2-py3-none-any.whl/GnuplotPy3/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .GnuplotPy3 import GnuplotPy3
+
+__all__ = [
+            "GnuplotPy3",
+          ]
+
+version = "0.0.2"
+
+print("""\
+
+Welcome to GnuplotPy3_{VERSION}""".format( VERSION = version ) )
+print()
