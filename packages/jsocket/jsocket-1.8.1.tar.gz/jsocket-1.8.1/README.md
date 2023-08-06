d0461fa48257ad83e94be64901ca52de17371531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:05 +0000

    Add package jsocket/1.8.1/jsocket-1.8.1.tar.gz

---
+++ packages/jsocket/jsocket-1.8.1.tar.gz/jsocket-1.8.1/README.md
@@ -0,0 +1,16 @@
+
+The final Python 2 package version is 1.6.1  
+Python 2 PyPi: https://pypi.org/project/jsocket/1.6.1/  
+Python 3 PyPi: https://pypi.org/project/jsocket/1.7/
+
+
+pip install jsocket   
+
+Example:  
+
+See the examples_servers.py file for an example of how to use the threaded server factory feature of this module.
+Can also upload the python_json_socket_3.ipynb notebook to Google colab
+
+Unit Test:
+Lettuce unit test exists under features directory
+See that directories README for instructions on installing python3 lettuce fork
---
+++ packages/jsocket/jsocket-1.8.1.tar.gz/jsocket-1.8.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, Extension
+
+setup(name='jsocket',
+      version='1.8.1',
+      description='Python JSON Server & Client',
+      author='Christopher Piekarski',
+      author_email='chris@cpiekarski.com',
+      maintainer='Christopher Piekarski',
+      maintainer_email='chris@cpiekarski.com',
+      license='OSI Approved Apache Software License',
+      keywords=['json','socket','server','client'],
+      packages=['jsocket'],
+      provides=['jsocket'],
+      python_requires='>=3.5',
+      classifiers=[
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Programming Language :: Python :: 3.5',
+                   'Operating System :: OS Independent',
+                   'Development Status :: 5 - Production/Stable',
+                   'Topic :: System :: Networking',
+                   'Topic :: Software Development :: Libraries :: Application Frameworks',
+                   'Topic :: System :: Distributed Computing',
+                   'Topic :: System :: Hardware :: Symmetric Multi-processing',
+                   ],
+      url='https://cpiekarski.com/2012/01/25/python-json-client-server-redux/'
+     )
