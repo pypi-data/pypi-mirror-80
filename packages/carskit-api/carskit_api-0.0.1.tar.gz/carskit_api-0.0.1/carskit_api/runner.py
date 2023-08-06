3d8de43ed62b372dc0da0c9ebf9bfc5e052ccb94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:32 +0000

    Add package carskit-api/0.0.1/carskit_api-0.0.1.tar.gz

---
+++ packages/carskit-api/carskit_api-0.0.1.tar.gz/carskit_api-0.0.1/carskit_api/runner.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+
+class Runner(object):
+    """
+    Class that runs the CARSKit java file.
+    It is only need the complete or relative path to the file to run it.
+    """
+
+    def __init__(self, javaFile_path: str):
+        self.javaFile_path = javaFile_path
+        self.current_path = os.path.abspath(os.getcwd())
+
+    def run_engine(self) -> str:
+        """
+        Runs the Java file and return the output of the execution.
+        """
+        os.chdir(self.javaFile_path)
+        output = subprocess.call(
+            ['java', '-jar', 'CARSKit-v0.3.5.jar', '-c', 'test.conf'])
+        os.chdir(self.current_path)
+        return output
