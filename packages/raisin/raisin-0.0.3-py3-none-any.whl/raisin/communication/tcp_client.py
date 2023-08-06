3c5be74349df246f4eb18a4c62d9e433c2088f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:11 +0000

    Add package raisin/0.0.3/raisin-0.0.3-py3-none-any.whl

---
+++ packages/raisin/raisin-0.0.3-py3-none-any.whl/raisin/communication/tcp_client.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import socket
+import select
+import sys
+import threading
+
+import raisin
+
+
+class ClientIpv4:
+    """
+    permet d'avoir un dialogue avec un serveur
+    """
+    def __init__(self, ipv4, port, parallelization_rate, signature):
+        assert 0 <= parallelization_rate <= 2
+        self.ipv4 = ipv4
+        self.port = port
+        self.parallelization_rate = parallelization_rate
+        self.signature = signature
+        self.must_die = False
+
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # creation d'un TCP/IP socket, SOCK_STREAM=>TCP
+        self.tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # on tente de reutiliser le port si possible
+        self.tcp_socket.connect((self.ipv4, self.port))                       # on etabli la connection
+
+    def send(self, question):
+        """
+        envoi une question brute, non serialisee.
+        ne retourne rien mais cette methode bloque tant que la question n'est pas partie
+        """
+        with raisin.Printer("Envoi de la question '%s'..." % question, signature=self.signature):
+            raisin.communication.tcp_server.send(
+                self.tcp_socket,
+                raisin.serialize(
+                    question,
+                    parallelization_rate=self.parallelization_rate,
+                    compresslevel=(-1 if self.parallelization_rate else 0),
+                    signature=self.signature),
+                signature=self.signature)
+
+    def receive(self):
+        """
+        attend que le serveur reponde.
+        retourne la reponsse deserialise du serveur
+        """
+        with raisin.Printer("Reception de la reponse...", signature=self.signature):
+            data = raisin.communication.tcp_server.receive(self.tcp_socket, signature=self.signature)
+
+        is_direct = data[0]
+        if is_direct:
+            return raisin.deserialize(data[1:], parallelization_rate=self.parallelization_rate, psw=None, signature=self.signature)
+        else:
+            with open(data[1:].decode("utf-8"), "rb") as f:
+                reply = raisin.deserialize(f, parallelization_rate=self.parallelization_rate, psw=None, signature=self.signature)
+            os.remove(data[1:].decode("utf-8"))
+            return reply
+
+    def close(self):
+        self.must_die = True
+        self.tcp_socket.close()
+
+    def __del__(self):
+        self.close()
+
+class ClientIpv6:
+    """
+    permet d'avoir un dialogue avec un serveur
+    """
+    def __init__(self, ipv6, port, parallelization_rate, signature):
+        assert 0 <= parallelization_rate <= 2
+        self.ipv6 = ipv6
+        self.port = port
+        self.parallelization_rate = parallelization_rate
+        self.signature = signature
+        self.must_die = False
+
+        self.tcp_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        self.tcp_socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, False)
+        self.tcp_socket.connect((self.ipv6, self.port))
+
+    def send(self, question):
+        """
+        envoi une question brute, non serialisee.
+        ne retourne rien mais cette methode bloque tant que la question n'est pas partie
+        """
+        with raisin.Printer("Envoi de la question '%s'..." % question, signature=self.signature):
+            raisin.communication.tcp_server.send(
+                self.tcp_socket,
+                raisin.serialize(
+                    question,
+                    parallelization_rate=self.parallelization_rate,
+                    compresslevel=(-1 if self.parallelization_rate else 0),
+                    signature=self.signature),
+                signature=self.signature)
+
+    def receive(self):
+        """
+        attend que le serveur reponde.
+        retourne la reponsse deserialise du serveur
+        """
+        with raisin.Printer("Reception de la reponse...", signature=self.signature):
+            data = raisin.communication.tcp_server.receive(self.tcp_socket, signature=self.signature)
+
+        is_direct = data[0]
+        if is_direct:
+            return raisin.deserialize(data[1:], parallelization_rate=self.parallelization_rate, psw=None, signature=self.signature)
+        else:
+            with open(data[1:].decode("utf-8"), "rb") as f:
+                reply = raisin.deserialize(f, parallelization_rate=self.parallelization_rate, psw=None, signature=self.signature)
+            os.remove(data[1:].decode("utf-8"))
+            return reply
+
+    def close(self):
+        self.must_die = True
+        self.tcp_socket.close()
+
+    def __del__(self):
+        self.close()
+
