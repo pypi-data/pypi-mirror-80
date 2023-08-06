fc8622d29be23e431d4f95ef8865359dece523bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:42 +0000

    Add package Hypyxel/0.0.7/Hypyxel-0.0.7.tar.gz

---
+++ packages/Hypyxel/Hypyxel-0.0.7.tar.gz/Hypyxel-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Hypyxel", 
+    version="0.0.7",
+    author="CraziiAce",
+    author_email="teddyjraz@gmail.com",
+    description="A python wrapper for the Hypixel API",
+    long_description="Usage: \n`from hypyxel import hypyxel`\n`hypyxel.get_endpoints()`",
+    long_description_content_type="text/markdown",
+    url="https://github.com/CraziiAce/Hypyxel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
