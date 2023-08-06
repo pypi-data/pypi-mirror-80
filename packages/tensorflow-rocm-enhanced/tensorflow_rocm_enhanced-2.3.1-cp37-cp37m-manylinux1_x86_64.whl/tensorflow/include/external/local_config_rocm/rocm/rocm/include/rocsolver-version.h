2dcf1c516aea4c7a8ce18b07953eac49de7cd75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:02 +0000

    Add package tensorflow-rocm-enhanced/2.3.1/tensorflow_rocm_enhanced-2.3.1-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/tensorflow-rocm-enhanced/tensorflow_rocm_enhanced-2.3.1-cp37-cp37m-manylinux1_x86_64.whl/tensorflow/include/external/local_config_rocm/rocm/rocm/include/rocsolver-version.h
@@ -0,0 +1,17 @@
+/* ************************************************************************
+ * Copyright 2019-2020 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+/* the configured version and settings
+ */
+#ifndef ROCSOLVER_VERSION_H_
+#define ROCSOLVER_VERSION_H_
+
+// clang-format off
+#define ROCSOLVER_VERSION_MAJOR 3
+#define ROCSOLVER_VERSION_MINOR 8
+#define ROCSOLVER_VERSION_PATCH 0
+#define ROCSOLVER_VERSION_TWEAK 144-rocm-rel-3.8-30-dcf793d
+// clang-format on
+
+#endif
