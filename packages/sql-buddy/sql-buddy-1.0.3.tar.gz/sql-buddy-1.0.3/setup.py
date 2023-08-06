8213e7a9f183584d642c3f83e078a236fd236407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:57 +0000

    Add package sql-buddy/1.0.3/sql-buddy-1.0.3.tar.gz

---
+++ packages/sql-buddy/sql-buddy-1.0.3.tar.gz/sql-buddy-1.0.3/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+
+setup(
+	name="sql-buddy",
+    version="1.0.3",
+    description="SQL study aid for the command line built with Python",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Captmoonshot/sql-buddy",
+    author="Sammy Lee",
+    author_email="sam@gygantor.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["sql_buddy"],
+    include_package_data=True,
+    install_requires=["prompt-toolkit==3.0.7", "Pygments==2.7.1", "sqlalchemy==1.3.19", "sqlalchemy-utils==0.36.6"],
+    entry_points={
+        "console_scripts": [
+            "sql_buddy=sql_buddy.__main__:main",
+        ]
+    },
+)
+
+
