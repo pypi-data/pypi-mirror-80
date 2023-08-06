a994b7215e4b8ce8443122f47710deeb60879836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:57 +0000

    Add package b-rabbit/1.2.1/b_rabbit-1.2.1-py2.py3-none-any.whl

---
+++ packages/b-rabbit/b_rabbit-1.2.1-py2.py3-none-any.whl/b_rabbit/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for b_rabbit."""
+
+from .b_rabbit import BRabbit
+
+__author__ = """Nidhal Baccouri"""
+__email__ = 'nidhalbacc@gmail.com'
+__version__ = '1.2.1'
+
+__all__ = [BRabbit]
