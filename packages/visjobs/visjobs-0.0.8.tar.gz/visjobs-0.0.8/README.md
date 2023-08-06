fc3a4ba62e2d2fad9d6a97bf3f4dfcd6e1bd264d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:39 +0000

    Add package visjobs/0.0.8/visjobs-0.0.8.tar.gz

---
+++ packages/visjobs/visjobs-0.0.8.tar.gz/visjobs-0.0.8/README.md
@@ -0,0 +1,26 @@
+# visjobs
+
+Visjobs offers plotting effective variables in effective way using atmospheric models.
+
+## 1.Installation
+
+#### 1.1. Dependencies
+- xarray
+- siphon
+- pandas
+- numpy
+- matplotlib
+- cartopy
+- datetime
+
+#### 1.2. Pypi Installation
+- pip install visjobs
+
+#### 1.3. Github Clone
+- git clone https://github.com/donmezkutay/visjobs
+
+## 2. Usage
+Usage can be found at [Visjobs Documentation Page]
+
+[Visjobs Documentation Page]: <https://donmezkutay.github.io/visjobs/>
+
---
+++ packages/visjobs/visjobs-0.0.8.tar.gz/visjobs-0.0.8/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="visjobs", # Replace with your own username
+    version="0.0.8",
+    author="Kutay Donmez & Berkay Donmez",
+    author_email="donmezk@outlook.com",
+    description="Get The Latest Atmospheric Model Data | Analyse | Visualize Easily",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/donmezkutay/visjobs",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "xarray",
+        "siphon",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "cartopy",
+        "datetime"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
