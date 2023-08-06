d16beeed8b3bc636fa4b17da6b2ce381ae116447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:39 +0000

    Add package ZnPyobject/0.0.2/ZnPyobject-0.0.2-py3-none-any.whl

---
+++ packages/ZnPyobject/ZnPyobject-0.0.2-py3-none-any.whl/ZnPyobject/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/27 下午6:29
+# @Author  : jinzening/jingpengyu
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+def znpyadd(a, b):
+    return a + b
+
+
+def znpyreduce(a, b):
+    return a - b
+
+
+def znpyride(a, b):
+    return a * b
