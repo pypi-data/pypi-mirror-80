c728d7bf4515953dcdaef7934f1b9dfc26ca7bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:13 +0000

    Add package pytest-html-reporter/0.2.3/pytest-html-reporter-0.2.3.tar.gz

---
+++ packages/pytest-html-reporter/pytest-html-reporter-0.2.3.tar.gz/pytest-html-reporter-0.2.3/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding="utf-8").read()
+
+
+setup(
+    name="pytest-html-reporter",
+    version="0.2.3",
+    author="Prashanth Sams",
+    author_email="sams.prashanth@gmail.com",
+    maintainer="Prashanth Sams",
+    maintainer_email="sams.prashanth@gmail.com",
+    license="MIT",
+    url="https://github.com/prashanth-sams/pytest-html-reporter",
+    description="Generates a static html report based on pytest framework",
+    long_description=read("README.rst"),
+    keywords=["pytest", "py.test", "html", "reporter", "report"],
+    packages=find_packages(),
+    python_requires=">=3.5",
+    install_requires=["pytest", "Pillow"],
+    classifiers=[
+        "Framework :: Pytest",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    entry_points={
+        "pytest11": [
+            "reporter = pytest_html_reporter.plugin",
+        ],
+    },
+)
