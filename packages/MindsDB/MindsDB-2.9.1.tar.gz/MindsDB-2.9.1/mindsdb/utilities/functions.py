bfbbca772588323fa52008313e9d66b36cf51060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:26 +0000

    Add package MindsDB/2.9.1/MindsDB-2.9.1.tar.gz

---
+++ packages/MindsDB/MindsDB-2.9.1.tar.gz/MindsDB-2.9.1/mindsdb/utilities/functions.py
@@ -0,0 +1,23 @@
+import argparse
+import datetime
+
+
+def args_parse():
+    parser = argparse.ArgumentParser(description='CL argument for mindsdb server')
+    parser.add_argument('--api', type=str, default=None)
+    parser.add_argument('--config', type=str, default=None)
+    return parser.parse_args()
+
+
+def cast_row_types(row, field_types):
+    '''
+    '''
+    keys = [x for x in row.keys() if x in field_types]
+    for key in keys:
+        t = field_types[key]
+        if t == 'Timestamp' and row[key] is not None:
+            timestamp = datetime.datetime.utcfromtimestamp(row[key])
+            row[key] = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+        elif t == 'Date':
+            timestamp = datetime.datetime.utcfromtimestamp(row[key])
+            row[key] = timestamp.strftime('%Y-%m-%d')
