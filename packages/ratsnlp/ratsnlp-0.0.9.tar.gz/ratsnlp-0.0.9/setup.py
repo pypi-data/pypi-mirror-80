211a4a51145a27c4d204ad8d3179ae535d334fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:58:27 +0000

    Add package ratsnlp/0.0.9/ratsnlp-0.0.9.tar.gz

---
+++ packages/ratsnlp/ratsnlp-0.0.9.tar.gz/ratsnlp-0.0.9/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+setuptools.setup(
+    name="ratsnlp",
+    version="0.0.9",
+    license='MIT',
+    author="ratsgo",
+    author_email="ratsgo@naver.com",
+    description="tools for Natural Language Processing",
+    long_description=open('README.md').read(),
+    url="https://github.com/ratsgo/ratsnlp",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data = {
+        'ratsnlp.nlpbook.classification': ['*.html'],
+        'ratsnlp.nlpbook.ner': ['*.html']
+    },
+    install_requires=[
+        "torch>=1.5.1",
+        "pytorch-lightning==0.8.5",
+        "transformers==3.0.2",
+        "Korpora>=0.1.0",
+        "flask==1.1.2",
+        "flask_cors==3.0.8",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
