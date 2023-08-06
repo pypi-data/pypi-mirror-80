90870474ae1ffc328db9b23c269b42c74f720d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:27 +0000

    Add package streamlit-pandas-profiling/0.0.5/streamlit_pandas_profiling-0.0.5-py3-none-any.whl

---
+++ packages/streamlit-pandas-profiling/streamlit_pandas_profiling-0.0.5-py3-none-any.whl/streamlit_pandas_profiling/frontend/build/precache-manifest.4e82b27059dfc521a9b83b0923ec1c2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a681fa2c176c76b9d727ee4d33b41564",
+    "url": "./index.html"
+  },
+  {
+    "revision": "54b8698ed5877b71d6dd",
+    "url": "./static/js/2.cd4c7de9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.cd4c7de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ebd2fbdb38c54047af4",
+    "url": "./static/js/main.5f04e9d6.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.5f04e9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca1f16084572af38cc0",
+    "url": "./static/js/runtime-main.f973e952.js"
+  }
+]);
