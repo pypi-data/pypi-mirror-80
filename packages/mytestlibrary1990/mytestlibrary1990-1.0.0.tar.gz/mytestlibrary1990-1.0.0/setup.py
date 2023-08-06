faec45647b9bf74872876007b19fd8e6765d0ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:17 +0000

    Add package mytestlibrary1990/1.0.0/mytestlibrary1990-1.0.0.tar.gz

---
+++ packages/mytestlibrary1990/mytestlibrary1990-1.0.0.tar.gz/mytestlibrary1990-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlibrary1990',
+    version='1.0.0',
+    #url='https://pypi.org/',
+    author='AA',
+    author_email='a-andou@kbsanhe.co.jp',
+    description='My Test Library.',
+    packages=find_packages()
+)
