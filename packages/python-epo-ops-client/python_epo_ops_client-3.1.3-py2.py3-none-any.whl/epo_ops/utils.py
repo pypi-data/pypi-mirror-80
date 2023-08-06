08c3a6c0a3fe5a00478ea21e6639eac9b8c6666d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:12 +0000

    Add package python-epo-ops-client/3.1.3/python_epo_ops_client-3.1.3-py2.py3-none-any.whl

---
+++ packages/python-epo-ops-client/python_epo_ops_client-3.1.3-py2.py3-none-any.whl/epo_ops/utils.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+import re
+from datetime import datetime
+
+from dateutil.tz import tzutc
+from six.moves import urllib
+
+from .exceptions import InvalidDate
+
+log = logging.getLogger(__name__)
+
+
+def makedirs(path, mode=0o777):
+    try:
+        os.makedirs(path, mode)
+    except OSError:
+        pass
+
+
+def now():
+    return datetime.now(tzutc())
+
+
+def quote(string):
+    parsed = urllib.parse.quote(string, safe="/\\")
+    return re.sub(r"~", "%7E", parsed)
+
+
+def validate_date(date):
+    if date is None or date == "":
+        return ""
+    try:
+        datetime.strptime(date, "%Y%m%d")
+        return date
+    except ValueError:
+        raise InvalidDate("{0} is not a valid YYYYMMDD date.".format(date))
