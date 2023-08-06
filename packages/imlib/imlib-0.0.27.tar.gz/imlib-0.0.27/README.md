d2be0edb913a137a94bff99d822d60550824e063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:12 +0000

    Add package imlib/0.0.27/imlib-0.0.27.tar.gz

---
+++ packages/imlib/imlib-0.0.27.tar.gz/imlib-0.0.27/README.md
@@ -0,0 +1,12 @@
+[![Python Version](https://img.shields.io/pypi/pyversions/imlib.svg)](https://pypi.org/project/imlib)
+[![PyPI](https://img.shields.io/pypi/v/imlib.svg)](https://pypi.org/project/imlib)
+[![Wheel](https://img.shields.io/pypi/wheel/imlib.svg)](https://pypi.org/project/imlib)
+[![Development Status](https://img.shields.io/pypi/status/imlib.svg)](https://github.com/adamltyson/imlib)
+[![Travis](https://img.shields.io/travis/com/adamltyson/imlib?label=Travis%20CI)](
+    https://travis-ci.com/adamltyson/imlib)
+[![Coverage Status](https://coveralls.io/repos/github/adamltyson/imlib/badge.svg?branch=master)](https://coveralls.io/github/adamltyson/imlib?branch=master)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/python/black)
+
+
+# imlib
+General image processing functions
---
+++ packages/imlib/imlib-0.0.27.tar.gz/imlib-0.0.27/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_namespace_packages
+
+requirements = [
+    "natsort",
+    "pandas",
+    "psutil",
+    "slurmio",
+    "configobj",
+    "micrometa",
+    "tqdm",
+    "PyYAML",
+    "scipy",
+    "scikit-image",
+    "seaborn",
+    "vtkplotter",
+]
+
+
+setup(
+    name="imlib",
+    version="0.0.27",
+    description="General data processing functions",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "sphinx",
+            "recommonmark",
+            "sphinx_rtd_theme",
+            "pydoc-markdown",
+            "black",
+            "pytest-cov",
+            "pytest",
+            "gitpython",
+            "coverage",
+        ]
+    },
+    python_requires=">=3.7",
+    packages=find_namespace_packages(exclude=("docs", "tests*")),
+    include_package_data=True,
+    url="https://github.com/adamltyson/imlib",
+    author="Adam Tyson",
+    author_email="adam.tyson@ucl.ac.uk",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    zip_safe=False,
+)
