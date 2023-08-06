322b278b284858fb705c6aaf7fc181d8d6e56799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:10 +0000

    Add package djangomosquito/0.0.4/djangomosquito-0.0.4.tar.gz

---
+++ packages/djangomosquito/djangomosquito-0.0.4.tar.gz/djangomosquito-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import re
+from setuptools import setup
+from os import path
+
+project_path = path.abspath(path.dirname(__file__))
+
+with open(path.join(project_path, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name = 'djangomosquito',
+    packages = ['djangomosquito'],
+    license = 'MIT',
+    version = '0.0.4',
+    description = 'Limit visitors to your django app by count',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    author = 'Sina Farhadi',
+    author_email = 'sina.farhadi.cyber@gmail.com',
+    url = 'https://github.com/E-RROR/django-mosquito/',
+    keywords = ['django','visit','filter','limit','mosquito', 'django mosquito'],
+    classifiers = [
+        "Framework :: Django"
+    ],
+)
