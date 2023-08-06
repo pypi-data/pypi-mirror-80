0635a6ea95615444698a554fbf4f5d524b62e164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:52 +0000

    Add package ec2ools/0.2.1/ec2ools-0.2.1.tar.gz

---
+++ packages/ec2ools/ec2ools-0.2.1.tar.gz/ec2ools-0.2.1/ec2ools/aws/eip.py
@@ -0,0 +1,51 @@
+import sys
+import os
+from ec2ools import aws
+from ec2ools.aws import MetaService
+
+
+class EipBase:
+
+    def allocate_eip(self, alloc_id):
+        """
+        """
+        meta = MetaService()
+        instance_id = meta.query('instance-id')
+        eip_info = self._query_eip(alloc_id)
+
+        if not eip_info:
+            raise Exception("Invalid AllocationID")
+
+        aws.client('ec2').associate_address(
+            InstanceId=instance_id,
+            AllocationId=eip_info['AllocationId'])
+
+    def _query_eip(self, alloc_id):
+        """ Query EIP record by allocation ID
+
+        Args:
+            alloc_id (str): Allocation ID of path to file containing Allocation ID
+
+        Return
+            (dict): The EIP record dict
+        """
+        if self._is_path(alloc_id):
+            alloc_id = self._read_path(alloc_id)
+
+        ip = aws.client('ec2').describe_addresses(AllocationIds=[alloc_id])
+
+        if ip.get('Addresses'):
+            return ip.get('Addresses')[0]
+
+    def _is_path(self, path):
+        """
+        """
+        if os.path.exists(path):
+            return True
+        return False
+
+    def _read_path(self, path):
+        """
+        """
+        with open(path, 'r') as f:
+            return f.read().strip()
---
+++ packages/ec2ools/ec2ools-0.2.1.tar.gz/ec2ools-0.2.1/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+with open('requirements.txt', 'r') as reqs:
+    requirements = reqs.read().split('\n')
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="John Hardy",
+    author_email='john@johnchardy.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Utilities to manage ec2 instances and asg's",
+    entry_points={
+        'console_scripts': [
+            'ec2ools=ec2ools.cli:main',
+            'ec2ools-eip=ec2ools.cli_eip:main',
+            'ec2ools-asg=ec2ools.cli_asg:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='ec2ools',
+    name='ec2ools',
+    packages=find_packages(),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/ibejohn818/ec2ools',
+    version='0.2.1',
+    zip_safe=False,
+)
