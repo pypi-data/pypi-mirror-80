72e1cdfad5c7ffa2fe62868b76ab281766d88197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:52 +0000

    Add package py-ms/2.6.1/py-ms-2.6.1.tar.gz

---
+++ packages/py-ms/py-ms-2.6.1.tar.gz/py-ms-2.6.1/pyms/flask/services/metrics.py
@@ -0,0 +1,84 @@
+import time
+import logging
+
+from flask import Blueprint, Response, request
+from prometheus_client import Counter, Histogram, generate_latest
+from pyms.flask.services.driver import DriverService
+
+# Based on https://github.com/sbarratt/flask-prometheus
+# and https://github.com/korfuri/python-logging-prometheus/
+
+FLASK_REQUEST_LATENCY = Histogram(
+    "http_server_requests_seconds", "Flask Request Latency", ["service", "method", "uri", "status"]
+)
+FLASK_REQUEST_COUNT = Counter(
+    "http_server_requests_count", "Flask Request Count", ["service", "method", "uri", "status"]
+)
+
+LOGGER_TOTAL_MESSAGES = Counter(
+    "logger_messages_total",
+    "Count of log entries by service and level.",
+    ["service", "level"],
+)
+
+
+class FlaskMetricsWrapper():
+    def __init__(self, app_name):
+        self.app_name = app_name
+
+    def before_request(self):  # pylint: disable=R0201
+        request.start_time = time.time()
+
+    def after_request(self, response):
+        if hasattr(request.url_rule, "rule"):
+            path = request.url_rule.rule
+        else:
+            path = request.path
+        request_latency = time.time() - request.start_time
+        FLASK_REQUEST_LATENCY.labels(self.app_name, request.method, path, response.status_code).observe(request_latency)
+        FLASK_REQUEST_COUNT.labels(self.app_name, request.method, path, response.status_code).inc()
+
+        return response
+
+
+class Service(DriverService):
+    """
+    Adds [Prometheus](https://prometheus.io/) metrics using the [Prometheus Client Library](https://github.com/prometheus/client_python).
+    """
+    config_resource = "metrics"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.metrics_blueprint = Blueprint("metrics", __name__)
+        self.serve_metrics()
+
+    @staticmethod
+    def monitor(app_name, app):
+        metric = FlaskMetricsWrapper(app_name)
+        app.before_request(metric.before_request)
+        app.after_request(metric.after_request)
+
+    def serve_metrics(self):
+        @self.metrics_blueprint.route("/metrics", methods=["GET"])
+        def metrics():  # pylint: disable=unused-variable
+            return Response(
+                generate_latest(),
+                mimetype="text/print()lain",
+                content_type="text/plain; charset=utf-8",
+            )
+
+    @staticmethod
+    def add_logger_handler(logger, service_name):
+        logger.addHandler(MetricsLogHandler(service_name))
+        return logger
+
+
+class MetricsLogHandler(logging.Handler):
+    """A LogHandler that exports logging metrics for Prometheus.io."""
+
+    def __init__(self, app_name):
+        super(MetricsLogHandler, self).__init__()
+        self.app_name = app_name
+
+    def emit(self, record):
+        LOGGER_TOTAL_MESSAGES.labels(self.app_name, record.levelname).inc()
---
+++ packages/py-ms/py-ms-2.6.1.tar.gz/py-ms-2.6.1/setup.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 by Alberto Vara <albertovara@paradigmadigital.com>
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+author = __import__('pyms').__author__
+author_email = __import__('pyms').__email__
+version = __import__('pyms').__version__
+
+if os.path.exists('README.md'):
+    long_description = codecs.open('README.md', 'r', 'utf-8').read()
+else:
+    long_description = ''
+
+install_min_requires = [
+    'flask>=1.1.2',
+    'python-json-logger>=0.1.10',
+    'pyyaml>=5.3.1',
+    'anyconfig>=0.9.11',
+    'cryptography>=2.9.2',
+]
+
+install_crypt_requires = [
+    'cryptography>=2.9.2',
+]
+
+install_aws_requires = [
+    'boto3==1.13.24',
+]
+
+
+install_request_requires = [
+    'requests>=2.23.0',
+]
+install_swagger_requires = [
+    'swagger-ui-bundle>=0.0.2',
+    'connexion[swagger-ui]>=2.7.0',
+    'swagger-ui-bundle>=0.0.6',
+    'semver>=2.10.1',
+    'prance>=0.18.2',
+]
+
+install_traces_requires = [
+    'jaeger-client>=4.3.0',
+    'flask-opentracing>=1.1.0',
+    'opentracing>=2.1',
+    'opentracing-instrumentation>=3.2.1',
+    'tornado>=4.3,<6.0'
+]
+
+install_metrics_requires = [
+    'prometheus_client>=0.8.0',
+]
+
+install_tests_requires = [
+    'requests-mock>=1.8.0',
+    'coverage>=5.1',
+    'pytest>=5.4.3',
+    'pytest-cov>=2.9.0',
+    'pylint>=2.5.2',
+    'flake8>=3.8.2',
+    'tox>=3.14.6',
+    'bandit>=1.6.2',
+    'mkdocs>=1.1.2',
+    'mkdocs-material>=5.2.2',
+    'lightstep>=4.4.3',
+    'safety==1.9.0'
+]
+
+install_all_requires = (install_request_requires + install_swagger_requires +
+                        install_traces_requires + install_metrics_requires + install_crypt_requires +
+                        install_aws_requires)
+
+setup(
+    name="py-ms",
+    version=version,
+    author=author,
+    author_email=author_email,
+    description="Library of utils to create REST Python Microservices",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Flask',
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    license="License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    platforms=["any"],
+    keywords="",
+    url='https://github.com/python-microservices/pyms/',
+    packages=find_packages(
+        exclude=['*.tests', '*.tests.*', 'tests.*', 'tests', '*.examples', '*.examples.*', 'examples.*', 'examples']),
+    setup_requires=[
+        'pytest-runner>=2.0,<3dev',
+    ],
+    install_requires=install_min_requires,
+    extras_require={
+        'all': install_all_requires,
+        'request': install_request_requires,
+        'swagger': install_swagger_requires,
+        'traces': install_traces_requires,
+        'metrics': install_metrics_requires,
+        'crypt': install_crypt_requires,
+        'aws': install_aws_requires,
+        'tests': install_tests_requires,
+    },
+    tests_require=install_all_requires + install_tests_requires,
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'pyms = pyms.cmd.main:Command'
+        ]
+    },
+    zip_safe=True,
+)
