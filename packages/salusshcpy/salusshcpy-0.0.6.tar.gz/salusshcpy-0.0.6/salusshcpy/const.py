eaa84f4d070346079eee309348ac93fab64fff33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:05 +0000

    Add package salusshcpy/0.0.6/salusshcpy-0.0.6.tar.gz

---
+++ packages/salusshcpy/salusshcpy-0.0.6.tar.gz/salusshcpy-0.0.6/salusshcpy/const.py
@@ -0,0 +1,20 @@
+# Temperature units
+TEMP_CELSIUS = "°C"
+
+# Supported features
+SUPPORT_TARGET_TEMPERATURE = 1
+SUPPORT_PRESET_MODE = 16
+
+# HVAC modes
+HVAC_MODE_OFF = "off"
+HVAC_MODE_HEAT = "heat"
+
+# HVAC states
+CURRENT_HVAC_OFF = "off"
+CURRENT_HVAC_HEAT = "heating"
+CURRENT_HVAC_IDLE = "idle"
+
+# Supported presets
+PRESET_FOLLOW_SCHEDULE = "Follow Schedule"
+PRESET_PERMANENT_HOLD = "Permanent Hold"
+PRESET_OFF = "Off"
---
+++ packages/salusshcpy/salusshcpy-0.0.6.tar.gz/salusshcpy-0.0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="salusshcpy",
+    version="0.0.6",
+    author="Emre Bolat",
+    author_email="emre-bolat@hotmail.de",
+    description="Salus Smart Home Controller API Python Library",
+    license='bsd-3-clause',
+    packages=["salusshcpy"],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: BSD License"
+    ],
+    platform='any',
+    python_requires='>=3.7',
+    install_requires=['requests>=2.22']
+)
