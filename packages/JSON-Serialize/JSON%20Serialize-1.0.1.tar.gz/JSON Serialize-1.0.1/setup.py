4ac0ef8c0561f1cede79582e71fbae8450ab1b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:02 +0000

    Add package JSON-Serialize/1.0.1/JSON%20Serialize-1.0.1.tar.gz

---
+++ packages/JSON-Serialize/JSON%20Serialize-1.0.1.tar.gz/JSON Serialize-1.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+README = open('README.md').read()
+
+setuptools.setup(
+    name = 'JSON Serialize',
+    version = '1.0.1',
+    url = 'https://github.com/gaming32/json-serialize',
+    author = 'Gaming32',
+    author_email = 'gaming32i64@gmail.com',
+    license = 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    description = 'Module that serializes Python objects into JSON',
+    long_description = README,
+    long_description_content_type = 'text/markdown',
+    py_modules = [
+        'json_serialize',
+    ]
+)
