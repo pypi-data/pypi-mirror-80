964ee5f303dbfd5c2da8c047abfe841a53f4aeca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:42 +0000

    Add package mytestlib725/1.0.0/mytestlib725-1.0.0.tar.gz

---
+++ packages/mytestlib725/mytestlib725-1.0.0.tar.gz/mytestlib725-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+   name='mytestlib725',
+   version='1.0.0',
+   url='https://damykouda.org/',
+   author='TakuyaKouda',
+   author_email='t-kouda@kbsanhe.co.jp',
+   description='MY Test Library.',
+   packages=find_packages()
+
+)
