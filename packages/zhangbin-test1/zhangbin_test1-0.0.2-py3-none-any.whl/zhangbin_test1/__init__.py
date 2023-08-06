473b91299d71cb2a6a59d729fa75838cff48a16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:20 +0000

    Add package zhangbin-test1/0.0.2/zhangbin_test1-0.0.2-py3-none-any.whl

---
+++ packages/zhangbin-test1/zhangbin_test1-0.0.2-py3-none-any.whl/zhangbin_test1/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+from . import zhangbin_test1
+
+name = 'zhangbin_test1'
---
+++ packages/zhangbin-test1/zhangbin_test1-0.0.2-py3-none-any.whl/zhangbin_test1/zhangbin_test1.py
@@ -0,0 +1,3 @@
+def output():
+    print('You can do anything you set your mind to, man')
+    print('bin')
