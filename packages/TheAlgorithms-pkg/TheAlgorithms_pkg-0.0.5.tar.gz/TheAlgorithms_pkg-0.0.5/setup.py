b252af39ae00d5b8245f3a7407face0d57580555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:41 +0000

    Add package TheAlgorithms-pkg/0.0.5/TheAlgorithms_pkg-0.0.5.tar.gz

---
+++ packages/TheAlgorithms-pkg/TheAlgorithms_pkg-0.0.5.tar.gz/TheAlgorithms_pkg-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TheAlgorithms_pkg", # Replace with your own username
+    version="0.0.5",
+    author="Vireakpheakkdey Tiv",
+    author_email="vtiv@deakin.edu.au",
+    description="A sample package to demonstrate how the packaging work",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tivpheakkdey/TheAlgoritms_pkg",
+    download_url = "https://github.com/tivpheakkdey/TheAlgoritms_pkg/archive/0.0.5.tar.gz",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
