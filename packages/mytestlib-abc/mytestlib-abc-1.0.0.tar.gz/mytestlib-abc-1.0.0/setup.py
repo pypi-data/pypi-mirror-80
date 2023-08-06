3db37660c4890b12c9e6c1fd48ce3c9b44fc6db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:23 +0000

    Add package mytestlib-abc/1.0.0/mytestlib-abc-1.0.0.tar.gz

---
+++ packages/mytestlib-abc/mytestlib-abc-1.0.0.tar.gz/mytestlib-abc-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-abc',
+    version='1.0.0',
+    url='https://pypi.org/project/setuptools/',
+    author='タパ　ブディバル',
+    author_email='b-thapa@kbsanhe.co.jp',
+    description='My Test Library.',
+    packages=find_packages()
+)
