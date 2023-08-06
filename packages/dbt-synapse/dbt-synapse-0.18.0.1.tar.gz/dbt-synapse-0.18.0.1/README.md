c3adf8fcc84a6c9791db5e470e25200bcbaf8585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:29 +0000

    Add package dbt-synapse/0.18.0.1/dbt-synapse-0.18.0.1.tar.gz

---
+++ packages/dbt-synapse/dbt-synapse-0.18.0.1.tar.gz/dbt-synapse-0.18.0.1/README.md
@@ -0,0 +1,163 @@
+# dbt-synapse
+
+custom [dbt](https://www.getdbt.com) adapter for [Azure Synapse](https://azure.microsoft.com/en-us/services/synapse-analytics/). Major credit due to @mikaelene and [his `sqlserver` custom adapter](https://github.com/mikaelene/dbt-sqlserver).
+Passing all tests in [dbt-integration-tests](https://github.com/fishtown-analytics/dbt-integration-tests/). 
+
+## major differences b/w `dbt-synapse` and `dbt-sqlserver`
+- macros use only Azure Synapse `T-SQL`. [Relevant GitHub issue](https://github.com/MicrosoftDocs/azure-docs/issues/55713)
+- use of [Create Table as Select (CTAS)](https://docs.microsoft.com/en-us/sql/t-sql/statements/create-table-as-select-azure-sql-data-warehouse?view=aps-pdw-2016-au7) means you don't need post-hooks to create indices
+- Azure Active Directory Authentication options
+- rewrite of snapshots because Synapse doesn't support `MERGE`.
+- external table creation via details from yaml.
+  - must first create  `EXTERNAL DATA SOURCE` and `EXTERNAL FILE FORMAT`s.
+
+## status & support
+as of now, only support for dbt `0.18.0`
+
+Passing all tests in [dbt-adapter-tests](https://github.com/fishtown-analytics/dbt-adapter-tests), except `test_dbt_ephemeral_data_tests`
+
+### outstanding work:
+-  `ephemeral` materializations (workaround for non-recursive CTEs)
+- auto-create  `EXTERNAL DATA SOURCE` and `EXTERNAL FILE FORMAT`s.
+- [officially rename the adapter from `sqlserver` to `synapse`](https://github.com/swanderz/dbt-synapse/pull/6)
+- Use CTAS to create seeds?
+- Add support for `ActiveDirectoryMsi`
+
+## Installation
+Easiest install is to use pip (not yet registered on PyPI).
+
+First install [ODBC Driver version 17](https://www.microsoft.com/en-us/download/details.aspx?id=56567).
+
+```bash
+pip install dbt-synapse
+```
+On Ubuntu make sure you have the ODBC header files before installing
+
+```
+sudo apt install unixodbc-dev
+```
+
+## Authentication
+the following is needed for every target definition for both SQL Server and Azure SQL.  The sections below details how to connect to SQL Server and Azure SQL specifically.
+```
+type: sqlserver
+driver: 'ODBC Driver 17 for SQL Server' (The ODBC Driver installed on your system)
+server: server-host-name or ip
+port: 1433
+schema: schemaname
+```
+### standard SQL Server authentication
+SQL Server credentials are supported for on-prem as well as cloud, and it is the default authentication method for `dbt-sqlsever`
+```
+user: username
+password: password
+```
+### Azure SQL-specific auth
+The following [`pyodbc`-supported ActiveDirectory methods](https://docs.microsoft.com/en-us/sql/connect/odbc/using-azure-active-directory?view=sql-server-ver15#new-andor-modified-dsn-and-connection-string-keywords) are available to authenticate to Azure SQL:
+- ActiveDirectory Password
+- ActiveDirectory Interactive
+- ActiveDirectory Integrated
+- Service Principal (a.k.a. AAD Application)
+- ~~ActiveDirectory MSI~~ (not implemented)
+
+#### ActiveDirectory Password 
+Definitely not ideal, but available
+```
+authentication: ActiveDirectoryPassword
+user: bill.gates@microsoft.com
+password: i<3opensource?
+```
+#### ActiveDirectory Interactive (*Windows only*)
+brings up the Azure AD prompt so you can MFA if need be.
+```
+authentication: ActiveDirectoryInteractive
+user: bill.gates@microsoft.com
+```
+#### ActiveDirectory Integrated (*Windows only*)
+uses your machine's credentials (might be disabled by your AAD admins)
+```
+authentication: ActiveDirectoryIntegrated
+```
+##### Service Principal
+`client_*` and `app_*` can be used interchangeably
+```
+tenant_id: ActiveDirectoryIntegrated
+client_id: clientid
+client_secret: ActiveDirectoryIntegrated
+```
+
+## Table Materializations
+CTAS allows you to materialize tables with indices and distributions at creation time, which obviates the need for post-hooks to set indices.
+
+### Example
+You can also configure `index` and `dist` in `dbt_project.yml`.
+#### `models/stage/absence.sql
+```
+{{
+    config(
+        index='HEAP',
+        dist='ROUND_ROBIN'
+        )
+}}
+
+select *
+from ...
+```
+
+is turned into the relative form (minus `__dbt`'s `_backup` and `_tmp` tables)
+
+```SQL
+  CREATE TABLE ajs_stg.absence_hours
+    WITH(
+      DISTRIBUTION = ROUND_ROBIN,
+      HEAP
+      )
+    AS (SELECT * FROM ajs_stg.absence_hours__dbt_tmp_temp_view)
+```
+#### Indices
+- `CLUSTERED COLUMNSTORE INDEX` (default)
+- `HEAP`
+- `CLUSTERED INDEX ({COLUMN})`
+  
+#### Distributions
+- `ROUND_ROBIN` (default)
+- `HASH({COLUMN})`
+- `REPLICATE`
+
+## example `YAML` for defining external tables
+```YAML
+sources:
+  - name: raw
+    schema: source
+    loader: ADLSblob
+    tables:
+      - name: absence_hours
+        description: |
+          from raw DW.
+        external:
+          data_source: SynapseContainer
+          location: /absence_hours_live/
+          file_format: CommaDelimited
+          reject_type: VALUE
+          reject_value: 0
+        columns:
+```
+## Changelog
+
+### v0.18.0.1
+- pull AD auth directly from `dbt-sqlserver` (https://github.com/swanderz/dbt-synapse/pull/13)
+- hotfix for broken `create_view()` macro (https://github.com/swanderz/dbt-synapse/pull/14)
+- get `dbt-adapter-tests` up and running (https://github.com/swanderz/dbt-synapse/pull/16)
+  - make `sqlserver__drop_schema()` also drop all tables and views associated with schema
+  - introduce `sqlserver__get_columns_in_query()` for use with testing
+  - align macro args with `dbt-base`
+
+### v0.18.0rc2
+
+#### Fixes:
+- added snapshot functionality
+
+### v0.18.0rc1
+
+#### Fixes:
+- initial release
---
+++ packages/dbt-synapse/dbt-synapse-0.18.0.1.tar.gz/dbt-synapse-0.18.0.1/dbt/adapters/sqlserver/connections.py
@@ -0,0 +1,276 @@
+from contextlib import contextmanager
+
+import pyodbc
+import os
+import time
+import struct
+
+import dbt.exceptions
+from dbt.adapters.base import Credentials
+from dbt.adapters.sql import SQLConnectionManager
+from azure.identity import DefaultAzureCredential
+
+from dbt.logger import GLOBAL_LOGGER as logger
+
+from dataclasses import dataclass
+from typing import Optional
+
+
+def create_token(tenant_id, client_id, client_secret):
+    # bc DefaultAzureCredential will look in env variables
+    os.environ["AZURE_TENANT_ID"] = tenant_id
+    os.environ["AZURE_CLIENT_ID"] = client_id
+    os.environ["AZURE_CLIENT_SECRET"] = client_secret
+
+    token = DefaultAzureCredential().get_token("https://database.windows.net//.default")
+    # convert to byte string interspersed with the 1-byte
+    # TODO decide which is cleaner?
+    # exptoken=b''.join([bytes({i})+bytes(1) for i in bytes(token.token, "UTF-8")])
+    exptoken = bytes(1).join([bytes(i, "UTF-8") for i in token.token]) + bytes(1)
+    # make c object with bytestring length prefix
+    tokenstruct = struct.pack("=i", len(exptoken)) + exptoken
+
+    return tokenstruct
+
+
+@dataclass
+class SQLServerCredentials(Credentials):
+    driver: str
+    host: str
+    database: str
+    schema: str
+    port: Optional[int] = 1433
+    UID: Optional[str] = None
+    PWD: Optional[str] = None
+    windows_login: Optional[bool] = False
+    tenant_id: Optional[str] = None
+    client_id: Optional[str] = None
+    client_secret: Optional[str] = None
+    # "sql", "ActiveDirectoryPassword" or "ActiveDirectoryInteractive", or
+    # "ServicePrincipal"
+    authentication: Optional[str] = "sql"
+    encrypt: Optional[str] = "yes"
+
+    _ALIASES = {
+        "user": "UID",
+        "username": "UID",
+        "pass": "PWD",
+        "password": "PWD",
+        "server": "host",
+        "trusted_connection": "windows_login",
+        "auth": "authentication",
+        "app_id": "client_id",
+        "app_secret": "client_secret",
+    }
+
+    @property
+    def type(self):
+        return "sqlserver"
+
+    def _connection_keys(self):
+        # return an iterator of keys to pretty-print in 'dbt debug'
+        # raise NotImplementedError
+        if self.windows_login is True:
+            self.authentication = "Windows Login"
+
+        return (
+            "server",
+            "database",
+            "schema",
+            "port",
+            "UID",
+            "authentication",
+            "encrypt",
+        )
+
+
+class SQLServerConnectionManager(SQLConnectionManager):
+    TYPE = "sqlserver"
+    TOKEN = None
+
+    @contextmanager
+    def exception_handler(self, sql):
+        try:
+            yield
+
+        except pyodbc.DatabaseError as e:
+            logger.debug("Database error: {}".format(str(e)))
+
+            try:
+                # attempt to release the connection
+                self.release()
+            except pyodbc.Error:
+                logger.debug("Failed to release connection!")
+                pass
+
+            raise dbt.exceptions.DatabaseException(str(e).strip()) from e
+
+        except Exception as e:
+            logger.debug(f"Error running SQL: {sql}")
+            logger.debug("Rolling back transaction.")
+            self.release()
+            if isinstance(e, dbt.exceptions.RuntimeException):
+                # during a sql query, an internal to dbt exception was raised.
+                # this sounds a lot like a signal handler and probably has
+                # useful information, so raise it without modification.
+                raise
+
+            raise dbt.exceptions.RuntimeException(e)
+
+    @classmethod
+    def open(cls, connection):
+
+        if connection.state == "open":
+            logger.debug("Connection is already open, skipping open.")
+            return connection
+
+        credentials = connection.credentials
+
+        try:
+            con_str = []
+            con_str.append(f"DRIVER={{{credentials.driver}}}")
+
+            if "\\" in credentials.host:
+                # if there is a backslash \ in the host name the host is a sql-server named instance
+                # in this case then port number has to be omitted
+                con_str.append(f"SERVER={credentials.host}")
+            else:
+                con_str.append(f"SERVER={credentials.host},{credentials.port}")
+
+            con_str.append(f"Database={credentials.database}")
+
+            type_auth = getattr(credentials, "authentication", "sql")
+
+            if "ActiveDirectory" in type_auth:
+                con_str.append(f"Authentication={credentials.authentication}")
+
+                if type_auth == "ActiveDirectoryPassword":
+                    con_str.append(f"UID={{{credentials.UID}}}")
+                    con_str.append(f"PWD={{{credentials.PWD}}}")
+                elif type_auth == "ActiveDirectoryInteractive":
+                    con_str.append(f"UID={{{credentials.UID}}}")
+                elif type_auth == "ActiveDirectoryIntegrated":
+                    # why is this necessary???
+                    con_str.remove("UID={None}")
+                elif type_auth == "ActiveDirectoryMsi":
+                    raise ValueError("ActiveDirectoryMsi is not supported yet")
+
+            elif getattr(credentials, "windows_login", False):
+                con_str.append(f"trusted_connection=yes")
+            elif type_auth == "sql":
+                con_str.append("Authentication=SqlPassword")
+                con_str.append(f"UID={{{credentials.UID}}}")
+                con_str.append(f"PWD={{{credentials.PWD}}}")
+
+            if not getattr(credentials, "encrypt", False):
+                con_str.append(f"Encrypt={credentials.encrypt}")
+
+            con_str_concat = ";".join(con_str)
+
+            index = []
+            for i, elem in enumerate(con_str):
+                if "pwd=" in elem.lower():
+                    index.append(i)
+
+            if len(index) != 0:
+                con_str[index[0]] = "PWD=***"
+
+            con_str_display = ";".join(con_str)
+
+            logger.debug(f"Using connection string: {con_str_display}")
+
+            if type_auth != "ServicePrincipal":
+                handle = pyodbc.connect(con_str_concat, autocommit=True)
+
+            elif type_auth == "ServicePrincipal":
+
+                # create token if it does not exist
+                if cls.TOKEN is None:
+                    tenant_id = getattr(credentials, "tenant_id", None)
+                    client_id = getattr(credentials, "client_id", None)
+                    client_secret = getattr(credentials, "client_secret", None)
+
+                    cls.TOKEN = create_token(tenant_id, client_id, client_secret)
+
+                handle = pyodbc.connect(
+                    con_str_concat, attrs_before={1256: cls.TOKEN}, autocommit=True
+                )
+
+            connection.state = "open"
+            connection.handle = handle
+            logger.debug(f"Connected to db: {credentials.database}")
+
+        except pyodbc.Error as e:
+            logger.debug(f"Could not connect to db: {e}")
+
+            connection.handle = None
+            connection.state = "fail"
+
+            raise dbt.exceptions.FailedToConnectException(str(e))
+
+        return connection
+
+    def cancel(self, connection):
+        logger.debug("Cancel query")
+        pass
+
+    def add_begin_query(self):
+        # return self.add_query('BEGIN TRANSACTION', auto_begin=False)
+        pass
+
+    def add_commit_query(self):
+        # return self.add_query('COMMIT TRANSACTION', auto_begin=False)
+        pass
+
+    def add_query(self, sql, auto_begin=True, bindings=None, abridge_sql_log=False):
+
+        connection = self.get_thread_connection()
+
+        if auto_begin and connection.transaction_open is False:
+            self.begin()
+
+        logger.debug('Using {} connection "{}".'.format(self.TYPE, connection.name))
+
+        with self.exception_handler(sql):
+            if abridge_sql_log:
+                logger.debug("On {}:\n{}....".format(connection.name, sql[0:512]))
+            else:
+                logger.debug("On {}:\n{}".format(connection.name, sql))
+            pre = time.time()
+
+            cursor = connection.handle.cursor()
+
+            # pyodbc does not handle a None type binding!
+            if bindings is None:
+                cursor.execute(sql)
+            else:
+                cursor.execute(sql, bindings)
+
+            logger.debug(
+                "SQL status: {} in {:0.2f} seconds".format(
+                    self.get_status(cursor), (time.time() - pre)
+                )
+            )
+
+            return connection, cursor
+
+    @classmethod
+    def get_credentials(cls, credentials):
+        return credentials
+
+    @classmethod
+    def get_status(cls, cursor):
+        if cursor.rowcount == -1:
+            status = "OK"
+        else:
+            status = str(cursor.rowcount)
+        return status
+
+    def execute(self, sql, auto_begin=True, fetch=False):
+        _, cursor = self.add_query(sql, auto_begin)
+        status = self.get_status(cursor)
+        if fetch:
+            table = self.get_result_from_cursor(cursor)
+        else:
+            table = dbt.clients.agate_helper.empty_table()
+        return status, table
