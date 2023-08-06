ad7caa30b9aadd4d953fc2fc5f5d7b94861c8894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:21 +0000

    Add package MydistributionsMF89/1.0/MydistributionsMF89-1.0.tar.gz

---
+++ packages/MydistributionsMF89/MydistributionsMF89-1.0.tar.gz/MydistributionsMF89-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='MydistributionsMF89',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['MydistributionsMF89'],
+      zip_safe=False)
