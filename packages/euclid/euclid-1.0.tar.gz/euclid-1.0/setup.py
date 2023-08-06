510665b5e63c30c33fa5b92adfecb88da804f247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:44 +0000

    Add package euclid/1.0/euclid-1.0.tar.gz

---
+++ packages/euclid/euclid-1.0.tar.gz/euclid-1.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+'''
+'''
+
+from distutils.core import setup
+
+setup(name='euclid',
+      version='1.0',
+      description='2D and 3D vector, matrix, quaternion and geometry module',
+      author='Alex Holkner',
+      author_email='alex@partiallydisassembled.net',
+      maintainer='Richard Jones',
+      maintainer_email='r1chardj0n3s@gmail.com',
+      url='https://github.com/r1chardj0n3s/euclid',
+      py_modules=['euclid'],
+      )
+
