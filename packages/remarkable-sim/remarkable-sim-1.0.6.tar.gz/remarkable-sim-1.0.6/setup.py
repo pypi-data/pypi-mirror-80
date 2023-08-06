49c332866d9cd2929ab9e13ceb3ec52a089b626d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:36 +0000

    Add package remarkable-sim/1.0.6/remarkable-sim-1.0.6.tar.gz

---
+++ packages/remarkable-sim/remarkable-sim-1.0.6.tar.gz/remarkable-sim-1.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+
+setup(
+    name='remarkable-sim',
+    version='1.0.6',
+    packages=['remarkable_sim'],
+    author="Evan Widloski",
+    author_email="evan@evanw.org",
+    description="evdev/framebuffer simulation for reMarkable desktop development",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license="GPLv3",
+    keywords="remarkable evdev sim simulator simulation tablet",
+    url="https://github.com/evidlo/remarkable_sim",
+    entry_points={
+        'console_scripts': [
+            'resim = remarkable_sim.sim:main',
+            'remarkable_sim = remarkable_sim.sim:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
