ef07e7e4eae7fe8a09dfcac2aaca5e13f327834d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:03 +0000

    Add package invenio-rest/1.2.2/invenio-rest-1.2.2.tar.gz

---
+++ packages/invenio-rest/invenio-rest-1.2.2.tar.gz/invenio-rest-1.2.2/run-tests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015-2020 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+pydocstyle invenio_rest tests docs && \
+isort invenio_rest tests --check-only --diff && \
+check-manifest --ignore ".travis-*" && \
+sphinx-build -qnNW docs docs/_build/html && \
+python setup.py test
