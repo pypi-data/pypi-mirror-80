82f8d2759120abbe65b461d8ed25f478fe92cfd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:34 +0000

    Add package lime-etl/0.1.7/lime_etl-0.1.7-py3-none-any.whl

---
+++ packages/lime-etl/lime_etl-0.1.7-py3-none-any.whl/lime_etl/domain/exceptions.py
@@ -0,0 +1,23 @@
+import typing
+
+from lime_etl.domain import job_dependency_errors
+
+
+class LimeETLException(Exception):
+    """Base class for exceptions arising from the lime-etl package"""
+
+    def __init__(self, message: str, /):
+        self.message = message
+        super().__init__(message)
+
+
+class DependencyErrors(LimeETLException):
+    def __init__(
+        self,
+        dependency_errors: typing.Set[job_dependency_errors.JobDependencyErrors],
+        /,
+    ):
+        self.dependency_errors = dependency_errors
+
+        msg = "\n".join(str(e) for e in sorted(dependency_errors))
+        super().__init__(msg)
