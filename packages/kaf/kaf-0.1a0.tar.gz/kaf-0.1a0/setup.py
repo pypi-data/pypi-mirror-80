4cb100e252e6e350403542c78f5cc01dafaca159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:23 +0000

    Add package kaf/0.1a0/kaf-0.1a0.tar.gz

---
+++ packages/kaf/kaf-0.1a0.tar.gz/kaf-0.1a0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='kaf',
+     version='v0.1-alpha',
+     download_url='https://github.com/skipperkongen/kaf/archive/v0.1-alpha.tar.gz',
+     licence='MIT',
+     author="Pimin Konstantin Kefaloukos",
+     author_email="skipperongen@gmail.com",
+     description="A mini-framework for Kafka apps",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/skipperkongen/kaf",
+     packages=['kaf'],
+     install_requires=[
+        'confluent_kafka'
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
