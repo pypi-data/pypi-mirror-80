4a488ecfa365d40c3304faef0d51edadea51deca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:11 +0000

    Add package roamnerd-private-cli/0.1.0/roamnerd_private_cli-0.1.0.tar.gz

---
+++ packages/roamnerd-private-cli/roamnerd_private_cli-0.1.0.tar.gz/roamnerd_private_cli-0.1.0/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['roamnerd_private_cli']
+install_requires = \
+['argh>=0.26.2,<0.27.0',
+ 'python-dateutil>=2.8.1,<3.0.0',
+ 'spacy>=2.3.2,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['roamnerd_private_cli = roamnerd_private_cli:main']}
+
+setup_kwargs = {
+    'name': 'roamnerd-private-cli',
+    'version': '0.1.0',
+    'description': 'A private CLI for roamNERd developers / power users. Runs all NER locally using spacy',
+    'long_description': None,
+    'author': 'hmprt',
+    'author_email': 'hmprt@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
