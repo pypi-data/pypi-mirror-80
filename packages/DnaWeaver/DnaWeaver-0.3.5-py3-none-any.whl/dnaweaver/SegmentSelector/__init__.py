7d42d58ce467010334d30e9a2097719bf96d9c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:35 +0000

    Add package DnaWeaver/0.3.5/DnaWeaver-0.3.5-py3-none-any.whl

---
+++ packages/DnaWeaver/DnaWeaver-0.3.5-py3-none-any.whl/dnaweaver/SegmentSelector/__init__.py
@@ -0,0 +1,5 @@
+from .FixedSizeSegmentSelector import FixedSizeSegmentSelector
+from .SegmentSelector import SegmentSelector
+from .TmSegmentSelector import TmSegmentSelector
+
+__all__ = ["FixedSizeSegmentSelector", "SegmentSelector", "TmSegmentSelector"]
