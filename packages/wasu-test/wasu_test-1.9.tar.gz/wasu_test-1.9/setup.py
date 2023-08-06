f44adb174f9d691f440a302b4b7e66b5cb41c86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:17 +0000

    Add package wasu-test/1.9/wasu_test-1.9.tar.gz

---
+++ packages/wasu-test/wasu_test-1.9.tar.gz/wasu_test-1.9/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wasu_test",
+    version="1.9",
+    author="XiaoDong Chen",
+    author_email="chenxiaodong@wasu.com",
+    description="The Wasu Auto TestFrameWork",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=['PyYAML>=5.3', 'PyMySql>=0.10.1',
+                      'xlrd>=1.2.0', 'requests==2.24.0',
+                      'selenium==3.141.0', 'python-benedict==0.19.0', 'Appium-Python-Client==1.02'
+                      ],
+    entry_points={
+
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
