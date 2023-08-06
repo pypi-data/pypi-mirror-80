b76210d6bb3cbe89204670b5c480ca9f8bf9c5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:35 +0000

    Add package answerer/2.0.2/answerer-2.0.2.tar.gz

---
+++ packages/answerer/answerer-2.0.2.tar.gz/answerer-2.0.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name="answerer",
+    packages=["answerer"],
+    version="2.0.2",
+    license="Apache",
+    description="A system that outputs a nice list of links to speed up issues answering.",
+    long_description="See here for more information: https://github.com/abrow425/gotIssues",
+    author="Andrew Brown (aka SherpDaWerp)",
+    author_email="abrow425@gmail.com",
+    url="https://github.com/abrow425/gotIssues",
+    download_url="https://github.com/abrow425/gotIssues/archive/v2.0.2.tar.gz",
+    keywords=["NATIONSTATES", "NS", "API"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.8",
+    ],
+    install_requires=[
+        "nspy_wrapper",
+        "urllib3",
+    ],
+)
