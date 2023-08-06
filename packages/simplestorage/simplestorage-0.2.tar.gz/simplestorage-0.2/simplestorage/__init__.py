374ec4085f49e3621b07cb3bb6fd08214db96d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:57 +0000

    Add package simplestorage/0.2/simplestorage-0.2.tar.gz

---
+++ packages/simplestorage/simplestorage-0.2.tar.gz/simplestorage-0.2/simplestorage/__init__.py
@@ -0,0 +1,24 @@
+from io import BytesIO, StringIO
+from .interface import StorageInterface
+
+interface_session = StorageInterface()
+
+
+def ls(uri: str):
+    return interface_session.ls(uri=uri)
+
+
+def save(data: bytes, uri: str):
+    return interface_session.save(data=data, uri=uri)
+
+
+def load(data: bytes, uri: str) -> BytesIO:
+    return interface_session.load(uri=uri)
+
+
+def loads(self, uri: str) -> StringIO:
+    return interface_session.loads(uri=uri)
+
+
+def delete(self, uri: str) -> bool:
+    return interface_session.delete(uri=uri)
