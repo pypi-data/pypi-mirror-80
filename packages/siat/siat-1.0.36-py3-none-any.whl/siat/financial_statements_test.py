11e7076b547e2c917cb565d3dccdb3180d217ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:53 +0000

    Add package siat/1.0.36/siat-1.0.36-py3-none-any.whl

---
+++ packages/siat/siat-1.0.36-py3-none-any.whl/siat/financial_statements_test.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 22:49:18 2020
+
+@author: Peter
+"""
+
+import os; os.chdir("S:/siat")
+from siat.financial_statements import *
+
+
+info=get_balance_sheet('W')
+infot=info.T
+
+
+#==============================================================================
+info=get_income_statements('MST')
+info=get_income_statements('MDT')
+
+info=get_cashflow_statements('MDT')
+info=get_cashflow_statements('MST')
+
+info=get_balance_sheet('MST')
+info=get_balance_sheet('MDT')
+
+info=get_financial_statements('MDT')
+info=get_financial_statements('MST')
