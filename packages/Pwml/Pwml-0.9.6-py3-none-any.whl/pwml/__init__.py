4389ecf001f53e8174e611ae0f3a6bcface08483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:04 +0000

    Add package Pwml/0.9.6/Pwml-0.9.6-py3-none-any.whl

---
+++ packages/Pwml/Pwml-0.9.6-py3-none-any.whl/pwml/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+from .classifiers import embedders
+from .classifiers import features
+from .classifiers import hierarchical
+
+from .utilities import filehelpers
+from .utilities import httphelpers
+from .utilities import imagehelpers
+from .utilities import mssqlhelpers
+
+__version__ = '0.9.6'
