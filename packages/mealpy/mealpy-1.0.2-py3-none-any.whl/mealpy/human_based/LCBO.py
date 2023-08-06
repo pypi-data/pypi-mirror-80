19a8ef2dba2d232ab4d1e7dea6671ff884b0cdc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:17 +0000

    Add package mealpy/1.0.2/mealpy-1.0.2-py3-none-any.whl

---
+++ packages/mealpy/mealpy-1.0.2-py3-none-any.whl/mealpy/human_based/LCBO.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+# ------------------------------------------------------------------------------------------------------%
+# Created by "Thieu Nguyen" at 11:16, 18/03/2020                                                        %
+#                                                                                                       %
+#       Email:      nguyenthieu2102@gmail.com                                                           %
+#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
+#       Github:     https://github.com/thieu1995                                                  %
+#-------------------------------------------------------------------------------------------------------%
+
+from numpy.random import uniform
+from numpy import array, mean, sqrt, ceil
+from mealpy.root import Root
+
+
+class BaseLCBO(Root):
+    """
+    The original version of: Life Choice-Based Optimization (LCBO)
+        (A novel life choice-based optimizer)
+    Link:
+        DOI: https://doi.org/10.1007/s00500-019-04443-z
+    """
+
+    def __init__(self, obj_func=None, lb=None, ub=None, problem_size=50, batch_size=10, verbose=True,
+                 epoch=750, pop_size=100, r1=2.35):
+        Root.__init__(self, obj_func, lb, ub, problem_size, batch_size, verbose)
+        self.epoch = epoch
+        self.pop_size = pop_size
+        self.r1 = r1
+
+    def train(self):
+        pop = [self.create_solution() for _ in range(self.pop_size)]
+        pop, g_best = self.get_sorted_pop_and_global_best_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
+
+        # epoch: current chance, self.epoch: number of chances
+        for epoch in range(self.epoch):
+            for i in range(0, self.pop_size):
+                rand = uniform()
+
+                if rand > 0.875:    # Update using Eq. 1, update from n best position
+                    n = int(ceil(sqrt(self.pop_size)))
+                    temp = array([uniform() * pop[j][self.ID_POS] for j in range(0, n)])
+                    temp = mean(temp, axis=0)
+                elif rand < 0.7:    # Update using Eq. 2-6
+                    f = (epoch + 1) / self.epoch
+                    if i != 0:
+                        better_diff = f * self.r1 * (pop[i - 1][self.ID_POS] - pop[i][self.ID_POS])
+                    else:
+                        better_diff = f * self.r1 * (g_best[self.ID_POS] - pop[i][self.ID_POS])
+                    best_diff = (1-f) * self.r1 * (pop[0][self.ID_POS] - pop[i][self.ID_POS])
+                    temp = pop[i][self.ID_POS] + uniform() * better_diff + uniform() * best_diff
+                else:
+                    temp = self.ub - (pop[i][self.ID_POS] - self.lb) * uniform(self.lb, self.ub)
+                fit = self.get_fitness_position(temp)
+                if fit < pop[i][self.ID_FIT]:
+                    pop[i] = [temp, fit]
+
+                ## Batch size idea
+                if i % self.batch_size:
+                    pop, g_best = self.update_sorted_population_and_global_best_solution(pop, self.ID_MIN_PROB, g_best)
+            self.loss_train.append(g_best[self.ID_FIT])
+            if self.verbose:
+                print(">Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
+        self.solution = g_best
+        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
+
+
+class LevyLCBO(BaseLCBO):
+    """
+    The modified version of: Life Choice-Based Optimization (LCBO) based on Levy_flight
+        (A novel life choice-based optimizer)
+    """
+
+    def __init__(self, obj_func=None, lb=None, ub=None, problem_size=50, batch_size=10, verbose=True,
+                 epoch=750, pop_size=100, r1=2.35):
+        BaseLCBO.__init__(self, obj_func, lb, ub, problem_size, batch_size, verbose, epoch, pop_size, r1)
+
+    def train(self):
+        pop = [self.create_solution() for _ in range(self.pop_size)]
+        pop, g_best = self.get_sorted_pop_and_global_best_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
+
+        # epoch: current chance, self.epoch: number of chances
+        for epoch in range(self.epoch):
+            for i in range(0, self.pop_size):
+                rand = uniform()
+
+                if rand > 0.875:  # Update using Eq. 1, update from n best position
+                    n = int(ceil(sqrt(self.pop_size)))
+                    pos_new = array([uniform() * pop[j][self.ID_POS] for j in range(0, n)])
+                    pos_new = mean(pos_new, axis=0)
+                elif rand < 0.7:  # Update using Eq. 2-6
+                    if uniform() < 0.5:
+                        f = (epoch + 1) / self.epoch
+                        if i != 0:
+                            better_diff = f * self.r1 * (pop[i - 1][self.ID_POS] - pop[i][self.ID_POS])
+                        else:
+                            better_diff = f * self.r1 * (g_best[self.ID_POS] - pop[i][self.ID_POS])
+                        best_diff = (1 - f) * self.r1 * (pop[0][self.ID_POS] - pop[i][self.ID_POS])
+                        pos_new = pop[i][self.ID_POS] + uniform() * better_diff + uniform() * best_diff
+                    else:
+                        pos_new = self.levy_flight(epoch, pop[i][self.ID_POS], g_best[self.ID_POS], case=0)  # My new updated here
+                else:
+                    pos_new = self.ub - (pop[i][self.ID_POS] - self.lb) * uniform(self.lb, self.ub)
+
+                fit = self.get_fitness_position(pos_new)
+                if fit < pop[i][self.ID_FIT]:
+                    pop[i] = [pos_new, fit]
+
+                ## Batch size idea
+                if i % self.batch_size:
+                    pop, g_best = self.update_sorted_population_and_global_best_solution(pop, self.ID_MIN_PROB, g_best)
+            self.loss_train.append(g_best[self.ID_FIT])
+            if self.verbose:
+                print(">Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
+        self.solution = g_best
+        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
+
+
+class ImprovedLCBO(BaseLCBO):
+    """
+    The improved version of: Life Choice-Based Optimization (LCBO) based on new ideas
+        (A novel life choice-based optimizer)
+    """
+
+    def __init__(self, obj_func=None, lb=None, ub=None, problem_size=50, batch_size=10, verbose=True,
+                 epoch=750, pop_size=100, r1=2.35):
+        BaseLCBO.__init__(self, obj_func, lb, ub, problem_size, batch_size, verbose, epoch, pop_size, r1)
+        self.n1 = int(ceil(sqrt(self.pop_size)))                        # n best position
+        self.n2 = self.n1 + int((self.pop_size - self.n1) / 2)          # 50% for both 2 group left
+
+    def train(self):
+        pop = [self.create_solution() for _ in range(self.pop_size)]
+        pop, g_best = self.get_sorted_pop_and_global_best_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
+
+        # epoch: current chance, self.epoch: number of chances
+        for epoch in range(self.epoch):
+            for i in range(0, self.pop_size):
+                ## Since we already sorted population, we know which ones are 1st group
+                if i < self.n1:
+                    pos_new = array([uniform() * pop[j][self.ID_POS] for j in range(0, self.n1)])
+                    pos_new = mean(pos_new, axis=0)
+                elif self.n1 <= i < self.n2:  # People in group 2 learning from the best person in the history,
+                    # because they want to be better than the current best person
+                    pos_new = self.levy_flight(epoch, pop[i][self.ID_POS], g_best[self.ID_POS], case=0)
+                else:  # People in group 3 learning from the current best person and the person slightly better than them,
+                    # because they don't have vision
+                    f = 1 - (epoch + 1) / self.epoch
+                    better_diff = f * self.r1 * (pop[i - 1][self.ID_POS] - pop[i][self.ID_POS])
+                    best_diff = (1 - f) * self.r1 * (pop[0][self.ID_POS] - pop[i][self.ID_POS])
+                    pos_new = pop[i][self.ID_POS] + uniform() * better_diff + uniform() * best_diff
+                fit = self.get_fitness_position(pos_new)
+                if fit < pop[i][self.ID_FIT]:
+                    pop[i] = [pos_new, fit]
+                else:
+                    pos_new = self.ub - (pop[i][self.ID_POS] - self.lb) * uniform(self.lb, self.ub)
+                    fit = self.get_fitness_position(pos_new)
+                    pop[i] = [pos_new, fit]
+
+                ## Batch size idea
+                if i % self.batch_size:
+                    pop, g_best = self.update_sorted_population_and_global_best_solution(pop, self.ID_MIN_PROB, g_best)
+            self.loss_train.append(g_best[self.ID_FIT])
+            if self.verbose:
+                print(">Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
+        self.solution = g_best
+        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
---
+++ packages/mealpy/mealpy-1.0.2-py3-none-any.whl/mealpy/system_based/AEO.py
@@ -0,0 +1,273 @@
+#!/usr/bin/env python
+# ------------------------------------------------------------------------------------------------------%
+# Created by "Thieu Nguyen" at 16:44, 18/03/2020                                                        %
+#                                                                                                       %
+#       Email:      nguyenthieu2102@gmail.com                                                           %
+#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
+#       Github:     https://github.com/thieu1995                                                  %
+#-------------------------------------------------------------------------------------------------------%
+
+from numpy.random import uniform, normal, randint
+from numpy import abs
+from copy import deepcopy
+from mealpy.root import Root
+
+
+class OriginalAEO(Root):
+    """
+    Original version of: Artificial Ecosystem-based Optimization
+        (Artificial ecosystem-based optimization: a novel nature-inspired meta-heuristic algorithm)
+    Link:
+        https://doi.org/10.1007/s00521-019-04452-x
+        https://www.mathworks.com/matlabcentral/fileexchange/72685-artificial-ecosystem-based-optimization-aeo
+    """
+
+    def __init__(self, obj_func=None, lb=None, ub=None, problem_size=50, batch_size=10, verbose=True,
+                 epoch=750, pop_size=100):
+        Root.__init__(self, obj_func, lb, ub, problem_size, batch_size, verbose)
+        self.epoch = epoch
+        self.pop_size = pop_size
+
+    def train(self):
+        pop = [self.create_solution() for _ in range(self.pop_size)]
+        # Sorted population in the descending order of the function fitness value
+        pop = sorted(pop, key=lambda item: item[self.ID_FIT], reverse=True)
+        g_best = deepcopy(pop[self.ID_MAX_PROB])
+        pop_new = deepcopy(pop)
+        for epoch in range(self.epoch):
+            ## Production
+            # Eq. 2, 3, 1
+            a = (1.0 - epoch / self.epoch) * uniform()
+            x1 = (1 - a) * pop[self.ID_MAX_PROB][self.ID_POS] + a * uniform(self.lb, self.ub)
+            fit = self.get_fitness_position(x1)
+            pop_new[0] = [x1, fit]
+
+            ## Consumption
+            for i in range(2, self.pop_size):
+                rand = uniform()
+                # Eq. 4, 5, 6
+                v1 = normal(0, 1)
+                v2 = normal(0, 1)
+                c = 0.5 * v1 / abs(v2)      # Consumption factor
+
+                j = randint(1, i)
+                ### Herbivore
+                if rand < 1.0/3:
+                    x_t1 = pop[i][self.ID_POS] + c * (pop[i][self.ID_POS] - pop[0][self.ID_POS])    # Eq. 6
+                ### Omnivore
+                elif 1.0/3 <= rand and rand <= 2.0/3:
+                    x_t1 = pop[i][self.ID_POS] + c * (pop[i][self.ID_POS] - pop[j][self.ID_POS])    # Eq. 7
+                ### Carnivore
+                else:
+                    r2 = uniform()
+                    x_t1 = pop[i][self.ID_POS] + c * (r2*(pop[i][self.ID_POS] - pop[0][self.ID_POS]) + (1-r2)*(pop[i][self.ID_POS] - pop[j][self.ID_POS]))
+                x_t1 = self.amend_position_faster(x_t1)
+                fit_t1 = self.get_fitness_position(x_t1)
+                pop_new[i] = [x_t1, fit_t1]
+
+            for i in range(0, self.pop_size):
+                if pop_new[i][self.ID_FIT] < pop[i][self.ID_FIT]:
+                    pop[i] = deepcopy(pop_new[i])
+
+            ## find current best used in decomposition
+            best = self.get_global_best_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
+
+            ## Decomposition
+            ### Eq. 10, 11, 12, 9
+            for i in range(0, self.pop_size):
+                r3 = uniform()
+                d = 3 * normal(0, 1)
+                e = r3 * randint(1, 3) - 1
+                h = 2 * r3 - 1
+                x_t1 = best[self.ID_POS] + d * (e*best[self.ID_POS] - h*pop[i][self.ID_POS])
+                x_t1 = self.amend_position_faster(x_t1)
+                fit_t1 = self.get_fitness_position(x_t1)
+                pop_new[i] = [x_t1, fit_t1]
+
+            for i in range(0, self.pop_size):
+                if pop_new[i][self.ID_FIT] < pop[i][self.ID_FIT]:
+                    pop[i] = deepcopy(pop_new[i])
+
+            pop = sorted(pop, key=lambda item: item[self.ID_FIT], reverse=True)
+            current_best = deepcopy(pop[self.ID_MAX_PROB])
+            if current_best[self.ID_FIT] < g_best[self.ID_FIT]:
+                g_best = deepcopy(current_best)
+            self.loss_train.append(g_best[self.ID_FIT])
+            if self.verbose:
+                print("> Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
+        self.solution = g_best
+        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
+
+
+class BaseAEO(Root):
+    """
+    This is my modified version of: Artificial Ecosystem-based Optimization
+        (Artificial ecosystem-based optimization: a novel nature-inspired meta-heuristic algorithm)
+
+        + Same results and sometime better than original version.
+        + Original version move the population at the same time. My version move after each position move.
+    """
+
+    def __init__(self, obj_func=None, lb=None, ub=None, problem_size=50, batch_size=10, verbose=True,
+                 epoch=750, pop_size=100):
+        Root.__init__(self, obj_func, lb, ub, problem_size, batch_size, verbose)
+        self.epoch = epoch
+        self.pop_size = pop_size
+
+    def train(self):
+        pop = [self.create_solution() for _ in range(self.pop_size)]
+        # Sorted population in the descending order of the function fitness value
+        pop = sorted(pop, key=lambda item: item[self.ID_FIT], reverse=True)
+        g_best = deepcopy(pop[self.ID_MAX_PROB])
+
+        for epoch in range(self.epoch):
+            ## Production
+            # Eq. 2, 3, 1
+            a = (1.0 - epoch / self.epoch) * uniform()
+            x1 = (1 - a) * pop[self.ID_MAX_PROB][self.ID_POS] + a * uniform(self.lb, self.ub)
+            fit = self.get_fitness_position(x1)
+            pop[0] = [x1, fit]
+
+            ## Consumption
+            for i in range(1, self.pop_size):
+                rand = uniform()
+                # Eq. 4, 5, 6
+                v1 = normal(0, 1)
+                v2 = normal(0, 1)
+                c = 0.5 * v1 / abs(v2)      # Consumption factor
+
+                if i == 1:
+                    x_t1 = pop[i][self.ID_POS] + c * (pop[i][self.ID_POS] - pop[0][self.ID_POS])  # Eq. 6
+                else:
+                    j = randint(1, i)
+                    ### Herbivore
+                    if rand < 1.0/3:
+                        x_t1 = pop[i][self.ID_POS] + c * (pop[i][self.ID_POS] - pop[0][self.ID_POS])    # Eq. 6
+                    ### Omnivore
+                    elif 1.0/3 <= rand and rand <= 2.0/3:
+                        x_t1 = pop[i][self.ID_POS] + c * (pop[i][self.ID_POS] - pop[j][self.ID_POS])    # Eq. 7
+                    ### Carnivore
+                    else:
+                        r2 = uniform()
+                        x_t1 = pop[i][self.ID_POS] + c * (r2*(pop[i][self.ID_POS] - pop[0][self.ID_POS]) + (1-r2)*(pop[i][self.ID_POS] - pop[j][self.ID_POS]))
+                fit_t1 = self.get_fitness_position(x_t1)
+                if fit_t1 < pop[i][self.ID_FIT]:
+                    pop[i] = [x_t1, fit_t1]
+
+                if i % self.batch_size:
+                    ## Update global best base on batch_size
+                    pop = sorted(pop, key=lambda item: item[self.ID_FIT], reverse=True)
+                    current_best = deepcopy(pop[self.ID_MAX_PROB])
+                    if current_best[self.ID_FIT] < g_best[self.ID_FIT]:
+                        g_best = deepcopy(current_best)
+
+            ## Decomposition
+            ### Eq. 10, 11, 12, 9
+            for i in range(0, self.pop_size):
+                r3 = uniform()
+                d = 3 * normal(0, 1)
+                e = r3 * randint(1, 3) - 1
+                h = 2 * r3 - 1
+                x_t1 = pop[self.ID_MAX_PROB][self.ID_POS] + d * (e*pop[self.ID_MAX_PROB][self.ID_POS] - h*pop[i][self.ID_POS])
+                fit_t1 = self.get_fitness_position(x_t1)
+                if fit_t1 < pop[i][self.ID_FIT]:
+                    pop[i] = [x_t1, fit_t1]
+
+                if i % self.batch_size:
+                    pop = sorted(pop, key=lambda item: item[self.ID_FIT], reverse=True)
+                    current_best = deepcopy(pop[self.ID_MAX_PROB])
+                    if current_best[self.ID_FIT] < g_best[self.ID_FIT]:
+                        g_best = deepcopy(current_best)
+
+            self.loss_train.append(g_best[self.ID_FIT])
+            if self.verbose:
+                print("> Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
+
+        self.solution = g_best
+        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
+
+
+class ImprovedAEO(Root):
+    """
+        This is Improved Artificial Ecosystem Optimization based on
+            + Levy_flight
+            + Global best solution
+    """
+
+    def __init__(self, obj_func=None, lb=None, ub=None, problem_size=50, batch_size=10, verbose=True,
+                 epoch=750, pop_size=100):
+        Root.__init__(self, obj_func, lb, ub, problem_size, batch_size, verbose)
+        self.epoch = epoch
+        self.pop_size = pop_size
+
+    def train(self):
+        pop = [self.create_solution() for _ in range(self.pop_size)]
+        # Sorted population in the descending order of the function fitness value
+        pop = sorted(pop, key=lambda item: item[self.ID_FIT], reverse=True)
+        g_best = deepcopy(pop[self.ID_MAX_PROB])
+        pop_new = deepcopy(pop)
+        for epoch in range(self.epoch):
+            ## Production
+            # Eq. 2, 3, 1
+            a = (1.0 - epoch / self.epoch) * uniform()
+            x1 = (1 - a) * pop[self.ID_MAX_PROB][self.ID_POS] + a * uniform(self.lb, self.ub)
+            fit = self.get_fitness_position(x1)
+            pop_new[0] = [x1, fit]  # From the best produce new one
+
+            ## Consumption
+            for i in range(2, self.pop_size):  # From others left
+                if uniform() < 0.5:
+                    rand = uniform()
+                    # Eq. 4, 5, 6
+                    c = 0.5 * normal(0, 1) / abs(normal(0, 1))  # Consumption factor
+
+                    j = randint(1, i)
+                    ### Herbivore
+                    if rand < 1.0 / 3:
+                        x_t1 = pop[i][self.ID_POS] + c * (pop[i][self.ID_POS] - pop[0][self.ID_POS])  # Eq. 6
+                    ### Carnivore
+                    elif 1.0 / 3 <= rand <= 2.0 / 3:
+                        x_t1 = pop[i][self.ID_POS] + c * (pop[i][self.ID_POS] - pop[j][self.ID_POS])  # Eq. 7
+                    ### Omnivore
+                    else:
+                        r2 = uniform()
+                        x_t1 = pop[i][self.ID_POS] + c * (
+                                    r2 * (pop[i][self.ID_POS] - pop[0][self.ID_POS]) + (1 - r2) * (pop[i][self.ID_POS] - pop[j][self.ID_POS]))
+                else:
+                    x_t1 = pop[i][self.ID_POS] + self.step_size_by_levy_flight(0.01, 1.5)*(pop[i][self.ID_POS] - g_best[self.ID_POS])
+                x_t1 = self.amend_position_faster(x_t1)
+                fit_t1 = self.get_fitness_position(x_t1)
+                pop_new[i] = [x_t1, fit_t1]
+
+            for i in range(0, self.pop_size):
+                if pop_new[i][self.ID_FIT] < pop[i][self.ID_FIT]:
+                    pop[i] = deepcopy(pop_new[i])
+
+            ## find current best used in decomposition
+            best = self.get_global_best_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
+
+            ## Decomposition
+            ### Eq. 10, 11, 12, 9
+            for i in range(0, self.pop_size):
+                if uniform() < 0.5:
+                    x_t1 = best[self.ID_POS] + normal(0, 1, self.problem_size)*(best[self.ID_POS] - pop[i][self.ID_POS])
+                else:
+                    x_t1 = g_best[self.ID_POS] + self.step_size_by_levy_flight(0.01, 0.5) * (g_best[self.ID_POS] - pop[i][self.ID_POS])
+                x_t1 = self.amend_position_faster(x_t1)
+                fit_t1 = self.get_fitness_position(x_t1)
+                pop_new[i] = [x_t1, fit_t1]
+
+            for i in range(0, self.pop_size):
+                if pop_new[i][self.ID_FIT] < pop[i][self.ID_FIT]:
+                    pop[i] = deepcopy(pop_new[i])
+
+            pop = sorted(pop, key=lambda item: item[self.ID_FIT], reverse=True)
+            current_best = deepcopy(pop[self.ID_MAX_PROB])
+            if current_best[self.ID_FIT] < g_best[self.ID_FIT]:
+                g_best = deepcopy(current_best)
+            self.loss_train.append(g_best[self.ID_FIT])
+            if self.verbose:
+                print("> Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
+
+        self.solution = g_best
+        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
