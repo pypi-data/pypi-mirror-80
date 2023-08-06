c2e397dec1024ed63d5cff53e25668a02580310b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:23 +0000

    Add package raytraverse/0.2.3/raytraverse-0.2.3-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/raytraverse/raytraverse-0.2.3-cp36-cp36m-manylinux2010_x86_64.whl/raytraverse/renderer/rcontrib.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 Stephen Wasilewski
+# =======================================================================
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# =======================================================================
+import os
+import shutil
+
+from raytraverse.renderer.radiancerenderer import RadianceRenderer
+from raytraverse.crenderer import cRcontrib
+import raytraverse
+
+path = os.path.dirname(raytraverse.__file__)
+rcontrib_capture_file = f'{path}/tmp_rcontrib_capture'
+
+if cRcontrib.version == "PyVirtual":
+    from raytraverse.renderer.sprenderer import SPRcontrib as Rcontrib
+else:
+    class Rcontrib(RadianceRenderer):
+        """singleton wrapper for c++ crenderer.cRcontrib singleton class"""
+        Engine = cRcontrib
+        name = 'rcontrib'
+        arg_prefix = "-o !cat"
