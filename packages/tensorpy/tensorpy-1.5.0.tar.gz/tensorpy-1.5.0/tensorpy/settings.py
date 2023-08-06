8eff9c44e9da1a3a9b6f2df80c983df58be8d338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:09 +0000

    Add package tensorpy/1.5.0/tensorpy-1.5.0.tar.gz

---
+++ packages/tensorpy/tensorpy-1.5.0.tar.gz/tensorpy-1.5.0/tensorpy/settings.py
@@ -0,0 +1,7 @@
+""" TensorPy Settings """
+
+IMAGENET_FOLDER = "/tmp/imagenet"  # Folder where ImageNet files get installed
+DOWNLOADS_FOLDER = "downloads_folder"  # Folder for saving downloaded files
+MIN_W_H = 50  # Minimum width/height for classifying images on a page
+MAX_THREADS = 3  # Total threads to spin up for classifying images on a page
+MAX_IMAGES_PER_PAGE = 15  # Limit of image classifications per page
