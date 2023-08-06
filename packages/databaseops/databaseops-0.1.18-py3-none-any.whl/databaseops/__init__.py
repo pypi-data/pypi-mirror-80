c78594c16ba2383d7ab70b0e9990e61d62bb7a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:50 +0000

    Add package databaseops/0.1.18/databaseops-0.1.18-py3-none-any.whl

---
+++ packages/databaseops/databaseops-0.1.18-py3-none-any.whl/databaseops/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+"""
+databaseops
+See https://github.com/AnkushBhise/databaseops/ for more information
+"""
+
+# Version of databaseops
+__version__ = "0.1.18"
+
+# Create imports from project to make every thing available at top level
+from databaseops.mysql.operations import MySQLOps
---
+++ packages/databaseops/databaseops-0.1.18-py3-none-any.whl/databaseops/mysql/__init__.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+"""
+This package created for operations on mysql database as it is dataframe
+"""
+
+# Version of databaseops sub-package mysql
+__version__ = "0.1.4"
