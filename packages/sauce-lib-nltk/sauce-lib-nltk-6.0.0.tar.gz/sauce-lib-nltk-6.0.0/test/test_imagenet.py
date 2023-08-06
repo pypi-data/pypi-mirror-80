8b1a9e2090dbe36317a09ff57ca606200cb6ab61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:59:03 +0000

    Add package sauce-lib-nltk/6.0.0/sauce-lib-nltk-6.0.0.tar.gz

---
+++ packages/sauce-lib-nltk/sauce-lib-nltk-6.0.0.tar.gz/sauce-lib-nltk-6.0.0/test/test_imagenet.py
@@ -0,0 +1,43 @@
+import unittest
+import json
+import os, os.path
+import shutil
+from rdflib import Graph, plugin
+from rdflib.parser import Parser
+from rdflib.serializer import Serializer
+from sauce.imagenet.imagenet import ImagenetGenerator
+
+class ImagenetGeneratorTestCase(unittest.TestCase):
+
+    
+    def setUp(self):
+        self.generator = ImagenetGenerator('./testoutput','http://localhost:1080/synsets','http://localhost:1080/word')
+        
+
+    def tearDown(self):
+        shutil.rmtree('./testoutput')
+        shutil.rmtree('./testoutputlimit')
+
+
+    def test_extraction(self):
+        self.generator.extract()
+        f = open("./testoutput/synsets.txt")
+        assert len(f.readlines()) == 5
+        assert len(os.listdir('./testoutput/raw')) == 5
+        f.close()
+        f2 = open("./testoutput/raw/n02119789.urls")
+        assert len(f2.readlines()) == 3
+        f2.close()
+
+    def test_cleanse(self):
+        self.generator.extract()
+        self.generator.cleanse_synsets()
+        assert len(os.listdir('./testoutput/cleansed')) == 5
+        f = open("./testoutput/cleansed/n02119789.urls")
+        assert len(f.readlines()) == 2
+        f.close()
+
+    def test_limit(self):
+        self.generatorLimit = ImagenetGenerator('./testoutputlimit','http://localhost:1080/synsets','http://localhost:1080/word/', limit=2)
+        self.generatorLimit.extract()
+        assert len(os.listdir('./testoutputlimit/raw')) == 2
