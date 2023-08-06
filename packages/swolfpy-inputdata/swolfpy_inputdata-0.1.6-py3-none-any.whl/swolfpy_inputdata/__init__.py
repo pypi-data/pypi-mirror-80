0aaede66b0c1d06b0d7ea1d4b486a154ab047a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:39 +0000

    Add package swolfpy-inputdata/0.1.6/swolfpy_inputdata-0.1.6-py3-none-any.whl

---
+++ packages/swolfpy-inputdata/swolfpy_inputdata-0.1.6-py3-none-any.whl/swolfpy_inputdata/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Mojtaba Sardarmehni
+
+Input data for the swolfpy's life_cycle process models
+"""
+
+__all__ = [
+    'MC',
+    'InputData',
+    'CommonData',
+    'Technosphere_Input',
+    'LF_Input',
+    'WTE_Input',
+    'Comp_Input',
+    'AD_Input',
+    'SS_MRF_Input',
+    'Reproc_Input',
+    'SF_Col_Input'
+]
+
+__version__ = '0.1.6'
+
+
+from .MC import MC
+from .InputData import InputData
+from .CommonData import CommonData
+from .Technosphere_Input import Technosphere_Input
+from .LF_Input import LF_Input
+from .WTE_Input import WTE_Input
+from .Comp_Input import Comp_Input
+from .AD_Input import AD_Input
+from .SS_MRF_Input import SS_MRF_Input
+from .Reproc_Input import Reproc_Input
+from .SF_Col_Input import SF_Col_Input
