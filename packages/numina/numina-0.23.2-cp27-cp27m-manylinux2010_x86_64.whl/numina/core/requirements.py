853cf485375720a4f73ab961f4da175cbafd4527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:37 +0000

    Add package numina/0.23.2/numina-0.23.2-cp27-cp27m-manylinux2010_x86_64.whl

---
+++ packages/numina/numina-0.23.2-cp27-cp27m-manylinux2010_x86_64.whl/numina/core/requirements.py
@@ -0,0 +1,82 @@
+#
+# Copyright 2008-2018 Universidad Complutense de Madrid
+#
+# This file is part of Numina
+#
+# SPDX-License-Identifier: GPL-3.0+
+# License-Filename: LICENSE.txt
+#
+
+"""
+Recipe requirement holders
+"""
+
+import six
+
+import numina.exceptions
+import numina.types.obsresult as obtypes
+
+from .dataholders import Requirement
+
+
+class ObservationResultRequirement(Requirement):
+    """The Recipe requires the result of an observation."""
+    def __init__(self, query_opts=None):
+
+        super(ObservationResultRequirement, self).__init__(
+            obtypes.ObservationResultType, "Observation Result",
+            query_opts=query_opts
+            )
+
+    def __repr__(self):
+        sclass = type(self).__name__
+        fmt = "%s(dest=%r, description='%s')"
+        msg = fmt % (sclass, self.dest, self.description)
+        return msg
+
+    def query(self, dal, obsres, options=None):
+        """
+
+        Parameters
+        ----------
+        dal
+        obsres
+        options
+
+        Returns
+        -------
+
+        """
+        from numina.core.query import ResultOf
+
+        q_options = self.query_options(options)
+
+        if isinstance(q_options, ResultOf):
+            dest_field = 'frames'
+            dest_type = list
+            # Field to insert the results
+            if not hasattr(obsres, dest_field):
+                setattr(obsres, dest_field, dest_type())
+
+            dest_obj = getattr(obsres, dest_field)
+
+            values = dal.search_result_relative(self.dest, self.type, obsres,
+                                                result_desc=q_options)
+
+            for partial in values:
+                dest_obj.append(partial.content)
+
+        return obsres
+
+    def on_query_not_found_from_type(self, notfound):
+        """
+
+        Parameters
+        ----------
+        notfound
+
+        Returns
+        -------
+
+        """
+        six.raise_from(numina.exceptions.NoResultFound('unable to complete ObservationResult'), notfound)
---
+++ packages/numina/numina-0.23.2-cp27-cp27m-manylinux2010_x86_64.whl/numina/types/array.py
@@ -0,0 +1,71 @@
+# Copyright 2008-2019 Universidad Complutense de Madrid
+#
+# This file is part of Numina
+#
+# SPDX-License-Identifier: GPL-3.0+
+# License-Filename: LICENSE.txt
+#
+
+
+import numpy
+import six
+
+from .datatype import DataType
+
+
+class ArrayType(DataType):
+    """A type of array."""
+    def __init__(self, fmt='%.18e', default=None):
+        super(ArrayType, self).__init__(ptype=numpy.ndarray, default=default)
+        self.fmt = fmt
+
+    def convert(self, obj):
+        return self.convert_to_array(obj)
+
+    def convert_to_array(self, obj):
+        return numpy.array(obj)
+
+    def _datatype_dump(self, obj, where):
+        return dump_numpy_array(obj, where, self.fmt)
+
+    def _datatype_load(self, obj):
+        if isinstance(obj, six.string_types):
+            # if is a string, it may be a pathname, try to load it
+
+            # heuristics, by extension
+            if obj.endswith('.csv'):
+                # try to open as a CSV file
+                res = numpy.loadtxt(obj, delimiter=',')
+            else:
+                res = numpy.loadtxt(obj)
+        else:
+            res = obj
+        return res
+
+
+class ArrayNType(ArrayType):
+    def __init__(self, dimensions, default=None):
+        super(ArrayNType, self).__init__(default=default)
+        self.N = dimensions
+
+
+def get_filename_obj(hdul, hint, ext):
+
+    if isinstance(hint, str):
+        filename = hint + ext
+    elif callable(hint):
+        filename = hint(hdul)
+    else:
+        raise ValueError('"hint" is neither string nor callable')
+    return filename
+
+
+def get_filename_numpy_array(obj, where):
+    filename = get_filename_obj(obj, where, '.txt')
+    return filename
+
+
+def dump_numpy_array(obj, where, fmt='%.18e'):
+    filename = get_filename_numpy_array(obj, where)
+    numpy.savetxt(filename, obj, fmt=fmt)
+    return filename
