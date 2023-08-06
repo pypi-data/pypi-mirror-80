67a823f8930edcdbf65704bd30fb9600ab7a04a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:19 +0000

    Add package bssapi-dbfread/0.1.0/BssApi_DbfRead-0.1.0-py3-none-any.whl

---
+++ packages/bssapi-dbfread/BssApi_DbfRead-0.1.0-py3-none-any.whl/bssapi_dbfread/__init__.py
@@ -0,0 +1,31 @@
+"""
+Read DBF files with Python.
+
+Example:
+
+    >>> from bssapi_dbfread import DBF
+    >>> for record in DBF('people.dbf'):
+    ...     print(record)
+    OrderedDict([('NAME', 'Alice'), ('BIRTHDATE', datetime.date(1987, 3, 1))])
+    OrderedDict([('NAME', 'Bob'), ('BIRTHDATE', datetime.date(1980, 11, 12))])
+
+Full documentation at https://dbfread.readthedocs.io/
+
+"""
+__author__ = 'Ole Martin Bjorndalen'
+__email__ = 'ombdalen@gmail.com'
+__url__ = 'https://dbfread.readthedocs.io/'
+__license__ = 'MIT'
+
+from .dbf import DBF
+from .deprecated_dbf import open, read
+from .exceptions import *
+from .field_parser import FieldParser, InvalidValue
+from .version import version_info, version as __version__
+from .codec_mazovia import register as register_mazovia_codec
+
+register_mazovia_codec()
+
+
+# Prevent splat import.
+__all__ = []
