664e89700181921bb32795e1ab8979c84a6b8d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:59:54 +0000

    Add package numina/0.23.1/numina-0.23.1-cp37-cp37m-manylinux2010_x86_64.whl

---
+++ packages/numina/numina-0.23.1-cp37-cp37m-manylinux2010_x86_64.whl/numina/dal/daliface.py
@@ -0,0 +1,43 @@
+#
+# Copyright 2014-2017 Universidad Complutense de Madrid
+#
+# This file is part of Numina
+#
+# SPDX-License-Identifier: GPL-3.0+
+# License-Filename: LICENSE.txt
+#
+
+"""DAL base class"""
+
+from abc import ABCMeta, abstractmethod
+
+from six import with_metaclass
+
+
+class DALInterface(with_metaclass(ABCMeta, object)):
+    """Abstract Base Class for DAL"""
+
+    @abstractmethod
+    def obsres_from_oblock_id(self, obsid):
+        # This implementation does not depend
+        # on the details of the DAL...
+        pass
+
+    @abstractmethod
+    def search_recipe_from_ob(self, ob, pipeline='default'):
+        # returns RecipeClass
+        pass
+
+    @abstractmethod
+    def search_product(self, name, tipo, obsres, options=None):
+        # returns StoredProduct
+        pass
+
+    @abstractmethod
+    def search_parameter(self, name, tipo, obsres, options=None):
+        # returns StoredProduct
+        pass
+
+    @abstractmethod
+    def search_result_relative(self, name, tipo, obsres, result_desc, options=None):
+        pass
