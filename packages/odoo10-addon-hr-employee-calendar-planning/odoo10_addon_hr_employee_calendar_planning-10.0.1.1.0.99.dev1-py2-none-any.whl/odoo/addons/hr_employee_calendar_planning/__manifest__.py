d0b2aabc0a400badb29d700527b42253aacb128a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:53 +0000

    Add package odoo10-addon-hr-employee-calendar-planning/10.0.1.1.0.99.dev1/odoo10_addon_hr_employee_calendar_planning-10.0.1.1.0.99.dev1-py2-none-any.whl

---
+++ packages/odoo10-addon-hr-employee-calendar-planning/odoo10_addon_hr_employee_calendar_planning-10.0.1.1.0.99.dev1-py2-none-any.whl/odoo/addons/hr_employee_calendar_planning/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Employee Calendar Planning",
+    "version": "10.0.1.1.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/hr_employee_views.xml",
+        "wizards/wizard_employee_calendar_views.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+}
---
+++ packages/odoo10-addon-hr-employee-calendar-planning/odoo10_addon_hr_employee_calendar_planning-10.0.1.1.0.99.dev1-py2-none-any.whl/odoo/addons/hr_employee_calendar_planning/readme/ROADMAP.rst
@@ -0,0 +1,3 @@
+
+* Avoid the regeneration of whole private calendars each time a change is
+  detected.
