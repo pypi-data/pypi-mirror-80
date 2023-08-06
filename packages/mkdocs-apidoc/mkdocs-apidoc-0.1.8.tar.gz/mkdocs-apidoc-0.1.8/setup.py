edf07cd8deaebd1c4ed7a53b27d276aead72965c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:18 +0000

    Add package mkdocs-apidoc/0.1.8/mkdocs-apidoc-0.1.8.tar.gz

---
+++ packages/mkdocs-apidoc/mkdocs-apidoc-0.1.8.tar.gz/mkdocs-apidoc-0.1.8/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mkdocs_apidoc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['attrs>=20.2.0,<21.0.0',
+ 'jinja2>=2.10,<3.0',
+ 'mkdocs>=1.1,<2.0',
+ 'numpydoc>=0.9.2,<0.10.0']
+
+entry_points = \
+{'mkdocs.plugins': ['mkdocs_apidoc = mkdocs_apidoc.plugin:ApiDocPlugin']}
+
+setup_kwargs = {
+    'name': 'mkdocs-apidoc',
+    'version': '0.1.8',
+    'description': '',
+    'long_description': None,
+    'author': 'Evan',
+    'author_email': 'evanmcurtin@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
