964dee78d6593aa1db00913aa3f1921be26e698b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:29 +0000

    Add package py2700/0.1.0/py2700-0.1.0.tar.gz

---
+++ packages/py2700/py2700-0.1.0.tar.gz/py2700-0.1.0/py2700/__init__.py
@@ -0,0 +1,3 @@
+from py2700.MeasurementType import MeasurementType
+from py2700.Multimeter import *
+
---
+++ packages/py2700/py2700-0.1.0.tar.gz/py2700-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py2700",
+    version="0.1.0",
+    author="ngpaladi",
+    author_email="py2700@noahpaladino.com",
+    description="A Python package to interface with a Keithley 2700 multimeter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ngpaladi/py2700",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'PyVISA',
+        'PyVISA-py',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
