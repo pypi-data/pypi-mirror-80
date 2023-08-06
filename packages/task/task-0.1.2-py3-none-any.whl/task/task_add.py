d8d2ba12ff663647f79a0de48b9391e33c569c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:25 +0000

    Add package task/0.1.2/task-0.1.2-py3-none-any.whl

---
+++ packages/task/task-0.1.2-py3-none-any.whl/task/task_add.py
@@ -0,0 +1,41 @@
+import click
+from datetime import datetime
+from click_default_group import DefaultGroup
+
+import utils
+
+
+@click.group(
+    name='add',
+    cls=DefaultGroup,
+    default='new-task',
+    default_if_no_args=True
+)
+def main():
+    """Add task"""
+
+
+def format_current_time() -> str:
+    date = str(datetime.now())
+    return date.split('.')[0]
+
+
+@main.command()
+@click.argument('task', type=click.STRING)
+@click.option('-p', '--project', help="specyfiy project")
+@click.option('-u', '--urgency', help="urgency")
+@click.option('-d', '--due', help="due date")
+def new_task(task: str, project: str = None, urgency: int = 0, due: str = None):
+    """Add task to database."""
+    if utils.does_db_exist()[0] is False:
+        utils.create_db()
+    else:
+        print("don't create db")
+
+    task_data = dict()
+    task_data['task'] = task
+    task_data['project'] = project
+    task_data['urgency'] = urgency
+    task_data['due'] = due
+    task_data['datetime'] = format_current_time()
+    utils.insert_to_db(task_data)
