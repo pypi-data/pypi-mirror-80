3e4ec1db9dac630591278ab0c571694916bf615d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:58:54 +0000

    Add package importfix/1.0/importfix-1.0.tar.gz

---
+++ packages/importfix/importfix-1.0.tar.gz/importfix-1.0/setup.py
@@ -0,0 +1,39 @@
+import os
+import re
+import setuptools
+
+varsVals = {}
+
+try:
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+except:
+    long_description = ""
+
+if os.path.isfile("variables"):
+    try:
+        with open("variables", "r") as fh:
+            variables = fh.read().strip().split("\n")
+        for v in variables:
+            key, value = v.split("=")
+            varsVals[key] = re.sub("['\"]", "", value)
+    except:
+        pass
+
+setuptools.setup(
+    name=varsVals["NAME"],
+    version=varsVals["VERSION"],
+    author=varsVals["AUTHOR"],
+    author_email=varsVals["AUTHOR_EMAIL"],
+    description=varsVals["DESCRIPTION"],
+    url=varsVals["URL"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
