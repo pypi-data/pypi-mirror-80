6ca74fb11d6f7ad9706a37964e4edd08cbde59cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:50 +0000

    Add package EasyGA/0.0.12/EasyGA-0.0.12.tar.gz

---
+++ packages/EasyGA/EasyGA-0.0.12.tar.gz/EasyGA-0.0.12/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='EasyGA',
+    version='0.0.12',
+    description='A ubiquitous or general purpuse GA',
+    #py_modules=["EasyGA"],
+    #package_dir={'':'src'},
+    packages=find_packages(),
+    python_requires='>=3.6',
+    url="https://github.com/danielwilczak101/EasyGA",
+    author="Daniel Wilczak",
+    author_email="danielwilczak101@gmail.com",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    classifier=[
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+        ],
+    install_requires = ["blessings ~= 1.7",
+                        ],
+    extra_require = {
+        "dev": [
+            "pytest>=3.7",
+            ],
+        },
+    )
