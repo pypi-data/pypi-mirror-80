67514917da7cf141c9e5d7b0c79e9d88af979dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:52 +0000

    Add package odoo12-addon-easy-my-coop-api/12.0.0.0.1.99.dev22/odoo12-addon-easy_my_coop_api-12.0.0.0.1.99.dev22.tar.gz

---
+++ packages/odoo12-addon-easy-my-coop-api/odoo12-addon-easy_my_coop_api-12.0.0.0.1.99.dev22.tar.gz/odoo12-addon-easy_my_coop_api-12.0.0.0.1.99.dev22/odoo/addons/easy_my_coop_api/services/subscription_request_service.py
@@ -0,0 +1,222 @@
+# Copyright 2019 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# pylint: disable=consider-merging-classes-inherited
+
+import logging
+
+from werkzeug.exceptions import BadRequest, NotFound
+
+from odoo import _
+from odoo.fields import Date
+
+from odoo.addons.base_rest.http import wrapJsonException
+from odoo.addons.component.core import Component
+
+from . import schemas
+
+_logger = logging.getLogger(__name__)
+
+
+class SubscriptionRequestService(Component):
+    _inherit = "emc.rest.service"
+    _name = "subscription.request.services"
+    _usage = "subscription-request"
+    _description = """
+        Subscription Request Services
+    """
+
+    def get(self, _id):
+        sr = self.env["subscription.request"].search(
+            [("_api_external_id", "=", _id)]
+        )
+        if sr:
+            return self._to_dict(sr)
+        else:
+            raise wrapJsonException(
+                NotFound(_("No subscription request for id %s") % _id)
+            )
+
+    def search(self, date_from=None, date_to=None):
+        _logger.info("search from {} to {}".format(date_from, date_to))
+
+        domain = []
+        if date_from:
+            date_from = Date.from_string(date_from)
+            domain.append(("date", ">=", date_from))
+        if date_to:
+            date_to = Date.from_string(date_to)
+            domain.append(("date", "<=", date_to))
+
+        requests = self.env["subscription.request"].search(domain)
+
+        response = {
+            "count": len(requests),
+            "rows": [self._to_dict(sr) for sr in requests],
+        }
+        return response
+
+    def create(self, **params):  # pylint: disable=method-required-super
+
+        params = self._prepare_create(params)
+        sr = self.env["subscription.request"].create(params)
+        return self._to_dict(sr)
+
+    def update(self, _id, **params):
+        params = self._prepare_update(params)
+        sr = self.env["subscription.request"].search(
+            [("_api_external_id", "=", _id)]
+        )
+        if not sr:
+            raise wrapJsonException(
+                NotFound(_("No subscription request for id %s") % _id)
+            )
+        sr.write(params)
+        return self._to_dict(sr)
+
+    def validate(self, _id, **params):
+        sr = self.env["subscription.request"].search(
+            [("_api_external_id", "=", _id)]
+        )
+        if not sr:
+            raise wrapJsonException(
+                NotFound(_("No subscription request for id %s") % _id)
+            )
+        if sr.state != "draft":
+            raise wrapJsonException(
+                BadRequest(
+                    _("Subscription request %s is not in draft state") % _id
+                )
+            )
+        sr.validate_subscription_request()
+        return self._to_dict(sr)
+
+    def _to_dict(self, sr):
+        sr.ensure_one()
+
+        if sr.capital_release_request:
+            invoice_ids = [
+                invoice.get_api_external_id()
+                for invoice in sr.capital_release_request
+            ]
+        else:
+            invoice_ids = []
+
+        share_product = sr.share_product_id.product_tmpl_id
+        return {
+            "id": sr.get_api_external_id(),
+            "name": sr.name,
+            "email": sr.email,
+            "state": sr.state,
+            "date": Date.to_string(sr.date),
+            "ordered_parts": sr.ordered_parts,
+            "share_product": self._one_to_many_to_dict(share_product),
+            "address": {
+                "street": sr.address,
+                "zip_code": sr.zip_code,
+                "city": sr.city,
+                "country": sr.country_id.code,
+            },
+            "lang": sr.lang,
+            "capital_release_request": invoice_ids,
+        }
+
+    def _get_country(self, code):
+        country = self.env["res.country"].search([("code", "=", code)])
+        if country:
+            return country
+        else:
+            raise wrapJsonException(
+                BadRequest(_("No country for isocode %s") % code)
+            )
+
+    def _get_share_product(self, template_id):
+        product = self.env["product.product"].search(
+            [("product_tmpl_id", "=", template_id)]
+        )
+        if product:
+            return product
+        else:
+            raise wrapJsonException(
+                BadRequest(_("No share for id %s") % template_id)
+            )
+
+    def _prepare_create(self, params):
+        """Prepare a writable dictionary of values"""
+        address = params["address"]
+        country = self._get_country(address["country"])
+
+        share_product_id = self._get_share_product(params["share_product"])
+
+        return {
+            "name": params["name"],
+            "email": params["email"],
+            "ordered_parts": params["ordered_parts"],
+            "share_product_id": share_product_id.id,
+            "address": address["street"],
+            "zip_code": address["zip_code"],
+            "city": address["city"],
+            "country_id": country.id,
+            "lang": params["lang"],
+        }
+
+    def _prepare_update(self, params):
+        if "address" in params:
+            address = params["address"]
+            if "country" in address:
+                country = self._get_country(address["country"]).id
+                address["country"] = country.id
+        else:
+            address = {}
+
+        if "share_product" in params:
+            share_product_id = self._get_share_product(
+                params["share_product"]
+            ).id
+        else:
+            share_product_id = None
+
+        params = {
+            "name": params.get("name"),
+            "email": params.get("email"),
+            "state": params.get("state"),
+            "ordered_parts": params.get("ordered_parts"),
+            "share_product_id": share_product_id,
+            "address": address.get("street"),
+            "zip_code": address.get("zip_code"),
+            "city": address.get("city"),
+            "country_id": address.get("country"),
+            "lang": params.get("lang"),
+        }
+        params = {k: v for k, v in params.items() if v is not None}
+        return params
+
+    def _validator_get(self):
+        return schemas.S_SUBSCRIPTION_REQUEST_GET
+
+    def _validator_return_get(self):
+        return schemas.S_SUBSCRIPTION_REQUEST_RETURN_GET
+
+    def _validator_search(self):
+        return schemas.S_SUBSCRIPTION_REQUEST_SEARCH
+
+    def _validator_return_search(self):
+        return schemas.S_SUBSCRIPTION_REQUEST_RETURN_SEARCH
+
+    def _validator_create(self):
+        return schemas.S_SUBSCRIPTION_REQUEST_CREATE
+
+    def _validator_return_create(self):
+        return schemas.S_SUBSCRIPTION_REQUEST_RETURN_GET
+
+    def _validator_update(self):
+        return schemas.S_SUBSCRIPTION_REQUEST_UPDATE
+
+    def _validator_return_update(self):
+        return schemas.S_SUBSCRIPTION_REQUEST_RETURN_GET
+
+    def _validator_validate(self):
+        return schemas.S_SUBSCRIPTION_REQUEST_VALIDATE
+
+    def _validator_return_validate(self):
+        return schemas.S_SUBSCRIPTION_REQUEST_RETURN_GET
---
+++ packages/odoo12-addon-easy-my-coop-api/odoo12-addon-easy_my_coop_api-12.0.0.0.1.99.dev22.tar.gz/odoo12-addon-easy_my_coop_api-12.0.0.0.1.99.dev22/odoo/addons/easy_my_coop_api/tests/test_subscription_requests.py
@@ -0,0 +1,205 @@
+# Copyright 2020 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+import json
+from datetime import timedelta
+
+from werkzeug.exceptions import BadRequest
+
+import odoo
+from odoo.fields import Date
+
+from odoo.addons.base_rest.controllers.main import _PseudoCollection
+from odoo.addons.component.core import WorkContext
+
+from .common import BaseEMCRestCase
+
+
+class TestSRController(BaseEMCRestCase):
+    def setUp(self):
+        super().setUp()
+        collection = _PseudoCollection("emc.services", self.env)
+        emc_services_env = WorkContext(
+            model_name="rest.service.registration", collection=collection
+        )
+
+        self.sr_service = emc_services_env.component(
+            usage="subscription-request"
+        )
+
+        self.demo_request_1 = self.browse_ref(
+            "easy_my_coop.subscription_request_1_demo"
+        )
+        self.demo_request_2 = self.browse_ref(
+            "easy_my_coop.subscription_request_waiting_demo"
+        )
+        self.demo_share_product = (
+            self.demo_request_1.share_product_id.product_tmpl_id
+        )
+
+        date = Date.to_string(self.demo_request_1.date)
+        self.demo_request_1_dict = {
+            "id": self.demo_request_1.get_api_external_id(),
+            "name": "Manuel Dublues",
+            "email": "manuel@demo.net",
+            "date": date,
+            "state": "draft",
+            "ordered_parts": 3,
+            "share_product": {
+                "id": self.demo_share_product.get_api_external_id(),
+                "name": self.demo_share_product.name,
+            },
+            "address": {
+                "street": "schaerbeekstraat",
+                "zip_code": "1111",
+                "city": "Brussels",
+                "country": "BE",
+            },
+            "lang": "en_US",
+            "capital_release_request": [],
+        }
+
+    def test_service(self):
+        # kept as example
+        # useful if you need to change data in database and check db type
+
+        result = self.sr_service.get(self.demo_request_1.get_api_external_id())
+        self.assertEquals(self.demo_request_1_dict, result)
+
+        all_sr = self.sr_service.search()
+        self.assertTrue(all_sr)
+
+        sr_date = self.demo_request_1.date
+        date_from = Date.to_string(sr_date - timedelta(days=1))
+        date_to = Date.to_string(sr_date + timedelta(days=1))
+
+        date_sr = self.sr_service.search(date_from=date_from, date_to=date_to)
+        self.assertTrue(date_sr)
+
+    def test_route_get(self):
+        external_id = self.demo_request_1.get_api_external_id()
+        route = "/api/subscription-request/%s" % external_id
+        content = self.http_get_content(route)
+        self.assertEquals(self.demo_request_1_dict, content)
+
+    # fixme works locally, not on travis: check later and move on
+    # @odoo.tools.mute_logger("odoo.addons.base_rest.http")
+    # def test_route_get_returns_not_found(self):
+    #     route = "/api/subscription-request/%s" % "99999"
+    #     response = self.http_get(route)
+    #     self.assertEquals(response.status_code, 404)
+
+    def test_route_get_string_returns_method_not_allowed(self):
+        route = "/api/subscription-request/%s" % "abc"
+        response = self.http_get(route)
+        self.assertEquals(response.status_code, 405)
+
+    def test_route_search_all(self):
+        route = "/api/subscription-request"
+        content = self.http_get_content(route)
+        self.assertIn(self.demo_request_1_dict, content["rows"])
+
+    def test_route_search_by_date(self):
+        sr_date = self.demo_request_1.date
+        date_from = Date.to_string(sr_date - timedelta(days=1))
+        date_to = Date.to_string(sr_date + timedelta(days=1))
+
+        route = "/api/subscription-request?date_from=%s" % date_from
+        content = self.http_get_content(route)
+        self.assertIn(self.demo_request_1_dict, content["rows"])
+
+        route = "/api/subscription-request?date_to=%s" % date_to
+        content = self.http_get_content(route)
+        self.assertIn(self.demo_request_1_dict, content["rows"])
+
+        route = "/api/subscription-request?date_from={}&date_to={}".format(
+            date_from, date_to
+        )
+        content = self.http_get_content(route)
+        self.assertIn(self.demo_request_1_dict, content["rows"])
+
+        route = "/api/subscription-request?date_from=%s" % "2300-01-01"
+        content = self.http_get_content(route)
+        self.assertEquals(content["count"], 0)
+
+        route = "/api/subscription-request?date_to=%s" % "1900-01-01"
+        content = self.http_get_content(route)
+        self.assertEquals(content["count"], 0)
+
+    @odoo.tools.mute_logger("odoo.addons.base_rest.http")
+    def test_route_search_acd_date_returns_bad_request(self):
+        route = "/api/subscription-request?date_from=%s" % "20200101"
+        response = self.http_get(route)
+        self.assertEquals(response.status_code, 400)
+
+    def test_route_create(self):
+        url = "/api/subscription-request"
+        data = {
+            "name": "Lisa des Danses",
+            "email": "lisa@desdanses.be",
+            "ordered_parts": 3,
+            "share_product": self.demo_share_product.id,
+            "address": {
+                "street": "schaerbeekstraat",
+                "zip_code": "1111",
+                "city": "Brussels",
+                "country": "BE",
+            },
+            "lang": "en_US",
+        }
+
+        response = self.http_post(url, data=data)
+        self.assertEquals(response.status_code, 200)
+        content = json.loads(response.content.decode("utf-8"))
+
+        content.pop("id")  # can't know id in advance
+        expected = {
+            **data,
+            **{
+                "date": Date.to_string(Date.today()),
+                "state": "draft",
+                "share_product": {
+                    "id": self.demo_share_product.get_api_external_id(),
+                    "name": self.demo_share_product.name,
+                },
+                "capital_release_request": [],
+            },
+        }
+        self.assertEquals(expected, content)
+
+    def test_route_update(self):
+        url = (
+            "/api/subscription-request/%s"
+            % self.demo_request_1.get_api_external_id()
+        )
+        data = {"state": "done"}
+
+        response = self.http_post(url, data=data)
+        self.assertEquals(response.status_code, 200)
+        content = json.loads(response.content.decode("utf-8"))
+
+        expected = self.demo_request_1_dict
+        expected["state"] = "done"
+        self.assertEquals(expected, content)
+
+    def test_route_validate(self):
+        url = (
+            "/api/subscription-request/%s/validate"
+            % self.demo_request_1.get_api_external_id()
+        )
+        response = self.http_post(url, data={})
+        self.assertEquals(response.status_code, 200)
+        content = json.loads(response.content.decode("utf-8"))
+
+        state = content.get("state")
+        self.assertEquals(state, "done")
+
+    def test_service_validate_draft_request(self):
+        self.sr_service.validate(self.demo_request_1.get_api_external_id())
+        self.assertEquals(self.demo_request_1.state, "done")
+        self.assertTrue(len(self.demo_request_1.capital_release_request) > 0)
+
+    def test_service_validate_done_request(self):
+        with self.assertRaises(BadRequest):
+            self.sr_service.validate(self.demo_request_2.get_api_external_id())
