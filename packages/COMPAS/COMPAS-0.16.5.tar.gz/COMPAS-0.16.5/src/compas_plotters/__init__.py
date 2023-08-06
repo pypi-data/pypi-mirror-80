f9859732da7fb903469e6f267ecac33c185b816b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:41 +0000

    Add package COMPAS/0.16.5/COMPAS-0.16.5.tar.gz

---
+++ packages/COMPAS/COMPAS-0.16.5.tar.gz/COMPAS-0.16.5/src/compas_plotters/__init__.py
@@ -0,0 +1,46 @@
+"""
+********************************************************************************
+compas_plotters
+********************************************************************************
+
+.. currentmodule:: compas_plotters
+
+
+Classes
+=======
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    NetworkPlotter
+    MeshPlotter
+
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+__version__ = '0.16.5'
+
+
+# core -> utilities
+
+# geometry plotter
+# network plotter
+# mesh plotter
+# object plotter
+
+from .core import *  # noqa: F401 F403
+from .artists import *  # noqa: F401 F403
+
+from .plotter import Plotter  # noqa: F401
+from .plotter2 import Plotter2  # noqa: F401
+
+from .networkplotter import NetworkPlotter  # noqa: F401
+from .meshplotter import MeshPlotter  # noqa: F401
+
+
+__all__ = [name for name in dir() if not name.startswith('_')]
