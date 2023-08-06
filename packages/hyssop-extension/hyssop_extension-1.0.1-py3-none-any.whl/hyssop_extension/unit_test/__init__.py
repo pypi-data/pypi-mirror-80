9a2a0b4bad204f2dd0952a4801585930ca528716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:34 +0000

    Add package hyssop-extension/1.0.1/hyssop_extension-1.0.1-py3-none-any.whl

---
+++ packages/hyssop-extension/hyssop_extension-1.0.1-py3-none-any.whl/hyssop_extension/unit_test/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2020-Present the hyssop authors and contributors.
+#
+# This module is part of hyssop and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+'''
+File created: September 4th 2020
+
+Modified By: hsky77
+Last Updated: September 4th 2020 17:50:05 pm
+'''
+
+from hyssop.unit_test import UnitTestTypes
+
+
+class HyssopExtensionUnitTestTypes(UnitTestTypes):
+    TestOrm = ('test_orm', 'orm', 'OrmTestCase')
