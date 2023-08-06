bc9b5244dc57a39a024b2dcacee3e248964660e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:01 +0000

    Add package opics/0.1.6/opics-0.1.6.tar.gz

---
+++ packages/opics/opics-0.1.6.tar.gz/opics-0.1.6/AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Main Author
+----------------
+* Jaspreet Jhoja <jaspreet@siepic.com> <jj@alumni.ubc.ca>
+
+Contributors
+------------
+* Mustafa Hammood <mustafa@siepic.com>
+* Stephen Lin <slin@siepic.com>
+
---
+++ packages/opics/opics-0.1.6.tar.gz/opics-0.1.6/opics/__init__.py
@@ -0,0 +1,20 @@
+"""Top-level package for OPICS."""
+
+from opics import libs
+from opics.network import Network
+from opics.utils import universal_sparam_filereader, LUT_processor, LUT_reader, netlistParser
+from opics.globals import c, f
+
+"Register ebeam library"
+import opics.libs.ebeam
+
+__author__ = 'Jaspreet Jhoja'
+__email__ = 'Jaspreet@siepic.com'
+__version__ = '0.1.6'
+
+#initialize OPICS package
+
+name = "opics"
+
+__all__ = ['Network', 'libs', "globals"]
+
