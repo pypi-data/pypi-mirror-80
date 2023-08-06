65f26e49c3e74cb0efb17f3eea11c2b41f5a41cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:56:34 +0000

    Add package nomad-cbec-id-cli/1.2.0/nomad-cbec-id-cli-1.2.0.tar.gz

---
+++ packages/nomad-cbec-id-cli/nomad-cbec-id-cli-1.2.0.tar.gz/nomad-cbec-id-cli-1.2.0/nomad_cbec_id_cli/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from nomad_cbec_id_cli.api.health_api import HealthApi
+from nomad_cbec_id_cli.api.user_api import UserApi
