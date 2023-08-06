1ffb1f31f586f8a3361d5c7121e2d43ce5ce3401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:29 +0000

    Add package rabbit-client/1.0.0a1/rabbit_client-1.0.0a1-py3-none-any.whl

---
+++ packages/rabbit-client/rabbit_client-1.0.0a1-py3-none-any.whl/rabbit/__main__.py
@@ -0,0 +1,86 @@
+import logging
+import os
+import sys
+
+from cleo import Application, Command
+
+from rabbit import __version__
+from rabbit.cli.consumer import Consumer
+from rabbit.cli.producer import Producer
+from rabbit.cli.pubsub import PubSub
+
+logging.basicConfig(level=logging.INFO)
+
+
+class PollingPublisherCommand(Command):
+    """
+    Start PollingPublisher sample application 📥📤
+
+    polling-publisher
+    """
+
+    def handle(self):
+        self.line(f"<info>>></info> <options=bold>starting polling-publisher...</>")
+        pubsub = PubSub()
+        try:
+            pubsub.run()
+        except KeyboardInterrupt:
+            self.line(f"<info>!></info> <options=bold>polling-publisher finished!</>")
+            raise SystemExit
+
+
+class ConsumerCommand(Command):
+    """
+    Start a consumer sample application 📥
+
+    consumer
+    """
+
+    def handle(self):
+        self.line(f"<info>>></info> <options=bold>starting consumer...</>")
+        consumer = Consumer()
+        try:
+            consumer.run()
+        except KeyboardInterrupt:
+            self.line(f"<info>!></info> <options=bold>consumer finished!</>")
+            raise SystemExit
+
+
+class EventCommand(Command):
+    """
+    Send a sample message 📨 to Consumer or PollingPublisher
+
+    send-event
+        {payload : payload file in json format.}
+        {--e|events=1 : events to send.}
+    """
+
+    def handle(self):
+        self.line(
+            f"<info>>></info> <options=bold>sending event to: "
+            f"[exchange: {os.getenv('SUBSCRIBE_EXCHANGE', 'default.in.exchange')}"
+            f" | topic: {os.getenv('SUBSCRIBE_TOPIC', '#')} | "
+            f"subscribe: {os.getenv('SUBSCRIBE_QUEUE', 'default.subscribe.queue')}]</>"
+        )
+        try:
+            with open(f"{self.argument('payload')}", "rb") as f:
+                payload = f.read()
+        except FileNotFoundError:
+            self.line(f"<error>File not found: {self.argument('payload')}</error>")
+            sys.exit(1)
+        try:
+            prod = Producer(payload, qtd=int(self.option("events")))
+            prod.send_event()
+            self.line(f"<info>!></info> <options=bold>event successfully submitted!</>")
+        except OSError:
+            self.line("<error>Failure to connect to RabbitMQ.</error>")
+
+
+application = Application("rabbit-client", f"{__version__}")
+application.add(PollingPublisherCommand())
+application.add(ConsumerCommand())
+application.add(EventCommand())
+
+
+if __name__ == "__main__":
+    application.run()
