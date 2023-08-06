42984fe5871565a7fcaf2c20e298167ad3161e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:34 +0000

    Add package bashrc/0.31.0/bashrc-0.31.0.tar.gz

---
+++ packages/bashrc/bashrc-0.31.0.tar.gz/bashrc-0.31.0/bashrc/scripts/secrets-pull.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+export starting="${BASH_SOURCE[0]}"; debug.sh starting
+
+test -n "${GITHUB_SECRETS_PATH}" || { error.sh GITHUB_SECRETS_PATH 'not defined'; exit 1; }
+cd "${GITHUB_SECRETS_PATH}" || { error.sh "${GITHUB_SECRETS_PATH}" 'not found'; exit 1; }
+gpull.sh
+cd - > /dev/null || exit 1
+unset starting
