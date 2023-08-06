d8a4101a97b6a2e91640bc5ddf9e587952396976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:06 +0000

    Add package pythonapp/0.1/pythonapp-0.1.tar.gz

---
+++ packages/pythonapp/pythonapp-0.1.tar.gz/pythonapp-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pythonapp',
+      version='0.1',
+      description='pythonapp',
+      url='https://github.com/ignaciojluque/pythonapp',
+      author='Ignacio Luque',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['pythonapp'],
+      zip_safe=False)
