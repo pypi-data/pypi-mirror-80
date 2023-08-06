d09697eeead7b929d2d271f9faef72fb7699e9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:08 +0000

    Add package metagraph/0.1.0/metagraph-0.1.0.tar.gz

---
+++ packages/metagraph/metagraph-0.1.0.tar.gz/metagraph-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name="metagraph",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Graph algorithm solver across multiple hardware backends",
+    author="Anaconda, Inc.",
+    packages=find_packages(include=["metagraph", "metagraph.*"]),
+    install_requires=[
+        "importlib_metadata",
+        "numpy",
+        "scipy",
+        "donfig",
+        "dask",
+        "networkx",
+        "pandas",
+        "python-louvain",
+        "nest-asyncio",
+    ],
+    include_package_data=True,
+    entry_points={"metagraph.plugins": ["plugins=metagraph.plugins:find_plugins",]},
+)
