b1e2da35afeda190db36b89d43cd1dea24c8e351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:11 +0000

    Add package NekoCAS/0.1/NekoCAS-0.1.tar.gz

---
+++ packages/NekoCAS/NekoCAS-0.1.tar.gz/NekoCAS-0.1/NekoCAS/__init__.py
@@ -0,0 +1,38 @@
+name = "NekoCAS"
+
+import requests
+
+
+class NekoCAS:
+    def __init__(self, domain, secret):
+        self.domain = domain
+        self.secret = secret
+
+    def set_domain(self, domain):
+        self.domain = domain
+
+    def set_secret(self, secret):
+        self.secret = secret
+
+    def validate(self, ticket):
+        params = {
+            'service': self.secret,
+            'ticket': ticket
+        }
+        target_url = self.domain + "/validate"
+        try:
+            r = requests.get(target_url, params=params)
+            r.raise_for_status()
+            r = r.json()
+            
+            if not r['success']:
+                return None, 'invalid ticket'
+
+            return {
+                       'name': r['data']['name'],
+                       'email': r['data']['email'],
+                       'token': r['data']['token'],
+                       'message': r['message']
+                   }, None
+        except requests.RequestException as e:
+            return None, e.message
---
+++ packages/NekoCAS/NekoCAS-0.1.tar.gz/NekoCAS-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NekoCAS',
+    version='0.1',
+    description='NekoCAS SDK for Python',
+    url='https://github.com/NekoWheel/NekoCAS_Python_SDK',
+    author='E99p1ant',
+    author_email='i@github.red',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+)
