c5809748771fb613c4fe7c2ffd0cd352bf400e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:45 +0000

    Add package hemlock-survey/0.0.23/hemlock_survey-0.0.23-py3-none-any.whl

---
+++ packages/hemlock-survey/hemlock_survey-0.0.23-py3-none-any.whl/hemlock/qpolymorphs/select.py
@@ -0,0 +1,97 @@
+"""# Select (dropdown)"""
+
+from ..app import db, settings
+from ..functions.debug import click_choices
+from ..models import ChoiceQuestion
+from .input_group import InputGroup
+
+settings['Select'] = {
+    'align': 'left',
+    'inline': False,
+    'debug': click_choices,
+    'multiple': False,
+    'size': None,
+}
+
+
+class Select(InputGroup, ChoiceQuestion):
+    """
+    Select questions allow participants to select one or more options from a 
+    dropdown menu.
+
+    Inherits from [`hemlock.InputGroup`](input_group.md) and 
+    [`hemlock.ChoiceQuestion`](question.md).
+
+    Parameters
+    ----------
+    label : str or bs4.BeautifulSoup, default=''
+        Select question label.
+
+    choices : list of str or hemlock.Option, default=[]
+        Options which participants can select. String inputs are automatically
+        converted to `hemlock.Option` objects.
+
+    template : str, default='hemlock/select.html'
+        Template for the select body.
+
+    Attributes
+    ----------
+    align : str, default='left'
+        Choice alignment; `'left'`, `'center'`, or `'right'`.
+        
+    multiple : bool, default=False
+        Indicates that the participant may select multiple choices.
+
+    select : bs4.Tag
+        `<select>` tag.
+
+    size : int or None, default=None
+        Number of rows of choices to display.
+
+    Examples
+    --------
+    ```python
+    from hemlock import Page, Select, Option, push_app_context
+
+    app = push_app_context()
+
+    Page(Select('<p>Select one.</p>', ['World','Moon','Star'])).preview()
+    ```
+    """
+    id = db.Column(db.Integer, db.ForeignKey('question.id'), primary_key=True)
+    __mapper_args__ = {'polymorphic_identity': 'select'}
+
+    def __init__(
+            self, label='', choices=[], template='hemlock/select.html', 
+            **kwargs
+        ):
+        super().__init__(label, choices, template, **kwargs)
+
+    @property
+    def multiple(self):
+        return 'multiple' in self.select.attrs
+
+    @multiple.setter
+    def multiple(self, val):
+        assert isinstance(val, bool)
+        if val:
+            self.select['multiple'] = None
+        else:
+            self.select.attrs.pop('multiple', None)
+        self.body.changed()
+
+    @property
+    def select(self):
+        return self.body.select_one('select#'+self.model_id)
+
+    @property
+    def size(self):
+        return self.select.attrs.get('size')
+
+    @size.setter
+    def size(self, val):
+        if not val:
+            self.select.attrs.pop('size', None)
+        else:
+            self.select['size'] = val
+        self.body.changed()
---
+++ packages/hemlock-survey/hemlock_survey-0.0.23-py3-none-any.whl/hemlock/qpolymorphs/textarea.py
@@ -0,0 +1,105 @@
+"""# Textarea"""
+
+from ..app import db, settings
+from ..functions.debug import send_keys
+from ..models import Question
+from .input_group import InputGroup
+
+from flask import render_template
+
+settings['Textarea'] = {'debug': send_keys, 'rows': 3}
+
+
+class Textarea(InputGroup, Question):
+    """
+    Textareas provide large text boxes for free responses.
+
+    Inherits from [`hemlock.qpolymorphs.InputGroup`](input_group.md) and 
+    [`hemlock.Question`](question.md).
+
+    Parameters
+    ----------
+    label : str or bs4.BeautifulSoup, default=''
+        Textarea label.
+
+    template : str, default='hemlock/textarea.html'
+        Template for the textarea body.
+
+    Attributes
+    ----------
+    rows : int, default=3
+        Number of rows of text to display.
+
+    textarea : bs4.Tag
+        The `<textarea>` tag.
+
+    Notes
+    -----
+    Textareas have a default javascript which displays the character and word 
+    count to participants. This will be appended to any `js` and `extra_js`
+    arguments passed to the constructor.
+
+    Examples
+    --------
+    ```python
+    from hemlock import Page, Textarea, push_app_context
+
+    app = push_app_context()
+
+    Page(Textarea('<p>This is a textarea.</p>')).preview()
+    ```
+    """
+    id = db.Column(db.Integer, db.ForeignKey('question.id'), primary_key=True)
+    __mapper_args__ = {'polymorphic_identity': 'textarea'}
+
+    def __init__(self, page=None, template='hemlock/textarea.html', **kwargs):
+        super().__init__(page, template, **kwargs)
+        self.add_internal_js(
+            render_template('hemlock/textarea.js', self_=self)
+        )
+
+    @property
+    def rows(self):
+        return self.textarea.attrs.get('rows')
+
+    @rows.setter
+    def rows(self, val):
+        self.textarea['rows'] = val
+        self.body.changed()
+
+    @property
+    def placeholder(self):
+        return self.textarea.get('placeholder')
+
+    @placeholder.setter
+    def placeholder(self, val):
+        self.textarea['placeholder'] = val
+        self.body.changed()
+
+    @property
+    def textarea(self):
+        return self.body.select_one('textarea#'+self.model_id)
+
+    def textarea_from_driver(self, driver):
+        """
+        Get textarea from the webdriver for debugging.
+        
+        Parameters
+        ----------
+        driver : selenium.webdriver.chrome.webdriver.WebDriver
+            Selenium webdriver (does not need to be `Chrome`).
+
+        Returns
+        -------
+        textarea : selenium.webdriver.remote.webelement.WebElement
+            Web element of the `<textarea>` tag associated with this model.
+        """
+        return driver.find_element_by_css_selector('textarea#'+self.model_id)
+
+    def _render(self, body=None):
+        body = body or self.body.copy()
+        textarea = body.select_one('#'+self.model_id)
+        textarea.string = self.response or self.default or ''
+        return super()._render(body)
+
+    
