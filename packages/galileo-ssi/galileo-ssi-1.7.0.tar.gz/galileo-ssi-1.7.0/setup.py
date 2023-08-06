c6ea1be7448fb0bd8f6b19cebd7e15f31835629e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:15 +0000

    Add package galileo-ssi/1.7.0/galileo-ssi-1.7.0.tar.gz

---
+++ packages/galileo-ssi/galileo-ssi-1.7.0.tar.gz/galileo-ssi-1.7.0/setup.py
@@ -0,0 +1,26 @@
+"""Galileo SSI setup."""
+
+from setuptools import setup
+
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='galileo-ssi',
+    version='1.7.0',
+    description='Python parser for Galileo SSI instrument',
+    author='Benoit Seignovert',
+    author_email='benoit.a.seignovert@jpl.nasa.gov',
+    url='https://github.com/seignovert/galileo-ssi',
+    install_requires=requirements,
+    python_requires='>=3.6',
+    license='MIT',
+    packages=['ssi', 'ssi.isis', 'ssi.geol'],
+    long_description=long_description,
+    include_package_data=True,
+)
