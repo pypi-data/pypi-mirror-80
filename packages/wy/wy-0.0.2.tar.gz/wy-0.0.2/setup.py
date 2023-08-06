df059a79f9c33a7e13fc94e793900aeb58996529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:25 +0000

    Add package wy/0.0.2/wy-0.0.2.tar.gz

---
+++ packages/wy/wy-0.0.2.tar.gz/wy-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wy", 
+    version="0.0.2",
+    author="Korakot Chaovavanich",
+    author_email="korakot@gmail.com",
+    description="Convenient tools for WayScript",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/korakot/wy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
