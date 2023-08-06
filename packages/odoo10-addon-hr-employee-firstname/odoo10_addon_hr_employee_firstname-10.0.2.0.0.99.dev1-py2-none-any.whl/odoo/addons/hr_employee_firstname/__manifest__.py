3993877fca0302a663e0a69119ee0c201ff57d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:01 +0000

    Add package odoo10-addon-hr-employee-firstname/10.0.2.0.0.99.dev1/odoo10_addon_hr_employee_firstname-10.0.2.0.0.99.dev1-py2-none-any.whl

---
+++ packages/odoo10-addon-hr-employee-firstname/odoo10_addon_hr_employee_firstname-10.0.2.0.0.99.dev1-py2-none-any.whl/odoo/addons/hr_employee_firstname/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# ©  2010 - 2014 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
+# Copyright 2016-2019 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'HR Employee First Name, Last Name',
+    'version': '10.0.2.0.0',
+    'author': "Savoir-faire Linux, "
+              "Fekete Mihai (Forest and Biomass Services Romania), "
+              "Onestein, "
+              "Odoo Community Association (OCA)",
+    'maintainer': 'Savoir-faire Linux',
+    'website': 'https://github.com/OCA/hr/',
+    'license': 'AGPL-3',
+    'category': 'Human Resources',
+    'summary': 'Adds First Name to Employee',
+    'depends': [
+        'hr',
+        'partner_firstname',
+    ],
+    'data': [
+        'views/hr_view.xml',
+    ],
+    "post_init_hook": "post_init_hook",
+    'demo': [],
+    'test': [],
+    'installable': True,
+}
---
+++ packages/odoo10-addon-hr-employee-firstname/odoo10_addon_hr_employee_firstname-10.0.2.0.0.99.dev1-py2-none-any.whl/odoo/addons/hr_employee_firstname/init_hook.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2019 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import SUPERUSER_ID
+from odoo.api import Environment
+
+
+def post_init_hook(cr, pool):
+    env = Environment(cr, SUPERUSER_ID, {})
+    env['hr.employee']._install_employee_firstname()
