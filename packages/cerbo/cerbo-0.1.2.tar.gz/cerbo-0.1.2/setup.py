9c6d0d2020eb8325904e12ebb0bf7fa3d827c33e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:19 +0000

    Add package cerbo/0.1.2/cerbo-0.1.2.tar.gz

---
+++ packages/cerbo/cerbo-0.1.2.tar.gz/cerbo-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cerbo", # Replace with your own username
+    version="0.1.2",
+    author="StartOnAI",
+    author_email="startonaicom@gmail.com",
+    description="Perform Efficient ML/DL Modelling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StartOnAI/Cerbo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
