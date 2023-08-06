f5f8b3be154aa7c62210c0781bf1131cdc555c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:57 +0000

    Add package ZnProject/0.0.1/ZnProject-0.0.1.tar.gz

---
+++ packages/ZnProject/ZnProject-0.0.1.tar.gz/ZnProject-0.0.1/ZnProject/operation.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/27 下午4:45
+# @Author  : jinzening
+# @File    : add.py
+# @Software: PyCharm
+
+
+def znadd(a, b):
+    return a + b
+
+
+def znreduce(a, b):
+    return a - b
+
+
+def ride(a, b):
+    return a * b
+
+
+
+
