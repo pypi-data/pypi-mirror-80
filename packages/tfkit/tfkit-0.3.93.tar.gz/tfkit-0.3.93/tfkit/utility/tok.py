365e55e6e1cb91cfd5aa10e62ea26bb6f2eb5dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:38 +0000

    Add package tfkit/0.3.93/tfkit-0.3.93.tar.gz

---
+++ packages/tfkit/tfkit-0.3.93.tar.gz/tfkit-0.3.93/tfkit/utility/tok.py
@@ -0,0 +1,39 @@
+from collections import OrderedDict
+
+import nlp2
+from tqdm import tqdm
+
+
+def tok_begin(tokenizer):
+    return tokenizer._cls_token or tokenizer._bos_token or 'cls'
+
+
+def tok_sep(tokenizer):
+    return tokenizer._sep_token or tokenizer._eos_token or 'sep'
+
+
+def tok_mask(tokenizer):
+    return tokenizer._mask_token or 'msk'
+
+def tok_pad(tokenizer):
+    return tokenizer._pad_token or 'pad'
+
+def get_topP_unk_token(tokenizer, file_paths: list, topP: float):
+    unk_count_dict = OrderedDict()
+    for path in file_paths:
+        for input_sent in tqdm(nlp2.read_files_yield_lines(path)):
+            for tok in nlp2.split_sentence_to_array(input_sent):
+                if tokenizer._unk_token in tokenizer.tokenize(tok):
+                    unk_count_dict[tok] = unk_count_dict.get(tok, 0) + 1
+    top_range = int(len(unk_count_dict) * (topP / 100))
+    return list(unk_count_dict.keys())[:top_range]
+
+
+def get_freqK_unk_token(tokenizer, file_paths: list, freqK: int):
+    unk_count_dict = OrderedDict()
+    for path in file_paths:
+        for input_sent in tqdm(nlp2.read_files_yield_lines(path)):
+            for tok in nlp2.split_sentence_to_array(input_sent):
+                if tokenizer._unk_token in tokenizer.tokenize(tok):
+                    unk_count_dict[tok] = unk_count_dict.get(tok, 0) + 1
+    return [key for key, value in unk_count_dict.items() if value >= freqK]
