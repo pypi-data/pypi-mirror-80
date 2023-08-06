97a17a978d0c29eab1febec5294d9b42baf945f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:18 +0000

    Add package danecode/0.1.2/danecode-0.1.2.tar.gz

---
+++ packages/danecode/danecode-0.1.2.tar.gz/danecode-0.1.2/danecode/__init__.py
@@ -0,0 +1,48 @@
+import jellyfish
+
+from .country_data import departments
+
+
+def get_data(department, municipality):
+
+    department = department.lower()
+    municipality=municipality.lower()
+
+    department, department_distance = get_less_distant_item(
+        department, departments.keys())
+
+    municipalities = departments[department]['municipalities']
+    municipality, municipality_distance = get_less_distant_item(
+        municipality, municipalities.keys())
+
+
+    dane_code =  departments[department]['municipalities']\
+        [municipality]['dane_code']
+
+    place_data = {
+        "department":department,
+        "department_code": departments[department]['dane_code'],
+        "department_levenshtein_distance": department_distance,
+        "municipality":municipality,
+        "municipality_code": dane_code,
+        "minicipality_levenshtein_distance": municipality_distance,
+    }
+
+    return place_data
+
+
+def get_less_distant_item(to_compare, options):
+    last_distance = None
+
+    for option in options:
+        distance = jellyfish.levenshtein_distance(to_compare, option)
+
+        if last_distance == None:
+            last_distance = distance
+        elif distance > last_distance:
+            continue
+
+        last_distance = distance
+        best_value = option
+
+    return best_value, distance
---
+++ packages/danecode/danecode-0.1.2.tar.gz/danecode-0.1.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="danecode",
+    version="0.1.2",
+    author="Moship",
+    author_email="opensource@moship.io",
+    description="A simple util to get dane codes by department and municipality",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/moship",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "jellyfish"
+    ]
+)
