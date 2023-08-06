10a5daf5b9e39577a370efd7454c186f334d27e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:38 +0000

    Add package dgio/0.0.1/dgio-0.0.1-py3-none-any.whl

---
+++ packages/dgio/dgio-0.0.1-py3-none-any.whl/dgio/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Package __init__"""
+
+from dgio._version import VERSION_INFO
+from dgio._version import VERSION_MINOR
+from dgio._version import VERSION_MAJOR
+from dgio._version import VERSION_PATCH
+from dgio._version import __version__
+
+
+__all__ = [
+    "VERSION_MAJOR",
+    "VERSION_MINOR",
+    "VERSION_PATCH",
+    "VERSION_INFO",
+    "__version__",
+]
