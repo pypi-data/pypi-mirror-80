e033219af92d697f3323309153a8abcdbfcf1d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:29 +0000

    Add package arc3o/0.1/arc3o-0.1.tar.gz

---
+++ packages/arc3o/arc3o-0.1.tar.gz/arc3o-0.1/arc3o/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .core_functions import satsim_complete_parallel
+from .core_functions import satsim_complete_1month
+from .core_functions import new_outputpath
+from .core_functions import prep_time
+
+
+__all__ = ['satsim_complete_parallel', 'satsim_complete_1month', 'new_outputpath', 'prep_time']
+
+__version__='0.1'
+
