7a6bc0eba7d7a710c72031d84eb18b03721dda19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:12 +0000

    Add package openscm-runner/0.4.0/openscm-runner-0.4.0.tar.gz

---
+++ packages/openscm-runner/openscm-runner-0.4.0.tar.gz/openscm-runner-0.4.0/src/openscm_runner/adapters/base.py
@@ -0,0 +1,66 @@
+"""
+Base class for adapters
+"""
+from abc import ABC, abstractmethod
+
+
+class _Adapter(ABC):  # pylint: disable=too-few-public-methods
+    """
+    Base class for adapters
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialise the adapter
+
+        Parameters
+        ----------
+        *args
+            Passed to the adapter's ``_init_model`` method
+
+        **kwargs
+            Passed to the adapter's ``_init_model`` method
+        """
+        self._init_model(*args, **kwargs)
+
+    @abstractmethod
+    def _init_model(self, *args, **kwargs):
+        """
+        Initialise the model
+
+        Parameters
+        ----------
+        *args
+            Arguments used to initialise the model
+
+        **kwargs
+            Keyword arguments used to initialise the model
+        """
+
+    def run(self, scenarios, cfgs, output_variables):
+        """
+        Parameters
+        ----------
+        scenarios : :obj:`pyam.IamDataFrame`
+            Scenarios to run
+
+        cfgs : list[dict]
+            The config with which to run the model
+
+        output_variables : list[str]
+            Variables to include in the output
+
+        Returns
+        -------
+        :obj:`ScmRun`
+            Model output
+        """
+        return self._run(scenarios, cfgs, output_variables)
+
+    @abstractmethod
+    def _run(self, scenarios, cfgs, output_variables):
+        """
+        Run the model.
+
+        This method is the internal implementation of the :meth:`run` interface
+        """
---
+++ packages/openscm-runner/openscm-runner-0.4.0.tar.gz/openscm-runner-0.4.0/src/openscm_runner/run.py
@@ -0,0 +1,88 @@
+"""
+High-level run function
+"""
+import logging
+
+import scmdata
+from tqdm.autonotebook import tqdm
+
+from .adapters import FAIR, MAGICC7
+
+LOGGER = logging.getLogger(__name__)
+
+
+def run(
+    climate_models_cfgs,
+    scenarios,
+    output_variables=("Surface Temperature",),
+    full_config=False,
+):  # pylint: disable=W9006
+    """
+    Run a number of climate models over a number of scenarios
+
+    Parameters
+    ----------
+    climate_models_cfgs : dict[str: list]
+        Dictionary where each key is a model and each value is the configs
+        with which to run the model. The configs are passed to the model
+        adapter.
+
+    scenarios : :obj:`pyam.IamDataFrame`
+        Scenarios to run
+
+    output_variables : list[str]
+        Variables to include in the output
+
+    full_config : bool
+        Include the configuration used to run each model in the output's
+        metadata
+
+    Returns
+    -------
+    :obj:`scmdata.ScmDataFrame`
+        Model output
+
+    Raises
+    ------
+    NotImplementedError
+        ``full_config`` is ``True``, we haven't worked out how this should
+        behave yet.
+    """
+    if full_config:
+        raise NotImplementedError("Returning full config is not yet implemented")
+
+    res = []
+    for climate_model, cfgs in tqdm(climate_models_cfgs.items(), desc="Climate models"):
+        if climate_model == "MAGICC7":
+            runner = MAGICC7()
+        elif climate_model.upper() == "FAIR":  # allow various capitalisations
+            runner = FAIR()
+        else:
+            raise NotImplementedError(
+                "No adapter available for {}".format(climate_model)
+            )
+
+        model_res = runner.run(scenarios, cfgs, output_variables=output_variables)
+        res.append(model_res)
+
+    for i, model_res in enumerate(res):
+        if i < 1:
+            key_meta = set(model_res.meta.columns.tolist())
+
+        model_meta = set(model_res.meta.columns.tolist())
+        climate_model = model_res.get_unique_meta("climate_model")
+        if model_meta != key_meta:  # noqa
+            raise AssertionError(
+                "{} meta: {}, expected meta: {}".format(
+                    climate_model, model_meta, key_meta
+                )
+            )
+
+    if len(res) == 1:
+        LOGGER.info("Only one model run, returning its results")
+        scmdf = res[0]
+    else:
+        LOGGER.info("Appending model results")
+        scmdf = scmdata.run_append(res)
+
+    return scmdf
