9f317b9ee9252ca8cd88445692900bc12d8f0276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:41 +0000

    Add package qip-installer/2.1.1/qip-installer-2.1.1.tar.gz

---
+++ packages/qip-installer/qip-installer-2.1.1.tar.gz/qip-installer-2.1.1/test/conftest.py
@@ -0,0 +1,59 @@
+# :coding: utf-8
+
+import os
+import shutil
+import tempfile
+import uuid
+
+import pytest
+
+
+@pytest.fixture()
+def unique_name():
+    """Return a unique name."""
+    return "unique-{0}".format(uuid.uuid4())
+
+
+@pytest.fixture()
+def temporary_file(request):
+    """Return a temporary file path."""
+    file_handle, path = tempfile.mkstemp()
+    os.close(file_handle)
+
+    def cleanup():
+        """Remove temporary file."""
+        try:
+            os.remove(path)
+        except OSError:
+            pass
+
+    request.addfinalizer(cleanup)
+    return path
+
+
+@pytest.fixture()
+def temporary_directory(request):
+    """Return a temporary directory path."""
+    path = tempfile.mkdtemp()
+
+    def cleanup():
+        """Remove temporary directory."""
+        shutil.rmtree(path)
+
+    request.addfinalizer(cleanup)
+
+    return path
+
+
+@pytest.fixture()
+def logger(mocker):
+    """Mock the 'qip.logging' module and return logger."""
+    import qip.logging
+    mocker.patch.object(qip.logging, "configure")
+    mocker.patch.object(qip.logging, "root")
+
+    mock_logger = mocker.Mock()
+    mocker.patch.object(
+        qip.logging, "Logger", return_value=mock_logger
+    )
+    return mock_logger
