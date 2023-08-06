d022f6ae4505629668b4e6dcd4351dded5f7dd13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:59 +0000

    Add package nornir-ansible/2020.9.26/nornir_ansible-2020.9.26.tar.gz

---
+++ packages/nornir-ansible/nornir_ansible-2020.9.26.tar.gz/nornir_ansible-2020.9.26/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""nornir ansible inventory plugin"""
+import setuptools
+
+__author__ = "Carl Montanari"
+
+with open("README.md", "r") as f:
+    README = f.read()
+
+setuptools.setup(
+    name="nornir_ansible",
+    version="2020.09.26",
+    author=__author__,
+    author_email="carl.r.montanari@gmail.com",
+    description="",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/carlmontanari/nornir_ansible",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "mypy_extensions>=0.4.1,<1.0.0",
+        "ruamel.yaml>=0.16.10,<1.0.0",
+        "nornir>=3.0.0a4,<4.0.0",
+    ],
+    extras_require={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+    entry_points="""
+    [nornir.plugins.inventory]
+    AnsibleInventory=nornir_ansible.plugins.inventory:AnsibleInventory
+    """,
+)
