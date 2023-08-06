213ad2eea07e0d3d1675ba2daff65099c146e1ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:04 +0000

    Add package larq/0.10.1/larq-0.10.1.tar.gz

---
+++ packages/larq/larq-0.10.1.tar.gz/larq-0.10.1/larq/__init__.py
@@ -0,0 +1,27 @@
+from larq import (
+    activations,
+    callbacks,
+    constraints,
+    context,
+    layers,
+    math,
+    metrics,
+    models,
+    optimizers,
+    quantizers,
+    utils,
+)
+
+__all__ = [
+    "layers",
+    "activations",
+    "callbacks",
+    "constraints",
+    "context",
+    "math",
+    "metrics",
+    "models",
+    "quantizers",
+    "optimizers",
+    "utils",
+]
