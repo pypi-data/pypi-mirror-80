94876862ef3f99bccc1598084f37d0bbbf0508f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:21 +0000

    Add package xknx/0.14.4/xknx-0.14.4-py3-none-any.whl

---
+++ packages/xknx/xknx-0.14.4-py3-none-any.whl/xknx/io/connectionstate.py
@@ -0,0 +1,24 @@
+"""Abstraction to send ConnectonStateRequest and wait for ConnectionStateResponse."""
+from xknx.knxip import HPAI, ConnectionStateResponse, KNXIPFrame, KNXIPServiceType
+
+from .request_response import RequestResponse
+
+
+class ConnectionState(RequestResponse):
+    """Class to send ConnectonStateRequest and wait for ConnectionStateResponse."""
+
+    def __init__(self, xknx, udp_client, communication_channel_id):
+        """Initialize ConnectionState class."""
+        self.udp_client = udp_client
+        super().__init__(xknx, self.udp_client, ConnectionStateResponse)
+        self.communication_channel_id = communication_channel_id
+
+    def create_knxipframe(self):
+        """Create KNX/IP Frame object to be sent to device."""
+        (local_addr, local_port) = self.udpclient.getsockname()
+        knxipframe = KNXIPFrame(self.xknx)
+        knxipframe.init(KNXIPServiceType.CONNECTIONSTATE_REQUEST)
+        knxipframe.body.communication_channel_id = self.communication_channel_id
+        knxipframe.body.control_endpoint = HPAI(ip_addr=local_addr, port=local_port)
+
+        return knxipframe
