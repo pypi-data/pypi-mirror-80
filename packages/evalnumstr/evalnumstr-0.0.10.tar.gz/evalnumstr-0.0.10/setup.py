beb76e8bf796b6acbc704005c51f4090d7c3a633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:43 +0000

    Add package evalnumstr/0.0.10/evalnumstr-0.0.10.tar.gz

---
+++ packages/evalnumstr/evalnumstr-0.0.10.tar.gz/evalnumstr-0.0.10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='evalnumstr',
+      version='0.0.1',
+      description='Evaluate number string',
+      packages=['evalnumstr'],
+      author_email='foton-pc@inbox.ru',
+      zip_safe=False,
+      home_page='https://ppbe.ru',
+      author='FotonPC',
+      platform=['windows','linux'])
