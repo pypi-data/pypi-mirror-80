e69f0b80c5d7258ab3a868ebdd7e4341d4fad8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:37 +0000

    Add package mitosheet/0.1.15/mitosheet-0.1.15-py2.py3-none-any.whl

---
+++ packages/mitosheet/mitosheet-0.1.15-py2.py3-none-any.whl/mitosheet/tests/test_transpile.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Mito.
+# Distributed under the terms of the Modified BSD License.
+
+from ..transpile import transpile
+from mitosheet.tests.test_utils import create_mito_wrapper
+
+
+def test_transpile_single_column():
+    mito = create_mito_wrapper(['abc'])
+    mito.set_formula('=A', 'B', add_column=True)
+    code_container = transpile(mito.mito_widget.widget_state_container)
+    assert code_container['code'] == 'df[\'B\'] = df[\'A\']'
+
+
+def test_transpile_multiple_columns_no_relationship():
+    mito = create_mito_wrapper(['abc'])
+    mito.add_column('B')
+    mito.add_column('C')
+    code_container = transpile(mito.mito_widget.widget_state_container)
+    assert 'df[\'B\'] = 0' in code_container['code']
+    assert 'df[\'C\'] = 0' in code_container['code']
+
+
+def test_transpile_multiple_columns_linear():
+    mito = create_mito_wrapper(['abc'])
+    mito.set_formula('=A', 'B', add_column=True)
+    mito.set_formula('=B', 'C', add_column=True)
+    code_container = transpile(mito.mito_widget.widget_state_container)
+    assert code_container['code'] == 'df[\'B\'] = df[\'A\']\n\ndf[\'C\'] = df[\'B\']'
