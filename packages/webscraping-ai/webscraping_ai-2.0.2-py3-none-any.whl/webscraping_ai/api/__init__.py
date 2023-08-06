1d3e284b75b158ab8fef717bec6b675d9f0b474b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:46 +0000

    Add package webscraping-ai/2.0.2/webscraping_ai-2.0.2-py3-none-any.whl

---
+++ packages/webscraping-ai/webscraping_ai-2.0.2-py3-none-any.whl/webscraping_ai/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from webscraping_ai.api.html_api import HTMLApi
+from webscraping_ai.api.selected_html_api import SelectedHTMLApi
