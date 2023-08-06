257b51940a5293bbeb11181813695882a9df03f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:31 +0000

    Add package mkvimdb/0.2.2/mkvimdb-0.2.2.tar.gz

---
+++ packages/mkvimdb/mkvimdb-0.2.2.tar.gz/mkvimdb-0.2.2/tests/test_mkvimdb.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `mkvimdb` package."""
+
+
+import unittest
+
+from mkvimdb import mkvimdb
+
+
+class TestMkvimdb(unittest.TestCase):
+    """Tests for `mkvimdb` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
