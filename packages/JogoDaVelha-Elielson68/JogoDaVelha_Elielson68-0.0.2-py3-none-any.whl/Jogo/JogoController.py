a58b220dd4e8462e9f3e8c7a42a5a6c06674d4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:24 +0000

    Add package JogoDaVelha-Elielson68/0.0.2/JogoDaVelha_Elielson68-0.0.2-py3-none-any.whl

---
+++ packages/JogoDaVelha-Elielson68/JogoDaVelha_Elielson68-0.0.2-py3-none-any.whl/Jogo/JogoController.py
@@ -0,0 +1,25 @@
+from Tabuleiro.tabuleiro import Tabuleiro
+from Tabuleiro.jogador import Jogador
+
+def MenuOpcoes() -> None:
+    '''
+    Imprimi na tela o menu de opções que o jogo possui
+    :return: None
+    '''
+    print("\t\tJOGO DA VELHA\n")
+    print("1 - Jogar contra outro Player")
+    print("2 - Jogar contra CPU")
+    print("3 - Personalizar jogo")
+    print("4 - Jogar CPU contra CPU")
+    print("5 - Sair")
+
+def JogarNovamente(tabuleiro: Tabuleiro, jogador1: Jogador, jogador2: Jogador)->int:
+    '''
+    Retorna '1' caso o jogador desejar jogar novamente, retorna '0' caso não.
+    :return: Number int
+    '''
+    usuario = int(input("Deseja jogar novamente?\n1 - Sim\n2 - Não\nVocê: "))
+    jogador1.ResetarMovimentos()
+    jogador2.ResetarMovimentos()
+    tabuleiro.ResetarCasas()
+    return usuario
