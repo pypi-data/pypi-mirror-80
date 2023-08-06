180d0218a061cf2af1f04a6099fb612836dd41f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:15 +0000

    Add package ontodev-cogs/0.1.1.1/ontodev-cogs-0.1.1.1.tar.gz

---
+++ packages/ontodev-cogs/ontodev-cogs-0.1.1.1.tar.gz/ontodev-cogs-0.1.1.1/cogs/pull.py
@@ -0,0 +1,36 @@
+import shutil
+import sys
+
+from cogs.helpers import *
+
+
+def msg():
+    return "Copy fetched sheets to their local paths"
+
+
+def pull(args):
+    """Copy cached sheets to their local paths."""
+    set_logging(args.verbose)
+    validate_cogs_project()
+
+    cached_sheets = get_cached_sheets()
+    tracked_sheets = get_tracked_sheets()
+    remove_sheets = [s for s in cached_sheets if s not in tracked_sheets.keys()]
+    for sheet_title, details in tracked_sheets.items():
+        cached_sheet = f".cogs/tracked/{sheet_title}.tsv"
+        local_sheet = details["Path"]
+        if os.path.exists(cached_sheet):
+            logging.info(f"Writing '{sheet_title}' to {local_sheet}")
+            shutil.copyfile(cached_sheet, local_sheet)
+    for sheet_title in remove_sheets:
+        logging.info(f"Removing '{sheet_title}' from cached sheets")
+        os.remove(f".cogs/tracked/{sheet_title}.tsv")
+
+
+def run(args):
+    """Wrapper for pull function."""
+    try:
+        pull(args)
+    except CogsError as e:
+        logging.critical(str(e))
+        sys.exit(1)
---
+++ packages/ontodev-cogs/ontodev-cogs-0.1.1.1.tar.gz/ontodev-cogs-0.1.1.1/cogs/share.py
@@ -0,0 +1,53 @@
+import sys
+
+from cogs.helpers import *
+
+
+def msg():
+    return "Share the spreadsheet with a user"
+
+
+def share_spreadsheet(title, spreadsheet, user, role):
+    """Share a sheet with a user (email) as role (reader, writer, owner)"""
+    logging.info(f"Sharing spreadsheet '{title}' with {user} as '{role}'")
+    try:
+        spreadsheet.share(user, perm_type="user", role=role)
+    except gspread.exceptions.APIError as e:
+        logging.error(f"Unable to share spreadsheet '{title}'\n" + e.response.text)
+
+
+def share(args):
+    """Share the project spreadsheet with email addresses as reader, writer, or owner."""
+    set_logging(args.verbose)
+    validate_cogs_project()
+
+    config = get_config()
+    gc = get_client_from_config(config)
+
+    title = config["Title"]
+    spreadsheet = gc.open(title)
+
+    if args.owner:
+        resp = input(
+            f"WARNING: Transferring ownership to {args.owner} will prevent COGS from performing "
+            f"administrative actions on spreadsheet '{title}'. Do you wish to proceed? [y/n]\n"
+        )
+        if resp.lower().strip() == "y":
+            share_spreadsheet(title, spreadsheet, args.owner, "owner")
+        else:
+            print(f"Ownership of spreadsheet '{title}' will not be transferred.")
+
+    if args.reader:
+        share_spreadsheet(title, spreadsheet, args.reader, "reader")
+
+    if args.writer:
+        share_spreadsheet(title, spreadsheet, args.writer, "writer")
+
+
+def run(args):
+    """Wrapper for share function."""
+    try:
+        share(args)
+    except CogsError as e:
+        logging.critical(str(e))
+        sys.exit(1)
