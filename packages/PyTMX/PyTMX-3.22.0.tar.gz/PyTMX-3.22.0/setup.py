db1a5518059f203a58f23b20b0501c0b93eb1d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:44 +0000

    Add package PyTMX/3.22.0/PyTMX-3.22.0.tar.gz

---
+++ packages/PyTMX/PyTMX-3.22.0.tar.gz/PyTMX-3.22.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# python setup.py sdist upload -r pypi
+# twine upload dist/*
+from setuptools import setup
+
+setup(name="PyTMX",
+      version='3.22.0',
+      description='loads tiled tmx maps.  for python 2.7 and 3.3+',
+      author='bitcraft',
+      author_email='leif.theden@gmail.com',
+      packages=['pytmx'],
+      install_requires=['six'],
+      license="LGPLv3",
+      long_description='https://github.com/bitcraft/PyTMX',
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Development Status :: 5 - Production/Stable",
+          "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Topic :: Games/Entertainment",
+          "Topic :: Multimedia :: Graphics",
+          "Topic :: Software Development :: Libraries :: pygame",
+      ]
+      )
