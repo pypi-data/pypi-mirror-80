905c77d4c55fbaf400b6dfab503678d9106ef37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:24 +0000

    Add package fastx-barber/0.1.0/fastx_barber-0.1.0-py3-none-any.whl

---
+++ packages/fastx-barber/fastx_barber-0.1.0-py3-none-any.whl/fastx_barber/const.py
@@ -0,0 +1,59 @@
+"""
+@author: Gabriele Girelli
+@contact: gigi.ga90@gmail.com
+"""
+
+from enum import Enum
+from typing import DefaultDict, Tuple
+
+__version__ = "0.1.0b"
+
+
+class FastxFormats(Enum):
+    """Fastx formats
+
+    Extends:
+        Enum
+
+    Variables:
+        FASTA {str}
+        FASTQ {str}
+        NONE {str} -- Not fastx
+    """
+
+    FASTA = "fasta"
+    FASTQ = "fastq"
+    NONE = "None"
+
+    @classmethod
+    def has_value(self, value):
+        return value in self._value2member_map_
+
+
+class FastxExtensions(Enum):
+    """Fastx extensions
+
+    Extends:
+        Enum
+
+    Variables:
+        FASTA {tuple}
+        FASTQ {tuple}
+    """
+
+    FASTA = (".fa", ".fasta")
+    FASTQ = (".fq", ".fastq")
+
+    @classmethod
+    def has_value(self, value):
+        return any(value in v for v in self._value2member_map_)
+
+
+QFLAG_START = "q"
+DEFAULT_PHRED_OFFSET = 33
+
+PATTERN_EXAMPLE = "^(?<UMI>.{8})(?<BC>.{8})(?<CS>GATC){s<2}"
+
+"""Flag data, contains matched str, start, and end position"""
+FlagData = Tuple[str, int, int]
+FlagStatsType = DefaultDict[str, DefaultDict[str, int]]
---
+++ packages/fastx-barber/fastx_barber-0.1.0-py3-none-any.whl/fastx_barber/io.py
@@ -0,0 +1,93 @@
+"""
+@author: Gabriele Girelli
+@contact: gigi.ga90@gmail.com
+"""
+
+import glob
+import os
+from rich.progress import track  # type: ignore
+import tempfile
+from typing import Optional, Tuple
+
+
+def is_gzipped(path: str) -> Tuple[str, str, bool]:
+    """
+    Returns:
+        Tuple[str, str, bool] -- basename, extension, gzipped status
+    """
+    base, ext = os.path.splitext(path)
+    return (base, ext, ".gz" == ext)
+
+
+class ChunkMerger(object):
+    _do_remove: bool = True
+    _tempdir: Optional[tempfile.TemporaryDirectory]
+    _split_by: Optional[str] = None
+
+    def __init__(
+        self,
+        tempdir: Optional[tempfile.TemporaryDirectory] = None,
+        split_by: Optional[str] = None,
+    ):
+        super(ChunkMerger, self).__init__()
+        self._tempdir = tempdir
+        self._split_by = split_by
+
+    @property
+    def do_remove(self) -> bool:
+        return self._do_remove
+
+    @do_remove.setter
+    def do_remove(self, do_remove: bool) -> None:
+        self._do_remove = do_remove
+
+    def __merge_simple(
+        self, path: str, last_chunk_id: int, desc: Optional[str] = None
+    ) -> None:
+        with open(path, "wb") as OH:
+            for cid in track(
+                range(1, last_chunk_id + 1), description=desc, transient=False
+            ):
+                chunk_path = f".tmp.chunk{cid}.{path}"
+                if self._tempdir is not None:
+                    chunk_path = os.path.join(self._tempdir.name, chunk_path)
+                if not os.path.isfile(chunk_path):
+                    continue
+                with open(chunk_path, "rb") as CH:
+                    OH.write(CH.read())
+                if self._do_remove:
+                    os.remove(chunk_path)
+
+    def __merge_split(
+        self, path: str, last_chunk_id: int, desc: Optional[str] = None
+    ) -> None:
+        output_dir = os.path.dirname(path)
+        output_base = os.path.basename(path)
+        for cid in track(
+            range(1, last_chunk_id + 1), description=desc, transient=False
+        ):
+            chunk_path = f"{self._split_by}_split.*.tmp.chunk{cid}.{path}"
+            if self._tempdir is not None:
+                chunk_path = os.path.join(self._tempdir.name, chunk_path)
+            flist = glob.glob(chunk_path)
+            for fname in flist:
+                split_value = (
+                    os.path.basename(fname).split("_split.")[1].split(".tmp.chunk")[0]
+                )
+                with open(
+                    os.path.join(
+                        output_dir,
+                        f"{self._split_by}_split.{split_value}.{output_base}",
+                    ),
+                    "ab",
+                ) as OH:
+                    with open(fname, "rb") as CH:
+                        OH.write(CH.read())
+                    if self._do_remove:
+                        os.remove(fname)
+
+    def do(self, path: str, last_chunk_id: int, desc: Optional[str] = None) -> None:
+        if self._split_by is None:
+            self.__merge_simple(path, last_chunk_id, desc)
+        else:
+            self.__merge_split(path, last_chunk_id, desc)
