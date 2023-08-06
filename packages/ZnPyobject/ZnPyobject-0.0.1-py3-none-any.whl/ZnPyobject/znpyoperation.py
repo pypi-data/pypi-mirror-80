45e8eb5b7ac2069151ec41effc1358cd7165edca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:14 +0000

    Add package ZnPyobject/0.0.1/ZnPyobject-0.0.1-py3-none-any.whl

---
+++ packages/ZnPyobject/ZnPyobject-0.0.1-py3-none-any.whl/ZnPyobject/znpyoperation.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/27 下午4:45
+# @Author  : jinzening/jingpengyu
+# @File    : add.py
+# @Software: PyCharm
+
+
+class Operation:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def znpyadd(self):
+        return self.a + self.b
+
+    def znpyreduce(self):
+        return self.a + self.b
+
+    def znpyride(self):
+        return self.a * self.b
+
+
+
+
