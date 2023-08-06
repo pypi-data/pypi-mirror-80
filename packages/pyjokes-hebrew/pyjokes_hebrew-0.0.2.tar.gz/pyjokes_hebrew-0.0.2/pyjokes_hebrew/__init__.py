8abcbc9ddcfcc3eb1154652c461d832bba816ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:36 +0000

    Add package pyjokes-hebrew/0.0.2/pyjokes_hebrew-0.0.2.tar.gz

---
+++ packages/pyjokes-hebrew/pyjokes_hebrew-0.0.2.tar.gz/pyjokes_hebrew-0.0.2/pyjokes_hebrew/__init__.py
@@ -0,0 +1,22 @@
+import requests
+import random
+
+
+url = "https://api.npoint.io/72747f3291dd6ac2ec62"
+
+data = requests.get(url).json()
+
+
+
+
+def get_random_joke():
+    random_index = random.randint(0, 10)
+    joke_list = list(data[random_index].values())
+    #joke_type = list(data[random_index].keys())
+    joke_index = random.randint(0, len(joke_list[0])-1)
+    print(joke_list[0][joke_index])
+
+
+
+
+
