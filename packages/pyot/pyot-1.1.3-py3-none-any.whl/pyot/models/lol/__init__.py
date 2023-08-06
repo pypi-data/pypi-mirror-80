96d64894ce24a42315e662661040a1b5eb08b1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:16 +0000

    Add package pyot/1.1.3/pyot-1.1.3-py3-none-any.whl

---
+++ packages/pyot/pyot-1.1.3-py3-none-any.whl/pyot/models/lol/__init__.py
@@ -0,0 +1,16 @@
+from .championmastery import ChampionMasteries, ChampionMastery
+from .league import League, ChallengerLeague, GrandmasterLeague, MasterLeague, SummonerLeague, DivisionLeague
+from .clash import ClashPlayers, ClashTeam, ClashTournaments, ClashTournament
+from .match import Match, MatchTimeline, Timeline, MatchHistory
+from .profileicon import ProfileIcon, ProfileIcons
+from .spectator import FeaturedGames, CurrentGame
+from .championrotation import ChampionRotation
+from .thirdpartycode import ThirdPartyCode
+from .merakichampion import MerakiChampion
+from .merakiitem import MerakiItem
+from .spell import Spell, Spells
+from .champion import Champion
+from .summoner import Summoner
+from .item import Item, Items
+from .rune import Rune, Runes
+from .status import Status
