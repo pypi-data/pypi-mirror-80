a87bfff3d10079f0d309fed94c3ce5b81cdab67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:58 +0000

    Add package rastasteady/0.2.20/rastasteady-0.2.20.tar.gz

---
+++ packages/rastasteady/rastasteady-0.2.20.tar.gz/rastasteady-0.2.20/rastasteady/cli.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import click
+import os
+import pathlib
+from rastasteady.rastasteady import RastaSteady
+
+@click.command(no_args_is_help = True, context_settings = dict(max_content_width = 120))
+
+# main argument: the video file
+@click.argument('video', type = str, required = True)
+
+# optional parameters
+@click.option('--debug', type = bool, is_flag = True, default = False, help = 'Muestra informacion durante los procesos del video.', show_default = True)
+@click.option('--dual', type = bool, is_flag = True, default = False, help = 'Crea fichero dual con video original y procesado. Requiere efecto RastaView.', show_default = True)
+@click.option('--no-rastaview', type = bool, is_flag = True, default = False, help = 'No crea efecto RastaView del video. Require estabilizar video.', show_default = True)
+@click.option('--no-reusar', type = bool, is_flag = True, default = False, help = 'Borra ficheros temporales si existen.', show_default = True)
+@click.option('--perfil', type = click.Choice(['low', 'medium', 'high', 'ultra'], case_sensitive = False), default = 'high', help='Perfil de estabilizado: bajo (peor estabilización), medio, alto (mejor estabilización), ultra.', show_default = True)
+
+# version
+# TODO: this version string should be centralized in other place
+@click.version_option('0.2.20')
+
+# main code
+def cli(video, debug, dual, no_rastaview, no_reusar, perfil):
+    """RastaSteady es un software de estabilizacion de video para el sistema DJI FPV digital."""
+    # calculate input directory and temporary location
+    inputpathlib = pathlib.Path(video)
+    tmppathlib = pathlib.Path(str(os.getcwd()) + '/rastasteady-' + inputpathlib.stem + '/.placeholder')
+
+    print('RastaSteady 0.2.20. Estabiliza y amplia el FOV del video de tu sistema DJI FPV.')
+    print('\nProcesanto fichero %s:' % inputpathlib.name)
+
+    # if asked, delete any existing file in the temporary location
+    if no_reusar:
+        print(' * Eliminando ficheros existentes.\n')
+        for file in ['transforms.trf', 'xmap.pgm', 'ymap.pgm', 'cropped.mp4', 'dual.mp4', 'rastaview.mp4', 'stabilized.mp4']:
+            if tmppathlib.with_name(file).is_file():
+                tmppathlib.with_name(file).unlink()
+
+    # create the rastasteady object
+    myVideo = RastaSteady(inputpathlib, tmppathlib, debug=debug)
+
+    # stabilize the file and apply rastaview and/or dual if they apply
+    print(' * Generando stabilized.mp4 usando el perfil de estabilizado \'%s\'.' % perfil)
+    myVideo.stabilize(perfil)
+    if not no_rastaview:
+        print('\n * Generando rastaview.mp4.')
+        myVideo.rastaview()
+    if dual:
+        print('\n * Generando dual.mp4.')
+        myVideo.dual()
+
+    print('\nProceso finalizado!\nLos ficheros generados se encuentran en el directorio %s.\n' % str(tmppathlib.parent.absolute()))
---
+++ packages/rastasteady/rastasteady-0.2.20.tar.gz/rastasteady-0.2.20/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rastasteady',
+    version='0.2.20',
+    description='RastaSteady es un software de estabilizacion de video para el sistema DJI FPV digital.',
+    long_description='',
+    install_requires=[
+        'click',
+        'flask'
+    ],
+    entry_points='''
+        [console_scripts]
+        rastasteady=rastasteady.cli:cli
+        rastasteady-cli=rastasteady.cli:cli
+        rastasteady-web=rastasteady.web.web:web
+    ''',
+    packages=find_packages(),
+    zip_safe=False,
+)
