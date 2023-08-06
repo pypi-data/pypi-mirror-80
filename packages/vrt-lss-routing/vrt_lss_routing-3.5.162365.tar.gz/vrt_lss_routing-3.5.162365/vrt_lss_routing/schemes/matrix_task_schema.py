b8419d0e4b307803771620bc5d50997957e91f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:11 +0000

    Add package vrt-lss-routing/3.5.162365/vrt_lss_routing-3.5.162365.tar.gz

---
+++ packages/vrt-lss-routing/vrt_lss_routing-3.5.162365.tar.gz/vrt_lss_routing-3.5.162365/vrt_lss_routing/schemes/matrix_task_schema.py
@@ -0,0 +1,73 @@
+MATRIX_TASK_SCHEMA = '''{
+    "additionalProperties": false,
+    "properties": {
+        "departure_time": {
+            "format": "date-time",
+            "type": "string"
+        },
+        "geo_provider": {
+            "maxLength": 256,
+            "minLength": 1,
+            "type": "string"
+        },
+        "transport_type": {
+            "default": "CAR",
+            "enum": [
+                "CAR",
+                "TRUCK",
+                "CAR_GT",
+                "TUK_TUK",
+                "BICYCLE",
+                "PEDESTRIAN",
+                "PUBLIC_TRANSPORT"
+            ],
+            "nullable": false,
+            "type": "string"
+        },
+        "waypoints": {
+            "items": {
+                "additionalProperties": false,
+                "nullable": true,
+                "properties": {
+                    "duration": {
+                        "default": 0,
+                        "format": "int32",
+                        "maximum": 43800,
+                        "minimum": 0,
+                        "type": "integer"
+                    },
+                    "latitude": {
+                        "format": "double",
+                        "maximum": 90,
+                        "minimum": -90,
+                        "type": "number"
+                    },
+                    "longitude": {
+                        "format": "double",
+                        "maximum": 180,
+                        "minimum": -180,
+                        "type": "number"
+                    },
+                    "name": {
+                        "maxLength": 1024,
+                        "minLength": 0,
+                        "type": "string"
+                    }
+                },
+                "required": [
+                    "latitude",
+                    "longitude"
+                ],
+                "type": "object"
+            },
+            "maxItems": 9000,
+            "minItems": 2,
+            "type": "array",
+            "uniqueItems": false
+        }
+    },
+    "required": [
+        "waypoints"
+    ],
+    "type": "object"
+}'''
