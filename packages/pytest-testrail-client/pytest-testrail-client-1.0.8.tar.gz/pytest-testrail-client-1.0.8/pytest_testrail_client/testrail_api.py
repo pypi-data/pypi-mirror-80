cfff413c02224f8e64821e0421f0356afde900ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:01 +0000

    Add package pytest-testrail-client/1.0.8/pytest-testrail-client-1.0.8.tar.gz

---
+++ packages/pytest-testrail-client/pytest-testrail-client-1.0.8.tar.gz/pytest-testrail-client-1.0.8/pytest_testrail_client/testrail_api.py
@@ -0,0 +1,106 @@
+"""
+Description
+"""
+from __future__ import annotations
+from . import _category
+from ._exception import TestRailConfigurationError
+from ._session import Session
+
+
+def validate_setup(tr: TestRailAPI, project_id):
+    if not project_id:
+        TestRailConfigurationError(
+            f'Project ID must be set. Invalid value {project_id}')
+    project_id = int(project_id)
+    if tr.projects.get_project(project_id).suite_mode != 3:
+        TestRailConfigurationError(
+            f'Project suite_mode {tr.projects.get_project().suite_mode} not compatible with this plugin. '
+            f'Use multiple test suites to manage cases')
+
+
+class TestRailAPI(Session):
+
+    @property
+    def cases(self):
+        """http://docs.gurock.com/testrail-api2/reference-cases"""
+        return _category.Cases(self)
+
+    @property
+    def case_fields(self):
+        """http://docs.gurock.com/testrail-api2/reference-cases-fields"""
+        return _category.CaseFields(self)
+
+    @property
+    def case_types(self):
+        """http://docs.gurock.com/testrail-api2/reference-cases-types"""
+        return _category.CaseTypes(self)
+
+    @property
+    def configurations(self):
+        """http://docs.gurock.com/testrail-api2/reference-configs"""
+        return _category.Configurations(self)
+
+    @property
+    def milestones(self):
+        """http://docs.gurock.com/testrail-api2/reference-milestones"""
+        return _category.Milestones(self)
+
+    @property
+    def plans(self):
+        """http://docs.gurock.com/testrail-api2/reference-plans"""
+        return _category.Plans(self)
+
+    @property
+    def priorities(self):
+        """http://docs.gurock.com/testrail-api2/reference-priorities"""
+        return _category.Priorities(self)
+
+    @property
+    def projects(self):
+        """http://docs.gurock.com/testrail-api2/reference-projects"""
+        return _category.Projects(self)
+
+    @property
+    def results(self):
+        """http://docs.gurock.com/testrail-api2/reference-results"""
+        return _category.Results(self)
+
+    @property
+    def result_fields(self):
+        """http://docs.gurock.com/testrail-api2/reference-results-fields"""
+        return _category.ResultFields(self)
+
+    @property
+    def runs(self):
+        """http://docs.gurock.com/testrail-api2/reference-runs"""
+        return _category.Runs(self)
+
+    @property
+    def sections(self):
+        """http://docs.gurock.com/testrail-api2/reference-runs"""
+        return _category.Sections(self)
+
+    @property
+    def statuses(self):
+        """http://docs.gurock.com/testrail-api2/reference-sections"""
+        return _category.Statuses(self)
+
+    @property
+    def suites(self):
+        """http://docs.gurock.com/testrail-api2/reference-suites"""
+        return _category.Suites(self)
+
+    @property
+    def templates(self):
+        """http://docs.gurock.com/testrail-api2/reference-templates"""
+        return _category.Templates(self)
+
+    @property
+    def tests(self):
+        """http://docs.gurock.com/testrail-api2/reference-tests"""
+        return _category.Tests(self)
+
+    @property
+    def users(self):
+        """http://docs.gurock.com/testrail-api2/reference-users"""
+        return _category.Users(self)
---
+++ packages/pytest-testrail-client/pytest-testrail-client-1.0.8.tar.gz/pytest-testrail-client-1.0.8/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="pytest-testrail-client",
+    version="1.0.8",
+    use_scm_version=False,
+    description="pytest plugin for Testrail",
+    long_description=open("README.rst").read(),
+    author="Sergiu Popescu",
+    author_email="popescunsergiu@gmail.com",
+    url="https://github.com/popescunsergiu/pytest-testrail-client",
+    download_url="https://github.com/popescunsergiu/pytest-testrail-client/archive/v1.0.8.tar.gz",
+    packages=setuptools.find_packages(exclude=('tests', 'dev_tools')),
+    install_requires=[
+        "cucumber-tag-expressions>=2.0.0",
+        "gherkin-official>=4.1.0",
+        "pytest>=4.2",
+        "pytest-bdd>=3.3.0",
+        "requests",
+    ],
+    entry_points={
+        "pytest11": [
+            "pytest-testrail-client = pytest_testrail_client.pytest_testrail_client",
+        ]
+    },
+    setup_requires=["setuptools_scm"],
+    license="Mozilla Public License 2.0 (MPL 2.0)",
+    keywords="py.test pytest qa ",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
