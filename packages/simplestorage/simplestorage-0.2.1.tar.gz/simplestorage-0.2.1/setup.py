0c91c637f76d78a63673b1a2c8166c3a08fcc2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:26 +0000

    Add package simplestorage/0.2.1/simplestorage-0.2.1.tar.gz

---
+++ packages/simplestorage/simplestorage-0.2.1.tar.gz/simplestorage-0.2.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='simplestorage',
+    version='0.2.1',
+    description='Storage client for local and gcloud',
+    url='https://github.com/tbchk/simple-storage',
+    download_url='https://github.com/tbchk/simple-storage/archive/v0.2.1.tar.gz',
+    author='Juan F. Hernandez',
+    author_email='jhernandez426@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=("tests",)),
+    keywords=['google', 'cloud', 'storage', 'local'],
+    install_requires=[
+        'google-cloud-storage',
+        'pydantic'
+    ],
+    zip_safe=False)
