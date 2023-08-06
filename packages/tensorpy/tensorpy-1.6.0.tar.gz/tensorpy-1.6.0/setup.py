edb86f5d39354c54fd422a99fcdb83834fbb5773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:13 +0000

    Add package tensorpy/1.6.0/tensorpy-1.6.0.tar.gz

---
+++ packages/tensorpy/tensorpy-1.6.0.tar.gz/tensorpy-1.6.0/setup.py
@@ -0,0 +1,81 @@
+"""
+The setup package to install TensorPy dependencies.
+*> This does NOT include TensorFlow installation.
+*> To install TensorFlow, use "./install.sh"
+"""
+
+from setuptools import setup, find_packages  # noqa
+import os
+import sys
+
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+long_description = None
+try:
+    with open(os.path.join(this_directory, 'README.md'), 'rb') as f:
+        long_description = f.read().decode('utf-8')
+except IOError:
+    long_description = 'Easy Image Classification with TensorFlow!'
+
+if sys.argv[-1] == 'publish':
+    reply = None
+    input_method = input
+    if not sys.version_info[0] >= 3:
+        input_method = raw_input  # noqa
+    reply = str(input_method(
+        '>>> Confirm release PUBLISH to PyPI? (yes/no): ')).lower().strip()
+    if reply == 'yes':
+        print("\n*** Checking code health with flake8:\n")
+        flake8_status = os.system("flake8 --exclude=temp")
+        if flake8_status != 0:
+            print("\nWARNING! Fix flake8 issues before publishing to PyPI!\n")
+            sys.exit()
+        else:
+            print("*** No flake8 issues detected. Continuing...")
+        print("\n*** Rebuilding distribution packages: ***\n")
+        os.system('rm -f dist/*.egg; rm -f dist/*.tar.gz; rm -f dist/*.whl')
+        os.system('python setup.py sdist bdist_wheel')  # Create new tar/wheel
+        print("\n*** Installing twine: *** (Required for PyPI uploads)\n")
+        os.system("python -m pip install --upgrade 'twine>=1.15.0'")
+        print("\n*** Installing tqdm: *** (Required for PyPI uploads)\n")
+        os.system("python -m pip install --upgrade 'tqdm>=4.49.0'")
+        print("\n*** Publishing The Release to PyPI: ***\n")
+        os.system('python -m twine upload dist/*')  # Requires ~/.pypirc Keys
+        print("\n*** The Release was PUBLISHED SUCCESSFULLY to PyPI! :) ***\n")
+    else:
+        print("\n>>> The Release was NOT PUBLISHED to PyPI! <<<\n")
+    sys.exit()
+
+setup(
+    name='tensorpy',
+    version='1.6.0',
+    description='Easy Image Classification with TensorFlow!',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/TensorPy/TensorPy',
+    platforms=["Linux", "Unix", "Mac OS-X"],
+    author='Michael Mintz',
+    author_email='mdmintz@gmail.com',
+    maintainer='Michael Mintz',
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    python_requires='>=3.5, <3.8',
+    install_requires=[
+        'tensorflow==1.15.4',
+        'pip>=20.2.3',
+        'six>=1.15.0',
+        'requests>=2.24.0',
+        'Pillow==7.2.0',
+        'beautifulsoup4==4.9.1',
+    ],
+    packages=['tensorpy'],
+    entry_points={
+        'console_scripts': [
+            'classify = tensorpy.classify:main',
+        ],
+    },
+)
---
+++ packages/tensorpy/tensorpy-1.6.0.tar.gz/tensorpy-1.6.0/tensorpy/download_imagenet.py
@@ -0,0 +1,47 @@
+""" Downloading the ImageNet Inception database. """
+# This file is based on:
+# https://github.com/tensorflow/models/blob/master/tutorials/image/imagenet/classify_image.py  # noqa
+
+import os
+import sys
+import tarfile
+from six.moves import urllib
+from tensorpy import settings
+import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string(
+    'model_dir', settings.IMAGENET_FOLDER,
+    """Path to classify_image_graph_def.pb, """
+    """imagenet_synset_to_human_label_map.txt, and """
+    """imagenet_2012_challenge_label_map_proto.pbtxt.""")
+DATA_URL = ('http://download.tensorflow.org/models/'
+            'image/imagenet/inception-2015-12-05.tgz')
+
+
+def maybe_download_and_extract():
+    """Download and extract model tar file."""
+    dest_directory = FLAGS.model_dir
+    if not os.path.exists(dest_directory):
+        os.makedirs(dest_directory)
+    filename = DATA_URL.split('/')[-1]
+    filepath = os.path.join(dest_directory, filename)
+    print("ImageNet Inception DB filepath: %s" % filepath)
+    if not os.path.exists(filepath):
+        def _progress(count, block_size, total_size):
+            sys.stdout.write('\r>> Downloading %s %.1f%%' % (
+                filename,
+                float(count * block_size) / float(total_size) * 100.0))
+            sys.stdout.flush()
+        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+        print()
+        statinfo = os.stat(filepath)
+        print('Succesfully downloaded', filename, statinfo.st_size, 'bytes.')
+    tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+
+
+def main(_):
+    maybe_download_and_extract()
+
+
+tf.compat.v1.app.run()
