6fbc2ba8d37c2db342657aed5c22ef30051fae4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:07 +0000

    Add package fireant/7.4.1/fireant-7.4.1.tar.gz

---
+++ packages/fireant/fireant-7.4.1.tar.gz/fireant-7.4.1/fireant/middleware/decorators.py
@@ -0,0 +1,71 @@
+import time
+from functools import wraps
+
+from fireant.middleware.slow_query_logger import (
+    query_logger,
+    slow_query_logger,
+)
+
+
+def log_middleware(func):
+
+    @wraps(func)
+    def wrapper(database, *queries, **kwargs):
+        results = []
+
+        for query in queries:
+            start_time = time.time()
+            query_logger.debug(query)
+
+            results.append(func(database, query, **kwargs)[0])
+
+            duration = round(time.time() - start_time, 4)
+            query_log_msg = '[{duration} seconds]: {query}'.format(duration=duration,
+                                                                   query=query)
+            query_logger.info(query_log_msg)
+
+            if database.slow_query_log_min_seconds is not None and duration >= database.slow_query_log_min_seconds:
+                slow_query_logger.warning(query_log_msg)
+
+        return results
+
+    return wrapper
+
+
+class QueryCancelled(Exception):
+    pass
+
+
+def connection_middleware(func):
+
+    @wraps(func)
+    def wrapper(database, *queries, **kwargs):
+        connection = kwargs.pop('connection', None)
+        if connection:
+            return func(database, *queries, connection=connection, **kwargs)
+        with database.connect() as connection:
+            try:
+                return func(database, *queries, connection=connection, **kwargs)
+            except KeyboardInterrupt:
+                # A KeyboardInterrupt is used as a means of cancelling queries
+                if hasattr(connection, "cancel"):
+                    connection.cancel()
+                else:
+                    connection.close()
+
+        raise QueryCancelled("Query was cancelled")
+
+    return wrapper
+
+
+def apply_middlewares(wrapped_func):
+
+    @wraps(wrapped_func)
+    def wrapper(database, *args, **kwargs):
+        func = wrapped_func
+        for middleware in reversed(database.middlewares):
+            func = middleware(func)
+
+        return func(database, *args, **kwargs)
+
+    return wrapper
