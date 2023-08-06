f94ac669a7ffefcb79ece5b4fa2bef29a8566413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:14 +0000

    Add package swh.lister/0.1.5/swh.lister-0.1.5-py3-none-any.whl

---
+++ packages/swh.lister/swh.lister-0.1.5-py3-none-any.whl/swh/lister/pytest_plugin.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2019-2020  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import logging
+
+import pytest
+from sqlalchemy import create_engine
+
+from swh.lister import SUPPORTED_LISTERS, get_lister
+from swh.lister.core.models import initialize
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def lister_db_url(postgresql_proc, postgresql):
+    db_url = "postgresql://{user}@{host}:{port}/{dbname}".format(
+        host=postgresql_proc.host,
+        port=postgresql_proc.port,
+        user="postgres",
+        dbname="tests",
+    )
+    logger.debug("lister db_url: %s", db_url)
+    return db_url
+
+
+@pytest.fixture
+def lister_under_test():
+    """Fixture to determine which lister to test"""
+    return "core"
+
+
+@pytest.fixture
+def swh_lister(mock_get_scheduler, lister_db_url, swh_scheduler, lister_under_test):
+    assert lister_under_test in SUPPORTED_LISTERS
+    lister = get_lister(lister_under_test, db_url=lister_db_url)
+    initialize(create_engine(lister_db_url), drop_tables=True)
+
+    return lister
