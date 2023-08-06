8214d9616b7c2152f3468860c971472dbd00a50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:24 +0000

    Add package fastx-barber/0.1.0/fastx-barber-0.1.0.tar.gz

---
+++ packages/fastx-barber/fastx-barber-0.1.0.tar.gz/fastx-barber-0.1.0/fastx_barber/match.py
@@ -0,0 +1,70 @@
+"""
+@author: Gabriele Girelli
+@contact: gigi.ga90@gmail.com
+"""
+
+from abc import ABCMeta, abstractmethod
+from fastx_barber.seqio import SimpleFastxRecord
+import regex as re  # type: ignore
+from typing import Any, Match, Pattern, Tuple
+
+
+class ABCMatcher(metaclass=ABCMeta):
+    """Record matcher abstract base class
+
+    Extends:
+        metaclass=ABCMeta
+
+    Variables:
+        _matched_count {int} -- number of matched records
+        _unmatched_count {int} -- number of unmatched records
+    """
+
+    _matched_count: int = 0
+    _unmatched_count: int = 0
+
+    def __init__(self, pattern: Pattern):
+        """Init matcher class
+
+        Arguments:
+            pattern {Pattern} -- pattern to be matched
+        """
+        self._pattern = pattern
+
+    @property
+    def matched_count(self) -> int:
+        return self._matched_count
+
+    @property
+    def unmatched_count(self) -> int:
+        return self._unmatched_count
+
+    @abstractmethod
+    def match(self, record: Any) -> Tuple[Match, bool]:
+        """Match a record with the provided pattern
+
+        Decorators:
+            abstractmethod
+
+        Arguments:
+            record {Any} -- record to be matched
+
+        Returns:
+            Tuple[Match, bool] -- match and matched status
+        """
+        pass
+
+
+class FastxMatcher(ABCMatcher):
+    def __init__(self, pattern: Pattern):
+        super(FastxMatcher, self).__init__(pattern)
+
+    def match(self, record: SimpleFastxRecord) -> Tuple[Match, bool]:
+        name, seq, _ = record
+        match = re.match(self._pattern, seq)
+        matched = match is not None
+        if matched:
+            self._matched_count += 1
+        else:
+            self._unmatched_count += 1
+        return (match, matched)
---
+++ packages/fastx-barber/fastx-barber-0.1.0.tar.gz/fastx-barber-0.1.0/fastx_barber/scripts/__init__.py
@@ -0,0 +1,25 @@
+"""
+@author: Gabriele Girelli
+@contact: gigi.ga90@gmail.com
+"""
+
+from fastx_barber.scripts import arguments
+from fastx_barber.scripts import flag, flag_extract
+from fastx_barber.scripts import flag_filter, flag_regex, flag_split, flag_stats
+from fastx_barber.scripts import match
+from fastx_barber.scripts import trim, trim_len, trim_qual, trim_regex
+
+__all__ = [
+    "arguments",
+    "flag",
+    "flag_extract",
+    "flag_filter",
+    "flag_regex",
+    "flag_split",
+    "flag_stats",
+    "match",
+    "trim",
+    "trim_len",
+    "trim_qual",
+    "trim_regex",
+]
