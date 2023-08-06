c6b6c9b1b2384424bdc5749380829a023b2b0bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:00 +0000

    Add package poetry-demo-jerber/0.1.1/poetry-demo-jerber-0.1.1.tar.gz

---
+++ packages/poetry-demo-jerber/poetry-demo-jerber-0.1.1.tar.gz/poetry-demo-jerber-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_demo_jerber', 'poetry_demo_jerber.Models', 'poetry_demo_jerber.Outer']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.6.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'poetry-demo-jerber',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Jeremy Berman',
+    'author_email': 'jerber@sas.upenn.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
