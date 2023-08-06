8f9f47760e01cb39a4ed79343dc01b86204e6a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:02 +0000

    Add package elastic-cloud/0/elastic-cloud-0.tar.gz

---
+++ packages/elastic-cloud/elastic-cloud-0.tar.gz/elastic-cloud-0/setup.py
@@ -0,0 +1,12 @@
+import sys
+from setuptools import setup
+
+setup(
+    name="elastic-cloud",
+    version="0",
+    description="Reserving this package for future use",
+    url="https://github.com/elastic",
+    license="Apache-2.0",
+    author="Seth Michael Larson",
+    author_email="seth.larson@elastic.co"
+)
