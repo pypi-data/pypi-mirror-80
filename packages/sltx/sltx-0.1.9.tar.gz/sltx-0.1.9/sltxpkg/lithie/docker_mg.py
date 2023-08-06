65ed025c93d4709efaad2b8768c3c411cfde6ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:55:51 +0000

    Add package sltx/0.1.9/sltx-0.1.9.tar.gz

---
+++ packages/sltx/sltx-0.1.9.tar.gz/sltx-0.1.9/sltxpkg/lithie/docker_mg.py
@@ -0,0 +1,42 @@
+# Docker control
+import json
+import os
+
+import docker
+from sltxpkg.globals import DOCKER_URL
+
+# TODO for now we need a reading PAT?
+
+
+class DockerCtrl:
+    def __init__(self):
+        self.client = docker.from_env()
+        self.client.ping()
+
+    def update_img(self, profile: str):
+        target = DOCKER_URL.format(**locals())
+        print("Pulling image:", target, "this may take a few minutes")
+        for line in self.client.api.pull(target, tag='latest', stream=True):
+            # default values
+            d = {'status': 'unknown', 'progress': '', 'id': ''}
+            d = {**d, **json.loads(line)}
+            print("   {status} {progress} {id}".format(**d))
+
+    def run_in_container(self, profile: str, command: str):
+        if profile.startswith(":"):
+            target = profile[1:]
+        else:
+            target = DOCKER_URL.format(**locals())
+        print("Launching container", target)
+        run = self.client.containers.run(
+            target, command=command, detach=True, remove=True, working_dir='/home/lithie-user/data',
+            user='lithie-user', privileged=True, network_mode='bridge',
+            volumes={
+                os.getcwd(): {
+                    'bind': '/home/lithie-user/data',
+                    'mount': 'rw'
+                }
+            })
+        for l in run.logs(stdout=True, stderr=True, stream=True, timestamps=True):
+            print(l.decode('utf-8'), end='')
+        print("Container completed.")
