d79815849526c18aa8df7c871e716a0b5859abe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:13 +0000

    Add package sparsedat/1.0.0a10/sparsedat-1.0.0a10.tar.gz

---
+++ packages/sparsedat/sparsedat-1.0.0a10.tar.gz/sparsedat-1.0.0a10/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="sparsedat",
+    version="1.0.0a10",
+    packages=[
+        "sparsedat"
+    ],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pandas"
+    ]
+)
