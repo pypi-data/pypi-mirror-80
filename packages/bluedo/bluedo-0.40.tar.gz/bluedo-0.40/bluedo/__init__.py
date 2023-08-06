92c672c16ffb9f1856f6d5305213a196817e905f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:03 +0000

    Add package bluedo/0.40/bluedo-0.40.tar.gz

---
+++ packages/bluedo/bluedo-0.40.tar.gz/bluedo-0.40/bluedo/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+from . import bluedoapp
+
+def main(args=None):
+    app = bluedoapp.BlueDo()
+    return app.run(sys.argv)
+
+
+if __name__ == '__main__':
+    sys.exit(main(args=sys.argv))
