542dd8912cf32b255235efcf1e94d0bd7387eed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:31 +0000

    Add package pytranscoder-ffmpeg/2.2.1/pytranscoder_ffmpeg-2.2.1-py3-none-any.whl

---
+++ packages/pytranscoder-ffmpeg/pytranscoder_ffmpeg-2.2.1-py3-none-any.whl/pytranscoder/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '2.2.1'
+__author__ = 'Marshall L Smith Jr <marshallsmithjr@gmail.com>'
+__license__ = 'GPLv3'
+
+
+#
+# Global state indicators
+#
+verbose = False
+keep_source = False
+dry_run = False
