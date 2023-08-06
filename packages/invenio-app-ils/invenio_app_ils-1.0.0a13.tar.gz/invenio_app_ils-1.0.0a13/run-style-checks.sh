b3f0b8a5d8e17d5c983fb453966894457dd6855b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:52 +0000

    Add package invenio-app-ils/1.0.0a13/invenio_app_ils-1.0.0a13.tar.gz

---
+++ packages/invenio-app-ils/invenio_app_ils-1.0.0a13.tar.gz/invenio_app_ils-1.0.0a13/run-style-checks.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# invenio-app-ils is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+pytest --quiet --no-cov -m pep8 tests/api/*/ && \
+pydocstyle invenio_app_ils tests docs && \
+isort invenio_app_ils tests --check-only --diff && \
+check-manifest --ignore ".travis-*,docs/_build*" && \
+sphinx-build -qnNW docs docs/_build/html
