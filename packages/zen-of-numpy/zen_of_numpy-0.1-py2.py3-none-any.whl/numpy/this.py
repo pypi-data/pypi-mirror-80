4fec9f82825309d0a07636c38df2f8a0ec4cc033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:26 +0000

    Add package zen-of-numpy/0.1/zen_of_numpy-0.1-py2.py3-none-any.whl

---
+++ packages/zen-of-numpy/zen_of_numpy-0.1-py2.py3-none-any.whl/numpy/this.py
@@ -0,0 +1,25 @@
+s = """Gur Mra bs AhzCl, ol Jvz Tyraa
+
+Ornhgvshy vf orggre guna htyl.
+Htyl vf orggre guna fybj.
+Fvzcyr vf orggre guna pbzcyrk.
+pbzcyrk128 vf orggre guna pbzcyrk64.
+Syng vf orggre guna eniry.
+Fcnefr vf orggre guna qrafr vs lbh'er ybj ba ENZ.
+Ernqnovyvgl, jung'f gung?
+Fcrpvny pnfrf nera'g fcrpvny rabhtu gb oernx gur ehyrf, rkprcg sbe AnA.
+Nygubhtu ac.nyypybfr orngf chevgl.
+Reebef fubhyq frtsnhyg gur ehagvzr.
+Hayrff cnffvat fvyragyl.
+Va gur snpr bs bcgvzvmngvba, ershfr gur grzcgngvba gb ac.irpgbevmr.
+Gurer fubhyq or bar-- naq cersrenoyl zber guna bar --bofpher jnl gb qb vg.
+Nygubhtu gung jnl znl abg jbex hagvy lbh genafcbfr naq erfuncr.
+OYNF vf orggre guna ab OYNF.
+Nygubhtu ab OYNF vf bsgra orggre guna *fybj* OYNF.
+Vs gur vzcyrzragngvba unf n sbe-ybbc, vg'f n onq vqrn.
+Vs gur vzcyrzragngvba unf SBEGENA, vg znl or n tbbq vqrn.
+Anzrfcnprf ner bar ubaxvat terng vqrn -- url FpvCl, yrg'f funer bar bs gubfr!"""
+
+import codecs
+
+print(codecs.encode(s, "rot13"))
