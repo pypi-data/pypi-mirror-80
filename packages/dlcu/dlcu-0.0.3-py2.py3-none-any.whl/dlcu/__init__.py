249f31caa0cb8cdda6622a2da85f2eca7d551665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:55 +0000

    Add package dlcu/0.0.3/dlcu-0.0.3-py2.py3-none-any.whl

---
+++ packages/dlcu/dlcu-0.0.3-py2.py3-none-any.whl/dlcu/__init__.py
@@ -0,0 +1,6 @@
+"""This package contains material from the DeepLabCut-Utils repository by DeepLabCut."""
+
+from .plot import *
+from .time_in_each_roi import *
+
+from .version import __version__, VERSION
