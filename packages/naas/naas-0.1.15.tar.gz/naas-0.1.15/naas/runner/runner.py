a39336e16a009532213c06164c094d5431718270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:05 +0000

    Add package naas/0.1.15/naas-0.1.15.tar.gz

---
+++ packages/naas/naas-0.1.15.tar.gz/naas-0.1.15/naas/runner/runner.py
@@ -0,0 +1,160 @@
+from sentry_sdk.integrations.sanic import SanicIntegration
+from naas.runner.controllers.scheduler import SchedulerController
+from naas.runner.controllers.assets import AssetsController
+from naas.runner.controllers.notebooks import NbController
+from naas.runner.controllers.jobs import JobsController
+from naas.runner.controllers.logs import LogsController
+from naas.runner.controllers.env import EnvController
+from naas.runner.notifications import Notifications
+from naas.runner.scheduler import Scheduler
+from naas.runner.notebooks import Notebooks
+from sanic_openapi import swagger_blueprint
+from naas.runner.logger import Logger
+from .proxy import escape_kubernet
+from naas.runner.jobs import Jobs
+from multiprocessing import Process
+from naas.types import t_main
+from sanic import Sanic
+import sentry_sdk
+import asyncio
+import getpass
+import uuid
+import os
+import json
+import sys
+import nest_asyncio
+
+# TODO remove this fix when papermill support uvloop of Sanic support option to don't use uvloop
+asyncio.set_event_loop_policy(None)
+nest_asyncio.apply()
+
+
+class Runner:
+    # Declare semaphore variable
+    __name = "naas_runner"
+    __naas_folder = ".naas"
+    __tasks_sem = None
+    # Declare path variable
+    __path_lib_files = os.path.dirname(os.path.abspath(__file__))
+    __path_user_files = None
+    __port = 5000
+    __single_user_api_path = None
+    __html_files = "html"
+    __assets_files = "assets"
+    __manager_index = "manager.html"
+    __log_filename = "logs.csv"
+    __tasks_files = "jobs.json"
+    __info_file = "info.json"
+    __app = None
+    __nb = None
+    __http_server = None
+    __notif = None
+    __jobs = None
+    __scheduler = None
+    __daemon = None
+    __sentry = None
+    __logger = None
+    __testing = False
+    tz = None
+    user = None
+    __shell_user = None
+    public_url = None
+    proxy_url = None
+
+    def __init__(self):
+        self.__path_user_files = os.environ.get(
+            "JUPYTER_SERVER_ROOT", f'/home/{os.environ.get("NB_USER", "ftp")}'
+        )
+        self.__port = int(os.environ.get("NAAS_RUNNER_PORT", 5000))
+        self.__user = os.environ.get("JUPYTERHUB_USER", "joyvan@naas.com")
+        self.__shell_user = os.environ.get("USER", None)
+        self.__public_url = os.environ.get("JUPYTERHUB_URL", "")
+        self.__proxy_url = os.environ.get("PUBLIC_PROXY_API", "http://localhost:3002")
+        self.__notifications_url = os.environ.get(
+            "NOTIFICATIONS_API", "http://localhost:3003"
+        )
+        self.__tz = os.environ.get("TZ", "Europe/Paris")
+        self.__path_naas_files = os.path.join(
+            self.__path_user_files, self.__naas_folder
+        )
+        self.__path_html_files = os.path.join(self.__path_lib_files, self.__html_files)
+        self.__path_manager_index = os.path.join(
+            self.__path_html_files, self.__manager_index
+        )
+        self.__path_pidfile = os.path.join(self.__path_naas_files, f"{self.__name}.pid")
+
+    def __main(self, debug=True):
+        self.init_app()
+        uid = str(uuid.uuid4())
+        self.__logger.info({"id": uid, "type": t_main, "status": "start API"})
+        self.__app.run(host="0.0.0.0", port=self.__port, debug=debug, access_log=debug)
+
+    def __version(self):
+        version = None
+        try:
+            with open(
+                os.path.join(self.__path_lib_files, self.__info_file), "r"
+            ) as json_file:
+                version = json.load(json_file)
+        except IOError:
+            version = {"error": "cannot get info.json"}
+        return version
+
+    async def initialize_before_start(self, app, loop):
+        self.__jobs = Jobs(self.__logger, loop=loop)
+        self.__nb = Notebooks(self.__logger, loop, self.__notif)
+        self.__app.add_route(
+            NbController.as_view(self.__logger, self.__jobs, self.__nb),
+            "/notebooks/<token>",
+        )
+        self.__scheduler = Scheduler(self.__logger, self.__jobs, self.__nb, loop)
+        self.__app.add_route(
+            SchedulerController.as_view(self.__logger, self.__scheduler),
+            "/scheduler/<mode>",
+        )
+        self.__app.add_route(
+            JobsController.as_view(self.__logger, self.__jobs), "/jobs"
+        )
+        self.__app.add_route(
+            AssetsController.as_view(self.__logger, self.__jobs, self.__path_lib_files),
+            "/assets/<token>",
+        )
+        self.__scheduler.start()
+
+    def init_app(self):
+        self.__app = Sanic(__name__)
+        self.__logger = Logger()
+        self.__notif = Notifications(self.__logger)
+        self.__app.register_listener(
+            self.initialize_before_start, "before_server_start"
+        )
+        self.__app.add_route(
+            EnvController.as_view(
+                self.__logger,
+                self.__user,
+                self.__public_url,
+                self.__proxy_url,
+                self.__notifications_url,
+                self.__tz,
+            ),
+            "/env",
+        )
+        self.__app.add_route(LogsController.as_view(self.__logger), "/logs")
+        self.__app.static("/", self.__path_manager_index, name="manager.html")
+        self.__app.blueprint(swagger_blueprint)
+        uid = str(uuid.uuid4())
+        self.__logger.info({"id": uid, "type": t_main, "status": "init API"})
+        return self.__app
+
+    def start(self, deamon=True, port=None, debug=False):
+        user = getpass.getuser()
+        if user != self.__shell_user:
+            raise Exception(f"{user} not autorized, use {self.__shell_user} instead")
+        if port:
+            self.__port = port
+        print("Start Runner")
+        try:
+            self.__main(debug)
+        except KeyboardInterrupt:
+            print("Shutdown server")
+            sys.exit()
