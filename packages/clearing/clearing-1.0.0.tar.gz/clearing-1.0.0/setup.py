5749f88c10a79586345d8ec694bfb05ed2bf4d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:17 +0000

    Add package clearing/1.0.0/clearing-1.0.0.tar.gz

---
+++ packages/clearing/clearing-1.0.0.tar.gz/clearing-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+name='clearing',
+version='1.0.0',
+description='Cross-Platform Terminal Clearing Module.',
+long_description=long_description,
+long_description_content_type="text/markdown",
+url='https://github.com/c00lhawk607/Clearing',
+author='Jordan Dixon',
+author_email='jordandixon2004@outlook.com',
+license='GNU General Public License v3 (GPLv3)',
+packages=['clearing'],
+classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+  "Operating System :: OS Independent",
+],
+zip_safe=True,
+python_requires=">=3.0",
+)
