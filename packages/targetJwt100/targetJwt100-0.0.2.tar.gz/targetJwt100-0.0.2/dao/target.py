54f9535ad12e2b8d39265e66a7caa12e5556e7c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:58 +0000

    Add package targetJwt100/0.0.2/targetJwt100-0.0.2.tar.gz

---
+++ packages/targetJwt100/targetJwt100-0.0.2.tar.gz/targetJwt100-0.0.2/dao/target.py
@@ -0,0 +1,8 @@
+#  ! / user/ bin/ python
+#  -*-  coding utf-8 -*-
+# author : entang   
+# time : 2020/9/22
+
+
+def hello_world():
+    print('Hello, world')
---
+++ packages/targetJwt100/targetJwt100-0.0.2.tar.gz/targetJwt100-0.0.2/setup.py
@@ -0,0 +1,22 @@
+#  ! / user/ bin/ python
+#  -*-  coding utf-8 -*-
+# author : entang   
+# time : 2020/9/24
+
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="targetJwt100",
+  version="0.0.2",
+  author="Example Author",
+  author_email="author@example.com",
+  description="A small example package",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/pypa/sampleproject",
+  packages=setuptools.find_packages(),
+  classifiers=[ ],
+)
