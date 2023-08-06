7a445d44cb088559db61762e7a1c7e279f123da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:53 +0000

    Add package pygazpar/0.1.14/pygazpar-0.1.14-py3-none-any.whl

---
+++ packages/pygazpar/pygazpar-0.1.14-py3-none-any.whl/pygazpar/__init__.py
@@ -0,0 +1,3 @@
+from pygazpar.enum import PropertyNameEnum
+from pygazpar.client import Client
+from pygazpar.client import LoginError
---
+++ packages/pygazpar/pygazpar-0.1.14-py3-none-any.whl/pygazpar/__main__.py
@@ -0,0 +1,48 @@
+import argparse
+import sys
+import json
+import traceback
+
+from pygazpar.client import Client
+
+def main():
+    """Main function"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username",
+                      required=True,
+                      help="GRDF username (email)")    
+    parser.add_argument("-p", "--password",
+                      required=True,
+                      help="GRDF password")    
+    parser.add_argument("-w", "--webdriver",
+                      required=True,
+                      help="Firefox webdriver executable (geckodriver)")    
+    parser.add_argument("-s", "--wait_time",
+                      required=False,
+                      type=int,
+                      default=30,
+                      help="Wait time in seconds (see https://selenium-python.readthedocs.io/waits.html for details)")    
+    parser.add_argument("-t", "--tmpdir",
+                      required=False,
+                      default="/tmp",
+                      help="tmp directory (default is /tmp)")
+    parser.add_argument("-l", "--lastNRows",
+                      required=False,
+                      type=int,
+                      default=0,
+                      help="Get only the last N rows (default is 0: it means all rows are retrieved)")                           
+
+    args = parser.parse_args()
+
+    client = Client(args.username, args.password, args.webdriver, int(args.wait_time), args.tmpdir, int(args.lastNRows))
+
+    try:
+        client.update()
+    except BaseException:
+        print('An error occured while querying PyGazpar library : %s', traceback.format_exc())
+        return 1
+
+    print(json.dumps(client.data(), indent=2))
+
+if __name__ == '__main__':
+    sys.exit(main())
