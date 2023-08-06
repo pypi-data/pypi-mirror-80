829a76c157f14c9510eeaf9c06916d06ff791c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:22 +0000

    Add package QCharted/1.1.2/QCharted-1.1.2.tar.gz

---
+++ packages/QCharted/QCharted-1.1.2.tar.gz/QCharted-1.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='QCharted',
+    version='1.1.2',
+    author="Bernhard Arnold",
+    author_email="bernhard.arnold@oeaw.ac.at",
+    url = "https://github.com/arnobaer/QCharted",
+    description="Plotting large data series using PyQtChart.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    py_modules=['QCharted'],
+    install_requires=[
+        'numpy>=1.17',
+        'PyQt5>=5.12',
+        'PyQtChart>=5.12',
+    ],
+    test_suite='tests',
+    license="GPLv3",
+)
