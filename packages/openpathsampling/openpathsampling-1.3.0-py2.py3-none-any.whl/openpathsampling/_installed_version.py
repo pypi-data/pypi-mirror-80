4f62ac102eb734a89b8d72ae926882b1e1efd3b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:51 +0000

    Add package openpathsampling/1.3.0/openpathsampling-1.3.0-py2.py3-none-any.whl

---
+++ packages/openpathsampling/openpathsampling-1.3.0-py2.py3-none-any.whl/openpathsampling/_installed_version.py
@@ -0,0 +1,3 @@
+_installed_version = '1.3.0'
+_installed_git_hash = '631d65d0f882692f1a9676ba2e747d55631ce536'
+_version_setup_depth = 1
---
+++ packages/openpathsampling/openpathsampling-1.3.0-py2.py3-none-any.whl/openpathsampling/engines/features/kinetics.py
@@ -0,0 +1,58 @@
+from .shared import KineticContainerStore, KineticContainer, unmask_quantity
+from openpathsampling.netcdfplus import WeakLRUCache
+
+variables = ['kinetics', 'is_reversed']
+lazy = ['kinetics']
+flip = ['is_reversed']
+
+dimensions = ['n_atoms', 'n_spatial']
+
+
+def netcdfplus_init(store):
+    kinetic_store = KineticContainerStore()
+    kinetic_store.set_caching(WeakLRUCache(10000))
+
+    name = store.prefix + 'kinetics'
+
+    # tell the KineticContainerStore to base its dimensions on names
+    # prefixed with the store name to make sure each snapshot store has
+    # its own kinetics store
+    kinetic_store.set_dimension_prefix_store(store)
+
+    store.storage.create_store(name, kinetic_store, False)
+    store.create_variable(
+        'kinetics', 'lazyobj.' + name,
+        description="the snapshot index (0..n_momentum-1) 'frame' of "
+                    "snapshot '{idx}'.")
+
+    store.create_variable(
+        'is_reversed', 'bool',
+        description="the indicator if momenta should be flipped.")
+
+
+@property
+def velocities(self):
+    """
+    The velocities in the configuration. If the snapshot is reversed a
+    copy of the original (unreversed) velocities is made which is then
+    returned
+    """
+    if self.kinetics is not None:
+        vel = unmask_quantity(self.kinetics.velocities)
+        if self.is_reversed:
+            return -1.0 * vel
+        else:
+            return vel
+
+    return None
+
+
+@velocities.setter
+def velocities(self, value):
+    if value is not None:
+        kc = KineticContainer(velocities=value)
+    else:
+        kc = None
+
+    self.is_reversed = False
+    self.kinetics = kc
