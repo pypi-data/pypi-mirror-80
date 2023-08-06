526b8cb7ccc29c59df7e37d3944f03664d18743c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:54:54 +0000

    Add package johnnydep/1.6/johnnydep-1.6.tar.gz

---
+++ packages/johnnydep/johnnydep-1.6.tar.gz/johnnydep-1.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name="johnnydep",
+    version="1.6",
+    description="Display dependency tree of Python distribution",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=["johnnydep"],
+    author="Wim Glenn",
+    author_email="hey@wimglenn.com",
+    license="MIT",
+    url="https://github.com/wimglenn/johnnydep",
+    install_requires=[
+        "anytree",
+        "structlog",
+        "tabulate",
+        "wimpy",
+        "colorama",  # pretty output for structlog
+        "cachetools",
+        "distlib",
+        "oyaml",
+        "toml",
+        "pip",
+        "packaging >= 17",
+        "wheel >= 0.32.0",
+        "setuptools >= 38.3",  # for pkg_resources
+        "pkginfo >= 1.4.2",
+    ],
+    entry_points={
+        "console_scripts": [
+            "johnnydep = johnnydep.cli:main",
+            "pipper = johnnydep.pipper:main",
+        ]
+    },
+)
