d29ff0407d81f814baf9602b2801928a1217cff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:46 +0000

    Add package pm4py/1.5.2.1/pm4py-1.5.2.1.tar.gz

---
+++ packages/pm4py/pm4py-1.5.2.1.tar.gz/pm4py-1.5.2.1/pm4py/objects/conversion/log/versions/df_to_event_log_1v.py
@@ -0,0 +1,42 @@
+from pm4py.objects.log.log import EventLog, Trace, Event
+from pm4py.util import xes_constants as xes
+from pm4py.util import constants as pm4_constants
+import deprecation
+
+
+@deprecation.deprecated(deprecated_in='1.3.0', removed_in='2.0.0', current_version='',
+                        details='conversion versions are deprecated; use conversion variants instead')
+def apply(df, parameters=None):
+    """
+    Convert a dataframe into a log containing 1 case per variant (only control-flow
+    perspective is considered)
+
+    Parameters
+    -------------
+    df
+        Dataframe
+    parameters
+        Parameters of the algorithm
+
+    Returns
+    -------------
+    log
+        Event log
+    """
+    from pm4py.statistics.traces.pandas import case_statistics
+
+    if parameters is None:
+        parameters = {}
+    variant_stats = case_statistics.get_variant_statistics(df, parameters=parameters)
+    activity_key = parameters[
+        pm4_constants.PARAMETER_CONSTANT_ACTIVITY_KEY] if pm4_constants.PARAMETER_CONSTANT_ACTIVITY_KEY in parameters else xes.DEFAULT_NAME_KEY
+    log = EventLog()
+    for vd in variant_stats:
+        variant = vd['variant'].split(",")
+        trace = Trace()
+        for activity in variant:
+            event = Event()
+            event[activity_key] = activity
+            trace.append(event)
+        log.append(trace)
+    return log
