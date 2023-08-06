73157d597f8f779cf9ab7e23a8e0e6a12acc822f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:16 +0000

    Add package kmail/0.0.1/kmail-0.0.1-py3-none-any.whl

---
+++ packages/kmail/kmail-0.0.1-py3-none-any.whl/kmail/__init__.py
@@ -0,0 +1,15 @@
+import smtplib 
+def kmail(sender,password,receiver,subject,body):
+    try:
+        s = smtplib.SMTP('smtp.gmail.com', 587) 
+        s.ehlo()
+        s.starttls() 
+        s.login(sender,password) 
+        tm='Subject:'+subject +'\n\n'+body
+        s.sendmail(sender,receiver,tm) 
+        s.quit()
+    except smtplib.SMTPAuthenticationError:
+        print("The username and/or password you entered is incorrect \nOR \nsmtplib.SMTPAuthenticationError:security issue visit and enable less secure app https://myaccount.google.com/lesssecureapps")
+
+    except :
+        print('internet not found') 
