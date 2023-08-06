9c46849935fe5db325415315b6cefcfda923e6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:03 +0000

    Add package mutagenesis-visualization/0.0.1.6/mutagenesis_visualization-0.0.1.6-py3-none-any.whl

---
+++ packages/mutagenesis-visualization/mutagenesis_visualization-0.0.1.6-py3-none-any.whl/mutagenesis_visualization/__init__.py
@@ -0,0 +1,9 @@
+from .mutagenesis_visualization import *
+
+__author__ = "Frank Hidalgo"
+__version__ = "0.0.1"
+__title__ = "Mutagenesis Visualization"
+__license__ = "GPLv3"
+__author_email__ = "fhidalgoruiz@berkeley.edu"
+
+
