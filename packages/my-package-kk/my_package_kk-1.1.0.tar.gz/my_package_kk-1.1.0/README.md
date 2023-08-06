c408eb205fc4cc69b9974a3a91492d089a98600c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:08 +0000

    Add package my-package-kk/1.1.0/my_package_kk-1.1.0.tar.gz

---
+++ packages/my-package-kk/my_package_kk-1.1.0.tar.gz/my_package_kk-1.1.0/README.md
@@ -0,0 +1,6 @@
+Hi there. This is my project.
+
+print_name()
+===
+
+returns random name and its length 
---
+++ packages/my-package-kk/my_package_kk-1.1.0.tar.gz/my_package_kk-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='my_package_kk',
+    version='1.1.0',
+    author='kk12pt',
+    author_email='spam@12pt.pl',
+    packages=find_packages(),
+    include_package_data=True,
+    description='hi fi super star',
+    install_requires = ['names']
+)
