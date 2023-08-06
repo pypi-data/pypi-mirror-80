345074098a13efa4c8b7a6aa03c7fdf0c1c0d3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:54 +0000

    Add package feedsearch-crawler/0.2.7/feedsearch-crawler-0.2.7.tar.gz

---
+++ packages/feedsearch-crawler/feedsearch-crawler-0.2.7.tar.gz/feedsearch-crawler-0.2.7/feedsearch_crawler/crawler/item_parser.py
@@ -0,0 +1,21 @@
+import logging
+from types import AsyncGeneratorType
+from typing import Union
+
+from feedsearch_crawler.crawler.response import Response
+from feedsearch_crawler.crawler.request import Request
+from feedsearch_crawler.crawler.item import Item
+from abc import ABC, abstractmethod
+
+
+class ItemParser(ABC):
+    def __init__(self, crawler):
+        self.crawler = crawler
+        self.follow = crawler.follow
+        self.logger = logging.getLogger("feedsearch_crawler")
+
+    @abstractmethod
+    async def parse_item(
+        self, request: Request, response: Response, *args, **kwargs
+    ) -> Union[Item, AsyncGeneratorType]:
+        raise NotImplementedError("Not Implemented")
