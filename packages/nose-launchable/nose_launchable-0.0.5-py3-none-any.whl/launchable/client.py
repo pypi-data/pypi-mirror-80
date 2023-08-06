c9b2682b68b17110632212d4041f5bd8a2309ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:11 +0000

    Add package nose-launchable/0.0.5/nose_launchable-0.0.5-py3-none-any.whl

---
+++ packages/nose-launchable/nose_launchable-0.0.5-py3-none-any.whl/launchable/client.py
@@ -0,0 +1,71 @@
+import os
+from launchable.version import __version__
+
+import requests
+
+
+class LaunchableClientFactory:
+    BASE_URL_KEY = "LAUNCHABLE_BASE_URL"
+    TOKEN_KEY = "LAUNCHABLE_TOKEN"
+
+    DEFAULT_BASE_URL = "https://api.mercury.launchableinc.com"
+
+    @classmethod
+    def prepare(cls):
+        url, org, wp, token = cls._parse_options()
+
+        return LaunchableClient(url, org, wp, token, requests)
+
+    @classmethod
+    def _parse_options(cls):
+        token = os.environ[cls.TOKEN_KEY]
+
+        _, user, _ = token.split(":", 2)
+        org, workplace = user.split("/", 1)
+
+        return cls._get_base_url(), org, workplace, token
+
+    @classmethod
+    def _get_base_url(cls):
+        return os.getenv(cls.BASE_URL_KEY) or cls.DEFAULT_BASE_URL
+
+
+class LaunchableClient:
+    # TODO: Stop using a fictitious id once the server starts dynamic reordering
+    FICTITIOUS_ID = "1"
+    CLIENT_NAME = "nose-launchable"
+
+    def __init__(self, base_url, org_name, workspace_name, token, http):
+        self.base_url = base_url
+        self.org_name = org_name
+        self.workspace_name = workspace_name
+        self.token = token
+        self.http = http
+
+    def infer(self, test):
+        url = "{}/intake/organizations/{}/workspaces/{}/inference".format(
+            self.base_url,
+            self.org_name,
+            self.workspace_name
+        )
+
+        headers = {
+            'Content-Type': 'application/json',
+            'X-Client-Name': self.CLIENT_NAME,
+            'X-Client-Version': __version__,
+            'Authorization': 'Bearer {}'.format(self.token)
+        }
+        body = self._request_body(test)
+
+        res = self.http.post(url, headers=headers, json=body)
+        res.raise_for_status()
+
+        return res.json()
+
+    def _request_body(self, test):
+        return {
+            "test": test,
+            "session": {"id": self.FICTITIOUS_ID, "subject": self.FICTITIOUS_ID, "flavors": {}},
+        }
+
+
---
+++ packages/nose-launchable/nose_launchable-0.0.5-py3-none-any.whl/launchable/plugin.py
@@ -0,0 +1,36 @@
+import logging
+import traceback
+
+from nose.plugins import Plugin
+from launchable.client import LaunchableClientFactory
+from launchable.manager import parse_test, reorder
+
+log = logging.getLogger("nose.plugins.launchable")
+
+
+class Launchable(Plugin):
+    name = "launchable"
+
+    def options(self, parser, env):
+        super(Launchable, self).options(parser, env=env)
+        parser.add_option("--launchable", action='store_true', dest="launchable", help="Enable Launchable API interaction")
+
+    def configure(self, options, conf):
+        super(Launchable, self).configure(options, conf)
+        self.enabled = options.launchable
+
+    def prepareTest(self, test):
+        try:
+            t = parse_test(test)
+
+            launchable_client = LaunchableClientFactory.prepare()
+            order = launchable_client.infer(t)
+
+            reorder(test, order)
+
+            print("Tests have been successfully reordered")
+            return
+        except Exception as error:
+            log.warning("An exception occurred while reordering. Executing tests in standard order: {}".format(error))
+            log.warning(traceback.format_exc())
+            return test
