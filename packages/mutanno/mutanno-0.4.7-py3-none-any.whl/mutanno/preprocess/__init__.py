1d24789653380aa66bc03460c7511be4d2c6233b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:18 +0000

    Add package mutanno/0.4.7/mutanno-0.4.7-py3-none-any.whl

---
+++ packages/mutanno/mutanno-0.4.7-py3-none-any.whl/mutanno/preprocess/__init__.py
@@ -0,0 +1,18 @@
+
+
+def run_preproc_vep(data):
+    from .preproc_vep import PreprocVEP
+    pp = PreprocVEP(data)
+    pp.run()
+
+
+def run_preproc_clinvar(data):
+    from .preproc_clinvar import PreprocClinVar
+    pp = PreprocClinVar(data)
+    pp.run()
+
+
+def run_preproc_gnomad(data):
+    from .preproc_gnomad import PreprocGnomAD
+    pp = PreprocGnomAD(data)
+    pp.run()
