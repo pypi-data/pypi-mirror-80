f54b013235587e10fe77b7dca4877c4b8d473350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:08 +0000

    Add package zm-dsnd-probability/0.1/zm_dsnd_probability-0.1.tar.gz

---
+++ packages/zm-dsnd-probability/zm_dsnd_probability-0.1.tar.gz/zm_dsnd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='zm_dsnd_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['zm_dsnd_probability'],
+      author = 'Zimei Chen',
+      author_email = 'chenzm1106@gmail.com',
+      zip_safe=False)
