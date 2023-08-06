16fe572a42a452606694a024bf8398918cc04d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:47 +0000

    Add package pytwanalysis/0.0.2/pytwanalysis-0.0.4-py3-none-any.whl

---
+++ packages/pytwanalysis/pytwanalysis-0.0.4-py3-none-any.whl/pytwanalysis/__init__.py
@@ -0,0 +1,7 @@
+"""
+Twitter data analysis with MongoDB and networkx
+:Author: Lia Nogueira, 2020
+"""
+
+from pytwanalysis.py_twitter_analysis import TwitterAnalysis
+from pymongo import MongoClient
