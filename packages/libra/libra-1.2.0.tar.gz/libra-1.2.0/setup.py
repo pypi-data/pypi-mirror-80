532f3886797285643d1cea839ff2e20782be2bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:41 +0000

    Add package libra/1.2.0/libra-1.2.0.tar.gz

---
+++ packages/libra/libra-1.2.0.tar.gz/libra-1.2.0/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="libra",  # Replace with your own username
+    version="1.2.0",
+    author="Palash Shah",
+    author_email="ps9cmk@virginia.edu",
+    description="Ergonomic machine learning for everyone",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Palashio/libra",
+    install_requires=['colorama',
+                      'transformers==2.11.0',
+                      'tensorflow==2.2.0',
+                      'keras==2.4.3',
+                      'numpy',
+                      'pandas',
+                      'sklearn',
+                      'matplotlib',
+                      'tabulate',
+                      'textblob',
+                      'seaborn',
+                      'keras-tuner',
+                      'spacy',
+                      'jellyfish',
+                      'autocorrect',
+                      'pillow',
+                      'prince',
+                      'opencv-python',
+                      'nltk',
+                      'xgboost',
+                      'download'],
+    packages=find_packages(exclude=('tests',)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
