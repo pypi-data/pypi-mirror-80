e7671da8e1c9f2ee41ffd2eb38ca174bd1f58578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:23 +0000

    Add package typefu/0.0.1/typefu-0.0.1-py3-none-any.whl

---
+++ packages/typefu/typefu-0.0.1-py3-none-any.whl/typefu/mssql.py
@@ -0,0 +1,19 @@
+mapper = {
+        35: 'string',  # text
+        36: 'string',  # uniqueidentifier
+        40: 'string',  # date
+        48: 'int',  # tinyint
+        52: 'int',  # smallint
+        56: 'int',  # int
+        58: 'string',  # smalldatetime
+        61: 'timestamp-millis',  # datetime
+        62: 'float',  # float
+        99: 'string',  # ntext
+        104: 'boolean',  # bit
+        106: 'double',  # decimal
+        127: 'long',  # bigint
+        167: 'string',  # varchar
+        175: 'string',  # char
+        231: 'string',  # nvarchar
+        241: 'string'  # xml
+    }
