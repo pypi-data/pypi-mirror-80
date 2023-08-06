939b264a6510ef53d145889eb23c2dbb88e0ed2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:45 +0000

    Add package pyniviz/1.0.3/pyniviz-1.0.3.tar.gz

---
+++ packages/pyniviz/pyniviz-1.0.3.tar.gz/pyniviz-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyniviz',
+    version='1.0.3',
+    packages=['pyniviz'],
+    url='https://github.com/robbiemallett/pyniviz',
+    license='MIT',
+    author='Robbie Mallett',
+    install_requires=['pandas','numpy','scipy','matplotlib'],
+    author_email='robbie.mallett.17@ucl.ac.uk',
+    long_description='pyniviz is a Python tool for visualising the output of the SNOWPACK model. For information and a link to the documentation please visit the github page.'
+)
