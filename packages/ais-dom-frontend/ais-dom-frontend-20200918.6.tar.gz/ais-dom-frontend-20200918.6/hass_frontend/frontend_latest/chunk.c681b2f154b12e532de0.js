183bd94f8465ed75aab9211e9a3563bec290973a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:49 +0000

    Add package ais-dom-frontend/20200918.6/ais-dom-frontend-20200918.6.tar.gz

---
+++ packages/ais-dom-frontend/ais-dom-frontend-20200918.6.tar.gz/ais-dom-frontend-20200918.6/hass_frontend/frontend_latest/chunk.c681b2f154b12e532de0.js
@@ -0,0 +1,2 @@
+(self.webpackJsonp=self.webpackJsonp||[]).push([[44],{914:function(e,r,n){"use strict";n.r(r),n.d(r,"codeMirror",(function(){return c})),n.d(r,"codeMirrorCss",(function(){return i}));var s=n(578),o=n.n(s),t=n(785),a=(n(786),n(787),n(11));o.a.commands.save=e=>{Object(a.a)(e.getWrapperElement(),"editor-save")};const c=o.a,i=t.a}}]);
+//# sourceMappingURL=chunk.c681b2f154b12e532de0.js.map
