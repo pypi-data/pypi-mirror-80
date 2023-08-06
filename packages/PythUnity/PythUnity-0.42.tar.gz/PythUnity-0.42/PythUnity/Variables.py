f109bfaa4db773236d0e9431e13aecc65cba5554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:08 +0000

    Add package PythUnity/0.42/PythUnity-0.42.tar.gz

---
+++ packages/PythUnity/PythUnity-0.42.tar.gz/PythUnity-0.42/PythUnity/Variables.py
@@ -0,0 +1,20 @@
+global parts
+parts = []
+
+global deltaTime
+deltaTime = .1
+
+global prefabs
+prefabs = {}
+
+global starts
+starts = []
+
+global backgroundColor
+backgroundColor = (255, 255, 255)
+
+global mousePosition
+mousePosition = (0, 0)
+
+global screenRect
+screenRect = (800, 600)
