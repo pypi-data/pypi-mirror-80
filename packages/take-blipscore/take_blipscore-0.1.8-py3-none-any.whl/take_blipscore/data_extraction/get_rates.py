ada98551be819da8707e08c0b3015806144a8450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:31 +0000

    Add package take-blipscore/0.1.8/take_blipscore-0.1.8-py3-none-any.whl

---
+++ packages/take-blipscore/take_blipscore-0.1.8-py3-none-any.whl/take_blipscore/data_extraction/get_rates.py
@@ -0,0 +1,50 @@
+_author__ = "Juliana Guamá"
+__version__ = "0.1.4"
+
+import os
+import typing as tp
+import importlib as il
+from warnings import warn
+
+import take_blipscore.data_extraction.metrics as m
+
+
+def load(path: str) -> tp.Type[m.PipelineCaller]:
+    """Load a function by import for node.
+
+    :param path: Path with package, module and function name.
+    :type path: ``str``
+    :return: Imported class.
+    :rtype: ``type``
+    """
+    module_name, name = path.rsplit('.', 1)
+    module = il.import_module(module_name)
+    return getattr(module, name)
+
+
+def get_rates(metrics: dict, **kwargs):
+    """Run all metrics pipelines and return rates.
+    
+    :param metrics: Metrics by name with parameters.
+    :type metrics: ``dict`` to ``str``
+    :return: Rates by metric name.
+    :rtype: ``dict`` to ``str``
+    """
+    out = {"error": dict()}
+    for metric_name, metric_params in metrics.items():
+        os.chdir("..")
+        try:
+            os.chdir(metric_params["chdir"])
+        except Exception as e:
+            warn("Chdir error on " + str(metric_name) + "\n" + str(e))
+        
+        rate_class = load(metric_params["class_path"])
+        try:
+            out[metric_name] = rate_class(**kwargs).get_rate()
+        except Exception as e:
+            out[metric_name] = 0
+            out["error"][metric_name] = e
+            warn("Pipeline processing error on " + str(metric_name) + "\n" + str(e))
+    
+    os.chdir("../BlipScore")
+    return out
