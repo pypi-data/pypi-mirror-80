8dbd005d837b691ff1aa42e42ba63734b56de53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:39 +0000

    Add package mdformat/0.3.1/mdformat-0.3.1.tar.gz

---
+++ packages/mdformat/mdformat-0.3.1.tar.gz/mdformat-0.3.1/mdformat/_api.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from typing import Iterable, Union
+
+from markdown_it import MarkdownIt
+
+import mdformat.plugins
+from mdformat.renderer import MDRenderer
+
+
+def text(
+    md: str, *, extensions: Iterable[str] = (), codeformatters: Iterable[str] = ()
+) -> str:
+    """Format a Markdown string."""
+    markdown_it = MarkdownIt(renderer_cls=MDRenderer)
+    markdown_it.options["parser_extension"] = []
+    for name in extensions:
+        plugin = mdformat.plugins.PARSER_EXTENSIONS[name]
+        plugin.update_mdit(markdown_it)
+        markdown_it.options["parser_extension"].append(plugin)
+    markdown_it.options["codeformatters"] = {
+        lang: mdformat.plugins.CODEFORMATTERS[lang] for lang in codeformatters
+    }
+    return markdown_it.render(md)
+
+
+def file(
+    f: Union[str, Path],
+    *,
+    extensions: Iterable[str] = (),
+    codeformatters: Iterable[str] = (),
+) -> None:
+    """Format a Markdown file in place."""
+    if isinstance(f, str):
+        f = Path(f)
+    try:
+        is_file = f.is_file()
+    except OSError:  # Catch "OSError: [WinError 123]" on Windows
+        is_file = False
+    if not is_file:
+        raise ValueError(f'Can not format "{f}". It is not a file.')
+    original_md = f.read_text(encoding="utf-8")
+    formatted_md = text(
+        original_md, extensions=extensions, codeformatters=codeformatters
+    )
+    f.write_text(formatted_md, encoding="utf-8")
