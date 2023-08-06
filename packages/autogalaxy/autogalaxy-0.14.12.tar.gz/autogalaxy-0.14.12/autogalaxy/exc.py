ec4fe15456aafa989f6b238bf07c415e75568955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:26 +0000

    Add package autogalaxy/0.14.12/autogalaxy-0.14.12.tar.gz

---
+++ packages/autogalaxy/autogalaxy-0.14.12.tar.gz/autogalaxy-0.14.12/autogalaxy/exc.py
@@ -0,0 +1,34 @@
+import autofit as af
+from autofit.exc import PipelineException
+
+
+class CosmologyException(Exception):
+    pass
+
+
+class ProfileException(Exception):
+    pass
+
+
+class GalaxyException(Exception):
+    pass
+
+
+class PlaneException(Exception):
+    pass
+
+
+class PlottingException(Exception):
+    pass
+
+
+class PixelizationException(af.exc.FitException):
+    pass
+
+
+class UnitsException(Exception):
+    pass
+
+
+class SetupException(Exception):
+    pass
