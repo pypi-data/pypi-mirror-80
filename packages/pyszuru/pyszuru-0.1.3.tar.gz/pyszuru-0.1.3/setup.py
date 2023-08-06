e348a135c53f2ebe5b7abfb868d4ae1a29beee48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:19 +0000

    Add package pyszuru/0.1.3/pyszuru-0.1.3.tar.gz

---
+++ packages/pyszuru/pyszuru-0.1.3.tar.gz/pyszuru-0.1.3/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import subprocess
+
+
+def get_git_version():
+    return subprocess.run(
+        ["git", "describe", "--abbrev=0"],
+        capture_output=True,
+        text=True,
+        check=True,
+    ).stdout.strip()
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyszuru",
+    version=get_git_version(),
+    author="Shyam Sunder",
+    author_email="sgsunder1@gmail.com",
+    description="Python interface for szurubooru",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sgsunder/python-szurubooru",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
