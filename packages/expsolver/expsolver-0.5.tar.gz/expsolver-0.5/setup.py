72c14044e7148cd9bf337b178d2412f76d90494d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:33 +0000

    Add package expsolver/0.5/expsolver-0.5.tar.gz

---
+++ packages/expsolver/expsolver-0.5.tar.gz/expsolver-0.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='expsolver',
+      version='0.5',
+      description='Expression solver (exp=0) Git: https://github.com/kgraghav/expsolver',
+      packages=['expsolver'],
+      long_description = 'README.md',
+      zip_safe=False)
