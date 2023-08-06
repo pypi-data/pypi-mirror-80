54187e2b9bd8a41aa085c20dd17ffd59df6f5ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:16 +0000

    Add package pyusbdux/1.6.1/pyusbdux-1.6.1.tar.gz

---
+++ packages/pyusbdux/pyusbdux-1.6.1.tar.gz/pyusbdux-1.6.1/pyusbdux.h
@@ -0,0 +1,29 @@
+#ifndef PY_USBDUX_H
+#define PY_USBDUX_H
+
+typedef float* sample_p;
+
+void open(int comediDeviceNumber);
+void open();
+
+void start(int nChan);
+void start(int nChan, double fs);
+
+int hasSampleAvailable();
+
+float getSamplingRate();
+
+sample_p getSampleFromBuffer();
+
+void stop();
+
+void digital_out(int channel, int value);
+int digital_in(int channel);
+void analogue_out(int channel, int value);
+int get_analogue_out_max_raw_value();
+
+const char* get_board_name();
+
+void close();
+
+#endif
