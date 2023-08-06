a1e2f8dac91b954c2bc35081d547068bccf1fb55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:01 +0000

    Add package ashe/0.0.3/ashe-0.0.3-py3-none-any.whl

---
+++ packages/ashe/ashe-0.0.3-py3-none-any.whl/ashe/__init__.py
@@ -0,0 +1,18 @@
+from .op_dict import merge
+from .op_dict import remove
+from .op_sys import size
+from .op_str import find
+from .file import read
+from .file import write
+
+
+__version__ = "0.0.3"
+
+__all__ = [
+    "merge",
+    "remove",
+    "size",
+    "find",
+    "read",
+    "write"
+]
