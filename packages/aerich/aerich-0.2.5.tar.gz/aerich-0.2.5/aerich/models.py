c1908c7805c2480bbf925e4ffa5d9c3bfa4c6d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:16 +0000

    Add package aerich/0.2.5/aerich-0.2.5.tar.gz

---
+++ packages/aerich/aerich-0.2.5.tar.gz/aerich-0.2.5/aerich/models.py
@@ -0,0 +1,11 @@
+from tortoise import Model, fields
+
+MAX_VERSION_LENGTH = 255
+
+
+class Aerich(Model):
+    version = fields.CharField(max_length=MAX_VERSION_LENGTH)
+    app = fields.CharField(max_length=20)
+
+    class Meta:
+        ordering = ["-id"]
