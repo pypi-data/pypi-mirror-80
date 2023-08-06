a43106bd58201008f4283a79f9d0e31a66bf333c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:59 +0000

    Add package kalong/0.1.1/kalong-0.1.1.tar.gz

---
+++ packages/kalong/kalong-0.1.1.tar.gz/kalong-0.1.1/kalong/utils/__init__.py
@@ -0,0 +1,29 @@
+import os
+import signal
+import socket
+import sys
+import threading
+
+try:
+    from log_colorizer import basicConfig
+except ImportError:
+    from logging import basicConfig  # noqa
+
+
+def current_origin():
+    return f'{socket.getfqdn()}__{os.getpid()}--{threading.get_ident()}'
+
+
+class fake_argv:
+    def __init__(self, *args):
+        self.args = args
+
+    def __enter__(self):
+        self._old_args = sys.argv
+        sys.argv = self.args
+
+    def __exit__(self, *err):
+        sys.argv = self._old_args
+
+
+USER_SIGNAL = signal.SIGILL if sys.platform == "win32" else signal.SIGUSR1
