1a1480b461e79f089982fb44f8240fc8651615f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:50 +0000

    Add package fast-eval/0.1.5/fast-eval-0.1.5.tar.gz

---
+++ packages/fast-eval/fast-eval-0.1.5.tar.gz/fast-eval-0.1.5/fast_eval/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import argparse
+from fast_eval import util
+def main():
+    print('coucou')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config",
+                        help="path of json config file")
+    parser.add_argument("archive_path",
+                        help="path of archive from arche")
+    parser.add_argument("--ws",
+                        help="where to build workspace")
+    fe = FastEval(parser.parse_args())
