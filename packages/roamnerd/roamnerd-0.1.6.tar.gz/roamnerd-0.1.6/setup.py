18d27e2f0b689f999567f9b71fde2ce2706cd812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:22 +0000

    Add package roamnerd/0.1.6/roamnerd-0.1.6.tar.gz

---
+++ packages/roamnerd/roamnerd-0.1.6.tar.gz/roamnerd-0.1.6/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['roamnerd']
+install_requires = \
+['argh>=0.26.2,<0.27.0', 'requests>=2.24.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['roamnerd = roamnerd:main']}
+
+setup_kwargs = {
+    'name': 'roamnerd',
+    'version': '0.1.6',
+    'description': 'A command-line tool for accessing the roamNERd API to annotate .txt files',
+    'long_description': None,
+    'author': 'hmprt',
+    'author_email': 'hmprt@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.0,<4.0',
+}
+
+
+setup(**setup_kwargs)
