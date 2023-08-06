ceb02bf5f1b63f754759e953123bebbed9118126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:49 +0000

    Add package egter/0.2/egter-0.2.tar.gz

---
+++ packages/egter/egter-0.2.tar.gz/egter-0.2/egter/__init__.py
@@ -0,0 +1,8 @@
+version = '0.2'
+
+"""
+Modules:
+crypt
+crypt.defaults
+customs
+"""
---
+++ packages/egter/egter-0.2.tar.gz/egter-0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='egter',
+    version='0.2',
+    packages=['egter'],
+    url='https://notabug.org/EgTer/egter-py',
+    license='GNU GPL v3',
+    author='EgTer',
+    author_email='annom2017@mail.ru',
+    description='My collection of my tools'
+)
