42c7e69602762d65a1e99ce73cf398aeb5b4c87a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:00:49 +0000

    Add package myrich/0.1.0/myrich-0.1.0-py3-none-any.whl

---
+++ packages/myrich/myrich-0.1.0-py3-none-any.whl/myrich/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Shell-like using Rich for render rich text content
+# coding: utf-8
+
+__package_name__ = "myrich"
+__description__ = "Shell-like using Rich for render rich text content"
+from ._version import version_info, __version__
+
+from rich import get_console
+
+
+__all__ = ["__package_name__", "version_info", "__version__", "get_console"]
