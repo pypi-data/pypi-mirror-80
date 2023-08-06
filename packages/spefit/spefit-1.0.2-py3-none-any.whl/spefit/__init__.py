76ced3869116632dbd11d59a6ff3ac6b4ac53944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:28 +0000

    Add package spefit/1.0.2/spefit-1.0.2-py3-none-any.whl

---
+++ packages/spefit/spefit-1.0.2-py3-none-any.whl/spefit/__init__.py
@@ -0,0 +1,29 @@
+"""spefit - Fitting of Single Photoelectron Spectra."""
+from .container import ChargeContainer
+from .cost import Cost, UnbinnedNLL, BinnedNLL, LeastSquares
+from .pdf import (
+    PDF,
+    PDFParameter,
+    PMTSingleGaussian,
+    SiPMModifiedPoisson,
+    SiPMGentile,
+)
+from .fitter import minimize_with_iminuit, CameraFitter
+
+__all__ = [
+    "ChargeContainer",
+    "Cost",
+    "UnbinnedNLL",
+    "BinnedNLL",
+    "LeastSquares",
+    "PDF",
+    "PDFParameter",
+    "PMTSingleGaussian",
+    "SiPMModifiedPoisson",
+    "SiPMGentile",
+    "minimize_with_iminuit",
+    "CameraFitter",
+]
+
+
+__version__ = "1.0.2"
---
+++ packages/spefit/spefit-1.0.2-py3-none-any.whl/spefit/common/__init__.py
@@ -0,0 +1,1 @@
+"""Common mathematical Formulae, vectorized with numba"""
