9227c1a9b741cd20bb19577db86573faefd3ef6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:05 +0000

    Add package raytraverse/0.2.2/raytraverse-0.2.2.tar.gz

---
+++ packages/raytraverse/raytraverse-0.2.2.tar.gz/raytraverse-0.2.2/release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clean=$(git status --porcelain --untracked-files=no | wc -l)
+if [ "$clean" -lt 1 ]; then
+    if [[ $# == 1 && ($1 == "patch" || $1 == "minor" || $1 == "major") ]]; then
+        git checkout release
+        git merge master
+        bumpversion --tag --commit "$1"
+        make dist
+        make coveralls
+        twine upload dist/*.tar.gz
+        git push --follow-tags
+        git checkout master
+        git merge release
+        git push --follow-tags
+    else
+        echo usage: ./release.sh "[patch/minor/major]"
+    fi
+else
+    echo working directory is not clean!
+    git status --porcelain --untracked-files=no
+fi
---
+++ packages/raytraverse/raytraverse-0.2.2.tar.gz/raytraverse-0.2.2/src/Version.c
@@ -0,0 +1,1 @@
+char VersionID[]="RADIANCE 5.4a 2020-09-25 LBNL (..)";
