e109a0f81d7d1d749d02ff661ea5553e63c46c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:31 +0000

    Add package globus-search-cli/0.5.0/globus_search_cli-0.5.0-py3-none-any.whl

---
+++ packages/globus-search-cli/globus_search_cli-0.5.0-py3-none-any.whl/globus_search_cli/printing.py
@@ -0,0 +1,19 @@
+import json
+import sys
+
+
+def safeprint(s):
+    try:
+        print(s)
+        sys.stdout.flush()
+    except IOError:
+        pass
+
+
+def format_output(dataobject):
+    if isinstance(dataobject, str):
+        safeprint(dataobject)
+    else:
+        safeprint(
+            json.dumps(dataobject, indent=2, separators=(",", ": "), sort_keys=True)
+        )
