a9068600c60b30c400c08f485ae883b3ffe79112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:02 +0000

    Add package django-vendor/0.1.11/django_vendor-0.1.11-py3-none-any.whl

---
+++ packages/django-vendor/django_vendor-0.1.11-py3-none-any.whl/vendor/models/utils.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+from django.contrib.sites.models import Site
+
+###########
+# UTILITIES
+###########
+
+def random_string(length=8, check=[]):
+    letters= string.digits + string.ascii_uppercase
+    value = ''.join(random.sample(letters,length))
+
+    if value not in check:
+        return value
+    return random_string(length=length, check=check)
+
+def generate_sku():
+    return random_string()
+
+def set_default_site_id():
+    return Site.objects.get_current()
