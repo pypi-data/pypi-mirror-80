663a4080064e67aef41a41b0febafa3fc5078932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:14 +0000

    Add package restart/2.6.1/restart-2.6.1.tar.gz

---
+++ packages/restart/restart-2.6.1.tar.gz/restart-2.6.1/setup.py
@@ -0,0 +1,26 @@
+"""Setuptools info for the model pip package."""
+
+import setuptools  # type: ignore # noqa:
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="restart",
+    version="2.6.1",
+    # this script doesn't exist
+    # scripts=["model"],
+    author="Restart Partners",
+    author_email="lucas@restart.us",  # need a valid email here
+    description="COVID-19 decision model tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/restartus/restart",
+    packages=setuptools.find_packages(include=["restart"]),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
