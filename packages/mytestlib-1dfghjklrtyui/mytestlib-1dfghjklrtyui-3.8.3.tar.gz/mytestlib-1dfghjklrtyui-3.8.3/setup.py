09feca65fc14865aa5ecaf4e8cacc407ced7dc49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:42 +0000

    Add package mytestlib-1dfghjklrtyui/3.8.3/mytestlib-1dfghjklrtyui-3.8.3.tar.gz

---
+++ packages/mytestlib-1dfghjklrtyui/mytestlib-1dfghjklrtyui-3.8.3.tar.gz/mytestlib-1dfghjklrtyui-3.8.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-1dfghjklrtyui',
+    version='3.8.3',
+    url='https://pypi.org/manage/projects/',
+    author='e-ootuki',
+    author_email='e-ootuki@senjusoft.co.jp',
+    description='My Test Library.',
+    packages=find_packages()
+)
