e25f154a9c7e4b2df661fc7b6802c8ecd1dfe5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:11 +0000

    Add package django-spaghetti-and-meatballs/0.3.1/django_spaghetti_and_meatballs-0.3.1-py3-none-any.whl

---
+++ packages/django-spaghetti-and-meatballs/django_spaghetti_and_meatballs-0.3.1-py3-none-any.whl/django_spaghetti/tests/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import url, include
+from django_spaghetti.views import plate, Plate
+
+urlpatterns = [
+    url(r'^test/plate_settings$', Plate.as_view(
+        settings={
+            'apps': ['auth'],
+            'exclude': {},
+        }
+    ), name='test_plate_settings'),
+    url(r'^test/plate_show_m2m_field_detail$', Plate.as_view(
+        override_settings={
+            'show_m2m_field_detail': True
+        }
+    ), name='test_plate_show_m2m_field_detail'),
+    url(r'^test/plate_override$', Plate.as_view(
+        override_settings={
+            'exclude': {
+                'tests': ['policestation']
+            },
+        },
+        meatball_template_name="tests/meatball.html"
+    ), name='test_plate_override$'),
+    url(r'plate', include('django_spaghetti.urls', namespace="spaghetti")),
+]
