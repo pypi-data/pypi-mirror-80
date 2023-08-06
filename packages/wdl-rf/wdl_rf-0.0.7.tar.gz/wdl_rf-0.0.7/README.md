e5dd17d8cb8457bbe0dc28a05aefb5ead29a76b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:06 +0000

    Add package wdl-rf/0.0.7/wdl_rf-0.0.7.tar.gz

---
+++ packages/wdl-rf/wdl_rf-0.0.7.tar.gz/wdl_rf-0.0.7/README.md
@@ -0,0 +1,3 @@
+# wdl_rf  
+A two-stage algorithm WDL-RF allows end-to-end learning of prediction pipelines whose inputs are of arbitrary size, which contains the molecular fingerprint generation stage through a new weighted deep learning method and the bioactivity prediction stage by the random forest model.   
+
---
+++ packages/wdl-rf/wdl_rf-0.0.7.tar.gz/wdl_rf-0.0.7/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wdl_rf", # Replace with your own username
+    version="0.0.7",
+    author="xhj,zqm",
+    description="Create molecular fingerprint features of Ligands and predicting Bioactivities Acting with G Protein-coupled Receptors",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
