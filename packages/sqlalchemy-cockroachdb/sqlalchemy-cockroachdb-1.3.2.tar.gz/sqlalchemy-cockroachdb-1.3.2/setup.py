dea46ca5b95b8b1eacf63d7ef49e19046f0066b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:54 +0000

    Add package sqlalchemy-cockroachdb/1.3.2/sqlalchemy-cockroachdb-1.3.2.tar.gz

---
+++ packages/sqlalchemy-cockroachdb/sqlalchemy-cockroachdb-1.3.2.tar.gz/sqlalchemy-cockroachdb-1.3.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sqlalchemy-cockroachdb',
+    version='1.3.2',
+    author='Cockroach Labs',
+    author_email='cockroach-db@googlegroups.com',
+    url='https://github.com/cockroachdb/sqlalchemy-cockroachdb',
+    description='CockroachDB adapter for SQLAlchemy',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        ],
+
+    packages=['cockroachdb', 'cockroachdb.sqlalchemy'],
+    entry_points={
+        'sqlalchemy.dialects': [
+            'cockroachdb = cockroachdb.sqlalchemy.dialect:CockroachDBDialect',
+        ],
+    },
+)
