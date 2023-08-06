ab3f1e5b6e5926d150248e461e30e74215506a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:48 +0000

    Add package zaber-motion/1.6.1/zaber_motion-1.6.1.tar.gz

---
+++ packages/zaber-motion/zaber_motion-1.6.1.tar.gz/zaber_motion-1.6.1/zaber_motion/exceptions/invalid_packet_exception.py
@@ -0,0 +1,10 @@
+﻿# ===== THIS FILE IS GENERATED FROM A TEMPLATE ===== #
+# ============== DO NOT EDIT DIRECTLY ============== #
+
+from .motion_lib_exception import MotionLibException
+
+
+class InvalidPacketException(MotionLibException):
+    """
+    Thrown when a packet from a device cannot be parsed.
+    """
