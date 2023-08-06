8d1a91f7599a00784abfa05079ee1d6f70316cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:13 +0000

    Add package bashrc/0.31.2/bashrc-0.31.2.tar.gz

---
+++ packages/bashrc/bashrc-0.31.2.tar.gz/bashrc-0.31.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The setup script."""
+from setuptools import setup, find_packages
+
+# noinspection PyUnresolvedReferences
+from bashrc import package, project, scripts_relative, readme, description, install_requires, GITHUB_EMAIL, \
+    REALNAME, GITHUB_URL
+
+setup(
+    author=REALNAME,
+    author_email=GITHUB_EMAIL,
+    description=description,
+    entry_points={
+        'console_scripts': [
+            f'rc = {package.name}:app',
+        ],
+    },
+    include_package_data=True,
+    install_requires=install_requires,
+    name=package.name,
+    package_data={
+        project.name: [f'{project.name}/scripts/*'],
+    },
+    packages=find_packages(include=[package.name]),
+    scripts=scripts_relative,
+    setup_requires=[],
+    tests_require=[],
+    url=f'{GITHUB_URL}/',
+    version='0.31.2',
+    zip_safe=False,
+)
