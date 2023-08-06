e692530b246a322122df442573277d3f5217bbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:15 +0000

    Add package musicbot/0.7.4/musicbot-0.7.4-py3-none-any.whl

---
+++ packages/musicbot/musicbot-0.7.4-py3-none-any.whl/musicbot/schema/create.sql
@@ -0,0 +1,8 @@
+\i ./schemas.sql
+\i ./extensions.sql
+\i ./user.sql
+\i ./raw_music.sql
+\i ./filter.sql
+\i ./playlist.sql
+\i ./stat.sql
+\i ./grants.sql
---
+++ packages/musicbot/musicbot-0.7.4-py3-none-any.whl/musicbot/schema/schemas.sql
@@ -0,0 +1,2 @@
+create schema if not exists musicbot_public;
+create schema if not exists musicbot_private;
