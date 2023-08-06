d263b41b8343083f7efd87b82f6a31c217969f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:04 +0000

    Add package exceptiom/1.1.0/exceptiom-1.1.0.tar.gz

---
+++ packages/exceptiom/exceptiom-1.1.0.tar.gz/exceptiom-1.1.0/exceptiom/exceptiom.py
@@ -0,0 +1,11 @@
+from jokes_en import jokes
+import random
+
+
+class Exceptiom(Exception):
+    def __init__(self, *args, **kwargs):
+        length_jokes = len(jokes)
+        joke_to_pick = random.randint(0, length_jokes-1)
+        selected_jokes = jokes[joke_to_pick]
+        Exception.__init__(self, selected_jokes, args, kwargs)
+
