6d4c74f9e7b914405b54e271b87d0e626b7fbb06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:25 +0000

    Add package focal/0.0.2/focal-0.0.2-py3-none-any.whl

---
+++ packages/focal/focal-0.0.2-py3-none-any.whl/focal/__init__.py
@@ -0,0 +1,10 @@
+from focal.filesys import FileCollection, FileBytesReader
+
+from focal.stores.local_store import (
+    QuickBinaryStore,
+    QuickTextStore,
+    QuickJsonStore,
+    QuickPickleStore,
+    QuickStore,
+    DirStore
+)
