cf5acf52646a9a6fcd5d2d07c73dfe8c57b6772f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:28 +0000

    Add package audiomentations/0.12.0/audiomentations-0.12.0-py3-none-any.whl

---
+++ packages/audiomentations/audiomentations-0.12.0-py3-none-any.whl/audiomentations/__init__.py
@@ -0,0 +1,22 @@
+from .augmentations.transforms import (
+    AddImpulseResponse,
+    FrequencyMask,
+    TimeMask,
+    AddGaussianSNR,
+    AddGaussianNoise,
+    TimeStretch,
+    PitchShift,
+    Shift,
+    Normalize,
+    Trim,
+    Resample,
+    ClippingDistortion,
+    AddBackgroundNoise,
+    AddShortNoises,
+    PolarityInversion,
+    Gain,
+    Mp3Compression,
+)
+from .core.composition import Compose
+
+__version__ = "0.12.0"
