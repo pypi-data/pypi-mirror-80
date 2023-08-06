2d83575b54a3081c1065f923219cd4ed24e40fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:12 +0000

    Add package dask/2.28.0/dask-2.28.0.tar.gz

---
+++ packages/dask/dask-2.28.0.tar.gz/dask-2.28.0/dask/tests/test_highgraph.py
@@ -0,0 +1,59 @@
+import os
+
+import pytest
+
+import dask.array as da
+from dask.utils_test import inc
+from dask.highlevelgraph import HighLevelGraph, BasicLayer
+
+
+def test_visualize(tmpdir):
+    pytest.importorskip("graphviz")
+    fn = str(tmpdir)
+    a = da.ones(10, chunks=(5,))
+    b = a + 1
+    c = a + 2
+    d = b + c
+    d.dask.visualize(fn)
+    assert os.path.exists(fn)
+
+
+def test_basic():
+    a = {"x": 1}
+    b = {"y": (inc, "x")}
+    layers = {"a": a, "b": b}
+    dependencies = {"a": set(), "b": {"a"}}
+    hg = HighLevelGraph(layers, dependencies)
+
+    assert dict(hg) == {"x": 1, "y": (inc, "x")}
+
+
+def test_keys_values_items_methods():
+    a = da.ones(10, chunks=(5,))
+    b = a + 1
+    c = a + 2
+    d = b + c
+    hg = d.dask
+
+    keys, values, items = hg.keys(), hg.values(), hg.items()
+    assert all(isinstance(i, list) for i in [keys, values, items])
+    assert keys == [i for i in hg]
+    assert values == [hg[i] for i in hg]
+    assert items == [(k, v) for k, v in zip(keys, values)]
+
+
+def test_cull():
+    a = {"x": 1, "y": (inc, "x")}
+    layers = {
+        "a": BasicLayer(
+            a, dependencies={"x": set(), "y": {"x"}}, global_dependencies=set()
+        )
+    }
+    dependencies = {"a": set()}
+    hg = HighLevelGraph(layers, dependencies)
+
+    culled_by_x = hg.cull({"x"})
+    assert dict(culled_by_x) == {"x": 1}
+
+    culled_by_y = hg.cull({"y"})
+    assert dict(culled_by_y) == a
