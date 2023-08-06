ab061e570f3d2ff9560fa82b4ab2da95c19d055e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:47 +0000

    Add package pyMapVBVD/0.4.0/pyMapVBVD-0.4.0.tar.gz

---
+++ packages/pyMapVBVD/pyMapVBVD-0.4.0.tar.gz/pyMapVBVD-0.4.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import setuptools
+import versioneer
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'rt') as f:
+    install_requires = [l.strip() for l in f.readlines()]
+
+
+setuptools.setup(name='pyMapVBVD',
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        description='Python twix file reader',
+        author='Will Clarke',
+        author_email='william.clarke@ndcn.ox.ac.uk',
+        url='https://github.com/wexeee/pymapvbvd',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        packages=setuptools.find_packages(),
+        install_requires=install_requires,
+        license_file='LICENSE',
+        classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.7',         
+     )
