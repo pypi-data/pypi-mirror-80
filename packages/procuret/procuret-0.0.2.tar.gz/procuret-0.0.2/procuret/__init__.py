88d1f95e8fd0204f15bb8f9b7a2cecefac96dff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:47 +0000

    Add package procuret/0.0.2/procuret-0.0.2.tar.gz

---
+++ packages/procuret/procuret-0.0.2.tar.gz/procuret-0.0.2/procuret/__init__.py
@@ -0,0 +1,4 @@
+from procuret.session import Session, Lifecycle, Perspective
+from procuret.instalment_link import InstalmentLink, CommunicationOption
+from procuret.ancillary.entity_headline import EntityHeadline
+from procuret import errors
