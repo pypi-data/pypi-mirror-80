d2ad47d70f1285d87d9845dfa9779b98e7859449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:27 +0000

    Add package write_patch/0.1/write_patch-0.1.tar.gz

---
+++ packages/write_patch/write_patch-0.1.tar.gz/write_patch-0.1/setup.py
@@ -0,0 +1,12 @@
+﻿from setuptools import setup
+
+setup(name='write_patch',
+      version='0.1',
+      description='This is the write patch module for the turtle module, so turtle can write text with angle argument.配合Python海龟画图写字补丁模块，这样就能让海龟写斜着的字了',
+      url='https://github.com/lixingqiu/write_patch',
+      author='lixingqiu',
+      author_email='406273900@qq.com',
+      license='MIT',
+      packages=['write_patch'],
+      zip_safe=False)
+
