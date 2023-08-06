6406f796d654b1db9af02ff8047a5bd89048da8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:15 +0000

    Add package PriceDb/2.10.5/PriceDb-2.10.5.tar.gz

---
+++ packages/PriceDb/PriceDb-2.10.5.tar.gz/PriceDb-2.10.5/pricedb/dal.py
@@ -0,0 +1,104 @@
+"""
+Data layer
+"""
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+Base = declarative_base()
+
+
+class Price(Base):
+    """ Price entity """
+    __tablename__ = 'price'
+
+    id = Column(Integer, primary_key=True)
+    security_id = Column(Integer, ForeignKey('security.id'))
+    # namespace = Column(String)
+    # symbol = Column(String)
+
+    date = Column(String)
+    time = Column(String)
+    value = Column(Integer)
+    denom = Column(Integer)
+    currency = Column(String)
+
+    # https://docs.sqlalchemy.org/en/13/orm/basic_relationships.html#one-to-many
+    security = relationship('Security', back_populates='prices')
+
+    def __repr__(self):
+        actual_value = 0
+        if self.denom:
+            actual_value = self.value / self.denom
+        actual_value_str = f"{actual_value:.2f}"
+
+        formatted_symbol = ""
+        if self.security:
+            symbol = f"{self.security.namespace}:{self.security.symbol}" if self.security.namespace else self.security.symbol
+            if symbol is None:
+                symbol = ""
+            formatted_symbol = f"{symbol:<13}"
+
+        return f"<Price ({formatted_symbol},{self.date} {self.time},{actual_value_str:>6} {self.currency})>"
+
+
+class Security(Base):
+    '''
+    The security / symbol entity
+    '''
+    __tablename__ = "security"
+
+    id = Column(Integer, primary_key=True)
+    symbol = Column(String, unique=True)
+    namespace = Column(String)
+    updater = Column(String)
+    currency = Column(String)
+    ledger_symbol = Column(String)
+
+    prices = relationship('Price')
+
+    def __repr__(self):
+        return f"<Security ({self.id}, {self.namespace}:{self.symbol})>"
+
+
+def get_default_session():
+    '''
+    Return the default database session. The db file path is read from the config.
+    Implementation using the Yaml config.
+    '''
+    from pricedb.config import package_name
+    from usersconfig.configuration import Configuration
+    from .config import ConfigKeys
+
+    cfg = Configuration(package_name).load()
+    # print(cfg)
+    
+    db_path = cfg[ConfigKeys.price_database_path.name]
+    if not db_path:
+        raise ValueError("Price database not set in the configuration file!")
+    return get_session(db_path)
+
+# def get_default_session():
+#     """ Return the default session. The path is read from the default config. """
+#     from .config import Config, ConfigKeys
+
+#     db_path = Config().get(ConfigKeys.price_database_path)
+#     if not db_path:
+#         raise ValueError("Price database not set in the configuration file!")
+#     return get_session(db_path)
+
+def get_session(db_path: str):
+    """ Creates and opens a database session """
+    # connection
+    con_str = "sqlite:///" + db_path
+    # Display all SQLite info with echo.
+    engine = create_engine(con_str, echo=False)
+
+    # create metadata (?)
+    Base.metadata.create_all(engine)
+
+    # create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    return session
---
+++ packages/PriceDb/PriceDb-2.10.5.tar.gz/PriceDb-2.10.5/pricedb/mappers.py
@@ -0,0 +1,76 @@
+""" Mapping entities to domain model objects """
+from datetime import datetime
+from decimal import Decimal
+from pydatum import Datum
+from . import dal
+from .dal import Price
+from .model import PriceModel, SecuritySymbol
+
+
+class PriceMapper:
+    """ Map price entity """
+    def __init__(self):
+        pass
+
+    def map_entity(self, entity: dal.Price) -> PriceModel:
+        """ Map the price entity """
+        if not entity:
+            return None
+
+        result = PriceModel()
+        result.currency = entity.currency
+
+        # date/time
+        dt_string = entity.date
+        format_string = "%Y-%m-%d"
+        if entity.time:
+            dt_string += f"T{entity.time}"
+            format_string += "T%H:%M:%S"
+        price_datetime = datetime.strptime(dt_string, format_string)
+        result.datum = Datum()
+        result.datum.from_datetime(price_datetime)
+        assert isinstance(result.datum, Datum)
+
+        # result.symbol = SecuritySymbol(entity.security.namespace, entity.security.symbol)
+        mnemonic = entity.security.ledger_symbol if entity.security.ledger_symbol else entity.security.symbol
+        result.symbol = SecuritySymbol(entity.security.namespace, mnemonic)
+
+        # Value
+        value = Decimal(entity.value) / Decimal(entity.denom)
+        result.value = Decimal(value)
+
+        return result
+
+    def map_model(self, model: PriceModel) -> Price:
+        """ Parse into the Price entity, ready for saving """
+        # assert isinstance(model, PriceModel)
+        assert isinstance(model.symbol, SecuritySymbol)
+        assert isinstance(model.datum, Datum)
+
+        entity = Price()
+
+        # Format date as ISO string
+        date_iso = f"{model.datum.value.year}-{model.datum.value.month:02d}-{model.datum.value.day:02d}"
+        entity.date = date_iso
+
+        entity.time = f"{model.datum.value.hour:02d}:{model.datum.value.minute:02d}:{model.datum.value.second:02d}"
+
+        # Symbol
+        entity.security_id = model.security_id
+        # properly mapped symbols have a namespace, except for the US markets
+        # if model.symbol.namespace:
+        #     entity.namespace = model.symbol.namespace.upper()
+        # entity.symbol = model.symbol.mnemonic.upper()
+
+        assert isinstance(model.value, Decimal)
+        # Find number of decimal places
+        dec_places = abs(model.value.as_tuple().exponent)
+        entity.denom = 10 ** dec_places
+        # Price value
+        entity.value = int(model.value * entity.denom)
+
+        # Currency
+        entity.currency = model.currency.upper()
+
+        # self.logger.debug(f"{entity}")
+        return entity
