ba950992354b71dcf151e42fca23ff9b6332576d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:23 +0000

    Add package mytestlib-ts/1.0.0/mytestlib-ts-1.0.0.tar.gz

---
+++ packages/mytestlib-ts/mytestlib-ts-1.0.0.tar.gz/mytestlib-ts-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-ts',
+    version='1.0.0',
+    url='https://pypi.org/manage/projects/',
+    author='SeYaTa',
+    author_email='u84bc8fed7jvgE05SE@softbank.ne.jp',
+    description='My Test Library.',
+    packages=find_packages()
+)
