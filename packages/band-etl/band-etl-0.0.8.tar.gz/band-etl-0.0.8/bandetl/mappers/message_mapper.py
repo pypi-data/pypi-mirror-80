c84c9ed85f5ea7bc69bb3442e84183cd0bc3870f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:41 +0000

    Add package band-etl/0.0.8/band-etl-0.0.8.tar.gz

---
+++ packages/band-etl/band-etl-0.0.8.tar.gz/band-etl-0.0.8/bandetl/mappers/message_mapper.py
@@ -0,0 +1,23 @@
+from bandetl.utils.string_utils import json_dumps
+
+
+def map_messages(block, tx):
+    for index, raw_msg in enumerate(tx.get('tx', {}).get('value', {}).get('msg', [])):
+        message_type = raw_msg.get('type')
+        normalized_message_type = normalize_message_type(message_type)
+
+        yield {
+            'type': 'message',
+            'block_height': block.get('block_height'),
+            'block_timestamp': block.get('block_timestamp'),
+            'block_timestamp_truncated': block.get('block_timestamp_truncated'),
+            'txhash': tx.get('txhash'),
+            'message_type': message_type,
+            'normalized_message_type': normalized_message_type,
+            normalized_message_type: raw_msg.get('value'),
+            'index': index,
+        }
+
+
+def normalize_message_type(message_type):
+    return message_type.replace('-', '_').replace('/', '_')
