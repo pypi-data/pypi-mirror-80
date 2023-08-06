41ac91f1a7285fd6bb257681af0a74ea762abd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:14 +0000

    Add package kltpicker/1.2.2/kltpicker-1.2.2.tar.gz

---
+++ packages/kltpicker/kltpicker-1.2.2.tar.gz/kltpicker-1.2.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='kltpicker',
+      version='1.2.2',
+      description='KLT picker',
+      url='http://github.com/dalitco54/kltpick',
+      author='Dalit Cohen',
+      author_email='dalitcohen@mail.tau.ac.il',
+      packages=find_packages(),
+      license='GNU General Public License v3.0',
+      entry_points = {
+        "console_scripts": ['kltpicker = kltpicker.main:main']
+        },
+      install_requires=[
+          'numpy>=1.16',
+          'mrcfile',
+          'scipy>=1.3',
+          'pyfftw',
+          'progressbar2',
+          'numba'
+      ],
+      extras_require={'gpu':['cupy']},
+      python_requires='~=3.6',
+      zip_safe=False)
