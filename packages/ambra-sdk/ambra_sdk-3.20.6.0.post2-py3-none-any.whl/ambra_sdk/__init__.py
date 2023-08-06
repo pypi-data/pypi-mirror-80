9920898ac67ede2a5036bee5384cad9ed75fbcb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:46 +0000

    Add package ambra-sdk/3.20.6.0.post2/ambra_sdk-3.20.6.0.post2-py3-none-any.whl

---
+++ packages/ambra-sdk/ambra_sdk-3.20.6.0.post2-py3-none-any.whl/ambra_sdk/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '3.20.6.0-2'
+
+# from footer in api.html
+API_VERSION = 'LBL0022 v40.0 2020-08-26'
+# equal for API_VERSION based on dictionary.html
+MODELS_VERSION = 'LBL0022 v40.0 2020-08-26'
+# from storage_api.html
+STORAGE_VERSION = 'LBL0038 v10.0 2020-08-26'
