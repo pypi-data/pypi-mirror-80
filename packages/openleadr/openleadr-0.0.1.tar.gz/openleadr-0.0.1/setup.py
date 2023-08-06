7fe481aae5714ad9800eeb736f0d4a71f2b6baac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:57 +0000

    Add package openleadr/0.0.1/openleadr-0.0.1.tar.gz

---
+++ packages/openleadr/openleadr-0.0.1.tar.gz/openleadr-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='openleadr',
+    version='0.0.1',
+    description='OpenADR client and server for Python',
+    long_description='OpenLEADR is a friendly and compliant OpenADR implementation for Python.',
+    url='https://openleadr.elaad.io',
+    author='Stan Janssen',
+    author_email='openleadr@finetuned.nl',
+    packages=['openleadr'],
+    classifiers=['Development Status :: 1 - Planning'],
+)
