ac977d49dc2193de129c9aeeee038a967ca3a583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:00 +0000

    Add package rollbar-agent/0.4.4/rollbar-agent-0.4.4.tar.gz

---
+++ packages/rollbar-agent/rollbar-agent-0.4.4.tar.gz/rollbar-agent-0.4.4/setup.py
@@ -0,0 +1,44 @@
+import os.path
+from setuptools import Command, find_packages, setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+README_PATH = os.path.join(HERE, 'README.rst')
+try:
+    README = open(README_PATH).read()
+except IOError:
+    README = ''
+
+setup(
+    name='rollbar-agent',
+    data_files=[('', ['rollbar-agent', 'rollbar-agent-init.sh', 'rollbar-agent.conf', 'LICENSE', 'requirements.txt'])],
+    version='0.4.4',
+    description='Rollbar server-side agent',
+    long_description=README,
+    author='Rollbar, Inc.',
+    author_email='support@rollbar.com',
+    url='http://github.com/rollbar/rollbar-agent',
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Bug Tracking",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Quality Assurance",
+        ],
+    install_requires=[
+        'requests',
+        ],
+    )
+
+
+
+
+
+
+
