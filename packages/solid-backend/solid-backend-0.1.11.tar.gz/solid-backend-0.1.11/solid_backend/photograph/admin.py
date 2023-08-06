2bfbea753f9c0225fad82939b7d70b2de677014b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:31 +0000

    Add package solid-backend/0.1.11/solid-backend-0.1.11.tar.gz

---
+++ packages/solid-backend/solid-backend-0.1.11.tar.gz/solid-backend-0.1.11/solid_backend/photograph/admin.py
@@ -0,0 +1,57 @@
+from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+
+from .forms import PhotographForm
+from .models import Photograph
+
+fields = [
+    ("img", "dzi_option"),
+    "img_alt",
+    "description",
+    "dzi_file",
+    "img_original_width",
+    "img_original_height",
+    ("length_value", "length_unit", "pixel_number"),
+    "img_original_scale",
+    "audio",
+    "audio_duration",
+    "date",
+    "author",
+    "license",
+]
+readonly_fields = [
+    "dzi_file",
+    "img_original_width",
+    "img_original_height",
+    "img_original_scale",
+    "audio_duration",
+]
+
+
+class DeepZoomAdmin(admin.ModelAdmin):
+    def delete_queryset(self, request, queryset):
+        """
+        Call DeepZoom model delete method for each object during bulk delete via admin
+        action to ensure that the Deep Zoom directory trees will be deleted.
+        """
+
+        for object in queryset:
+            object.delete()
+
+
+class PhotographInline(GenericTabularInline):
+    model = Photograph
+    form = PhotographForm
+    extra = 1
+    fields = fields
+    readonly_fields = readonly_fields
+
+
+class PhotographAdmin(DeepZoomAdmin):
+    form = PhotographForm
+    fields = fields
+    readonly_fields = readonly_fields
+    list_display = ["id", "img", "dzi_option", "author", "license"]
+
+
+admin.site.register(Photograph, PhotographAdmin)
---
+++ packages/solid-backend/solid-backend-0.1.11.tar.gz/solid-backend-0.1.11/solid_backend/slideshow/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+from .models import Slideshow, SlideshowImage, SlideshowPage
+
+
+class SlideshowPageInline(admin.TabularInline):
+    model = SlideshowPage
+    extra = 1
+
+
+class SlideshowImageInline(admin.TabularInline):
+    model = SlideshowImage
+    extra = 1
+
+
+class SlideshowAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "img"]
+    inlines = [SlideshowPageInline]
+
+
+admin.site.register(Slideshow, SlideshowAdmin)
+
+
+class SlideshowPageAdmin(admin.ModelAdmin):
+    list_display = ["id", "show", "position", "title"]
+    inlines = [SlideshowImageInline]
+
+
+admin.site.register(SlideshowPage, SlideshowPageAdmin)
+
+
+class SlideshowImageAdmin(admin.ModelAdmin):
+    list_display = ["id", "page", "position", "title", "img"]
+
+
+admin.site.register(SlideshowImage, SlideshowImageAdmin)
