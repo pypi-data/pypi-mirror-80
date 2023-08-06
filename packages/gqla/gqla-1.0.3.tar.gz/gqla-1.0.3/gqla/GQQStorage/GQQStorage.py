0c007062071e28095def45b5ba0c472e07632dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:11 +0000

    Add package gqla/1.0.3/gqla-1.0.3.tar.gz

---
+++ packages/gqla/gqla-1.0.3.tar.gz/gqla-1.0.3/gqla/GQQStorage/GQQStorage.py
@@ -0,0 +1,34 @@
+from gqla.GQQuery import BasicQuery
+from gqla.GQQuery import BasicQueryGenerator, NormalRule, RecursiveRule
+from gqla.abstracts import AbstractQuery, AbstractStorage
+
+
+class BasicStorage(AbstractStorage):
+
+    def __init__(self, properties=None, generator=BasicQueryGenerator(NormalRule(), RecursiveRule())):
+        super().__init__()
+        self._query = {}
+        self.generator = generator
+        self._properties = properties
+        self.generator.properties = properties
+
+    @property
+    def properties(self):
+        return self._properties
+
+    @properties.setter
+    def properties(self, value):
+        self._properties = value
+        self.generator.properties = value
+
+    @property
+    def storage(self):
+        return self._query
+
+    def add(self, name: str, query: AbstractQuery):
+        self._query[name] = query
+
+    def create(self, alias, name, item, recursive_depth, only_fields=False):
+        query = BasicQuery(name, self.generator, item)
+        query.regenerate(only_fields)
+        self.add(alias, query)
