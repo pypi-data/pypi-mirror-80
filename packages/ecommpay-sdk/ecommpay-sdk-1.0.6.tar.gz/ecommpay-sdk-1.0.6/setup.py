ae7552964dd2d673e986d5b5d067866f59d09c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:43 +0000

    Add package ecommpay-sdk/1.0.6/ecommpay-sdk-1.0.6.tar.gz

---
+++ packages/ecommpay-sdk/ecommpay-sdk-1.0.6.tar.gz/ecommpay-sdk-1.0.6/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+import pypandoc
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+# README = pypandoc.convert_file("./README.md", "rst").replace("\r","")
+
+setup(
+    name='ecommpay-sdk',
+    version='1.0.6',
+    url='https://github.com/ITECOMMPAY/paymentpage-sdk-python',
+    license='MIT',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
