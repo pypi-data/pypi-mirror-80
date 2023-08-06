a95385905b2be217617787b6807ef0c84fe9bc78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:51 +0000

    Add package ladybug-grasshopper-dotnet/1.0.1/ladybug-grasshopper-dotnet-1.0.1.tar.gz

---
+++ packages/ladybug-grasshopper-dotnet/ladybug-grasshopper-dotnet-1.0.1.tar.gz/ladybug-grasshopper-dotnet-1.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ladybug-grasshopper-dotnet",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Ladybug Tools",
+    author_email="info@ladybug.tools",
+    description="GHA files for the Ladybug Grasshopper Plugin.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ladybug-tools/ladybug-grasshopper-dotnet",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent"
+    ],
+    license="AGPL-3.0"
+)
