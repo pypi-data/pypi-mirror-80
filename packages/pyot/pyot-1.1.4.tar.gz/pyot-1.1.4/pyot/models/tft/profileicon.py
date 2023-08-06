ac677a7f565b6c475053bc3a6d6165edb5ce3bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:04 +0000

    Add package pyot/1.1.4/pyot-1.1.4.tar.gz

---
+++ packages/pyot/pyot-1.1.4.tar.gz/pyot-1.1.4/pyot/models/tft/profileicon.py
@@ -0,0 +1,58 @@
+from .__core__ import PyotCore
+from pyot.utils import tft_url
+from pyot.core.exceptions import NotFound
+from typing import List, Iterator
+
+
+class ProfileIcon(PyotCore):
+    id: int
+    icon_path: str
+
+    class Meta(PyotCore.Meta):
+        rules = {"cdragon_profile_icon_full": ["id"]}
+
+    def __init__(self, id: int = None, locale: str = None):
+        self._lazy_set(locals())
+
+    def filter(self, data):
+        for item in data:
+            if item["id"] == self.id:
+                return item
+        raise NotFound
+    
+    def _refactor(self):
+        if self.locale.lower() == "en_us":
+            self.meta.server = "default"
+        load = getattr(self.meta, "load")
+        load.pop("id")
+
+    def _transform(self, data):
+        data["iconPath"] = tft_url(data["iconPath"])
+        return data
+
+
+class ProfileIcons(PyotCore):
+    icons: List[ProfileIcon]
+    
+    class Meta(PyotCore.Meta):
+        rules = {"cdragon_profile_icon_full": []}
+
+    def __init__(self, locale: str = None):
+        self._lazy_set(locals())
+
+    def __getitem__(self, item):
+        return self.icons[item]
+
+    def __iter__(self) -> Iterator[ProfileIcon]:
+        return iter(self.icons)
+
+    def _refactor(self):
+        if self.locale.lower() == "en_us":
+            self.meta.server = "default"
+
+    def _transform(self, data_):
+        icons = []
+        for data in data_:
+            data["iconPath"] = tft_url(data["iconPath"])
+            icons.append({"data": data})
+        return {"icons": icons}
---
+++ packages/pyot/pyot-1.1.4.tar.gz/pyot-1.1.4/pyot/stores/__init__.py
@@ -0,0 +1,10 @@
+try:
+    from .djangocache import DjangoCache
+except ImportError:
+    pass
+from .riotapi import RiotAPI
+from .omnistone import Omnistone
+from .rediscache import RedisCache
+from .diskcache import DiskCache
+from .merakicdn import MerakiCDN
+from .cdragon import CDragon
