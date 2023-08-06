783f3de6d281fa38dda45b38870de0e4e2d4ee03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:06 +0000

    Add package cognito-assume-role/0.0.15/cognito-assume-role-0.0.15.tar.gz

---
+++ packages/cognito-assume-role/cognito-assume-role-0.0.15.tar.gz/cognito-assume-role-0.0.15/setup.py
@@ -0,0 +1,25 @@
+import io
+from setuptools import setup
+
+
+setup(
+    name="cognito-assume-role",
+    version="0.0.15",
+    description="Assumes an IAM role in boto3 using Cognito credentials",
+    author="Mathew Moon",
+    author_email="mmoon@quinovas.com",
+    url="https://github.com/QuiNovas/cognito-assume-role",
+    license="Apache 2.0",
+    long_description=io.open("README.rst", encoding="utf-8").read(),
+    long_description_content_type="text/x-rst",
+    packages=["cognito_assume_role"],
+    package_dir={"cognito_assume_role": "src/cognito_assume_role"},
+    install_requires=["boto3", "botocore", "warrant"],
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
