9e6e5cf7b2da736d33c7b5ef02e38b6d1f7e3c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:58 +0000

    Add package rbtv-api/0.0.5/rbtv-api-0.0.5.tar.gz

---
+++ packages/rbtv-api/rbtv-api-0.0.5.tar.gz/rbtv-api-0.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from io import open
+
+with open("readme.md", "r", encoding="utf-8") as fr:
+	long_description = fr.read()
+
+setup(
+	author="Dobatymo",
+	name="rbtv-api",
+	version="0.0.5",
+	url="https://github.com/Dobatymo/rbtv-api",
+	description="Simple Python wrapper for the JSON Rocket Beans TV API",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	classifiers=[
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: ISC License (ISCL)",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 3",
+	],
+	py_modules=["rbtv"],
+	install_requires=["requests", "genutility"],
+	use_2to3=False
+)
