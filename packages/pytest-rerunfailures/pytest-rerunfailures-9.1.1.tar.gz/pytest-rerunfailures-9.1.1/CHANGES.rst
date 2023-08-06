4c7df4c650af53a59ff311f404a6730510027f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:20 +0000

    Add package pytest-rerunfailures/9.1.1/pytest-rerunfailures-9.1.1.tar.gz

---
+++ packages/pytest-rerunfailures/pytest-rerunfailures-9.1.1.tar.gz/pytest-rerunfailures-9.1.1/CHANGES.rst
@@ -0,0 +1,253 @@
+Changelog
+=========
+
+9.1.1 (2020-09-29)
+------------------
+
+Compatibility fix.
+++++++++++++++++++
+
+- Ignore ``--result-log`` command line option when used together with ``pytest
+  >= 6.1.0``, as it was removed there. This is a quick fix, use an older
+  version of pytest, if you want to keep this feature for now.
+  (Thanks to `@ntessore`_ for the PR)
+
+- Support up to pytest 6.1.0.
+
+.. _@ntessore: https://github.com/ntessore
+
+
+9.1 (2020-08-26)
+----------------
+
+Features
+++++++++
+
+- Add a new flag ``--only-rerun`` to allow for users to rerun only certain
+  errors.
+
+Other changes
++++++++++++++
+
+- Drop dependency on ``mock``.
+
+- Add support for pre-commit and add a linting tox target.
+  (`#117 <https://github.com/pytest-dev/pytest-rerunfailures/pull/117>`_)
+  (PR from `@gnikonorov`_)
+
+.. _@gnikonorov: https://github.com/gnikonorov
+
+
+9.0 (2020-03-18)
+----------------
+
+Backwards incompatible changes
+++++++++++++++++++++++++++++++
+
+- Drop support for pytest version 4.4, 4.5 and 4.6.
+
+- Drop support for Python 2.7.
+
+
+Features
+++++++++
+
+- Add support for pytest 5.4.
+
+- Add support for Python 3.8.
+
+
+8.0 (2019-11-18)
+----------------
+
+Backwards incompatible changes
+++++++++++++++++++++++++++++++
+
+- Drop support for pytest version 3.10, 4.0, 4.1, 4.2 and 4.3
+
+- Drop support for Python 3.4.
+
+Features
+++++++++
+
+- Add support for pytest version 4.4, 4.5, 4.6, 5.0, 5.1 and 5.2.
+
+Bug fixes
++++++++++
+
+- Explicitly depend on setuptools to ensure installation when working in
+  environments without it.
+  (`#98 <https://github.com/pytest-dev/pytest-rerunfailures/pull/98>`_)
+  (PR from `@Eric-Arellano`_)
+
+.. _@Eric-Arellano: https://github.com/Eric-Arellano
+
+
+7.0 (2019-03-28)
+----------------
+
+Backwards incompatible changes
+++++++++++++++++++++++++++++++
+
+- Drop support for pytest version 3.8 and 3.9.
+
+Features
+++++++++
+
+- Add support for pytest version 4.2 and 4.3.
+
+Bug fixes
++++++++++
+
+- Fixed #83 issue about ignored ``pytest_runtest_logfinish`` hooks.
+  (`#83 <https://github.com/pytest-dev/pytest-rerunfailures/issues/83>`_)
+  (PR from `@KillAChicken`_)
+
+.. _@KillAChicken: https://github.com/KillAChicken
+
+
+6.0 (2019-01-08)
+----------------
+
+Backwards incompatible changes
+++++++++++++++++++++++++++++++
+
+- Drop support for pytest version 3.6 and 3.7.
+
+Features
+++++++++
+
+- Add support for pytest version 4.0 and 4.1.
+
+Bug fixes
++++++++++
+
+- Fixed #77 regression issue introduced in 4.2 related to the ``rerun``
+  attribute on the test report.
+  (`#77 <https://github.com/pytest-dev/pytest-rerunfailures/issues/77>`_)
+  (Thanks to `@RibeiroAna`_ for the PR).
+
+.. _@RibeiroAna: https://github.com/RibeiroAna
+
+
+5.0 (2018-11-06)
+----------------
+
+- Drop support for pytest versions < 3.6 to reduce the maintenance burden.
+
+- Add support up to pytest version 3.10. Thus supporting the newest 5 pytest
+  releases.
+
+- Add support for Python 3.7.
+
+- Fix issue can occur when used together with `pytest-flake8`
+  (`#73 <https://github.com/pytest-dev/pytest-rerunfailures/issues/73>`_)
+
+
+4.2 (2018-10-04)
+----------------
+
+- Fixed #64 issue related to ``setup_class`` and ``fixture`` executions on rerun (Thanks to
+  `@OlegKuzovkov`_ for the PR).
+
+- Added new ``execution_count`` attribute to reflect the number of test case executions according to #67 issue.
+  (Thanks to `@OlegKuzovkov`_ for the PR).
+
+.. _@OlegKuzovkov: https://github.com/OlegKuzovkov
+
+
+4.1 (2018-05-23)
+----------------
+
+- Add support for pytest 3.6 by using ``Node.get_closest_marker()`` (Thanks to
+  `@The-Compiler`_ for the PR).
+
+.. _@The-Compiler: https://github.com/The-Compiler
+
+4.0 (2017-12-23)
+----------------
+
+- Added option to add a delay time between test re-runs (Thanks to `@Kanguros`_
+  for the PR).
+
+- Added support for pytest >= 3.3.
+
+- Drop support for pytest < 2.8.7.
+
+.. _@Kanguros: https://github.com/Kanguros
+
+
+3.1 (2017-08-29)
+----------------
+
+- Restored compatibility with pytest-xdist. (Thanks to `@davehunt`_ for the PR)
+
+.. _@davehunt: https://github.com/davehunt
+
+
+3.0 (2017-08-17)
+----------------
+
+- Add support for Python 3.6.
+
+- Add support for pytest 2.9 up to 3.2
+
+- Drop support for Python 2.6 and 3.3.
+
+- Drop support for pytest < 2.7.
+
+
+2.2 (2017-06-23)
+----------------
+
+- Ensure that other plugins can run after this one, in case of a global setting
+  ``--rerun=0``. (Thanks to `@sublee`_ for the PR)
+
+.. _@sublee: https://github.com/sublee
+
+2.1.0 (2016-11-01)
+------------------
+
+- Add default value of ``reruns=1`` if ``pytest.mark.flaky()`` is called
+  without arguments.
+
+- Also offer a distribution as universal wheel. (Thanks to `@tltx`_ for the PR)
+
+.. _@tltx: https://github.com/tltx
+
+
+2.0.1 (2016-08-10)
+-----------------------------
+
+- Prepare CLI options to pytest 3.0, to avoid a deprecation warning.
+
+- Fix error due to missing CHANGES.rst when creating the source distribution
+  by adding a MANIFEST.in.
+
+
+2.0.0 (2016-04-06)
+------------------
+
+- Drop support for Python 3.2, since supporting it became too much of a hassle.
+  (Reason: Virtualenv 14+ / PIP 8+ do not support Python 3.2 anymore.)
+
+
+1.0.2 (2016-03-29)
+------------------
+
+- Add support for `--resultlog` option by parsing reruns accordingly. (#28)
+
+
+1.0.1 (2016-02-02)
+------------------
+
+- Improve package description and include CHANGELOG into description.
+
+
+1.0.0 (2016-02-02)
+------------------
+
+- Rewrite to use newer API of pytest >= 2.3.0
+
+- Improve support for pytest-xdist by only logging the final result.
+  (Logging intermediate results will finish the test rather rerunning it.)
---
+++ packages/pytest-rerunfailures/pytest-rerunfailures-9.1.1.tar.gz/pytest-rerunfailures-9.1.1/test_pytest_rerunfailures.py
@@ -0,0 +1,542 @@
+import random
+import time
+from unittest import mock
+
+import pkg_resources
+import pytest
+
+
+pytest_plugins = "pytester"
+
+PYTEST_GTE_61 = pkg_resources.parse_version(
+    pytest.__version__
+) >= pkg_resources.parse_version("6.1")
+
+
+def temporary_failure(count=1):
+    return """
+            import py
+            path = py.path.local(__file__).dirpath().ensure('test.res')
+            count = path.read() or 1
+            if int(count) <= {0}:
+                path.write(int(count) + 1)
+                raise Exception('Failure: {{0}}'.format(count))""".format(
+        count
+    )
+
+
+def check_outcome_field(outcomes, field_name, expected_value):
+    field_value = outcomes.get(field_name, 0)
+    assert (
+        field_value == expected_value
+    ), "outcomes.{} has unexpected value. Expected '{}' but got '{}'".format(
+        field_name, expected_value, field_value
+    )
+
+
+def assert_outcomes(
+    result, passed=1, skipped=0, failed=0, error=0, xfailed=0, xpassed=0, rerun=0,
+):
+    outcomes = result.parseoutcomes()
+    check_outcome_field(outcomes, "passed", passed)
+    check_outcome_field(outcomes, "skipped", skipped)
+    check_outcome_field(outcomes, "failed", failed)
+    check_outcome_field(outcomes, "xfailed", xfailed)
+    check_outcome_field(outcomes, "xpassed", xpassed)
+    check_outcome_field(outcomes, "rerun", rerun)
+
+
+def test_error_when_run_with_pdb(testdir):
+    testdir.makepyfile("def test_pass(): pass")
+    result = testdir.runpytest("--reruns", "1", "--pdb")
+    result.stderr.fnmatch_lines_random("ERROR: --reruns incompatible with --pdb")
+
+
+def test_no_rerun_on_pass(testdir):
+    testdir.makepyfile("def test_pass(): pass")
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result)
+
+
+def test_no_rerun_on_skipif_mark(testdir):
+    reason = str(random.random())
+    testdir.makepyfile(
+        """
+        import pytest
+        @pytest.mark.skipif(reason='{}')
+        def test_skip():
+            pass
+    """.format(
+            reason
+        )
+    )
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result, passed=0, skipped=1)
+
+
+def test_no_rerun_on_skip_call(testdir):
+    reason = str(random.random())
+    testdir.makepyfile(
+        """
+        import pytest
+        def test_skip():
+            pytest.skip('{}')
+    """.format(
+            reason
+        )
+    )
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result, passed=0, skipped=1)
+
+
+def test_no_rerun_on_xfail_mark(testdir):
+    testdir.makepyfile(
+        """
+        import pytest
+        @pytest.mark.xfail()
+        def test_xfail():
+            assert False
+    """
+    )
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result, passed=0, xfailed=1)
+
+
+def test_no_rerun_on_xfail_call(testdir):
+    reason = str(random.random())
+    testdir.makepyfile(
+        """
+        import pytest
+        def test_xfail():
+            pytest.xfail('{}')
+    """.format(
+            reason
+        )
+    )
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result, passed=0, xfailed=1)
+
+
+def test_no_rerun_on_xpass(testdir):
+    testdir.makepyfile(
+        """
+        import pytest
+        @pytest.mark.xfail()
+        def test_xpass():
+            pass
+    """
+    )
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result, passed=0, xpassed=1)
+
+
+def test_rerun_fails_after_consistent_setup_failure(testdir):
+    testdir.makepyfile("def test_pass(): pass")
+    testdir.makeconftest(
+        """
+        def pytest_runtest_setup(item):
+            raise Exception('Setup failure')"""
+    )
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result, passed=0, error=1, rerun=1)
+
+
+def test_rerun_passes_after_temporary_setup_failure(testdir):
+    testdir.makepyfile("def test_pass(): pass")
+    testdir.makeconftest(
+        """
+        def pytest_runtest_setup(item):
+            {}""".format(
+            temporary_failure()
+        )
+    )
+    result = testdir.runpytest("--reruns", "1", "-r", "R")
+    assert_outcomes(result, passed=1, rerun=1)
+
+
+def test_rerun_fails_after_consistent_test_failure(testdir):
+    testdir.makepyfile("def test_fail(): assert False")
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result, passed=0, failed=1, rerun=1)
+
+
+def test_rerun_passes_after_temporary_test_failure(testdir):
+    testdir.makepyfile(
+        """
+        def test_pass():
+            {}""".format(
+            temporary_failure()
+        )
+    )
+    result = testdir.runpytest("--reruns", "1", "-r", "R")
+    assert_outcomes(result, passed=1, rerun=1)
+
+
+def test_rerun_passes_after_temporary_test_failure_with_flaky_mark(testdir):
+    testdir.makepyfile(
+        """
+        import pytest
+        @pytest.mark.flaky(reruns=2)
+        def test_pass():
+            {}""".format(
+            temporary_failure(2)
+        )
+    )
+    result = testdir.runpytest("-r", "R")
+    assert_outcomes(result, passed=1, rerun=2)
+
+
+def test_reruns_if_flaky_mark_is_called_without_options(testdir):
+    testdir.makepyfile(
+        """
+        import pytest
+        @pytest.mark.flaky()
+        def test_pass():
+            {}""".format(
+            temporary_failure(1)
+        )
+    )
+    result = testdir.runpytest("-r", "R")
+    assert_outcomes(result, passed=1, rerun=1)
+
+
+def test_reruns_if_flaky_mark_is_called_with_positional_argument(testdir):
+    testdir.makepyfile(
+        """
+        import pytest
+        @pytest.mark.flaky(2)
+        def test_pass():
+            {}""".format(
+            temporary_failure(2)
+        )
+    )
+    result = testdir.runpytest("-r", "R")
+    assert_outcomes(result, passed=1, rerun=2)
+
+
+def test_no_extra_test_summary_for_reruns_by_default(testdir):
+    testdir.makepyfile(
+        """
+        def test_pass():
+            {}""".format(
+            temporary_failure()
+        )
+    )
+    result = testdir.runpytest("--reruns", "1")
+    assert "RERUN" not in result.stdout.str()
+    assert "1 rerun" in result.stdout.str()
+
+
+def test_extra_test_summary_for_reruns(testdir):
+    testdir.makepyfile(
+        """
+        def test_pass():
+            {}""".format(
+            temporary_failure()
+        )
+    )
+    result = testdir.runpytest("--reruns", "1", "-r", "R")
+    result.stdout.fnmatch_lines_random(["RERUN test_*:*"])
+    assert "1 rerun" in result.stdout.str()
+
+
+def test_verbose(testdir):
+    testdir.makepyfile(
+        """
+        def test_pass():
+            {}""".format(
+            temporary_failure()
+        )
+    )
+    result = testdir.runpytest("--reruns", "1", "-v")
+    result.stdout.fnmatch_lines_random(["test_*:* RERUN*"])
+    assert "1 rerun" in result.stdout.str()
+
+
+def test_no_rerun_on_class_setup_error_without_reruns(testdir):
+    testdir.makepyfile(
+        """
+        class TestFoo(object):
+            @classmethod
+            def setup_class(cls):
+                assert False
+
+            def test_pass():
+                pass"""
+    )
+    result = testdir.runpytest("--reruns", "0")
+    assert_outcomes(result, passed=0, error=1, rerun=0)
+
+
+def test_rerun_on_class_setup_error_with_reruns(testdir):
+    testdir.makepyfile(
+        """
+        class TestFoo(object):
+            @classmethod
+            def setup_class(cls):
+                assert False
+
+            def test_pass():
+                pass"""
+    )
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result, passed=0, error=1, rerun=1)
+
+
+@pytest.mark.skipif(PYTEST_GTE_61, reason="--result-log removed in pytest>=6.1")
+def test_rerun_with_resultslog(testdir):
+    testdir.makepyfile(
+        """
+        def test_fail():
+            assert False"""
+    )
+
+    result = testdir.runpytest("--reruns", "2", "--result-log", "./pytest.log")
+
+    assert_outcomes(result, passed=0, failed=1, rerun=2)
+
+
+@pytest.mark.parametrize("delay_time", [-1, 0, 0.0, 1, 2.5])
+def test_reruns_with_delay(testdir, delay_time):
+    testdir.makepyfile(
+        """
+        def test_fail():
+            assert False"""
+    )
+
+    time.sleep = mock.MagicMock()
+
+    result = testdir.runpytest("--reruns", "3", "--reruns-delay", str(delay_time))
+
+    if delay_time < 0:
+        result.stdout.fnmatch_lines(
+            "*UserWarning: Delay time between re-runs cannot be < 0. "
+            "Using default value: 0"
+        )
+        delay_time = 0
+
+    time.sleep.assert_called_with(delay_time)
+
+    assert_outcomes(result, passed=0, failed=1, rerun=3)
+
+
+@pytest.mark.parametrize("delay_time", [-1, 0, 0.0, 1, 2.5])
+def test_reruns_with_delay_marker(testdir, delay_time):
+    testdir.makepyfile(
+        """
+        import pytest
+
+        @pytest.mark.flaky(reruns=2, reruns_delay={})
+        def test_fail_two():
+            assert False""".format(
+            delay_time
+        )
+    )
+
+    time.sleep = mock.MagicMock()
+
+    result = testdir.runpytest()
+
+    if delay_time < 0:
+        result.stdout.fnmatch_lines(
+            "*UserWarning: Delay time between re-runs cannot be < 0. "
+            "Using default value: 0"
+        )
+        delay_time = 0
+
+    time.sleep.assert_called_with(delay_time)
+
+    assert_outcomes(result, passed=0, failed=1, rerun=2)
+
+
+def test_rerun_on_setup_class_with_error_with_reruns(testdir):
+    """
+     Case: setup_class throwing error on the first execution for parametrized test
+    """
+    testdir.makepyfile(
+        """
+        import pytest
+
+        pass_fixture = False
+
+        class TestFoo(object):
+            @classmethod
+            def setup_class(cls):
+                global pass_fixture
+                if not pass_fixture:
+                    pass_fixture = True
+                    assert False
+                assert True
+            @pytest.mark.parametrize('param', [1, 2, 3])
+            def test_pass(self, param):
+                assert param"""
+    )
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result, passed=3, rerun=1)
+
+
+def test_rerun_on_class_scope_fixture_with_error_with_reruns(testdir):
+    """
+    Case: Class scope fixture throwing error on the first execution
+    for parametrized test
+    """
+    testdir.makepyfile(
+        """
+        import pytest
+
+        pass_fixture = False
+
+        class TestFoo(object):
+
+            @pytest.fixture(scope="class")
+            def setup_fixture(self):
+                global pass_fixture
+                if not pass_fixture:
+                    pass_fixture = True
+                    assert False
+                assert True
+            @pytest.mark.parametrize('param', [1, 2, 3])
+            def test_pass(self, setup_fixture, param):
+                assert param"""
+    )
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result, passed=3, rerun=1)
+
+
+def test_rerun_on_module_fixture_with_reruns(testdir):
+    """
+    Case: Module scope fixture is not re-executed when class scope fixture throwing
+    error on the first execution for parametrized test
+    """
+    testdir.makepyfile(
+        """
+        import pytest
+
+        pass_fixture = False
+
+        @pytest.fixture(scope='module')
+        def module_fixture():
+            assert not pass_fixture
+
+        class TestFoo(object):
+            @pytest.fixture(scope="class")
+            def setup_fixture(self):
+                global pass_fixture
+                if not pass_fixture:
+                    pass_fixture = True
+                    assert False
+                assert True
+            def test_pass_1(self, module_fixture, setup_fixture):
+                assert True
+
+            def test_pass_2(self, module_fixture, setup_fixture):
+                assert True"""
+    )
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result, passed=2, rerun=1)
+
+
+def test_rerun_on_session_fixture_with_reruns(testdir):
+    """
+    Case: Module scope fixture is not re-executed when class scope fixture
+    throwing error on the first execution for parametrized test
+    """
+    testdir.makepyfile(
+        """
+        import pytest
+
+        pass_fixture = False
+
+        @pytest.fixture(scope='session')
+        def session_fixture():
+            assert not pass_fixture
+
+        class TestFoo(object):
+            @pytest.fixture(scope="class")
+            def setup_fixture(self):
+                global pass_fixture
+                if not pass_fixture:
+                    pass_fixture = True
+                    assert False
+                assert True
+
+            def test_pass_1(self, session_fixture, setup_fixture):
+                assert True
+            def test_pass_2(self, session_fixture, setup_fixture):
+                assert True"""
+    )
+    result = testdir.runpytest("--reruns", "1")
+    assert_outcomes(result, passed=2, rerun=1)
+
+
+def test_execution_count_exposed(testdir):
+    testdir.makepyfile("def test_pass(): assert True")
+    testdir.makeconftest(
+        """
+        def pytest_runtest_teardown(item):
+            assert item.execution_count == 3"""
+    )
+    result = testdir.runpytest("--reruns", "2")
+    assert_outcomes(result, passed=3, rerun=2)
+
+
+def test_rerun_report(testdir):
+    testdir.makepyfile("def test_pass(): assert False")
+    testdir.makeconftest(
+        """
+        def pytest_runtest_logreport(report):
+            assert hasattr(report, 'rerun')
+            assert isinstance(report.rerun, int)
+            assert report.rerun <= 2
+        """
+    )
+    result = testdir.runpytest("--reruns", "2")
+    assert_outcomes(result, failed=1, rerun=2, passed=0)
+
+
+def test_pytest_runtest_logfinish_is_called(testdir):
+    hook_message = "Message from pytest_runtest_logfinish hook"
+    testdir.makepyfile("def test_pass(): pass")
+    testdir.makeconftest(
+        r"""
+        def pytest_runtest_logfinish(nodeid, location):
+            print("\n{}\n")
+    """.format(
+            hook_message
+        )
+    )
+    result = testdir.runpytest("--reruns", "1", "-s")
+    result.stdout.fnmatch_lines(hook_message)
+
+
+@pytest.mark.parametrize(
+    "only_rerun_texts, should_rerun",
+    [
+        (["AssertionError"], True),
+        (["Assertion*"], True),
+        (["Assertion"], True),
+        (["ValueError"], False),
+        ([""], True),
+        (["AssertionError: "], True),
+        (["AssertionError: ERR"], True),
+        (["ERR"], True),
+        (["AssertionError,ValueError"], False),
+        (["AssertionError ValueError"], False),
+        (["AssertionError", "ValueError"], True),
+    ],
+)
+def test_only_rerun_flag(testdir, only_rerun_texts, should_rerun):
+    testdir.makepyfile('def test_only_rerun(): raise AssertionError("ERR")')
+
+    num_failed = 1
+    num_passed = 0
+    num_reruns = 1
+    num_reruns_actual = num_reruns if should_rerun else 0
+
+    pytest_args = ["--reruns", str(num_reruns)]
+    for only_rerun_text in only_rerun_texts:
+        pytest_args.extend(["--only-rerun", only_rerun_text])
+    result = testdir.runpytest(*pytest_args)
+    assert_outcomes(
+        result, passed=num_passed, failed=num_failed, rerun=num_reruns_actual
+    )
