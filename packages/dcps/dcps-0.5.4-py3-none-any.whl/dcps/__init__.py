8a4bba18ec92fa40592b2e9e122af0594f49d548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:00:45 +0000

    Add package dcps/0.5.4/dcps-0.5.4-py3-none-any.whl

---
+++ packages/dcps/dcps-0.5.4-py3-none-any.whl/dcps/__init__.py
@@ -0,0 +1,12 @@
+
+# Standard SCPI commands
+from dcps.SCPI import SCPI
+
+# Support of Rigol DP832A and other DP800 power supplies
+from dcps.RigolDP800 import RigolDP800
+
+# Support of Aim TTi PL-P Series power supplies
+from dcps.AimTTiPLP import AimTTiPLP
+
+# Support of BK Precision 9115 and related DC power supplies
+from dcps.BK9115 import BK9115
