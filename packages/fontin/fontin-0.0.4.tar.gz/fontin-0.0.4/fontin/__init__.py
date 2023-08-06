f2e4cc556fafdbd83beba3bbae19c7f4a676a8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:47 +0000

    Add package fontin/0.0.4/fontin-0.0.4.tar.gz

---
+++ packages/fontin/fontin-0.0.4.tar.gz/fontin-0.0.4/fontin/__init__.py
@@ -0,0 +1,4 @@
+import ctypes, sys, os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
---
+++ packages/fontin/fontin-0.0.4.tar.gz/fontin-0.0.4/fontin/__main__.py
@@ -0,0 +1,68 @@
+import os
+import tempfile
+import ctypes
+import sys
+from pyunpack import Archive
+
+from fontin.install_font import install_font
+from fontin import clear
+
+ALLFILES = os.listdir()
+FILE = ''
+TEMPDIR = tempfile.gettempdir()
+
+archive_extensions = ('.7z', '.ace', '.alz', '.a', '.arc', '.arj', '.bz2', '.cab', '.Z', '.cpio', '.deb', '.dms', '.gz', '.lrz', '.lha', '.lzh', 
+'.lz', '.lzma', '.lzo', '.rpm', '.rar', '.rz', '.tar', '.xz', '.zip', '.jar', '.zoo')
+
+font_file_extensions = ('.ttf', '.otf', '.svg', '.eot', '.woff')
+
+def find_archives():
+    archives = []
+    print('Searching for archives in the current directory...')
+    for file in ALLFILES:
+        if file.endswith(archive_extensions):
+            archives.append(file)
+    clear()
+    print('Found the following files:')
+    n=1
+    for archive in archives:
+        print('{}. {}'.format(str(n), archive))
+        n += 1
+    choice = int(input('Which one is the font archive? '))
+    FILE = archives[choice - 1]
+    return FILE
+
+def extract(file):
+    print('Extracting...')
+    extracted_path = TEMPDIR + '\\fontin\\' + file + '\\'
+    if not os.path.exists(extracted_path):
+        os.makedirs(extracted_path)
+    Archive(os.getcwd() + '\\' + file).extractall(extracted_path)
+    return extracted_path
+
+def install_all(path):
+    fonts = os.listdir(path)
+    for font in fonts:
+        if font.endswith(font_file_extensions):
+            font_path = path + '\\' + font
+            print('Installing font: ' + font)
+            install_font(font_path)
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def main():
+    if is_admin():
+        file = find_archives()
+        path = extract(file)
+        install_all(path)
+    else:
+        # Re-run the program with admin rights
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
+
+if __name__ == '__main__':
+    main()
