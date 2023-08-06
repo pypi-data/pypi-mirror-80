28232639d21387f466375aaf5af1e37a88e4678e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:45 +0000

    Add package wavimedical/0.0.3/wavimedical-0.0.3.tar.gz

---
+++ packages/wavimedical/wavimedical-0.0.3.tar.gz/wavimedical-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wavimedical", 
+    version="0.0.3",
+    author="Clayton Schneider",
+    author_email="clayton.schneider@colorado.edu",
+    description="WAVi Medical's EEG Analysis and ML Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/canlab/WAViMedEEG",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8.2',
+)
