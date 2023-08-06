9f8eed444926119bbcfc5e93545af008cf8cb921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:26 +0000

    Add package django-apar/1.1.6.50/django-apar-1.1.6.50.tar.gz

---
+++ packages/django-apar/django-apar-1.1.6.50.tar.gz/django-apar-1.1.6.50/aparnik/packages/shops/subscriptions/api/views.py
@@ -0,0 +1,22 @@
+from django.db.models import Q
+from rest_framework.permissions import ( AllowAny, IsAuthenticated )
+from rest_framework.generics import ListAPIView
+
+from aparnik.contrib.basemodels.api.serializers import ModelListPolymorphicSerializer
+from ..models import Subscription
+
+
+class SubscriptionListAPIView(ListAPIView):
+    serializer_class = ModelListPolymorphicSerializer
+    permission_classes = [IsAuthenticated]
+    # filter_backends = (filters.SearchFilter,)
+
+    def get_queryset(self, *args, **kwargs):
+        if 'product_id' in self.request.query_params:
+            product_id = self.request.query_params['product_id']
+            return Subscription.objects.active().filter(
+                Q(products__in=[product_id]) |
+                Q(type=Subscription.TYPE_ALL
+                  )
+            ).distinct()
+        return Subscription.objects.active()
